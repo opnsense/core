a2e63b70aa45d530ffa6c5cc009a6c006ebe893e
Charlie Root <root@OPNsense.localdomain>
2014-12-23 11:37:27 +0000

    Firmware upgrade is now fully functional

--- src/opnsense/scripts/pkg_upgrade.sh
+++ src/opnsense/scripts/pkg_upgrade.sh
@@ -2,12 +2,12 @@
 
 pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
 if [ "$pkg_running" == "" ]; then
-	if [ -f /tmp/pkg_upgrade.progress ]
+	if [ -f /tmp/pkg_upgrade.progress ]; then
 		# Remove leftovers from previous upgrade first
 		rm /tmp/pkg_upgrade.progress
 	fi
       # start pkg upgrade
-	pkg upgrade -n > /tmp/pkg_upgrade.progress & # Need to st this to -y for production, now set to -n for testing purpose
+	pkg upgrade -y > /tmp/pkg_upgrade.progress &
 	echo '***DONE***' >> /tmp/pkg_upgrade.progress
 else
 	echo 'Upgrade already in progress'
