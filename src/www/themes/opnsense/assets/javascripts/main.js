b50b4cd7a365658cfbad2a5a9da277ae9205dc92
Franco Fichtner <franco@opnsense.org>
2014-12-28 14:23:49 +0100

    themes: apply whitespace cleanups

--- src/www/themes/opnsense/assets/javascripts/main.js
+++ src/www/themes/opnsense/assets/javascripts/main.js
@@ -1,35 +1,35 @@
 
 $(document).ready(function(){
-   
+
     $('#btn-filter').click(function () {
         var btn = $(this)
         btn.button('loading')
-       
+
         /*
 $.ajax(...).always(function () {
             btn.button('reset')
         });
 */
-    }); 
-    
-    $(' #system, 
-        #interfaces, 
-        #firewall, 
-        #services, 
-        #vpn, 
-        #status, 
-        #diagnostics, 
+    });
+
+    $(' #system,
+        #interfaces,
+        #firewall,
+        #services,
+        #vpn,
+        #status,
+        #diagnostics,
         #help
     ').on('show.bs.collapse', function () {
-        
+
         // remove all actives
-        $("#mainmenu a.active-menu-title").removeClass('active-menu-title');  
+        $("#mainmenu a.active-menu-title").removeClass('active-menu-title');
         $("#mainmenu a + div.active-menu").removeClass('active-menu');
-      
+
         // remove all collaped
         $("#mainmenu .collapse.in").not(this).collapse('hide');
-        
-        $(this).prev('a').addClass('active-menu-title');  
+
+        $(this).prev('a').addClass('active-menu-title');
         $(this).addClass('active-menu');
-    });    
+    });
 });
