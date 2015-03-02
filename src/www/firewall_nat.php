7efba9edca36941c2ec226caebda0daedba225bc
Ad Schellevis <ad@opnsense.ocom>
2015-03-02 19:27:10 +0000

    display alias names, needs further cleanup (https://github.com/opnsense/core/issues/77)

--- src/www/firewall_nat.php
+++ src/www/firewall_nat.php
@@ -304,44 +304,49 @@ $main_buttons = array(
 									                  <td class="listr"  id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
 													    <?=$textss;?>
 													    <?php
-													    if ($alias_src_span_begin) echo htmlspecialchars(pprint_address($alias_src_span_begin));
-													    else echo htmlspecialchars(pprint_address($natent['source']));
-													    if ($alias_src_span_end) echo htmlspecialchars(pprint_address($alias_src_span_end));
+													    //if ($alias_src_span_begin) echo htmlspecialchars(pprint_address($alias_src_span_begin));
+													    //else 
+													    echo htmlspecialchars(pprint_address($natent['source']));
+													    //if ($alias_src_span_end) echo htmlspecialchars(pprint_address($alias_src_span_end));
 													    ?>
 													    <?=$textse;?>
 									                  </td>
 									                  <td class="listr"  id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
 													    <?=$textss;?>
 													    <?php
-													    if ($alias_src_port_span_begin) echo htmlspecialchars(pprint_port($alias_src_port_span_begin));
-													    else echo htmlspecialchars(pprint_port($natent['source']['port']));
-													    if($alias_src_port_span_end) echo htmlspecialchars(pprint_port($alias_src_port_span_end));
+													    //if ($alias_src_port_span_begin) echo htmlspecialchars(pprint_port($alias_src_port_span_begin));
+													    //else 
+													    echo htmlspecialchars(pprint_port($natent['source']['port']));
+													    //if($alias_src_port_span_end) echo htmlspecialchars(pprint_port($alias_src_port_span_end));
 													    ?><?=$textse;?>
 									                  </td>
 
 									                  <td class="listr"  id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
 													    <?=$textss;?>
 													    <?php
-													    if ($alias_dst_span_begin) echo htmlspecialchars(pprint_address($alias_dst_span_begin));
-													    else echo htmlspecialchars(pprint_address($natent['destination']));
-													    if ($alias_dst_span_end)  echo htmlspecialchars(pprint_address($alias_dst_span_end));
+													    //if ($alias_dst_span_begin) echo htmlspecialchars(pprint_address($alias_dst_span_begin));
+													    //else 
+													    echo htmlspecialchars(pprint_address($natent['destination']));
+													    //if ($alias_dst_span_end)  echo htmlspecialchars(pprint_address($alias_dst_span_end));
 													    ?><?=$textse;?>
 									                  </td>
 									                  <td class="listr"  id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
 													    <?=$textss;?>
 													    <?php
-													    if ($alias_dst_port_span_begin) echo htmlspecialchars(pprint_port($alias_dst_port_span_begin['address']));
-													    else echo htmlspecialchars(pprint_port($natent['destination']['port']));
-													    if ($alias_dst_port_span_end ) echo htmlspecialchars(pprint_port($alias_dst_port_span_end['address']));
+													    //if ($alias_dst_port_span_begin) echo htmlspecialchars(pprint_port($alias_dst_port_span_begin['address']));
+													    //else 
+													    echo htmlspecialchars(pprint_port($natent['destination']['port']));
+													    //if ($alias_dst_port_span_end ) echo htmlspecialchars(pprint_port($alias_dst_port_span_end['address']));
 													    ?><?=$textse;?>
 									                  </td>
 
 									                  <td class="listr" id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
 													    <?=$textss;?>
 													    <?php
-													    if ($alias_target_span_begin) echo htmlspecialchars(pprint_address($alias_target_span_begin));
-													    else  echo htmlspecialchars($natent['target']);
-													    if ($alias_target_span_end) echo htmlspecialchars(pprint_address($alias_target_span_end));
+													    //if ($alias_target_span_begin) echo htmlspecialchars(pprint_address($alias_target_span_begin));
+													    //else  
+													    echo htmlspecialchars($natent['target']);
+													    //if ($alias_target_span_end) echo htmlspecialchars(pprint_address($alias_target_span_end));
 													    ?><?=$textse;?>
 									                  </td>
 									                  <td class="listr"  id="frd<?=$nnats;?>" ondblclick="document.location='firewall_nat_edit.php?id=<?=$nnats;?>';">
@@ -356,9 +361,10 @@ $main_buttons = array(
 															}
 														?>
 													    <?=$textss;?><?php
-													    if ($alias_local_port_span_begin) echo htmlspecialchars(pprint_port($alias_local_port_span_begin['address']));
-													    else  echo htmlspecialchars(pprint_port($localport));
-													    if ($alias_local_port_span_end) echo htmlspecialchars(pprint_port($alias_local_port_span_end['address']));
+													    //if ($alias_local_port_span_begin) echo htmlspecialchars(pprint_port($alias_local_port_span_begin['address']));
+													    //else  
+													    echo htmlspecialchars(pprint_port($localport));
+													    //if ($alias_local_port_span_end) echo htmlspecialchars(pprint_port($alias_local_port_span_end['address']));
 													    ?><?=$textse;?>
 									                  </td>
 
