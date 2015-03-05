ee277400d35bb5e789d2732b2c39aead4669ae8c
Franco Fichtner <franco@opnsense.org>
2015-03-05 19:22:17 +0100

    pptp: more files got lost in a GUI bootstrap update

--- src/www/vpn_pptp.php
+++ src/www/vpn_pptp.php
@@ -28,6 +28,9 @@
 */
 
 require_once('guiconfig.inc');
+require_once('functions.inc');
+require_once('filter.inc');
+require_once('shaper.inc');
 require_once('vpn.inc');
 
 if (!is_array($config['pptpd']['radius'])) {
