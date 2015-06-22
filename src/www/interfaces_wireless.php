6048c126184e4697132c37e43cbc78736c82060f
Franco Fichtner <franco@opnsense.org>
2015-06-22 08:15:47 +0200

    interfaces: only need one edit button

--- src/www/interfaces_wireless.php
+++ src/www/interfaces_wireless.php
@@ -133,12 +133,9 @@ $main_buttons = array(
 						                    <?=htmlspecialchars($clone['descr']);?>&nbsp;
 						                  </td>
 						                  <td valign="middle" class="list nowrap">
-
 							                   <a href="interfaces_wireless_edit.php?id=<?=$i;?>" class="btn btn-default"><span class="glyphicon glyphicon-edit" title="<?=gettext("edit group");?>"></span></a>
 
 											   <a href="interfaces_wireless.php?act=del&amp;id=<?=$i;?>" class="btn btn-default"  onclick="return confirm('<?=gettext("Do you really want to delete this wireless clone?");?>')"><span class="glyphicon glyphicon-remove"></span></a>
-
-							                   <a href="interfaces_wireless_edit.php?id=<?=$i;?>"><img src="./themes/<?= $g['theme']; ?>/images/icons/icon_e.gif" width="17" height="17" border="0" alt="edit" ></a>
 							                </td>
 										</tr>
 									  <?php $i++; endforeach; ?>
