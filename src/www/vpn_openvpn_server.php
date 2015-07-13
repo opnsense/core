e84839953f7c81055fbe73aca91350fc825a04b1
Ad Schellevis <ad@opnsense.org>
2015-07-13 14:16:43 +0000

    (legacy) cleanups and moves related to openvpn.inc

--- src/www/vpn_openvpn_server.php
+++ src/www/vpn_openvpn_server.php
@@ -30,6 +30,22 @@
 require_once("guiconfig.inc");
 require_once("openvpn.inc");
 
+$openvpn_dh_lengths = array(1024, 2048, 4096 );
+$openvpn_cert_depths = array(
+	1 => "One (Client+Server)",
+	2 => "Two (Client+Intermediate+Server)",
+	3 => "Three (Client+2xIntermediate+Server)",
+	4 => "Four (Client+3xIntermediate+Server)",
+	5 => "Five (Client+4xIntermediate+Server)"
+);
+
+$openvpn_server_modes = array(
+	'p2p_tls' => gettext("Peer to Peer ( SSL/TLS )"),
+	'p2p_shared_key' => gettext("Peer to Peer ( Shared Key )"),
+	'server_tls' => gettext("Remote Access ( SSL/TLS )"),
+	'server_user' => gettext("Remote Access ( User Auth )"),
+	'server_tls_user' => gettext("Remote Access ( SSL/TLS + User Auth )"));
+
 if (!is_array($config['openvpn']['openvpn-server'])) {
     $config['openvpn']['openvpn-server'] = array();
 }
