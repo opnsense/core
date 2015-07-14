b6ba10772b37f14d9bc2d5d73b9bdb0e6bee52fe
Sascha Linke <s.linke@teamkind.de>
2015-07-14 15:32:11 +0200

    Added hide/show feature for settings Widget now translatable

--- src/www/widgets/widgets/system_log.widget.php
+++ src/www/widgets/widgets/system_log.widget.php
@@ -1,52 +1,91 @@
 <?php
-require_once("functions.inc");
+
+/*
+        Copyright (C) 2015 S. Linke <dev@devsash.de>
+        All rights reserved.
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice,
+           this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+
+        THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+        AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+        AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+        SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+        CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+        ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+        POSSIBILITY OF SUCH DAMAGE.
+*/
+
 require_once("guiconfig.inc");
-require_once('notices.inc');
-include_once("includes/functions.inc.php");
-require_once("script/load_phalcon.php");
+require_once("pfsense-utils.inc");
+require_once("functions.inc");;
 
 $system_logfile = '/var/log/system.log';
-if (!$config['widgets']['systemlogfiltercount'])
-{
-  $syslogEntriesToFetch = $config['syslog']['nentries'];
-  if (!$syslogEntriesToFetch) { $syslogEntriesToFetch = 20;  }
-}
-else
-{
-$syslogEntriesToFetch = $config['widgets']['systemlogfiltercount'];
+
+if (!$config['widgets']['systemlogfiltercount']){
+	$syslogEntriesToFetch = $config['syslog']['nentries'];
+	if (!$syslogEntriesToFetch) { 
+		$syslogEntriesToFetch = 20;  
+  	}
+} else {
+	$syslogEntriesToFetch = $config['widgets']['systemlogfiltercount'];
 }
 
-if(is_numeric($_POST['logfiltercount']))
-{
- $countReceived =  $_POST['logfiltercount'];
- $config['widgets']['systemlogfiltercount'] = $countReceived;
- write_config("Saved Widget System Log Filter Setting");
- Header("Location: /");
- exit(0);
+if(is_numeric($_POST['logfiltercount'])) {
+   $countReceived =  $_POST['logfiltercount'];
+   $config['widgets']['systemlogfiltercount'] = $countReceived;
+   write_config("Saved Widget System Log Filter Setting");
+   Header("Location: /index.php");
+   exit(0);
 }
-
 ?>
 
-<div class="table-responsive">
-	<table class="table table-striped table-sort" cellspacing="0" cellpadding="0">
-		<? dump_clog($system_logfile, $syslogEntriesToFetch, true, array(), array("ppp")); ?>
-	</table>
-	<table class="table" style="margin-bottom:0px;text-align:center;">
+<input type="hidden" id="system_log-config" name="system_log-config" value="" />
 
-		<tr class="formselect">
-		<td>Number of Log lines to display: </td>
+<div id="system_log-settings" class="widgetconfigdiv" style="display:none;">
+	<form action="/widgets/widgets/system_log.widget.php" method="post" name="iform">
+		<table class="table table-striped" summary="syslog widget">
+		<tr>	
+			<td><?=gettext("Number of Log lines to display");?>:</td>
 
-		<td>
-		<form action="/widgets/widgets/system_log.widget.php" method="post" name="iform">
-                <select name="logfiltercount" id="logfiltercount">
-		<?php for ($i = 1; $i <= 50; $i++) {?>
-		<option value="<?php echo $i;?>" <?php if ($syslogEntriesToFetch == $i) { echo "selected=\"selected\"";}?>><?php echo $i;?></option>
-		<?php } ?>
-		</td>
-		<td>
-		<input id="submit" name="submit" type="submit" class="btn btn-primary formbtn" value="Save" autocomplete="off">
-		</form>
-		</td>
+			<td>
+            <select name="logfiltercount" id="logfiltercount">
+			<?php for ($i = 1; $i <= 50; $i++) {?>
+			<option value="<?php echo $i;?>" <?php if ($syslogEntriesToFetch == $i) { echo "selected=\"selected\"";}?>><?php echo $i;?></option>
+			<?php } ?>
+			</td>
+			
+			<td>
+			<input id="submit" name="submit" type="submit" class="btn btn-primary formbtn" value="Save" autocomplete="off">
+			</td>
+		
 		</tr>
 	</table>
+   </form>
 </div>
+
+<div id="syslog-widgets" style="padding: 5px; overflow:scroll;">
+	<table class="table table-striped" cellspacing="0" cellpadding="0">
+		<?php dump_clog($system_logfile, $syslogEntriesToFetch, true, array(), array("ppp")); ?>
+	</table>
+		
+</div>
+
+<!-- needed to display the widget settings menu -->
+<script type="text/javascript">
+//<![CDATA[
+	selectIntLink = "system_log-configure";
+	textlink = document.getElementById(selectIntLink);
+	textlink.style.display = "inline";
+//]]>
+</script>
