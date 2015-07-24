a56a7a83857fe4b77c662606f7006a9a66e653fd
Ad Schellevis <ad@opnsense.org>
2015-07-24 08:00:14 +0000

    (legacy) missing dependency in services_opendns.php

--- src/www/services_opendns.php
+++ src/www/services_opendns.php
@@ -31,6 +31,7 @@ require_once("guiconfig.inc");
 require_once("system.inc");
 require_once("pfsense-utils.inc");
 require_once("services.inc");
+require_once("interfaces.inc");
 
 if (!is_array($config['opendns'])) {
 	$config['opendns'] = array();
