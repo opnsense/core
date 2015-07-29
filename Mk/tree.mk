4b64a89861fb4d331dc26cf05b2b9feebaba385a
Franco Fichtner <franco@opnsense.org>
2015-07-29 16:45:52 +0200

    pkg: generalise packaging magic; add .in suffix awareness
    o Makes live-mounting easier as the package config samples won't
      be clobbered anymore.
    
    o Automatically picks up ".in" files, runs replace and moves them
      to their permanent location.
    
    o Refactored src directory install while at it.

--- Mk/tree.mk
+++ Mk/tree.mk
@@ -4,12 +4,27 @@ install:
 .for TREE in ${TREES}
 	@mkdir -p ${DESTDIR}${ROOT}
 	@cp -vr ${TREE} ${DESTDIR}${ROOT}
+	@(cd ${TREE}; find * -type f) | while read FILE; do \
+		if [ $${FILE%%.in} != $${FILE} ]; then \
+			sed -i '' \
+			    -e "s=%%CORE_PACKAGESITE%%=${CORE_PACKAGESITE}=g" \
+			    -e "s=%%CORE_REPOSITORY%%=${CORE_REPOSITORY}=g" \
+			    ${DESTDIR}${ROOT}/${TREE}/$${FILE}; \
+			mv -v ${DESTDIR}${ROOT}/${TREE}/$${FILE} \
+			    ${DESTDIR}${ROOT}/${TREE}/$${FILE%%.in}; \
+		fi \
+	done
 .endfor
 
 plist:
 .for TREE in ${TREES}
-	@(cd ${TREE}; find * -type f) | \
-	    xargs -n1 printf "${ROOT}/${TREE}/%s\n"
+	@(cd ${TREE}; find * -type f) | while read FILE; do \
+		FILE="$${FILE%%.in}"; PREFIX=""; \
+		if [ $${FILE%%.sample} != $${FILE} ]; then \
+			PREFIX="@sample "; \
+		fi; \
+		echo "${PREFIX} ${ROOT}/${TREE}/$${FILE}"; \
+	done
 .endfor
 
 .PHONY: install plist
