c6446c25e8c9438db516d983f56cd0d3eb9ff120
Franco Fichtner <franco@opnsense.org>
2015-08-04 11:42:14 +0200

    openvpn: util.inc is included via guiconfig.inc

--- src/www/vpn_openvpn_export.php
+++ src/www/vpn_openvpn_export.php
@@ -33,10 +33,9 @@
 require_once("guiconfig.inc");
 require_once("openvpn.inc");
 require_once("filter.inc");
-require_once("util.inc");
 require_once("pfsense-utils.inc");
 require_once("interfaces.inc");
-require("openvpn-client-export.inc");
+require_once("openvpn-client-export.inc");
 
 global $current_openvpn_version, $current_openvpn_version_rev;
 
