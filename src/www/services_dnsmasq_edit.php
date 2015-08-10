c106ec385ba255ac28ba406d4a5d1184f9f4d289
Ad Schellevis <ad@opnsense.org>
2015-08-10 09:46:59 +0000

    (legacy) fix crashreport " PHP Warning:  Illegal string offset 'item' in /usr/local/www/services_dnsmasq_edit.php on line 250"

--- src/www/services_dnsmasq_edit.php
+++ src/www/services_dnsmasq_edit.php
@@ -247,7 +247,7 @@ include("head.inc");
 					                        </tr>
 					                        <?php
 					                          $counter = 0;
-					                          if($pconfig['aliases']['item']):
+					                          if(isset($pconfig['aliases']['item']) && is_array($pconfig['aliases']['item']))):
 					                            foreach($pconfig['aliases']['item'] as $item):
 					                              $host = $item['host'];
 					                              $domain = $item['domain'];
