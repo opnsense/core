dcec8fd962cc8423e26e90751d3632ddd10cc0ff
Ad Schellevis <ad@opnsense.org>
2015-08-14 13:36:47 +0000

    (legacy) refactor firewall_aliases.php

--- src/www/firewall_aliases.php
+++ src/www/firewall_aliases.php
@@ -31,82 +31,6 @@
 require_once("guiconfig.inc");
 require_once("filter.inc");
 
-if (!is_array($config['aliases'])) {
-        $config['aliases'] = array();
-}
-if (!is_array($config['aliases']['alias'])) {
-	$config['aliases']['alias'] = array();
-}
-$a_aliases = &$config['aliases']['alias'];
-
-$tab = ($_REQUEST['tab'] == "" ? "ip" : preg_replace("/\W/","",$_REQUEST['tab']));
-
-if ($_POST) {
-
-	if ($_POST['apply']) {
-		$retval = 0;
-
-		/* reload all components that use aliases */
-		$retval = filter_configure();
-
-		if(stristr($retval, "error") <> true)
-		    $savemsg = get_std_save_message($retval);
-		else
-		    $savemsg = $retval;
-		if ($retval == 0)
-			clear_subsystem_dirty('aliases');
-	}
-}
-
-if ($_GET['act'] == "del") {
-	if ($a_aliases[$_GET['id']]) {
-		/* make sure rule is not being referenced by any nat or filter rules */
-		$is_alias_referenced = false;
-		$referenced_by = false;
-		$alias_name = $a_aliases[$_GET['id']]['name'];
-		// Firewall rules
-		find_alias_reference(array('filter', 'rule'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('filter', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('filter', 'rule'), array('source', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('filter', 'rule'), array('destination', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
-		// NAT Rules
-		find_alias_reference(array('nat', 'rule'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'rule'), array('source', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'rule'), array('destination', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'rule'), array('target'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'rule'), array('local-port'), $alias_name, $is_alias_referenced, $referenced_by);
-		// NAT 1:1 Rules
-		//find_alias_reference(array('nat', 'onetoone'), array('external'), $alias_name, $is_alias_referenced, $referenced_by);
-		//find_alias_reference(array('nat', 'onetoone'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'onetoone'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		// NAT Outbound Rules
-		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('source', 'network'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('sourceport'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('dstport'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('target'), $alias_name, $is_alias_referenced, $referenced_by);
-		// Alias in an alias
-		find_alias_reference(array('aliases', 'alias'), array('address'), $alias_name, $is_alias_referenced, $referenced_by);
-		// Load Balancer
-		find_alias_reference(array('load_balancer', 'lbpool'),         array('port'), $alias_name, $is_alias_referenced, $referenced_by);
-		find_alias_reference(array('load_balancer', 'virtual_server'), array('port'), $alias_name, $is_alias_referenced, $referenced_by);
-		// Static routes
-		find_alias_reference(array('staticroutes', 'route'), array('network'), $alias_name, $is_alias_referenced, $referenced_by);
-		if($is_alias_referenced == true) {
-			$savemsg = sprintf(gettext("Cannot delete alias. Currently in use by %s"), $referenced_by);
-		} else {
-			unset($a_aliases[$_GET['id']]);
-			if (write_config()) {
-				filter_configure();
-				mark_subsystem_dirty('aliases');
-			}
-			header("Location: firewall_aliases.php?tab=" . $tab);
-			exit;
-		}
-	}
-}
-
 function find_alias_reference($section, $field, $origname, &$is_alias_referenced, &$referenced_by) {
 	global $config;
 	if(!$origname || $is_alias_referenced)
@@ -142,61 +66,151 @@ function find_alias_reference($section, $field, $origname, &$is_alias_referenced
 	}
 }
 
-$pgtitle = array(gettext("Firewall"),gettext("Aliases"));
+
+if (!isset($config['aliases'])) {
+        $config['aliases'] = array();
+}
+if (!isset($config['aliases']['alias'])) {
+	$config['aliases']['alias'] = array();
+}
+$a_aliases = &$config['aliases']['alias'];
+
+// determine selected tab
+$selected_tab = htmlspecialchars(($_REQUEST['tab'] == "" ? "ip" : preg_replace("/\W/","",$_REQUEST['tab'])));
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST['apply'])) {
+		/* reload all components that use aliases */
+		$retval = filter_configure();
+
+		if(stristr($retval, "error") <> true)
+		    $savemsg = get_std_save_message($retval);
+		else
+		    $savemsg = $retval;
+		if ($retval == 0)
+			clear_subsystem_dirty('aliases');
+	} elseif (isset($_POST['act']) && $_POST['act'] == "del") {
+    if (isset($_POST['id']) && isset($a_aliases[$_POST['id']])) {
+      // perform validation
+      /* make sure rule is not being referenced by any nat or filter rules */
+  		$is_alias_referenced = false;
+  		$referenced_by = false;
+  		$alias_name = $a_aliases[$_POST['id']]['name'];
+  		// Firewall rules
+  		find_alias_reference(array('filter', 'rule'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('filter', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('filter', 'rule'), array('source', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('filter', 'rule'), array('destination', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// NAT Rules
+  		find_alias_reference(array('nat', 'rule'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'rule'), array('source', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'rule'), array('destination', 'port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'rule'), array('target'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'rule'), array('local-port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// NAT 1:1 Rules
+  		//find_alias_reference(array('nat', 'onetoone'), array('external'), $alias_name, $is_alias_referenced, $referenced_by);
+  		//find_alias_reference(array('nat', 'onetoone'), array('source', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'onetoone'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// NAT Outbound Rules
+  		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('source', 'network'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('sourceport'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('destination', 'address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('dstport'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('nat', 'advancedoutbound', 'rule'), array('target'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// Alias in an alias
+  		find_alias_reference(array('aliases', 'alias'), array('address'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// Load Balancer
+  		find_alias_reference(array('load_balancer', 'lbpool'),         array('port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		find_alias_reference(array('load_balancer', 'virtual_server'), array('port'), $alias_name, $is_alias_referenced, $referenced_by);
+  		// Static routes
+  		find_alias_reference(array('staticroutes', 'route'), array('network'), $alias_name, $is_alias_referenced, $referenced_by);
+  		if($is_alias_referenced) {
+  			$savemsg = sprintf(gettext("Cannot delete alias. Currently in use by %s"), $referenced_by);
+  		} else {
+  			unset($a_aliases[$_POST['id']]);
+  			if (write_config()) {
+  				filter_configure();
+  				mark_subsystem_dirty('aliases');
+  			}
+  			header("Location: firewall_aliases.php?tab=" . $selected_tab);
+  			exit;
+  		}
+    }
+  }
+}
+
+
+$pgtitle = array(gettext("Firewall"), gettext("Aliases"));
 $shortcut_section = "aliases";
-
-include("head.inc");
-
 $main_buttons = array(
-	array('href'=>'firewall_aliases_edit.php?tab='.$tab, 'label'=>gettext("Add a new alias")),
+	array('href'=>'firewall_aliases_edit.php?tab='.$selected_tab, 'label'=>gettext("Add a new alias")),
 	array('href'=>'firewall_aliases_import.php', 'label'=>gettext("Bulk import aliases from list")),
-
 );
 
+
+include("head.inc");
 ?>
-
 <body>
+<script type="text/javascript">
+$( document ).ready(function() {
+	// link delete buttons
+	$(".act_delete").click(function(){
+		var id = $(this).attr("id").split('_').pop(-1);
+		BootstrapDialog.show({
+				type:BootstrapDialog.TYPE_INFO,
+				title: "<?= gettext("Aliasses");?>",
+				message: "<?=gettext("Do you really want to delete this alias? All elements that still use it will become invalid (e.g. filter rules)!");?>",
+				buttons: [{
+                label: "<?= gettext("No");?>",
+                action: function(dialogRef) {
+                    dialogRef.close();
+                }}, {
+									label: "<?= gettext("Yes");?>",
+									action: function(dialogRef) {
+										$.post(window.location, {act: 'del', id:id}, function(data) {
+													location.reload();
+										});
+										dialogRef.close();
+								}
+            }]
+		});
+	});
+});
+</script>
 <?php include("fbegin.inc"); ?>
-
 	<section class="page-content-main">
 		<div class="container-fluid">
 			<div class="row">
-
-				<?php if (isset($savemsg)) print_info_box($savemsg); ?>
-				<?php if (is_subsystem_dirty('aliases')): ?><p>
-				<?php print_info_box_np(gettext("The alias list has been changed.") . "<br />" . gettext("You must apply the changes in order for them to take effect."));?>
-				<?php endif; ?>
-
-			    <section class="col-xs-12">
-
-
-					<?php
+<?php if (isset($savemsg)) print_info_box($savemsg); ?>
+<?php if (is_subsystem_dirty('aliases')): ?>
+<?php print_info_box_np(gettext("The alias list has been changed.") . "<br />" . gettext("You must apply the changes in order for them to take effect."));?>
+<?php endif; ?>
+			  <section class="col-xs-12">
+<?php
 							$tab_array = array();
-							$tab_array[] = array(gettext("IP"),($tab=="ip" ? true : ($tab=="host" ? true : ($tab == "network" ? true : false))), "/firewall_aliases.php?tab=ip");
-							$tab_array[] = array(gettext("Ports"), ($tab=="port"? true : false), "/firewall_aliases.php?tab=port");
-							$tab_array[] = array(gettext("URLs"), ($tab=="url"? true : false), "/firewall_aliases.php?tab=url");
-							$tab_array[] = array(gettext("All"), ($tab=="all"? true : false), "/firewall_aliases.php?tab=all");
+							$tab_array[] = array(gettext("IP"),($selected_tab=="ip" ? true : ($selected_tab=="host" ? true : ($selected_tab == "network" ? true : false))), "/firewall_aliases.php?tab=ip");
+							$tab_array[] = array(gettext("Ports"), ($selected_tab=="port"? true : false), "/firewall_aliases.php?tab=port");
+							$tab_array[] = array(gettext("URLs"), ($selected_tab=="url"? true : false), "/firewall_aliases.php?tab=url");
+							$tab_array[] = array(gettext("All"), ($selected_tab=="all"? true : false), "/firewall_aliases.php?tab=all");
 							display_top_tabs($tab_array);
-						?>
-
-
-						<div class="tab-content content-box col-xs-12">
-	                        <form action="firewall_aliases.php" method="post" name="iform" id="iform">
-								<input type="hidden" name="tab" value="<?=htmlspecialchars($tab);?>" />
-
-		                        <div class="table-responsive">
-			                        <table class="table table-striped table-sort">
+?>
+					<div class="tab-content content-box col-xs-12">
+            <form action="firewall_aliases.php" method="post" name="iform" id="iform">
+              <input type="hidden" name="tab" value="<?=$selected_tab;?>" />
+                <div class="table-responsive">
+                  <table class="table table-striped">
 										<tr>
-											<td width="20%" class="listhdrr"><?=gettext("Name"); ?></td>
-											<td width="43%" class="listhdrr"><?=gettext("Values"); ?></td>
-											<td width="30%" class="listhdr"><?=gettext("Description"); ?></td>
-											<td width="7%" class="list"></td>
+											<td><?=gettext("Name"); ?></td>
+											<td><?=gettext("Values"); ?></td>
+											<td><?=gettext("Description"); ?></td>
+											<td>&nbsp;</td>
 										</tr>
-										<?php
+<?php
 										asort($a_aliases);
 										foreach ($a_aliases as $i=> $alias){
-											unset ($show_alias);
-											switch ($tab){
+											$show_alias = false;
+											switch ($selected_tab){
 											case "all":
 												$show_alias= true;
 												break;
@@ -215,63 +229,55 @@ $main_buttons = array(
 													$show_alias= true;
 												break;
 											}
-											if ($show_alias) {
+											if ($show_alias ){
 										?>
 										<tr>
-											<td class="listlr" ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
+											<td ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
 												<?=htmlspecialchars($alias['name']);?>
 											</td>
-											<td class="listr" ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
-											<?php
-											if ($alias["url"]) {
-												echo $alias["url"] . "<br />";
-											} else {
-												if(is_array($alias["aliasurl"])) {
-													$aliasurls = implode(", ", array_slice($alias["aliasurl"], 0, 10));
-													echo $aliasurls;
-													if(count($aliasurls) > 10) {
-														echo "...<br />";
-													}
-													echo "<br />\n";
-												}
-												$tmpaddr = explode(" ", $alias['address']);
-												$addresses = implode(", ", array_slice($tmpaddr, 0, 10));
-												echo $addresses;
-												if(count($tmpaddr) > 10) {
-													echo "...";
-												}
+<?php
+                        $alias_values = '';
+											  if (!empty($alias["url"])) {
+												    $alias_values = $alias["url"];
+											  } elseif(isset($alias["aliasurl"])) {
+                            $alias_values = implode(", ", array_slice(explode(" ", $alias['aliasurl']), 0, 5));
+													  if(count(explode(" ", $alias['aliasurl'])) > 5) {
+														    $alias_values .= "...";
+													  }
+											  } else {
+												    $alias_values = implode(", ", array_slice(explode(" ", $alias['address']), 0, 5));
+												    if(count(explode(" ", $alias['address'])) > 5) {
+													      $alias_values .= "...";
+												    }
 											}
-											?>
+?>
+                      <td ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
+                        <?= htmlspecialchars($alias_values); ?>
 											</td>
-											<td class="listbg" ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
-												<?=htmlspecialchars($alias['descr']);?>&nbsp;
+											<td ondblclick="document.location='firewall_aliases_edit.php?id=<?=$i;?>';">
+												<?=htmlspecialchars($alias['descr']);?>
 											</td>
-											<td valign="middle" class="list nowrap">
-												<table border="0" cellspacing="0" cellpadding="1" summary="icons">
-													<tr>
-														<td valign="middle">
-															 <a href="firewall_aliases_edit.php?id=<?=$i;?>" class="btn btn-default"><span class="glyphicon glyphicon-edit" title="<?=gettext("Edit alias")." ".$alias['name'];?>"></span></a>
-
-															 <a href="firewall_aliases.php?act=del&amp;tab=<?=$tab;?>&amp;id=<?=$i;?>" class="btn btn-default"  onclick="return confirm('<?=gettext("Do you really want to delete this alias? All elements that still use it will become invalid (e.g. filter rules)!");?>')"><span class="glyphicon glyphicon-remove"></span></a>
-
-															</td>
-													</tr>
-												</table>
+											<td>
+                        <a href="firewall_aliases_edit.php?id=<?=$i;?>" title="<?=gettext("Edit alias"); ?>" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"></span></a>
+                        <a id="del_<?=$i;?>" title="<?=gettext("delete alias"); ?>" class="act_delete btn btn-default btn-xs"><span class="glyphicon glyphicon-remove"></span></a>
 											</td>
 										</tr>
-										<?php
+<?php
 											} // if ($show_alias)
 										} // foreach
-										?>
-									</table>
-								</div>
-								<div class="container-fluid">
-								    <p><span class="vexpl"><span class="text-danger"><strong><?=gettext("Note:"); ?><br /></strong></span></span></p><div style="overflow:hidden; text-align:justify;"><p><span class="vexpl"><?=gettext("Aliases act as placeholders for real hosts, networks or ports. They can be used to minimize the number of changes that have to be made if a host, network or port changes. You can enter the name of an alias instead of the host, network or port in all fields that have a red background. The alias will be resolved according to the list above. If an alias cannot be resolved (e.g. because you deleted it), the corresponding element (e.g. filter/NAT/shaper rule) will be considered invalid and skipped."); ?></span></p></div>
+?>
+						      </table>
+                  <div class="container-fluid">
+								    <span class="text-danger">
+                      <strong><?=gettext("Note:"); ?><br />
+                      </strong></span>
+                    </span>
+                    <?=gettext("Aliases act as placeholders for real hosts, networks or ports. They can be used to minimize the number of changes that have to be made if a host, network or port changes. You can enter the name of an alias instead of the host, network or port in all fields that have a red background. The alias will be resolved according to the list above. If an alias cannot be resolved (e.g. because you deleted it), the corresponding element (e.g. filter/NAT/shaper rule) will be considered invalid and skipped."); ?>
 							    </div>
-	                        </form>
-						</div>
-			    </section>
+	          </form>
+					</div>
+			  </section>
 			</div>
-		</div>
+	   </div>
 	</section>
 <?php include("foot.inc"); ?>
