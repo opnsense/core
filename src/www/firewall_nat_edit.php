ab090d03a5a9162d7d00b6751dd078f1d19e8177
Ad Schellevis <ad@opnsense.org>
2015-08-21 09:07:25 +0000

    (legacy) rename all_help id in firewall_nat_edit.php

--- src/www/firewall_nat_edit.php
+++ src/www/firewall_nat_edit.php
@@ -504,7 +504,7 @@ $( document ).ready(function() {
                   <td width="22%"><?=gettext("Edit Redirect entry"); ?></td>
                   <td  width="78%" align="right">
                     <small><?=gettext("full help"); ?> </small>
-                    <i class="fa fa-toggle-off text-danger"  style="cursor: pointer;" id="show_all_help_opnvpn_server" type="button"></i></a>
+                    <i class="fa fa-toggle-off text-danger"  style="cursor: pointer;" id="show_all_help_page" type="button"></i></a>
                   </td>
                 </tr>
                 <tr>
