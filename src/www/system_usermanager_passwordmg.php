971a58713af90f0028a3f5697535f4f1fc7fb666
dom <djdomics@gmail.com>
2015-08-27 23:24:16 +0200

    Add username in gettext message

--- src/www/system_usermanager_passwordmg.php
+++ src/www/system_usermanager_passwordmg.php
@@ -97,6 +97,7 @@ include("head.inc");
 							  <table class="table table-striped">
                   <tr>
                     <td colspan="2"><?=$username?>'s <?=gettext("Password"); ?></td>
+                    <td colspan="2"><?=sprintf(gettext("%s's Password"),$username); ?></td>
 			            </tr>
 			            <tr>
 			              <td><?=gettext("Old password"); ?></td>
