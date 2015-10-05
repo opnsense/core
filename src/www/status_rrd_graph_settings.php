2d1f2af5b8b92cb8a5df837e6765fca53a331322
Franco Fichtner <franco@opnsense.org>
2015-10-05 10:20:07 +0200

    system/settings/rrd: prune unneeded settings
    Makes the file very light, best to be merged into misc settings.

--- src/www/status_rrd_graph_settings.php
+++ src/www/status_rrd_graph_settings.php
@@ -32,26 +32,6 @@ require_once("rrd.inc");
 require_once("interfaces.inc");
 
 $pconfig['enable'] = isset($config['rrd']['enable']);
-$pconfig['category'] = $config['rrd']['category'];
-$pconfig['style'] = $config['rrd']['style'];
-$pconfig['period'] = $config['rrd']['period'];
-
-$curcat = "settings";
-$categories = array('system' => gettext("System"),
-		'traffic' => gettext("Traffic"),
-		'packets' => gettext("Packets"),
-		'quality' => gettext("Quality"),
-		'captiveportal' => gettext("Captive Portal"));
-
-if(isset($config['ntpd']['statsgraph'])) {
-	$categories['ntpd'] = gettext("NTP");
-}
-
-$styles = array('inverse' => gettext("Inverse"),
-		'absolute' => gettext("Absolute"));
-$periods = array("absolute" => gettext("Absolute Timespans"),
-		"current" => gettext("Current Period"),
-		"previous" => gettext("Previous Period"));
 
 if ($_POST['ResetRRD']) {
 	mwexec('/bin/rm /var/db/rrd/*');
@@ -67,35 +47,11 @@ if ($_POST['ResetRRD']) {
 
         if (!$input_errors) {
                 $config['rrd']['enable'] = $_POST['enable'] ? true : false;
-                $config['rrd']['category'] = $_POST['category'];
-                $config['rrd']['style'] = $_POST['style'];
-                $config['rrd']['period'] = $_POST['period'];
                 write_config();
 
                 $retval = 0;
                 $retval = enable_rrd_graphing();
                 $savemsg = get_std_save_message();
-	}
-}
-
-
-
-$rrddbpath = "/var/db/rrd/";
-chdir($rrddbpath);
-$databases = glob("*.rrd");
-
-foreach($databases as $database) {
-	if(stristr($database, "wireless")) {
-		$wireless = true;
-	}
-	if(stristr($database, "-cellular") && !empty($config['ppps'])) {
-		$cellular = true;
-	}
-	if(stristr($database, "-vpnusers")) {
-		$vpnusers = true;
-	}
-	if(stristr($database, "captiveportal-") && is_array($config['captiveportal'])) {
-		$captiveportal = true;
 	}
 }
 
@@ -133,51 +89,6 @@ include("head.inc");
 												<input name="enable" type="checkbox" id="enable" value="yes" <?php if ($pconfig['enable']) echo "checked=\"checked\"" ?> onclick="enable_change(false)" />
 												&nbsp;<?=gettext("Enables the RRD graphing backend.");?>
 											</label>
-										</td>
-									</tr>
-									<tr>
-							<td width="22%" valign="top" class="vtable"><?=gettext("Default category");?></td>
-				                        <td width="78%" class="vtable">
-											<select name="category" id="category" class="form-control" style="z-index: -10;" >
-											<?php
-											foreach ($categories as $category => $categoryd) {
-												echo "<option value=\"$category\"";
-												if ($category == $pconfig['category']) echo " selected=\"selected\"";
-												echo ">" . htmlspecialchars($categoryd) . "</option>\n";
-											}
-											?>
-											</select>
-											<p class="text-muted"><em><small><?=gettext("This selects default category.");?></small></em></p>
-										</td>
-									</tr>
-									<tr>
-							<td width="22%" valign="top" class="vtable"><?=gettext("Default style");?></td>
-				                        <td width="78%" class="vtable">
-											<select name="style" class="form-control" style="z-index: -10;" >
-											<?php
-											foreach ($styles as $style => $styled) {
-												echo "<option value=\"$style\"";
-												if ($style == $pconfig['style']) echo " selected=\"selected\"";
-												echo ">" . htmlspecialchars($styled) . "</option>\n";
-											}
-											?>
-											</select>
-											<p class="text-muted"><em><small><?=gettext("This selects the default style.");?></small></em></p>
-										</td>
-									</tr>
-									<tr>
-										<td width="22%" valign="top" class="vtable"><?=gettext("Default period");?></td>
-										<td width="78%" class="vtable">
-											<select name="period" class="form-control" style="z-index: -10;" >
-											<?php
-											foreach ($periods as $period => $periodd) {
-												echo "<option value=\"$period\"";
-												if ($period == $pconfig['period']) echo " selected=\"selected\"";
-												echo ">" . htmlspecialchars($periodd) . "</option>\n";
-											}
-											?>
-											</select>
-											<p class="text-muted"><em><small><?=gettext("This selects the default period.");?></small></em></p>
 										</td>
 									</tr>
 									<tr>
