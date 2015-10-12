1f66fa6a953d8235874e41d43d58a9e3620be563
Ad Schellevis <ad@opnsense.org>
2015-10-12 15:28:08 +0000

    (captiveportal, new) add rc enable/disable config template

---
+++ src/opnsense/service/templates/OPNsense/Captiveportal/rc.conf.d
@@ -0,0 +1,10 @@
+{% set isEnabled=[] %}
+{% if helpers.exists('OPNsense.captiveportal.zones.zone') %}
+{%   for cpZone in  helpers.toList('OPNsense.captiveportal.zones.zone') %}
+{%     if cpZone.enabled|default('0') == '1' %}
+{%	do isEnabled.append(cpZone) %}
+{%     endif %}
+{%   endfor %}
+{% endif 
+%}
+captiveportal_enable="{% if isEnabled %}YES{% else %}NO{% endif %}"
