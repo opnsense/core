13b25f77003f7c01c484632c9fbb23411b56868e
Frédéric LIETART (TheLinuxFr) <stuff@thelinuxfr.org>
2015-11-12 20:53:59 +0100

    Fix : https://github.com/opnsense/core/blob/master/src/www/status_wireless.php#L190

--- src/www/status_wireless.php
+++ src/www/status_wireless.php
@@ -187,9 +187,9 @@ if(empty($if)) {
 								</table>
 				</div>
 
-				<b>Flags:</b> A = authorized, E = Extended Rate (802.11g), P = Power save mode<br />
-	<b>Capabilities:</b> E = ESS (infrastructure mode), I = IBSS (ad-hoc mode), P = privacy (WEP/TKIP/AES),
-		S = Short preamble, s = Short slot time
+				<b><?=gettext('Flags:') ?></b> <?=gettext('A = authorized, E = Extended Rate (802.11g), P = Power save mode') ?><br />
+	<b><?=gettext('Capabilities:') ?></b> <?=gettext('E = ESS (infrastructure mode), I = IBSS (ad-hoc mode), P = privacy (WEP/TKIP/AES),
+		S = Short preamble, s = Short slot time') ?>
                         </form>
 				</div>
 			    </section>
