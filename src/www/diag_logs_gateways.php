14204de46cbf038ac04abedcffef8f5fb148d640
Ad Schellevis <ad@opnsense.org>
2015-12-08 17:31:56 +0100

    (legacy) style cleanup diag_logs_gateways.php
    (cherry picked from commit 452a6010842d9d3f3d6c75ad405aa8ec128d7521)

--- src/www/diag_logs_gateways.php
+++ src/www/diag_logs_gateways.php
@@ -1,32 +1,32 @@
 <?php
 
 /*
-	Copyright (C) 2014 Deciso B.V.
-	Copyright (C) 2004-2009 Scott Ullrich
-	Copyright (C) 2012 Seth Mos
-	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
-	All rights reserved.
+  Copyright (C) 2014 Deciso B.V.
+  Copyright (C) 2004-2009 Scott Ullrich
+  Copyright (C) 2012 Seth Mos
+  Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
+  All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
 
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
 
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
 */
 
 require_once("guiconfig.inc");
@@ -39,58 +39,52 @@ $filtertext = '';
 $nentries = 50;
 
 if (isset($config['syslog']['nentries'])) {
-        $nentries = $config['syslog']['nentries'];
+    $nentries = $config['syslog']['nentries'];
 }
 
-if ($_POST['clear']) {
-	clear_clog($system_logfile);
+if (!empty($_POST['clear'])) {
+    clear_clog($system_logfile);
 }
 
 if (isset($_POST['filtertext'])) {
-	$filtertext = htmlspecialchars($_POST['filtertext']);
+    $filtertext = htmlspecialchars($_POST['filtertext']);
 }
 
-$pgtitle = array(gettext('System'), gettext('Log Files'), gettext('Gateways'));
 include("head.inc");
-
 ?>
 
 <body>
-
 <?php include("fbegin.inc"); ?>
-
-	<section class="page-content-main">
-		<div class="container-fluid">
-			<div class="row">
-
-				<?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
-
-			    <section class="col-xs-12">
-					<div class="tab-content content-box col-xs-12">
-
-							 <div class="table-responsive">
-								<table class="table table-striped table-sort">
-							<tr><td colspan="2">
-							<form id="clearform" name="clearform" action="diag_logs_gateways.php" method="post" class="__mt">
-					<input id="filtertext" name="filtertext" value="<?=$filtertext;?>"/>
-					<input id="filtersubmit" name="filtersubmit" type="submit" class="btn btn-primary" value="<?=gettext("Filter");?>" />
-						</form>
-						</td></tr>
-							<?php dump_clog($system_logfile, $nentries, $filtertext); ?>
-							<tr><td colspan="2">
-							<form action="diag_logs_gateways.php" method="post">
-								<input name="clear" type="submit" class="btn" value="<?= gettext("Clear log");?>" />
-							</form>
-							</td></tr>
-
-								</table>
-							 </div>
-
-						</div>
-				    </div>
-			</section>
-			</div>
-		</div>
-	</section>
-
+  <section class="page-content-main">
+    <div class="container-fluid">
+      <div class="row">
+        <?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
+          <section class="col-xs-12">
+            <div class="tab-content content-box col-xs-12">
+              <div class="table-responsive">
+                <table class="table table-striped">
+                  <tr>
+                    <td colspan="2">
+                      <form id="clearform" name="clearform" action="diag_logs_gateways.php" method="post" class="__mt">
+                        <input id="filtertext" name="filtertext" value="<?=$filtertext;?>"/>
+                        <input id="filtersubmit" name="filtersubmit" type="submit" class="btn btn-primary" value="<?=gettext("Filter");?>" />
+                      </form>
+                    </td>
+                  </tr>
+                  <?php dump_clog($system_logfile, $nentries, $filtertext); ?>
+                  <tr>
+                    <td colspan="2">
+                      <form action="diag_logs_gateways.php" method="post">
+                        <input name="clear" type="submit" class="btn" value="<?= gettext("Clear log");?>" />
+                      </form>
+                    </td>
+                  </tr>
+                </table>
+               </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
 <?php include("foot.inc"); ?>
