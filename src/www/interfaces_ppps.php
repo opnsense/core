c6455873fb06bc0d34b41c99cf24fed79acff637
Ad Schellevis <ad@opnsense.org>
2015-12-28 20:27:51 +0100

    (legacy) refactor interfaces_ppps.php

--- src/www/interfaces_ppps.php
+++ src/www/interfaces_ppps.php
@@ -1,146 +1,163 @@
 <?php
 
 /*
-	Copyright (C) 2014-2015 Deciso B.V.
-	Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
-	All rights reserved.
+  Copyright (C) 2014-2015 Deciso B.V.
+  Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>.
+  All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
 
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
 
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
 */
 
 require_once("guiconfig.inc");
 require_once("interfaces.inc");
 
-function ppp_inuse($num) {
-	global $config, $g;
-	$iflist = get_configured_interface_list(false, true);
-	if (!is_array($config['ppps']['ppp']))
-		return false;
-
-	foreach ($iflist as $if) {
-		if ($config['interfaces'][$if]['if'] == $config['ppps']['ppp'][$num]['if'])
-			return true;
-	}
-	return false;
+function ppp_inuse($ppp_intf) {
+    global $config;
+    if (isset($config['ppps']['ppp']) && !is_array($config['ppps']['ppp'])) {
+        foreach ($config['interfaces'] as $intf) {
+            if ($intf['if'] == $ppp_intf) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-if ($_GET['act'] == "del") {
-	/* check if still in use */
-	if (ppp_inuse($_GET['id'])) {
-		$input_errors[] = gettext("This point-to-point link cannot be deleted because it is still being used as an interface.");
-	} elseif (is_array($config['ppps']['ppp']) && is_array($config['ppps']['ppp'][$_GET['id']])) {
-
-		unset($config['ppps']['ppp'][$_GET['id']]['pppoe-reset-type']);
-		handle_pppoe_reset($config['ppps']['ppp'][$_GET['id']]);
-		unset($config['ppps']['ppp'][$_GET['id']]);
-		write_config();
-		header("Location: interfaces_ppps.php");
-		exit;
-	}
+if (!isset($config['ppps']['ppp']) || !is_array($config['ppps']['ppp'])) {
+    $a_ppps = array();
+} else {
+    $a_ppps = &$config['ppps']['ppp'];
 }
 
-if (!is_array($config['ppps'])) {
-	$config['ppps'] = array();
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($a_ppps[$_POST['id']])) {
+        $id = $_POST['id'];
+    }
 
-if (!is_array($config['ppps']['ppp'])) {
-	$config['ppps']['ppp'] = array();
+    if (!empty($_POST['action']) && $_POST['action'] == "del" && isset($id)) {
+        if (ppp_inuse($a_ppps[$id]['if'])) {
+            $input_errors[] = gettext("This point-to-point link cannot be deleted because it is still being used as an interface.");
+        } else {
+            unset($a_ppps[$id]['pppoe-reset-type']);
+            handle_pppoe_reset($a_ppps[$id]);
+            unset($a_ppps[$id]);
+            write_config();
+            header("Location: interfaces_ppps.php");
+            exit;
+        }
+    }
 }
-
-$a_ppps = $config['ppps']['ppp'];
 
 include("head.inc");
-
+legacy_html_escape_form_data($a_ppps);
 $main_buttons = array(
-	array('href'=>'interfaces_ppps_edit.php', 'label'=>gettext('Add')),
+  array('href'=>'interfaces_ppps_edit.php', 'label'=>gettext('Add')),
 );
 
 ?>
-
 <body>
+  <script type="text/javascript">
+  $( document ).ready(function() {
+    // link delete buttons
+    $(".act_delete").click(function(event){
+      event.preventDefault();
+      var id = $(this).data("id");
+      // delete single
+      BootstrapDialog.show({
+        type:BootstrapDialog.TYPE_INFO,
+        title: "<?= gettext("Point-to-Point");?>",
+        message: "<?=gettext("Do you really want to delete this PPP interface?");?>",
+        buttons: [{
+                  label: "<?= gettext("No");?>",
+                  action: function(dialogRef) {
+                      dialogRef.close();
+                  }}, {
+                  label: "<?= gettext("Yes");?>",
+                  action: function(dialogRef) {
+                    $("#id").val(id);
+                    $("#action").val("del");
+                    $("#iform").submit()
+                }
+              }]
+      });
+    });
+
+  });
+  </script>
 <?php include("fbegin.inc"); ?>
+  <section class="page-content-main">
+    <div class="container-fluid">
+      <div class="row">
+        <?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
+        <section class="col-xs-12">
+          <div class="tab-content content-box col-xs-12">
+            <form method="post" name="iform" id="iform">
+              <input type="hidden" id="action" name="action" value="">
+              <input type="hidden" id="id" name="id" value="">
+              <div class="table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th><?=gettext("Interface");?></th>
+                      <th><?=gettext("Interface(s)/Port(s)");?></th>
+                      <th><?=gettext("Description");?></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
 
-	<section class="page-content-main">
-		<div class="container-fluid">
-			<div class="row">
-
-				<?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
-
-			    <section class="col-xs-12">
-
-						<div class="tab-content content-box col-xs-12">
-
-		                        <form action="interfaces_assign.php" method="post" name="iform" id="iform">
-
-		                        <div class="table-responsive">
-			                        <table class="table table-striped table-sort">
-
-			                         <thead>
-                                            <tr>
-								<th width="20%" class="listtopic"><?=gettext("Interface");?></th>
-								<th width="20%" class="listtopic"><?=gettext("Interface(s)/Port(s)");?></th>
-								<th width="40%" class="listtopic"><?=gettext("Description");?></th>
-								<th width="10%" class="listtopic">&nbsp;</th>
-                                            </tr>
-                                        </thead>
-
-									<tbody>
-
-									  <?php $i = 0; foreach ($a_ppps as $id => $ppp): ?>
-						                <tr  ondblclick="document.location='interfaces_ppps_edit.php?id=<?=$i;?>'">
-									<td class="listr">
-											<?=htmlspecialchars($ppp['if']);?>
-						                  </td>
-						                  <td class="listr">
-											<?php
-												$portlist = explode(",", $ppp['ports']);
-												foreach ($portlist as $portid => $port) {
-													if ($port != get_real_interface($port) && $ppp['type'] != "ppp")
-														$portlist[$portid] = convert_friendly_interface_to_friendly_descr($port);
-												}
-												echo htmlspecialchars(implode(",", $portlist));
-											?>
-						                  </td>
-						                  <td class="listbg">
-						                    <?=htmlspecialchars($ppp['descr']);?>&nbsp;
-						                  </td>
-						                  <td valign="middle" class="list nowrap">
-
-							                   <a href="interfaces_ppps_edit.php?id=<?=$i;?>" class="btn btn-default"><span class="glyphicon glyphicon-edit" title="<?=gettext("edit group");?>"></span></a>
-
-											   <a href="interfaces_ppps.php?act=del&amp;id=<?=$i;?>" class="btn btn-default"  onclick="return confirm('<?=gettext("Do you really want to delete this PPP interface?");?>')"><span class="glyphicon glyphicon-remove"></span></a>
-
-							               </td>
-										</tr>
-									  <?php $i++; endforeach; ?>
-									</tbody>
-						              </table>
-							      </div>
-		                        </form>
-
-						</div>
-			    </section>
-			</div>
-		</div>
-	</section>
+<?php
+                  $i = 0;
+                  foreach ($a_ppps as $id => $ppp): ?>
+                  <tr>
+                    <td><?=$ppp['if'];?></td>
+                    <td>
+<?php
+                    $portlist = explode(",", $ppp['ports']);
+                    foreach ($portlist as $portid => $port) {
+                      if ($port != get_real_interface($port) && $ppp['type'] != "ppp")
+                        $portlist[$portid] = convert_friendly_interface_to_friendly_descr($port);
+                    }?>
+                      <?=htmlspecialchars(implode(",", $portlist));?>
+                    </td>
+                    <td><?=$ppp['descr'];?></td>
+                    <td>
+                      <a href="interfaces_ppps_edit.php?id=<?=$i;?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-edit" title="<?=gettext("edit group");?>"></span></a>
+                      <button title="<?=gettext("delete interface");?>" data-toggle="tooltip" data-placement="left" data-id="<?=$i;?>" class="btn btn-default btn-xs act_delete" type="submit">
+                        <span class=" glyphicon glyphicon-remove"></span>
+                      </button>
+                    </td>
+                  </tr>
+<?php
+                  $i++;
+                  endforeach; ?>
+                </tbody>
+              </table>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  </div>
+</section>
 
 <?php include("foot.inc"); ?>
