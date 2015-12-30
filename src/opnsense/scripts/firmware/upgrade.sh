219f740c2b9d3e8e8527a5d2f691cd1284fc5ee5
Franco Fichtner <franco@opnsense.org>
2015-12-30 17:43:24 +0100

    firmware: steps towards package actions
    o Adds a reinstall backend call
    o Moves pkg_upgrade.sh into its own dir
    o Don't clobber progress output when action is running
    o Rename action to clearly reflect it's purpose
    o Tweak EOL message a bit more
    
    This is a test of how far we can go with our backend restructuring
    without glitching firmware update temporarily.
    
    (cherry picked from commit a5c4d51da0692e4f2ac740f18c85eff48fbafaf3)

---
+++ src/opnsense/scripts/firmware/upgrade.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Copyright (C) 2015 Franco Fichtner <franco@opnsense.org>
+# Copyright (C) 2014 Deciso B.V.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+PKG_PROGRESS_FILE=/tmp/pkg_upgrade.progress
+PACKAGE=$1
+REBOOT=
+
+# Check if another pkg process is already running
+if [ -n "$(pgrep pkg)" ]; then
+	exit 0
+fi
+
+# Truncate upgrade progress file
+: > ${PKG_PROGRESS_FILE}
+
+echo "***GOT REQUEST TO UPGRADE: $PACKAGE***" >> ${PKG_PROGRESS_FILE}
+
+echo '***STARTING UPGRADE***' >> ${PKG_PROGRESS_FILE}
+if [ "$PACKAGE" == "all" ]; then
+	# update all installed packages
+	opnsense-update -p >> ${PKG_PROGRESS_FILE}
+	# restart the web server
+	/usr/local/etc/rc.restart_webgui >> ${PKG_PROGRESS_FILE}
+	# if we can update base, we'll do that as well
+	if opnsense-update -c; then
+		if opnsense-update -bk >> ${PKG_PROGRESS_FILE}; then
+			REBOOT=1
+		fi
+	fi
+elif [ "$PACKAGE" == "pkg" ]; then
+	pkg upgrade -y $PACKAGE >> ${PKG_PROGRESS_FILE}
+	echo  "*** PLEASE CHECK FOR MORE UPGRADES"
+else
+	echo "Cannot update $PACKAGE" >> ${PKG_PROGRESS_FILE}
+fi
+
+if [ -n "${REBOOT}" ]; then
+	echo '***REBOOT***' >> ${PKG_PROGRESS_FILE}
+	# give the frontend some time to figure out that a reboot is coming
+	sleep 5
+	/usr/local/etc/rc.reboot
+fi
+
+echo '***DONE***' >> ${PKG_PROGRESS_FILE}
