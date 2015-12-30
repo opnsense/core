9e07e782028b38d69a9cc60683c7e38fdbd998e5
Ad Schellevis <ad@opnsense.org>
2015-12-30 17:26:24 +0100

    (legacy) refactor interfaces_bridge_edit.php

--- src/www/interfaces_bridge.php
+++ src/www/interfaces_bridge.php
@@ -1,168 +1,179 @@
 <?php
 
 /*
-	Copyright (C) 2014-2015 Deciso B.V.
-	Copyright (C) 2008 Ermal Luçi
-	All rights reserved.
+    Copyright (C) 2014-2015 Deciso B.V.
+    Copyright (C) 2008 Ermal Luçi
+    All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
 
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
 
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
 */
 
 require_once("guiconfig.inc");
 require_once("interfaces.inc");
 
-if (!is_array($config['bridges'])) {
-	$config['bridges'] = array();
+
+if (!isset($config['bridges']['bridged']) || !is_array($config['bridges']['bridged'])) {
+    $a_bridges = array();
+} else {
+    $a_bridges = &$config['bridges']['bridged'] ;
 }
 
-if (!is_array($config['bridges']['bridged'])) {
-	$config['bridges']['bridged'] = array();
+
+function bridge_inuse($bridge_if) {
+    global $config;
+    foreach ($config['interfaces'] as $if => $intf) {
+        if ($intf['if'] == $bridge_if) {
+            return true;
+        }
+    }
+    return false;
 }
 
-$a_bridges = &$config['bridges']['bridged'] ;
 
-function bridge_inuse($num) {
-	global $config, $a_bridges;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($a_bridges[$_POST['id']])) {
+        $id = $_POST['id'];
+    }
 
-	$iflist = get_configured_interface_list(false, true);
-	foreach ($iflist as $if) {
-		if ($config['interfaces'][$if]['if'] == $a_bridges[$num]['bridgeif'])
-			return true;
-	}
-
-	return false;
-}
-
-if ($_GET['act'] == "del") {
-	if (!isset($_GET['id']))
-		$input_errors[] = gettext("Wrong parameters supplied");
-	else if (empty($a_bridges[$_GET['id']]))
-		$input_errors[] = gettext("Wrong index supplied");
-	/* check if still in use */
-	else if (bridge_inuse($_GET['id'])) {
-		$input_errors[] = gettext("This bridge cannot be deleted because it is assigned as an interface.");
-	} else {
-		if (!does_interface_exist($a_bridges[$_GET['id']]['bridgeif'])) {
-			log_error("Bridge interface does not exist, skipping ifconfig destroy.");
-		} else {
-			mwexec("/sbin/ifconfig " . $a_bridges[$_GET['id']]['bridgeif'] . " destroy");
-		}
-
-		unset($a_bridges[$_GET['id']]);
-
-		write_config();
-
-		header("Location: interfaces_bridge.php");
-		exit;
-	}
+    if (!empty($_POST['action']) && $_POST['action'] == "del" && isset($id)) {
+        if (bridge_inuse($a_bridges[$id]['bridgeif'])) {
+            $input_errors[] = gettext("This bridge cannot be deleted because it is assigned as an interface.");
+        } else {
+            if (!does_interface_exist($a_bridges[$id]['bridgeif'])) {
+                log_error("Bridge interface does not exist, skipping ifconfig destroy.");
+            } else {
+                mwexec("/sbin/ifconfig " . escapeshellarg($a_bridges[$id]['bridgeif']) . " destroy");
+            }
+            unset($a_bridges[$id]);
+            write_config();
+            header("Location: interfaces_bridge.php");
+            exit;
+        }
+    }
 }
 
 include("head.inc");
-
+legacy_html_escape_form_data($a_bridges);
 $main_buttons = array(
-	array('href'=>'interfaces_bridge_edit.php', 'label'=>gettext('Add')),
+  array('href'=>'interfaces_bridge_edit.php', 'label'=>gettext('Add')),
 );
 
 ?>
 
 <body>
-<?php include("fbegin.inc"); ?>
+  <script type="text/javascript">
+  $( document ).ready(function() {
+    // link delete buttons
+    $(".act_delete").click(function(event){
+      event.preventDefault();
+      var id = $(this).data("id");
+      // delete single
+      BootstrapDialog.show({
+        type:BootstrapDialog.TYPE_INFO,
+        title: "<?= gettext("Bridge");?>",
+        message: "<?=gettext("Do you really want to delete this bridge?");?>",
+        buttons: [{
+                  label: "<?= gettext("No");?>",
+                  action: function(dialogRef) {
+                      dialogRef.close();
+                  }}, {
+                  label: "<?= gettext("Yes");?>",
+                  action: function(dialogRef) {
+                    $("#id").val(id);
+                    $("#action").val("del");
+                    $("#iform").submit()
+                }
+              }]
+      });
+    });
 
-	<section class="page-content-main">
-		<div class="container-fluid">
-			<div class="row">
+  });
+  </script>
+  <?php include("fbegin.inc"); ?>
 
-				<?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
-
-			    <section class="col-xs-12">
-
-						<div class="tab-content content-box col-xs-12">
-
-		                        <form action="interfaces_bridge.php" method="post" name="iform" id="iform">
-
-		                        <div class="table-responsive">
-			                        <table class="table table-striped table-sort">
-
-			                         <thead>
-                                            <tr>
-								<th width="20%" class="listtopic"><?=gettext("Interface");?></th>
-								<th width="20%" class="listtopic"><?=gettext("Members");?></th>
-								<th width="50%" class="listtopic"><?=gettext("Description");?></th>
-								<th width="10%" class="listtopic">&nbsp;</th>
-                                            </tr>
-                                        </thead>
-
-									<tbody>
-
-									  <?php $i = 0; $ifdescrs = get_configured_interface_with_descr();
-											foreach ($a_bridges as $bridge): ?>
-						                <tr  ondblclick="document.location='interfaces_bridge_edit.php?id=<?=$i;?>'">
-						                  <td class="listlr">
-											<?=htmlspecialchars(strtoupper($bridge['bridgeif']));?>
-						                  </td>
-						                  <td class="listr">
-											<?php $members = explode(',', $bridge['members']);
-											$j = 0;
-											foreach ($members as $member) {
-												if (isset($ifdescrs[$member])) {
-													echo $ifdescrs[$member];
-													$j++;
-												}
-												if ($j > 0 && $j < count($members))
-													echo ", ";
-											}
-											?>
-						                  </td>
-						                  <td class="listbg">
-						                    <?=htmlspecialchars($bridge['descr']);?>&nbsp;
-						                  </td>
-						                  <td valign="middle" class="list nowrap">
-
-							                   <a href="interfaces_bridge_edit.php?id=<?=$i;?>" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="<?=gettext("edit group");?>"><span class="glyphicon glyphicon-edit"></span></a>
-
-											   <a href="interfaces_bridge.php?act=del&amp;id=<?=$i;?>" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="<?=gettext("delete group");?>" onclick="return confirm('<?=gettext("Do you really want to delete this bridge?");?>')"><span class="glyphicon glyphicon-remove"></span></a>
-
-							               </td>
-										</tr>
-									  <?php $i++; endforeach; ?>
-
-									</tbody>
-						              </table>
-							      </div>
-
-							      <div class="container-fluid">
-							      <p class="vexpl"><span class="text-danger"><strong>
-									  <?=gettext("Note:"); ?><br />
-									  </strong></span>
-									  <?=gettext("Here you can configure bridging of interfaces."); ?>
-								    </p>
-							      </div>
-
-		                        </form>
-
-						</div>
-			    </section>
-			</div>
-		</div>
-	</section>
-
+  <section class="page-content-main">
+    <div class="container-fluid">
+      <div class="row">
+        <?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
+          <section class="col-xs-12">
+            <div class="tab-content content-box col-xs-12">
+              <form action="interfaces_bridge.php" method="post" name="iform" id="iform">
+                <input type="hidden" id="action" name="action" value="">
+                <input type="hidden" id="id" name="id" value="">
+                <div class="table-responsive">
+                  <table class="table table-striped">
+                   <thead>
+                      <tr>
+                        <th><?=gettext("Interface");?></th>
+                        <th><?=gettext("Members");?></th>
+                        <th><?=gettext("Description");?></th>
+                        <th>&nbsp;</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+<?php
+                    $i = 0;
+                    $ifdescrs = get_configured_interface_with_descr();
+                    foreach ($a_bridges as $bridge): ?>
+                      <tr>
+                        <td><?=strtoupper($bridge['bridgeif']);?></td>
+                        <td>
+<?php
+                        $members = explode(',', $bridge['members']);
+                        $j = 0;
+                        foreach ($members as $member) {
+                            if (isset($ifdescrs[$member])) {
+                                echo htmlspecialchars($ifdescrs[$member]);
+                                $j++;
+                            }
+                            if ($j > 0 && $j < count($members)) {
+                                echo ", ";
+                            }
+                        }?>
+                        </td>
+                        <td><?=$bridge['descr'];?></td>
+                        <td>
+                          <a href="interfaces_bridge_edit.php?id=<?=$i;?>" class="btn btn-xs btn-default" data-toggle="tooltip" data-placement="left" title="<?=gettext("edit bridge");?>">
+                            <span class="glyphicon glyphicon-edit"></span>
+                          </a>
+                          <button title="<?=gettext("delete interface");?>" data-toggle="tooltip" data-placement="left" data-id="<?=$i;?>" class="btn btn-default btn-xs act_delete" type="submit">
+                            <span class=" glyphicon glyphicon-remove"></span>
+                          </button>
+                        </td>
+                      </tr>
+<?php
+                    $i++;
+                  endforeach; ?>
+                  </tbody>
+                </table>
+              </div>
+              <div class="container-fluid">
+                <span class="text-danger"><strong><?=gettext("Note:"); ?><br /></strong></span>
+                <?=gettext("Here you can configure bridging of interfaces."); ?>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
 <?php include("foot.inc"); ?>
