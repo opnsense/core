c3135f5f413daf45a1cb54803d19961640adbbd9
Ad Schellevis <ad@opnsense.org>
2016-01-08 16:42:03 +0100

    (ipsec, legacy) add ipsec (mobile) leases to diag and widget, rename "ipsec list_status" to "ipsec list status", closes https://github.com/opnsense/core/issues/139

--- src/www/diag_ipsec_leases.php
+++ src/www/diag_ipsec_leases.php
@@ -1,30 +1,30 @@
 <?php
 
 /*
-	Copyright (C) 2014 Deciso B.V.
-	Copyright (C) 2014 Ermal LUÇi
-	All rights reserved.
+    Copyright (C) 2016 Deciso B.V.
+    Copyright (C) 2014 Ermal LUÇi
+    All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
 
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
 
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
 */
 
 require_once("guiconfig.inc");
@@ -35,73 +35,92 @@ require_once("interfaces.inc");
 $shortcut_section = 'ipsec';
 
 include("head.inc");
-
-$mobile = array(); // TODO: temporary disabled ( https://github.com/opnsense/core/issues/139 )  ipsec_dump_mobile();
-
+$ipsec_leases = json_decode(configd_run("ipsec list leases"), true);
 ?>
 <body>
 
 <?php include("fbegin.inc"); ?>
+  <section class="page-content-main">
+    <div class="container-fluid">
+      <div class="row">
+        <section class="col-xs-12">
+          <div class="content-box">
+<?php
+            if (count($ipsec_leases) > 0):
+              foreach($ipsec_leases as $pool => $pool_data): ?>
+              <div class="content-box-main ">
+                <div class="table-responsive">
+                  <table class="table table-striped table-condensed">
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th colspan="3">
+                          <?=gettext("Pool: ");?> <?=$pool;?>
+                          <?=gettext("usage: ");?> <?=$pool_data['usage'];?>
+                          <?=gettext("online: ");?> <?=$pool_data['online'];?>
+                        </th>
+                        <th></th>
+                      </tr>
+                      <tr>
+                        <th></th>
+                        <th><?=gettext("User");?></th>
+                        <th><?=gettext("Host");?></th>
+                        <th><?=gettext("Status");?></th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+<?php
+                    if (count($pool_data['items']) > 0):?>
+<?php
+                    foreach ($pool_data['items'] as $lease): ?>
+                    <tr>
+                      <td></td>
+                      <td><?=htmlspecialchars($lease['user']);?></td>
+                      <td><?=htmlspecialchars($lease['address']);?></td>
+                      <td>
+                        <span class='glyphicon glyphicon-transfer text-<?=$lease['status'] == 'online' ?  "success" : "danger";?>'></span>
+                        (<?=htmlspecialchars($lease['status']);?>)
+                      </td>
+                      <td></td>
+                    </tr>
+<?php
+                    endforeach;
+                    else: ?>
+                    <tr>
+                      <td></td>
+                      <td colspan="3">
+                        <?=gettext("No leases from this pool yet.");?>
+                      </td>
+                      <td></td>
+                    </tr>
+<?php
+                    endif; ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
 
-	<section class="page-content-main">
-		<div class="container-fluid">
-			<div class="row">
-
-				<?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
-
-			    <section class="col-xs-12">
-					<div class="tab-content content-box col-xs-12">
-				    <div class="container-fluid">
-
-							<div class="table-responsive">
-
-
-							<?php if (isset($mobile['pool']) && is_array($mobile['pool'])): ?>
-								<?php foreach($mobile['pool'] as $pool): ?>
-									<table class="table table-striped table-sort">
-										<tr>
-											<td colspan="4" valign="top" class="listtopic">
-											<?php
-												echo gettext("Pool: ") . $pool['name'];
-												echo ' ' . gettext("usage: ") . $pool['usage'];
-												echo ' ' . gettext("online: ") . $pool['online'];
-											?>
-											</td>
-										</tr>
-										<?php if (is_array($pool['lease']) && count($pool['lease']) > 0): ?>
-										<tr>
-											<td class="listhdrr nowrap"><?=gettext("ID");?></td>
-											<td class="listhdrr nowrap"><?=gettext("Host");?></td>
-											<td class="listhdrr nowrap"><?=gettext("Status");?></td>
-											<td class="list nowrap"></td>
-										</tr>
-										<?php foreach ($pool['lease'] as $lease): ?>
-										<tr>
-											<td class="listlr"><?=htmlspecialchars($lease['id']);?></td>
-											<td class="listr"><?=htmlspecialchars($lease['host']);?></td>
-											<td class="listr"><?=htmlspecialchars($lease['status']);?></td>
-											<td class="list nowrap">
-											</td>
-										</tr>
-										<?php endforeach;
-										else: ?>
-										<tr>
-											<td>
-												<p><strong><?=gettext("No leases from this pool yet.");?></strong></p>
-											</td>
-										</tr>
-										<?php endif; ?>
-									</table>
-								<?php endforeach; ?>
-							<?php else: ?>
-								<p><strong><?=gettext("No IPsec pools.");?></strong></p>
-							<?php endif; ?>
-						</div>
-				    </div>
-					</div>
-			    </section>
-			</div>
-		</div>
-	</section>
+<?php
+              endforeach;
+            else: ?>
+            <div class="content-box-main ">
+              <div class="table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th><?=gettext("No IPsec pools.");?></th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+<?php
+            endif; ?>
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
 
 <?php include("foot.inc"); ?>
