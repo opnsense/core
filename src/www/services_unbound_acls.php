e6faf6d910773d8b272070f8756e00df30336979
Fabian Franz <fabian.franz@students.fh-hagenberg.at>
2016-01-17 13:29:36 +0100

    some translation fixes

--- src/www/services_unbound_acls.php
+++ src/www/services_unbound_acls.php
@@ -233,10 +233,10 @@ include("head.inc");
 											<br />
 											<span class="text-default">
 													<?=gettext("Choose what to do with DNS requests that match the criteria specified below.");?> <br />
-													<?=gettext("<b>Deny:</b> This action stops queries from hosts within the netblock defined below.");?> <br />
-													<?=gettext("<b>Refuse:</b> This action also stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client.");?> <br />
-													<?=gettext("<b>Allow:</b> This action allows queries from hosts within the netblock defined below.");?> <br />
-													<?=gettext("<b>Allow Snoop:</b> This action allows recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for your administrative host.");?> <br />
+													<?=sprintf(gettext("%sDeny:%s This action stops queries from hosts within the netblock defined below."),'<b>','</b>')?> <br />
+													<?=sprintf(gettext("%sRefuse:%s This action also stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client."),'<b>','</b>')?> <br />
+													<?=sprintf(gettext("%sAllow:%s This action allows queries from hosts within the netblock defined below."),'<b>','</b>')?> <br />
+													<?=sprintf(gettext("%sAllow Snoop:%s This action allows recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for your administrative host."),'<b>','</b>')?> <br />
 											</span>
 										</td>
 									</tr>
