3d7d907889e9238e374d1cb0014ef484c618c972
Franco Fichtner <franco@opnsense.org>
2016-01-27 15:01:19 +0100

    proxy: s/txz/tgz/g

--- src/opnsense/scripts/proxy/fetchACLs.py
+++ src/opnsense/scripts/proxy/fetchACLs.py
@@ -67,7 +67,7 @@ class ACLDownload(object):
         if self._source_data is not None:
             # handle compressed data
             if (len(self._url) > 8 and self._url[-7:] == '.tar.gz') \
-                or (len(self._url) > 4 and self._url[-4:] == '.txz'):
+                or (len(self._url) > 4 and self._url[-4:] == '.tgz'):
                 # source is in tar.gz format, extract all into a single string
                 try:
                     tf = tarfile.open(fileobj=StringIO.StringIO(self._source_data))
