984121a6c504154bee61f10131125f0dc4cfeb85
Ad Schellevis <ad@opnsense.org>
2016-02-16 20:12:40 +0100

    (legacy) zap split in pfsense-utils.inc, 3gstats.php, dyn_dns_status.widget.php
    (cherry picked from commit 4ab7775552b0eb5145748f7ee6645aa4b66cdbb1)

--- src/www/widgets/widgets/dyn_dns_status.widget.php
+++ src/www/widgets/widgets/dyn_dns_status.widget.php
@@ -55,7 +55,7 @@ if ($_REQUEST['getdyndnsstatus']) {
         $filename = "/conf/dyndns_{$dyndns['interface']}{$dyndns['type']}" . escapeshellarg($dyndns['host']) . "{$dyndns['id']}.cache";
         if (file_exists($filename)) {
             $ipaddr = dyndnsCheckIP($dyndns['interface']);
-            $cached_ip_s = split(":", file_get_contents($filename));
+            $cached_ip_s = preg_split(":", file_get_contents($filename), null);
             $cached_ip = $cached_ip_s[0];
             if ($ipaddr <> $cached_ip) {
                 echo "<font color='red'>";
