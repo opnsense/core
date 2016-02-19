46c8c2ab835a8adc0c8e6de189f2da2685b15e94
Franco Fichtner <franco@opnsense.org>
2016-02-19 08:31:04 +0100

    src: move ipsec.inc require to top pages
    For now plugins.inc and ipsec.inc will coexist on pages that
    may need only one or none to avoid unefined function errors.
    This will be pruned after investigating the contents of both
    files...
    
    (cherry picked from commit d7df9ecf10c4bc8950f4610894b9c5b5027055f9)

--- src/www/vpn_ipsec_keys_edit.php
+++ src/www/vpn_ipsec_keys_edit.php
@@ -30,6 +30,7 @@
 require_once("interfaces.inc");
 require_once("guiconfig.inc");
 require_once("plugins.inc");
+require_once("ipsec.inc");
 require_once("services.inc");
 
 if (!isset($config['ipsec']) || !is_array($config['ipsec'])) {
--- src/www/vpn_ipsec_phase2.php
+++ src/www/vpn_ipsec_phase2.php
@@ -30,6 +30,7 @@
 
 require_once("guiconfig.inc");
 require_once("interfaces.inc");
+require_once("ipsec.inc");
 require_once("plugins.inc");
 require_once("services.inc");
 
