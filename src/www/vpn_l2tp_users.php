c5215538ba90858d29112a056e70844b139bc877
Ad Schellevis <ad@opnsense.org>
2016-02-22 12:12:11 +0100

    small changes related to https://github.com/opnsense/core/pull/788
    (cherry picked from commit 8594ccea33fd75632f567482112ab0f4ad21c088)

--- src/www/vpn_l2tp_users.php
+++ src/www/vpn_l2tp_users.php
@@ -1,30 +1,30 @@
 <?php
 
 /*
-	Copyright (C) 2014-2015 Deciso B.V.
-	Copyright (C) 2005 Scott Ullrich (sullrich@gmail.com)
-	All rights reserved.
+    Copyright (C) 2014-2016 Deciso B.V.
+    Copyright (C) 2005 Scott Ullrich (sullrich@gmail.com)
+    All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
 
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
 
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
 */
 
 require_once("guiconfig.inc");
@@ -60,82 +60,67 @@ if ($_GET['act'] == "del") {
 $service_hook = 'l2tpd';
 
 include("head.inc");
-
-
 $main_buttons = array(
     array('label'=>gettext("add user"), 'href'=>'vpn_l2tp_users_edit.php'),
 );
-
 ?>
 
 <body>
 <?php include("fbegin.inc"); ?>
-
-	<section class="page-content-main">
-		<div class="container-fluid">
-			<div class="row">
-
-				<?php if (isset($savemsg)) {
+  <section class="page-content-main">
+    <div class="container-fluid">
+      <div class="row">
+        <?php if (isset($savemsg)) {
                     print_info_box($savemsg);
 } ?>
-				<?php if (isset($config['l2tp']['radius']['enable'])) {
+        <?php if (isset($config['l2tp']['radius']['enable'])) {
                     print_info_box(gettext("Warning: RADIUS is enabled. The local user database will not be used."));
 } ?>
-				<?php if (is_subsystem_dirty('l2tpusers')) :
+        <?php if (is_subsystem_dirty('l2tpusers')) :
 ?><br/>
-				<?php print_info_box_apply(gettext("The l2tp user list has been modified") . ".<br />" . gettext("You must apply the changes in order for them to take effect") . ".<br /><b>" . gettext("Warning: this will terminate all current l2tp sessions!") . "</b>");?>
-				<?php
+        <?php print_info_box_apply(gettext("The l2tp user list has been modified") . ".<br />" . gettext("You must apply the changes in order for them to take effect") . ".<br /><b>" . gettext("Warning: this will terminate all current l2tp sessions!") . "</b>");?>
+        <?php
 endif; ?>
-
-				<div id="inputerrors"></div>
-
-
-			    <section class="col-xs-12">
-
-					<div class="tab-content content-box col-xs-12">
-
-							<form action="vpn_l2tp_users.php" method="post" name="iform" id="iform">
-
-							 <div class="table-responsive">
-								<table class="table table-striped table-sort">
-								 <tr>
-							<td class="listhdrr"><?=gettext("Username");?></td>
-									<td class="listhdr"><?=gettext("IP address");?></td>
-									<td class="list"></td>
-								</tr>
-                                <?php $i = 0; foreach ($a_secret as $secretent) :
+          <section class="col-xs-12">
+            <div class="tab-content content-box col-xs-12">
+              <form method="post" name="iform" id="iform">
+                 <div class="table-responsive">
+                  <table class="table table-striped table-sort">
+                   <tr>
+                    <td class="listhdrr"><?=gettext("Username");?></td>
+                    <td class="listhdr"><?=gettext("IP address");?></td>
+                    <td class="list"></td>
+                  </tr>
+                  <?php $i = 0; foreach ($a_secret as $secretent) :
 ?>
-				                <tr>
-				                  <td class="listlr">
-				                    <?=htmlspecialchars($secretent['name']);?>
-				                  </td>
-				                  <td class="listr">
-                                <?php if ($secretent['ip'] == "") {
-                                    $secretent['ip'] = "Dynamic";
+                  <tr>
+                    <td class="listlr">
+                      <?=htmlspecialchars($secretent['name']);?>
+                    </td>
+                    <td class="listr">
+                          <?php if ($secretent['ip'] == "") {
+                              $secretent['ip'] = "Dynamic";
 } ?>
-				                    <?=htmlspecialchars($secretent['ip']);?>&nbsp;
-				                  </td>
-				                  <td class="list nowrap" width="150">
-					                    <a href="vpn_l2tp_users_edit.php?id=<?=$i;?>" class="btn btn-default"><span class="glyphicon glyphicon-edit"></span></a>
-
-                                        <a href="vpn_l2tp_users.php?act=del&amp;id=<?=$i;
-?>" class="btn btn-default" onclick="return confirm('<?=gettext("Do you really want to delete this user?");
-?>')"title="<?=gettext("delete user"); ?>"><span class="fa fa-trash text-muted"></span></a>
-
-					                 </td>
-								</tr>
-                                <?php $i++;
-
-endforeach; ?>
-
-				              </table>
-							 </div>
-
-							</form>
-					</div>
-			    </section>
-			</div>
-		</div>
-	</section>
+                          <?=htmlspecialchars($secretent['ip']);?>&nbsp;
+                    </td>
+                    <td class="list nowrap" width="150">
+                      <a href="vpn_l2tp_users_edit.php?id=<?=$i;?>" class="btn btn-default"><span class="glyphicon glyphicon-edit"></span></a>
+                      <a href="vpn_l2tp_users.php?act=del&amp;id=<?=$i;?>" class="btn btn-default"
+                         onclick="return confirm('<?=gettext("Do you really want to delete this user?");?>')"
+                         title="<?=gettext("delete user"); ?>"><span class="fa fa-trash text-muted"></span>
+                      </a>
+                    </td>
+                  </tr>
+<?php
+                  $i++;
+                  endforeach; ?>
+                </table>
+               </div>
+              </form>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
 
 <?php include("foot.inc");
