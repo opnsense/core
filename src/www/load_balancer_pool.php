ed8c1201b19184918542c216a247bb0abd0a735e
Ad Schellevis <ad@opnsense.org>
2016-03-04 17:25:23 +0100

    (legacy) zapp load_balancer_maintable.inc
    (cherry picked from commit fbb98523ff6d3fd8633ee00e799a17c6fcaf49ea)

--- src/www/load_balancer_pool.php
+++ src/www/load_balancer_pool.php
@@ -29,7 +29,6 @@
 
 require_once("guiconfig.inc");
 require_once("filter.inc");
-require_once("load_balancer_maintable.inc");
 require_once("services.inc");
 require_once("vslb.inc");
 require_once("interfaces.inc");
