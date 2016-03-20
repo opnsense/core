7d18504ceda37846d0d75d6d6213029bd9487be8
Ad Schellevis <ad@opnsense.org>
2016-03-20 20:34:36 +0100

    (proxy) work in progress sslbum, some work to be done....

--- src/opnsense/scripts/proxy/setup.sh
+++ src/opnsense/scripts/proxy/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SQUID_DIRS="/var/log/squid /var/run/squid /var/squid /var/squid/cache /var/squid/logs"
+SQUID_DIRS="/var/log/squid /var/run/squid /var/squid /var/squid/cache /var/squid/ssl /var/squid/logs"
 
 for SQUID_DIR in ${SQUID_DIRS}; do
 	mkdir -p ${SQUID_DIR}
@@ -8,4 +8,15 @@ for SQUID_DIR in ${SQUID_DIRS}; do
 	chmod -R 750 ${SQUID_DIR}
 done
 /usr/sbin/pw groupmod proxy -m squid
-/usr/local/sbin/squid -z
+/usr/local/sbin/squid -z > /dev/null 2>&1
+
+# wait a moment before exit, running squid -z and squid start without time between them sometimes results in
+# some vague errors.
+sleep 1
+
+# create ssl certificate store, in case sslbump is enabled we need this
+if [ ! -d /var/squid/ssl_crtd ]; then
+/usr/local/libexec/squid/ssl_crtd -c -s /var/squid/ssl_crtd > /dev/null 2>&1
+chown -R squid:squid /var/squid/ssl_crtd
+chmod -R 750 /var/squid/ssl_crtd
+fi
