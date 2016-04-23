068a4d65f3c839cb90138530846d2a029b6cab6c
Ad Schellevis <ad@opnsense.org>
2016-04-23 23:37:23 +0200

    (mvc) setup initial framework unit tests
    (cherry picked from commit c2036dfb012de0e515301ad428d346db83bb0894)

---
+++ src/opnsense/mvc/tests/app/models/OPNsense/Base/FieldTypes/IntegerFieldTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ *    Copyright (C) 2016 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace tests\OPNsense\Base\FieldTypes;
+
+// @CodingStandardsIgnoreStart
+require_once 'Field_Framework_TestCase.php';
+// @CodingStandardsIgnoreEnd
+
+use \OPNsense\Base\FieldTypes\IntegerField;
+
+class IntegerFieldTest extends Field_Framework_TestCase
+{
+
+    /**
+     * test construct
+     */
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf('\OPNsense\Base\FieldTypes\IntegerField', new IntegerField());
+    }
+
+    /**
+     * @expectedException \Phalcon\Validation\Exception
+     * @expectedExceptionMessage MaxMinValidator
+     */
+    public function testValueLargerThenMax()
+    {
+        $field = new IntegerField();
+        $field->setMaximumValue(100);
+        $field->setMinimumValue(10);
+        $field->setValue("120");
+
+        $this->validateThrow($field);
+    }
+
+    /**
+     * @expectedException \Phalcon\Validation\Exception
+     * @expectedExceptionMessage MaxMinValidator
+     */
+    public function testValueSmallerThenMin()
+    {
+        $field = new IntegerField();
+        $field->setMaximumValue(100);
+        $field->setMinimumValue(10);
+        $field->setValue("5");
+
+        $this->validateThrow($field);
+    }
+
+    /**
+     * not a number
+     */
+    public function testNotANumber()
+    {
+        $field = new IntegerField();
+        $field->setMaximumValue(100);
+        $field->setMinimumValue(10);
+        $field->setValue("5x1");
+
+        $this->assertContains('IntegerValidator', $this->validate($field));
+    }
+}
---
+++ src/opnsense/mvc/tests/setup.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *    Copyright (C) 2016 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+use Phalcon\Di;
+use Phalcon\Di\FactoryDefault;
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Read the configuration
+ */
+$config = include __DIR__ . "/app/config/config.php";
+
+/**
+ * Read auto-loader
+ */
+include __DIR__ . "/../app/config/loader.php";
+
+
+$di = new FactoryDefault();
+Di::reset();
+
+$di->set('config', $config);
+
+Di::setDefault($di);
