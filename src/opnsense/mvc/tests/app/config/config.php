c2036dfb012de0e515301ad428d346db83bb0894
Ad Schellevis <ad@opnsense.org>
2016-04-23 23:37:23 +0200

    (mvc) setup initial framework unit tests

---
+++ src/opnsense/mvc/tests/app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return new \Phalcon\Config(array(
+    'application' => array(
+        'controllersDir' => __DIR__ . '/../../../app/controllers/',
+        'modelsDir' => __DIR__ . '/../../../app/models/',
+        'viewsDir' => __DIR__ . '/../../../app/views/',
+        'pluginsDir' => __DIR__ . '/../../../app/plugins/',
+        'libraryDir' => __DIR__ . '/../../../app/library/',
+        'cacheDir' => __DIR__ . '/../../../app/cache/',
+        'baseUri' => '/opnsense_gui/',
+    ),
+    'globals' => array(
+        'config_path'    => '/conf/',
+        'temp_path'      => '/tmp/',
+        'debug'          => false,
+        'simulate_mode'  => false
+    )
+));
