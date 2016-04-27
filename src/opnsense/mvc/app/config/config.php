856cc8b161aabcab2c8e57cb3dd7061bf224f299
Franco Fichtner <franco@opnsense.org>
2016-04-27 10:42:19 +0200

    php: scrub dependencies for #915
    (cherry picked from commit 1d0ae14682a1102affbe276eb26e8169bb2e8849)

--- src/opnsense/mvc/app/config/config.php
+++ src/opnsense/mvc/app/config/config.php
@@ -1,13 +1,6 @@
 <?php
 
 return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
-    ),
     'application' => array(
         'controllersDir' => __DIR__ . '/../../app/controllers/',
         'modelsDir'      => __DIR__ . '/../../app/models/',
