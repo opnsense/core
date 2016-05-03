26f8e3dba8dd2eceed0beb27ce45f8ab77c91613
Franco Fichtner <franco@opnsense.org>
2016-05-03 17:17:22 +0200

    www: remove old cache disable, only used twice and uneeded
    (cherry picked from commit 53f6dace75a1f4bb5dcd433491412ab1445d2f34)

--- src/www/diag_backup.php
+++ src/www/diag_backup.php
@@ -32,10 +32,6 @@
 ini_set('max_execution_time', '0');
 ini_set('max_input_time', '0');
 
-/* omit no-cache headers because it confuses IE with file downloads */
-$omit_nocacheheaders = true;
-$nocsrf = true;
-
 require_once("guiconfig.inc");
 require_once("interfaces.inc");
 require_once("filter.inc");
--- src/www/legacy_traffic_stats.php
+++ src/www/legacy_traffic_stats.php
@@ -27,7 +27,7 @@
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
 */
-$omit_nocacheheaders = true;
+
 require_once('guiconfig.inc');
 require_once('interfaces.inc');
 require_once('pfsense-utils.inc');
