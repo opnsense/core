726e4c05bc1fd2396289c30eb16f71728d899fba
Franco Fichtner <franco@opnsense.org>
2016-05-07 18:23:36 +0200

    firewall: fix previous

--- src/www/firewall_rules_edit.php
+++ src/www/firewall_rules_edit.php
@@ -31,6 +31,7 @@
 require_once("guiconfig.inc");
 require_once("pfsense-utils.inc");
 require_once("interfaces.inc");
+require_once("services.inc");
 
 /* TCP flags */
 $tcpflags = array("syn", "ack", "fin", "rst", "psh", "urg", "ece", "cwr");
