2325fc61a3e74c27111fc5c739ef15934200be1b
Ad Schellevis <ad@opnsense.org>
2016-05-12 20:17:18 +0200

    fix alignment in System: Settings: General, closes https://github.com/opnsense/core/issues/803

--- src/www/system_general.php
+++ src/www/system_general.php
@@ -344,7 +344,7 @@ include("head.inc");
                 <table class="table table-striped table-condensed">
                   <thead>
                     <tr>
-                      <th><?=gettext("DNS Server"); ?></th>
+                      <th style="width:350px;"><?=gettext("DNS Server"); ?></th>
                       <th><?=gettext("Use gateway"); ?></th>
                     </tr>
                   </thead>
