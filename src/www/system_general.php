b891dc9fd8eb29e7bd778452395fc028402f4bea
Franco Fichtner <franco@opnsense.org>
2016-06-11 08:00:54 +0200

    system: only two sections for general settings now

--- src/www/system_general.php
+++ src/www/system_general.php
@@ -333,9 +333,6 @@ include("head.inc");
               </td>
             </tr>
             <tr>
-              <th colspan="2" valign="top" class="listtopic"><?=gettext("Firmware"); ?></th>
-            </tr>
-            <tr>
               <td><a id="help_for_language" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Language");?></td>
               <td>
                 <select name="language" class="selectpicker" data-size="10" data-style="btn-default" data-width="auto">
@@ -375,7 +372,30 @@ include("head.inc");
               </td>
             </tr>
             <tr>
-              <th colspan="2" valign="top" class="listtopic"><?=gettext("Name resolution"); ?></th>
+              <th colspan="2" valign="top" class="listtopic"><?=gettext("Networking"); ?></th>
+            </tr>
+            <tr>
+              <td><a id="help_for_prefer_ipv4" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Prefer IPv4 over IPv6"); ?></td>
+              <td>
+                <input name="prefer_ipv4" type="checkbox" id="prefer_ipv4" value="yes" <?= !empty($pconfig['prefer_ipv4']) ? "checked=\"checked\"" : "";?> />
+                <strong><?=gettext("Prefer to use IPv4 even if IPv6 is available"); ?></strong>
+                <div class="hidden" for="help_for_prefer_ipv4">
+                  <?=gettext("By default, if a hostname resolves IPv6 and IPv4 addresses ".
+                                      "IPv6 will be used, if you check this option, IPv4 will be " .
+                                      "used instead of IPv6."); ?>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td><a id="help_for_gw_switch_default" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Gateway switching");?> </td>
+              <td>
+                <input name="gw_switch_default" type="checkbox" id="gw_switch_default" value="yes" <?= !empty($pconfig['gw_switch_default']) ? "checked=\"checked\"" : "";?> />
+                <strong><?=gettext("Allow default gateway switching"); ?></strong><br />
+                <div class="hidden" for="help_for_gw_switch_default">
+                  <?=gettext("If the link where the default gateway resides fails " .
+                                      "switch the default gateway to another available one."); ?>
+                </div>
+              </td>
             </tr>
             <tr>
               <td><a id="help_for_dnsservers" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("DNS servers"); ?></td>
@@ -453,32 +473,6 @@ include("head.inc");
                 <div class="hidden" for="help_for_dnsservers_opt">
                   <?=gettext("By default localhost (127.0.0.1) will be used as the first DNS server where the DNS Forwarder or DNS Resolver is enabled and set to listen on Localhost, so system can use the local DNS service to perform lookups. ".
                   "Checking this box omits localhost from the list of DNS servers."); ?>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th colspan="2" valign="top" class="listtopic"><?=gettext("Networking"); ?></th>
-            </tr>
-            <tr>
-              <td><a id="help_for_prefer_ipv4" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Prefer IPv4 over IPv6"); ?></td>
-              <td>
-                <input name="prefer_ipv4" type="checkbox" id="prefer_ipv4" value="yes" <?= !empty($pconfig['prefer_ipv4']) ? "checked=\"checked\"" : "";?> />
-                <strong><?=gettext("Prefer to use IPv4 even if IPv6 is available"); ?></strong>
-                <div class="hidden" for="help_for_prefer_ipv4">
-                  <?=gettext("By default, if a hostname resolves IPv6 and IPv4 addresses ".
-                                      "IPv6 will be used, if you check this option, IPv4 will be " .
-                                      "used instead of IPv6."); ?>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td><a id="help_for_gw_switch_default" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Gateway switching");?> </td>
-              <td>
-                <input name="gw_switch_default" type="checkbox" id="gw_switch_default" value="yes" <?= !empty($pconfig['gw_switch_default']) ? "checked=\"checked\"" : "";?> />
-                <strong><?=gettext("Allow default gateway switching"); ?></strong><br />
-                <div class="hidden" for="help_for_gw_switch_default">
-                  <?=gettext("If the link where the default gateway resides fails " .
-                                      "switch the default gateway to another available one."); ?>
                 </div>
               </td>
             </tr>
