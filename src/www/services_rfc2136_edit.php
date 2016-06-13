a29d32db157e3aba114cc0b9641504e0ea4ab702
Franco Fichtner <franco@opnsense.org>
2016-06-13 07:53:00 +0200

    rfc2136: merge styling / translation fixes from master

--- src/www/services_rfc2136_edit.php
+++ src/www/services_rfc2136_edit.php
@@ -239,11 +239,9 @@ include("head.inc");
                     </td>
                   </tr>
                   <tr>
-                    <td>&nbsp;</td>
-                    <td>
-                      <span class="text-danger"><strong><?=gettext("Note:");?><br />
-                      </strong></span><?php printf(gettext("You must configure a DNS server in %sSystem: " .
-                      "General setup %sor allow the DNS server list to be overridden " .
+                    <td colspan="2">
+                      <?= sprintf(gettext("You must configure a DNS server in %sSystem: " .
+                      "General setup%s or allow the DNS server list to be overridden " .
                       "by DHCP/PPP on WAN for dynamic DNS updates to work."),'<a href="system_general.php">', '</a>');?>
                     </td>
                   </tr>
