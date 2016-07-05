aaa72bb9e1745b393792f22e27c36b1d9e351ee7
Franco Fichtner <franco@opnsense.org>
2016-07-05 20:18:58 +0200

    services: wrong service hook for dhcp here

--- src/www/services_dhcpv6_edit.php
+++ src/www/services_dhcpv6_edit.php
@@ -150,10 +150,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
 }
 
+$service_hook = 'dhcpd6';
 
-$service_hook = 'dhcpd';
 legacy_html_escape_form_data($pconfig);
+
 include("head.inc");
+
 ?>
 
 <body>
