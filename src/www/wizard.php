bc6af574ee5017425c876550fc317fddf64674ca
Franco Fichtner <franco@opnsense.org>
2016-07-08 09:55:51 +0200

    wizard: fix misalignment of titles
    PR: https://forum.opnsense.org/index.php?topic=3305
    (cherry picked from commit 6c0c6b6b3fbac1729a8cfe80d010a8d9fecbc4c1)

--- src/www/wizard.php
+++ src/www/wizard.php
@@ -70,11 +70,13 @@ function find_ip_interface($ip, $bits = null) {
 }
 
 
-$stepid = htmlspecialchars($_GET['stepid']);
-if (isset($_POST['stepid']))
+$stepid = '0';
+
+if (isset($_POST['stepid'])) {
 	$stepid = htmlspecialchars($_POST['stepid']);
-if (!$stepid)
-	$stepid = "0";
+} elseif (isset($_GET['stepid'])) {
+	$stepid = htmlspecialchars($_GET['stepid']);
+}
 
 $xml = '';
 if (isset($_GET['xml'])) {
@@ -123,8 +125,6 @@ if (!is_array($pkg)) {
 	die;
 }
 
-$description = $pkg['step'][$stepid]['description'];
-$title = $pkg['step'][$stepid]['title'];
 $totalsteps = $pkg['totalsteps'];
 
 if ($pkg['includefile']) {
@@ -155,16 +155,25 @@ if ($_POST && !$input_errors) {
 		}
 
 	}
+
 	// run custom php code embedded in xml config.
 	if($pkg['step'][$stepid]['stepsubmitphpaction'] <> "") {
 		eval($pkg['step'][$stepid]['stepsubmitphpaction']);
 	}
-	if (!$input_errors)
+
+	if (!$input_errors) {
 		write_config();
+	}
+
 	$stepid++;
-	if($stepid > $totalsteps)
+
+	if($stepid > $totalsteps) {
 		$stepid = $totalsteps;
+	}
 }
+
+$description = $pkg['step'][$stepid]['description'];
+$title = $pkg['step'][$stepid]['title'];
 
 function update_config_field($field, $updatetext, $unset, $arraynum, $field_type) {
 	global $config;
