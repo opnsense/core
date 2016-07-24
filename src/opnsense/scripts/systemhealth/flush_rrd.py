2692aed176607952998fcf0e7452c407144eb493
Ad Schellevis <ad@opnsense.org>
2016-07-24 12:54:38 +0200

    (rrd) add configd "systemhealth flush" command, add flush single report. closes https://github.com/opnsense/core/issues/984
    (cherry picked from commit b7657a6fdf4ff4a711d7345bc001defee906e1af)
    (cherry picked from commit 6bbfe4e0cac0e2fdd71bfd1587856f691f6a6cce)

---
+++ src/opnsense/scripts/systemhealth/flush_rrd.py
@@ -0,0 +1,46 @@
+#!/usr/local/bin/python2.7
+
+"""
+    Copyright (c) 2016 Deciso B.V. - Ad Schellevis
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+"""
+import glob
+import os
+import sys
+import os.path
+import ujson
+
+rrd_reports_dir = '/var/db/rrd'
+
+if len(sys.argv) < 2:
+    print ("usage %s rrd_filename" % os.path.basename(sys.argv[0]))
+    sys.exit(-1)
+else:
+    flushed = list()
+    for filename in glob.glob(rrd_reports_dir+'/*.rrd'):
+        if sys.argv[1] == '*' or os.path.basename(filename) == sys.argv[1]:
+            os.remove(filename)
+            flushed.append(os.path.basename(filename))
+    print (ujson.dumps({'flushed':flushed}))
