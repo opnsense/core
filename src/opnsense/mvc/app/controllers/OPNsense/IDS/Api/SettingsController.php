aaa0ddafe1179cebc68146bdc3c30b8b90c564b8
Ad Schellevis <ad@opnsense.org>
2016-08-12 09:33:37 +0200

    (ids) style fix

--- src/opnsense/mvc/app/controllers/OPNsense/IDS/Api/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/IDS/Api/SettingsController.php
@@ -72,7 +72,7 @@ class SettingsController extends ApiMutableModelControllerBase
 
 
             // fetch query parameters
-            $itemsPerPage = $this->request->getPost('rowCount', 'int', 9999);
+            $itemsPerPage = $this->request->getPost('rowCount', 'int', -1);
             $currentPage = $this->request->getPost('current', 'int', 1);
 
             if ($this->request->hasPost('sort') && is_array($this->request->getPost("sort"))) {
