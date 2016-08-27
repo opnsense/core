5c09fbdf4371582716ae603996775bef1200a97d
Ad Schellevis <ad@opnsense.org>
2016-08-27 18:58:13 +0200

    (mvc) support inheritance of OPNsense\Base\FieldTypes\BaseField children.

--- src/opnsense/mvc/app/models/OPNsense/Base/BaseModel.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/BaseModel.php
@@ -122,7 +122,16 @@ abstract class BaseModel
             if (class_exists($classname)) {
                 // construct field type object
                 $field_rfcls = new \ReflectionClass($classname);
-                if ($field_rfcls->getParentClass()->name != 'OPNsense\Base\FieldTypes\BaseField') {
+                $check_derived = $field_rfcls->getParentClass();
+                $is_derived_from_basefield = false;
+                while ($check_derived != false) {
+                    if ($check_derived->name == 'OPNsense\Base\FieldTypes\BaseField') {
+                        $is_derived_from_basefield = true;
+                        break;
+                    }
+                    $check_derived = $check_derived->getParentClass();
+                }
+                if (!$is_derived_from_basefield) {
                     // class found, but of wrong type. raise an exception.
                     throw new ModelException("class ".$field_rfcls->name." of wrong type in model definition");
                 }
