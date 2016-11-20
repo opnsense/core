ec679740a5a140280fc212f21e5f2cc5f0b85668
Franco Fichtner <franco@opnsense.org>
2016-11-20 11:22:49 +0100

    system: move system_cron_configure() to new home

--- src/www/firewall_rules_edit.php
+++ src/www/firewall_rules_edit.php
@@ -30,7 +30,7 @@
 
 require_once("guiconfig.inc");
 require_once("interfaces.inc");
-require_once("services.inc");
+require_once("system.inc");
 require_once("filter.inc");
 
 /* TCP flags */
--- src/www/system_advanced_firewall.php
+++ src/www/system_advanced_firewall.php
@@ -31,7 +31,6 @@
 
 require_once("guiconfig.inc");
 require_once("filter.inc");
-require_once("services.inc");
 require_once("system.inc");
 
 function default_table_entries_size()
