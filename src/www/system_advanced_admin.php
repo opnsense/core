d70e4b2652959ae08f657354cc284c2bf09559cb
Franco Fichtner <franco@opnsense.org>
2016-12-20 15:05:24 +0100

    firewall: glitch in previous

--- src/www/system_advanced_admin.php
+++ src/www/system_advanced_admin.php
@@ -412,13 +412,13 @@ include("head.inc");
                     <input name="noantilockout" type="checkbox" value="yes" <?= empty($pconfig['noantilockout']) ? '' : 'checked="checked"' ?>/>
                     <strong><?= gettext('Disable web GUI anti-lockout rule') ?></strong>
                     <div class="hidden" for="help_for_noantilockout">
-                      <?= sprintf(gettext("When this is unchecked, access to the web GUI " .
-                                          "on the %s interface is always permitted, regardless of the user-defined firewall " .
-                                          "rule set. Check this box to disable this automatically added rule, so access " .
-                                          "to the web GUI is controlled by the user-defined firewall rules " .
-                                          "(ensure you have a firewall rule in place that allows you in, or you will " .
-                                          "lock yourself out!)"), count($config['interfaces']) == 1 && !empty($config['interfaces']['wan']['if'] ?
-                                          gettext("WAN") : gettext("LAN"))); ?>
+                      <?= gettext("When this is unchecked, access to the web GUI " .
+                                  "on the %s interface is always permitted, regardless of the user-defined firewall " .
+                                  "rule set. Check this box to disable this automatically added rule, so access " .
+                                  "to the web GUI is controlled by the user-defined firewall rules " .
+                                  "(ensure you have a firewall rule in place that allows you in, or you will " .
+                                  "lock yourself out!)", count($config['interfaces']) == 1 && !empty($config['interfaces']['wan']['if']) ?
+                                  gettext('WAN') : gettext('LAN')) ?>
                       <em><?= gettext('Hint: the "Set interface(s) IP address" option in the console menu resets this setting as well.'); ?></em>
                     </div>
                   </td>
