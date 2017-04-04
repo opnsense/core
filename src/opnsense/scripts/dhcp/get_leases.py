07f5033bff78c54874e92724c7dbf9e54f2fada7
Ad Schellevis <ad@opnsense.org>
2017-04-04 20:32:48 +0200

    add configd command for "dhcpd list leases" for https://github.com/opnsense/core/pull/1472

---
+++ src/opnsense/scripts/dhcp/get_leases.py
@@ -0,0 +1,47 @@
+#!/usr/local/bin/python2.7
+
+"""
+    Copyright (c) 2017 Ad Schellevis
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+    --------------------------------------------------------------------------------------
+    list dhcp leases
+"""
+import sys
+sys.path.insert(0, "/usr/local/opnsense/site-python")
+import watchers.dhcpd
+import params
+import time
+import ujson
+
+app_params = {'inactive': '0'}
+params.update_params(app_params)
+
+result = list()
+dhcpdleases = watchers.dhcpd.DHCPDLease()
+for lease in dhcpdleases.watch():
+    if 'ends' not in lease or lease['ends'] is None or lease['ends'] > time.time() or app_params['inactive'] != '0':
+        result.append(lease)
+
+print (ujson.dumps(result))
