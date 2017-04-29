a14da189f8c80b8d7782d5297ca8d27829b9e461
Franco Fichtner <franco@opnsense.org>
2017-04-29 16:03:28 +0200

    mvc: try to fix crash report
    Seems like years ago when PHP 5.5 had trouble iterating non-
    existing arrays PHP 7 introduced a new problem with nodes
    that seem to be set but no longer available, yielding:
    
        PHP Warning:  OPNsense\Base\FieldTypes\InterfaceField::eventPostLoading():
          Node no longer exists in /usr/local/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/InterfaceField.php on line 132
    
    The same thing happened in the acme-client plugin and was
    allegedly fixed with the following code.  Even if this doesn't
    help it is harmless and we'll see through the crash reporter
    soon enough.

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/InterfaceField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/InterfaceField.php
@@ -129,10 +129,12 @@ class InterfaceField extends BaseField
             $allInterfacesDevices = array(); // mapping device -> interface handle (lan/wan/optX)
             $configObj = Config::getInstance()->object();
             // Iterate over all interfaces configuration and collect data
-            foreach ($configObj->interfaces->children() as $key => $value) {
-                $allInterfaces[$key] = $value;
-                if (!empty($value->if)) {
-                    $allInterfacesDevices[(string)$value->if] = $key;
+            if ($configObj->interfaces->count() > 0) {
+                foreach ($configObj->interfaces->children() as $key => $value) {
+                    $allInterfaces[$key] = $value;
+                    if (!empty($value->if)) {
+                        $allInterfacesDevices[(string)$value->if] = $key;
+                    }
                 }
             }
 
