1f1e01f4f86843900427fd4a0013bfa0c2394eaf
Alexander Shursha <kekek2@ya.ru>
2017-05-10 11:29:39 +0300

    Move from plugin_squid() to plugin_configure('webproxy_')

--- src/opnsense/scripts/OPNsense/Proxy/plugins.php
+++ src/opnsense/scripts/OPNsense/Proxy/plugins.php
@@ -5,5 +5,5 @@ require_once("config.inc");
 require_once("plugins.inc");
 
 if (isset($argv[1])) {
-    plugins_squid(trim($argv[1], " \n"));
+    plugins_configure("webproxy_" . trim($argv[1], " \n"));
 }
