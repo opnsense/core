a298341db6cb42ac3a2dd8eaf345cac1a8857b75
Franco Fichtner <franco@opnsense.org>
2017-06-09 19:31:54 +0200

    intrusion detection: or maybe this... #1233

--- src/opnsense/service/templates/OPNsense/IDS/rc.conf.d
+++ src/opnsense/service/templates/OPNsense/IDS/rc.conf.d
@@ -1,34 +1,11 @@
-{# Macro import #}
-{% from 'OPNsense/Macros/interface.macro' import physical_interface %}
 {% if helpers.exists('OPNsense.IDS.general') and OPNsense.IDS.general.enabled|default("0") == "1" %}
-suricata_enable="YES"
 suricata_opnsense_bootup_run="/usr/local/opnsense/scripts/suricata/setup.sh"
-
+suricata_enable="YES"
 {% if OPNsense.IDS.general.ips|default("0") == "1" %}
-# IPS mode, switch to netmap
-suricata_netmap=YES
-
+suricata_netmap="YES"
 {% else %}
-
 # IDS mode, pcap live mode
-{% set addFlags=[] %}
-{% for intfName in OPNsense.IDS.general.interfaces.split(',') %}
-{%   if loop.index == 1 %}
-{# enable first interface #}
-suricata_interface="{{ physical_interface(intfName) }}"
-{%   else %}
-{#   store additional interfaces to addFlags #}
-{%      do addFlags.append(physical_interface(intfName)) %}
-{%   endif %}
-{% endfor %}
-{#   append additional interfaces #}
-suricata_flags="-D --pcap {%
-   for intf in addFlags
-%} -i {{ intf }}  {% endfor
-%} "
-
-{% endif %}
-
+suricata_flags="-D --pcap"
 {% else %}
 suricata_enable="NO"
 {% endif %}
