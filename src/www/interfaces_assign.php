968a3afc8e38b3c08190046bfac29d0d2781bc0a
Franco Fichtner <franco@opnsense.org>
2017-08-05 20:29:52 +0200

    src: four errors in the copyrights
    * interfaces_assign.php: creation year addition for Jim McBeath
    * filter_log.inc: spurious attribution via 96899e89f0c
    * diag_traceroute.php: typo in split via d87c6ff556
    * Manuel Kasper e-mail missing in a few pages

--- src/www/interfaces_assign.php
+++ src/www/interfaces_assign.php
@@ -2,7 +2,7 @@
 
 /*
     Copyright (C) 2014-2015 Deciso B.V.
-    Copyright (C) Jim McBeath
+    Copyright (C) 2004 Jim McBeath
     Copyright (C) 2003-2005 Manuel Kasper <mk@neon1.net>
     All rights reserved.
 
