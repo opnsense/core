d6e9f2567ed41d610f6073f2a5bc26b9c9a6d88d
Ad Schellevis <ad@opnsense.org>
2017-10-24 21:13:12 +0200

    insight, default sqlite timeout can be a bit low at 5 seconds, which can result in a failed commit (and daemon exit).
    (cherry picked from commit 15e2d55d775c468c35a91f70807361c4015633ca)

--- src/opnsense/scripts/netflow/lib/aggregate.py
+++ src/opnsense/scripts/netflow/lib/aggregate.py
@@ -68,7 +68,7 @@ class AggMetadata(object):
         if not os.path.isdir(target_path):
             os.makedirs(target_path)
         # open sqlite database and cursor
-        self._db_connection = sqlite3.connect(self._filename,
+        self._db_connection = sqlite3.connect(self._filename, timeout=60,
                                               detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
         self._db_cursor = self._db_connection.cursor()
         # known tables
@@ -219,7 +219,7 @@ class BaseFlowAggregator(object):
             if not os.path.isdir(target_path):
                 os.makedirs(target_path)
             # open sqlite database
-            self._db_connection = sqlite3.connect(self.target_filename % self.resolution,
+            self._db_connection = sqlite3.connect(self.target_filename % self.resolution, timeout=60,
                                                   detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
             # open update/insert cursor
             self._update_cur = self._db_connection.cursor()
