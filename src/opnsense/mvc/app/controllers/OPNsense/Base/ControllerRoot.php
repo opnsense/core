416c76745af8870c13d66c0d702f9e0ca952385d
Franco Fichtner <franco@opnsense.org>
2017-11-19 20:30:22 +0000

    mvc: rework previous for #1218
    doAuth() is a good spot for language, but we need to set the
    system language even on auth failures, carfully checking if
    the session is valid (username is set), and only then changing
    to the user's preferred language.

--- src/opnsense/mvc/app/controllers/OPNsense/Base/ControllerRoot.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Base/ControllerRoot.php
@@ -1,30 +1,29 @@
 <?php
-/**
- *    Copyright (C) 2015 Deciso B.V.
+
+/*
+ * Copyright (C) 2015 Deciso B.V.
+ * All rights reserved.
  *
- *    All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    Redistribution and use in source and binary forms, with or without
- *    modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *    POSSIBILITY OF SUCH DAMAGE.
- *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace OPNsense\Base;
@@ -64,6 +63,40 @@ class ControllerRoot extends Controller
     }
 
     /**
+     * set system language according to configuration
+     */
+    protected function setLang()
+    {
+        $config = Config::getInstance()->object();
+        $lang = 'en_US';
+
+        foreach ($config->system->children() as $key => $node) {
+            if ($key == 'language') {
+                $lang = $node->__toString();
+                break;
+            }
+        }
+
+        if ($this->session->has('Username')) {
+            $username = $this->session->get('Username');
+            foreach ($config->system->user as $user) {
+                if ($username == $user->name->__toString() && isset($user->language)) {
+                    $lang = $user->language->__toString();
+                    break;
+                }
+            }
+        }
+
+        $locale = $lang . '.UTF-8';
+        bind_textdomain_codeset('OPNsense', $locale);
+        $this->translator = new ViewTranslator(array(
+            'directory' => '/usr/local/share/locale',
+            'defaultDomain' => 'OPNsense',
+            'locale' => $locale,
+        ));
+    }
+
+    /**
      * perform authentication, redirect user on non successful auth
      * @return bool
      */
@@ -73,6 +106,7 @@ class ControllerRoot extends Controller
             // user unknown
             $this->getLogger()->error("no active session, user not found");
             $this->response->redirect("/", true);
+            $this->setLang();
             return false;
         } elseif ($this->session->has("last_access")
             && $this->session->get("last_access") < (time() - 14400)) {
@@ -82,42 +116,11 @@ class ControllerRoot extends Controller
             $this->session->remove("Username");
             $this->session->remove("last_access");
             $this->response->redirect("/", true);
+            $this->setLang();
             return false;
         }
 
-        // User lang
-        $lang = '';
-        foreach (Config::getInstance()->object()->system->user as $user) {
-            if ($this->session->get("Username") == $user->name->__toString()) {
-                if (isset($user->language)) {
-                    $lang = $user->language->__toString();
-                }
-                break;
-            }
-        }
-
-        // System lang
-        if ($lang == '')
-        {
-            foreach (Config::getInstance()->object()->system->children() as $key => $node) {
-                if ($key == 'language') {
-                    $lang = $node->__toString();
-                    break;
-                }
-            }
-        }
-
-        // Set locale
-        if ($lang == '') {
-            $lang = 'en_US';
-        }
-        $locale = $lang . '.UTF-8';
-        bind_textdomain_codeset('OPNsense', $locale);
-        $this->translator = new ViewTranslator(array(
-            'directory' => '/usr/local/share/locale',
-            'defaultDomain' => 'OPNsense',
-            'locale' => $locale,
-        ));
+        $this->setLang();
 
         $this->session->set("last_access", time());
 
