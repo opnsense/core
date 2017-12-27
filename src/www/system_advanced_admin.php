fa3ad8025ae7c350a7492eae30736ae8ff5c6f78
Franco Fichtner <franco@opnsense.org>
2017-12-27 17:52:05 +0100

    webgui: flip-flop on old and new URL in case something went wrong
    The initial timeout of 20 seconds is long, but it's safer to wait so
    that we're not bouncing back to the old web GUI before it goes down.
    
    PR: https://github.com/opnsense/core/issues/1347

--- src/www/system_advanced_admin.php
+++ src/www/system_advanced_admin.php
@@ -1,33 +1,34 @@
 <?php
 
 /*
-    Copyright (C) 2014-2015 Deciso B.V.
-    Copyright (C) 2005-2010 Scott Ullrich <sullrich@gmail.com>
-    Copyright (C) 2008 Shrew Soft Inc. <mgrooms@shrew.net>
-    Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice,
-       this list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-
-    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (C) 2017 Franco Fichtner <franco@opnsense.org>
+ * Copyright (C) 2014-2015 Deciso B.V.
+ * Copyright (C) 2005-2010 Scott Ullrich <sullrich@gmail.com>
+ * Copyright (C) 2008 Shrew Soft Inc. <mgrooms@shrew.net>
+ * Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 require_once("guiconfig.inc");
 require_once("filter.inc");
@@ -289,20 +290,30 @@ include("head.inc");
                 dialogRef.getModalBody().html(
                     '<?= html_safe(gettext('The web GUI is reloading at the moment, please wait...')) ?>' +
                     ' <i class="fa fa-cog fa-spin"></i><br /><br /><?= html_safe(gettext('If the page does not reload go here:')) ?>' +
-                    ' <a href="<?= html_safe($url) ?>"><?= html_safe($url) ?></a>'
+                    ' <a href="<?= html_safe($url) ?>" target="_blank"><?= html_safe($url) ?></a>'
                 );
-                setTimeout(reloadWait, 10000);
+                setTimeout(reloadWaitNew, 20000);
             },
         });
 
-        function reloadWait() {
+        function reloadWaitNew () {
             $.ajax({
                 url: '<?= html_safe($url); ?>',
-                timeout: 2500
+                timeout: 1250
             }).fail(function () {
-                setTimeout(reloadWait, 2500);
+                setTimeout(reloadWaitOld, 1250);
             }).done(function () {
                 window.location.assign('<?= html_safe($url); ?>');
+            });
+        }
+        function reloadWaitOld () {
+            $.ajax({
+                url: '/system_advanced_admin.php',
+                timeout: 1250
+            }).fail(function () {
+                setTimeout(reloadWaitNew, 1250);
+            }).done(function () {
+                window.location.assign('/system_advanced_admin.php');
             });
         }
  <?php
