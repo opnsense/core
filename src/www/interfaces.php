d894dd825c1811fe2f29766f759d8521722e9edb
Ad Schellevis <ad@opnsense.org>
2018-02-23 19:01:47 +0100

    Interfaces / reconfigure
    PR: https://github.com/opnsense/core/issues/2221
    
    (cherry picked from commit 31282787dfb7de70ae8e5ba4e1c9aa6db43a34d1)
    (cherry picked from commit 1891293c6498fc6971e8f5c2856eab166a4d11a1)

--- src/www/interfaces.php
+++ src/www/interfaces.php
@@ -511,11 +511,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             if (file_exists('/tmp/.interfaces.apply')) {
                 $toapplylist = unserialize(file_get_contents('/tmp/.interfaces.apply'));
                 foreach ($toapplylist as $ifapply => $ifcfgo) {
+                    interface_bring_down($ifapply, $ifcfgo);
                     if (isset($config['interfaces'][$ifapply]['enable'])) {
-                        interface_bring_down($ifapply, $ifcfgo);
                         interface_configure($ifapply, true);
-                    } else {
-                        interface_bring_down($ifapply, $ifcfgo);
                     }
                 }
             }
@@ -555,10 +553,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         } else {
             $toapplylist = array();
         }
-        $toapplylist[$if]['ifcfg'] = $a_interfaces[$if];
-        $toapplylist[$if]['ppps'] = $a_ppps;
-        /* we need to be able remove IP aliases for IPv6 */
-        file_put_contents('/tmp/.interfaces.apply', serialize($toapplylist));
+        if (empty($toapplylist[$if])) {
+            // only flush if the running config is not in our list yet
+            $toapplylist[$if]['ifcfg'] = $a_interfaces[$if];
+            $toapplylist[$if]['ifcfg']['realif'] = get_real_interface($if);
+            $toapplylist[$if]['ifcfg']['realifv6'] = get_real_interface($if, "inet6");
+            $toapplylist[$if]['ppps'] = $a_ppps;
+            file_put_contents('/tmp/.interfaces.apply', serialize($toapplylist));
+        }
         header(url_safe('Location: /interfaces.php?if=%s', array($if)));
         exit;
     } else {
@@ -611,9 +613,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                     }
                 }
             case "dhcp":
-                if (in_array($a_interfaces[$if]['ipaddr'], array("ppp", "pppoe", "pptp", "l2tp"))) {
-                    $input_errors[] = sprintf(gettext("You have to reassign the interface to be able to configure as %s."),$pconfig['type']);
-                }
                 if (!empty($pconfig['adv_dhcp_config_file_override'] && !file_exists($pconfig['adv_dhcp_config_file_override_path']))) {
                     $input_errors[] = sprintf(gettext('The DHCP override file "%s" does not exist.'), $pconfig['adv_dhcp_config_file_override_path']);
                 }
@@ -926,6 +925,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // save form data
         if (count($input_errors) == 0) {
             $old_config = $a_interfaces[$if];
+            // retrieve our interface names before anything changes
+            $old_config['realif'] = get_real_interface($if);
+            $old_config['realifv6'] = get_real_interface($if, "inet6");
             $new_config = array();
             $new_ppp_config = array();
 
@@ -1254,6 +1256,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 $new_config['if'] = $a_ppps[$pppid]['ports'];
                 unset($a_ppps[$pppid]);
             }
+
             // save interface details
             $a_interfaces[$if] = $new_config;
 
@@ -1277,11 +1280,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             } else {
                 $toapplylist = array();
             }
-            $old_config['realif'] = get_real_interface($if);
-            $toapplylist[$if]['ifcfg'] = $old_config;
-            $toapplylist[$if]['ppps'] = $old_ppps;
 
-            file_put_contents('/tmp/.interfaces.apply', serialize($toapplylist));
+            if (empty($toapplylist[$if])) {
+                // only flush if the running config is not in our list yet
+                $toapplylist[$if]['ifcfg'] = $old_config;
+                $toapplylist[$if]['ppps'] = $old_ppps;
+                file_put_contents('/tmp/.interfaces.apply', serialize($toapplylist));
+            }
 
             mark_subsystem_dirty('interfaces');
 
