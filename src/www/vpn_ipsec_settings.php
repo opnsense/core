1a415b9ea2f57f3bbfc1be4b1d6bfa23c24e89c2
Ad Schellevis <ad@opnsense.org>
2018-02-24 15:21:34 +0100

    fix typo and style fixes for https://github.com/opnsense/core/pull/2222

--- src/www/vpn_ipsec_settings.php
+++ src/www/vpn_ipsec_settings.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $pconfig  = array();
     $pconfig['disablevpnrules'] = isset($config['system']['disablevpnrules']);
     $pconfig['preferoldsa_enable'] = isset($config['ipsec']['preferoldsa']);
-	$pconfig['auto_routes_disable'] = isset($config['ipsec']['auto_routes_disable']);
+    $pconfig['auto_routes_disable'] = isset($config['ipsec']['auto_routes_disable']);
     if (!empty($config['ipsec']['passthrough_networks'])) {
         $pconfig['passthrough_networks'] = explode(',', $config['ipsec']['passthrough_networks']);
     } else {
@@ -96,7 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         } elseif (isset($config['ipsec']['passthrough_networks'])) {
             unset($config['ipsec']['passthrough_networks']);
         }
-        if (isset($pconfig['auto_routes_disable']) && $pconfig['auto_routes_disable'] == "yes") {
+        if (!empty($pconfig['auto_routes_disable'])) {
             $config['ipsec']['auto_routes_disable'] = true;
         } elseif (isset($config['ipsec']['auto_routes_disable'])) {
             unset($config['ipsec']['auto_routes_disable']);
@@ -190,7 +190,7 @@ if (isset($input_errors) && count($input_errors) > 0) {
                     <tr>
                       <td><a id="help_for_auto_routes_disable" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Do not install routes"); ?></td>
                       <td style="width:78%" class="vtable">
-                        <input name="auto_routes_disable" type="checkbox" id="auto_routes_disable" value="yes" <?= !empty($pconfig['auto_routes_disable']) ? "checked=\"checked\" : """;?> />
+                        <input name="auto_routes_disable" type="checkbox" id="auto_routes_disable" value="yes" <?= !empty($pconfig['auto_routes_disable']) ? "checked=\"checked\"" : "";?> />
                         <strong><?=gettext("Do not automatically install routes"); ?></strong>
                         <output class="hidden" for="help_for_auto_routes_disable">
                             <?=gettext("By default, IPsec installs routes when a tunnel becomes active. " .
