a3b4aa57a33b144ddf9ca08cbfcde57f00e2cd06
Fabian Franz <franz.fabian.94@gmail.com>
2018-03-09 18:07:10 +0100

    remove debug and test code and add (C) header

--- src/opnsense/scripts/system/nextcloud.php
+++ src/opnsense/scripts/system/nextcloud.php
@@ -1,19 +1,33 @@
 #!/usr/local/bin/php
 <?php
-require_once 'config.inc';
 
-$testconfig = array(
-    'system' => array(
-        'remotebackup' => array(
-            'nextcloud_url' => 'http://localhost:4567',
-            'nextcloud_user' => 'testuser',
-            'nextcloud_password' => 'testuser',
-            'nextcloud_backupdir' => 'opnsense-backup'
-        ),
-        'hostname' => 'opn',
-        'domain' => 'sense'
-    )
-);
+/*
+    Copyright (C) 2018 Fabian Franz
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once 'config.inc';
 
 function upload_file_nextcloud($cfg, $file)
 {
@@ -51,7 +65,7 @@ function upload_file_nextcloud($cfg, $file)
         return -1;
     }
     curl_close($curl);
-    
+
     if (stristr($response, $backupdir) === FALSE) {
         $curl = curl_init();
         curl_setopt_array($curl, array(
@@ -113,4 +127,3 @@ function upload_file_nextcloud($cfg, $file)
 }
 
 exit(upload_file_nextcloud($config, '/conf/config.xml'));
-//$result = upload_file_nextcloud($testconfig, __file__);
