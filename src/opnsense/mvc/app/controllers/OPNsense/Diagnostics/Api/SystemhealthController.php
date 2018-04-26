d222879577b670475a052f550901da0a0ff6eee5
Franco Fichtner <franco@opnsense.org>
2018-04-26 06:55:12 +0000

    mvc: small audit on configdRun(), only use when needed

--- src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/SystemhealthController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/SystemhealthController.php
@@ -450,7 +450,7 @@ class SystemhealthController extends ApiControllerBase
         # Source of data: xml fields of corresponding .xml metadata
         $result = array();
         $backend = new Backend();
-        $response = $backend->configdpRun("systemhealth list");
+        $response = $backend->configdRun('systemhealth list');
         $healthList = json_decode($response, true);
         // search by topic and name, return array with filename
         if (is_array($healthList)) {
@@ -480,7 +480,7 @@ class SystemhealthController extends ApiControllerBase
         # Source of data: filelisting of /var/db/rrd/*.rrd
         $result = array();
         $backend = new Backend();
-        $response = $backend->configdpRun("systemhealth list");
+        $response = $backend->configdRun('systemhealth list');
         $healthList = json_decode($response, true);
 
         $result['data'] = array();
@@ -530,7 +530,7 @@ class SystemhealthController extends ApiControllerBase
         $xml = false;
         if ($rrd_details['filename'] != "") {
             $backend = new Backend();
-            $response = $backend->configdpRun("systemhealth fetch ", array($rrd_details['filename']));
+            $response = $backend->configdpRun('systemhealth fetch', array($rrd_details['filename']));
             if ($response != null) {
                 $xml = @simplexml_load_string($response);
             }
