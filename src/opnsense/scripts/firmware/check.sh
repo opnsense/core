bfe404f10f5879cd70160a693bdcbd061b39740e
Franco Fichtner <franco@opnsense.org>
2018-05-20 17:13:59 +0200

    firmware: many change, such wow
    A backport half-requested by @Adschellevis.  It doesn't make
    a lot of sense to split our current work into bits and pieces
    because if we don't push all of these changes now we end up
    needing to push them before the EoL date anyway and this is
    better to revert and improve.  Most changes have been on the
    development track for a while to mature, others are simple
    changes.
    
    Needless to say, requires more testing towards 18.1.9 next
    week.
    
    * Parse and return to be removed packages for update summary
    * Release type change properly updates the repository, summary
    * The firmware options can be set via XML files
    * Return repository errors in greater detail (4 new error types)
    * Add and use API for major upgrades
    * Make returned backend JSON a bit more human-readable
    * Fix leak of base/kernel update info on package manager updates
    * Refactor package manager update summary parsing for speed

--- src/opnsense/scripts/firmware/check.sh
+++ src/opnsense/scripts/firmware/check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2015-2017 Franco Fichtner <franco@opnsense.org>
+# Copyright (C) 2015-2018 Franco Fichtner <franco@opnsense.org>
 # Copyright (C) 2014 Deciso B.V.
 # All rights reserved.
 #
@@ -26,19 +26,17 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 # This script generates a json structured file with the following content:
-# connection: error|ok
+# connection: error|timeout|unauthenticated|misconfigured|unresolved|ok
 # repository: error|ok
 # last_ckeck: <date_time_stamp>
 # updates: <num_of_updates>
 # download_size: <size_of_total_downloads>
 # new_packages: array with { name: <package_name>, version: <package_version> }
 # reinstall_packages: array with { name: <package_name>, version: <package_version> }
+# remove_packages: array with { name: <package_name>, version: <package_version> }
 # downgrade_packages: array with { name: <package_name>, current_version: <current_version>, new_version: <new_version> }
 # upgrade_packages: array with { name: <package_name>, current_version: <current_version>, new_version: <new_version> }
 
-# TODO: Add object with items that will be removed
-
-# Variables used
 connection="error"
 repository="error"
 upgrade_needs_reboot="0"
@@ -49,15 +47,15 @@ pkg_running=""
 packes_output=""
 last_check="unknown"
 packages_upgraded=""
+pkg_upgraded=""
 packages_downgraded=""
 packages_new=""
 download_size=""
 itemcount=0
 linecount=0
-timer=0
-timeout=60 # Wait for a maximum number of seconds to determine connection issues
+timeout_update=30
+timeout_upgrade=60
 
-# File location variables
 tmp_pkg_output_file="/tmp/packages.output"
 tmp_pkg_update_file="/tmp/pkg_updates.output"
 
@@ -65,39 +63,66 @@ tmp_pkg_update_file="/tmp/pkg_updates.output"
 pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
 if [ "$pkg_running" == "" ]; then
       # start pkg update
-      pkg update -f > $tmp_pkg_update_file &
-      pkg_running="started" # Set running state to arbitrary value
-      timer=$timeout # Reset our timer
+      pkg update -f > $tmp_pkg_update_file 2>&1 &
+      timer=$timeout_update
+      pkg_running="started"
 
       # Timeout loop for pkg update -f
-      while [ "$pkg_running" != "" ] && [ $timer -ne 0 ];
-      do
+      while [ "$pkg_running" != "" ] && [ $timer -ne 0 ]; do
         sleep 1 # wait for 1 second
         pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
         timer=`echo $timer - 1 | bc`
       done
 
-      ## check if timeout is not reached
-      if [ $timer -gt 0 ] ; then
-        # Connection is ok
+      if [ $timer -eq 0 ]; then
+        # We have an connection issue and could not
+        # reach the pkg repository in timely fashion
+        # Kill all running pkg instances
+        pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
+        if [ "$pkg_running" != "" ]; then
+          killall pkg
+        fi
+        connection="timeout"
+      else
+        # parse early errors
+        if grep -q 'No address record' $tmp_pkg_update_file; then
+          # DNS resolution failed
+          connection="unresolved"
+          timer=0
+        elif grep -q 'Cannot parse configuration' $tmp_pkg_update_file; then
+          # configuration error
+          connection="misconfigured"
+          timer=0
+        elif grep -q 'Authentication error' $tmp_pkg_update_file; then
+          # TLS or authentication error
+          connection="unauthenticated"
+          timer=0
+        fi
+      fi
+
+      if [ $timer -gt 0 ]; then
+        # connection is still ok
         connection="ok"
-        # Now check if there are upgrades
-        pkg upgrade -n > $tmp_pkg_output_file &
-        # Reset timer before getting upgrade info
-        timer=$timeout # Reset our timer
-        pkg_running="started" # Set running state to arbitrary value
+        # now check what happens when we would go ahead
+        if [ -z "${1}" ]; then
+            pkg upgrade -n > $tmp_pkg_output_file &
+        else
+            # fetch before install lets us know more
+            pkg fetch -y "${1}" > $tmp_pkg_output_file &
+            pkg install -n "${1}" > $tmp_pkg_output_file &
+	fi
+        timer=$timeout_upgrade
+        pkg_running="started"
 
         # Timeout loop for pkg upgrade -n
-        while [ "$pkg_running" != "" ] && [ $timer -ne 0 ];
-        do
+        while [ "$pkg_running" != "" ] && [ $timer -ne 0 ]; do
           sleep 1 # wait for 1 second
-          #pkg_running=`ps | grep 'pkg update -f' | grep -v 'grep' | tail -n 1 | awk -F '[ ]' '{print $1}'`
           pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
           timer=`echo $timer - 1 | bc`
         done
 
         ## check if timeout is not reached
-        if [ $timer -gt 0 ] ; then
+        if [ $timer -gt 0 ]; then
           # Check for additional repository errors
           repo_ok=`cat $tmp_pkg_output_file | grep 'Unable to update repository'`
           if [ "$repo_ok" == "" ]; then
@@ -110,6 +135,7 @@ if [ "$pkg_running" == "" ]; then
             else
               download_size=`cat $tmp_pkg_output_file | grep 'to be downloaded' | awk -F '[ ]' '{print $1$2}'`
 
+              # see if packages indicate a new version (not revision) of base / kernel
               LQUERY=$(pkg query %v opnsense-update)
               RQUERY=$(pkg rquery %v opnsense-update)
               if [ "${LQUERY%%_*}" != "${RQUERY%%_*}" ]; then
@@ -117,124 +143,139 @@ if [ "$pkg_running" == "" ]; then
                 base_to_reboot="${RQUERY%%_*}"
               fi
 
-              # First check if there are new packages that need to be installed
-              for i in $(cat $tmp_pkg_output_file); do
-                if [ "$itemcount" -gt "$linecount" ]; then
-                  if [  `echo $linecount + 2 | bc` -eq "$itemcount" ]; then
-                    if [ "`echo $i | grep ':'`" == "" ]; then
-                      itemcount=0 # This is not a valid item so reset item count
-                    else
-                      i=`echo $i | tr -d :`
-                      #echo "name:$i"
-                      if [ "$packages_new" == "" ]; then
-                        packages_new=$packages_new"{\"name\":\"$i\"," # If it is the first item then we do not want a seperator
-                      else
-                        packages_new=$packages_new", {\"name\":\"$i\","
-                      fi
-                    fi
-                  fi
-                  if [  `echo $linecount + 1 | bc` -eq "$itemcount" ]; then
-                    packages_new=$packages_new"\"version\":\"$i\"}"
-                    itemcount=`echo $itemcount + 2 | bc` # Get ready for next item
-                  fi
-                fi
-                linecount=`echo $linecount + 1 | bc`
-                if [ "$i" == "INSTALLED:" ]; then
-                  itemcount=`echo $linecount + 2 | bc`
-                fi
-              done
+              MODE=
 
-              # Check if there are packages that need to be reinstalled
               for i in $(cat $tmp_pkg_output_file | cut -d '(' -f1); do
-                if [ "$itemcount" -gt "$linecount" ]; then
-                  if [  `echo $linecount + 1 | bc` -eq "$itemcount" ]; then
-                    if [ "`echo $i | grep '-'`" == "" ]; then
+                case ${MODE} in
+                DOWNGRADED:)
+                  if [ "$(expr $linecount + 4)" -eq "$itemcount" ]; then
+                    if [ "${i%:*}" = "${i}" ]; then
                       itemcount=0 # This is not a valid item so reset item count
-                    else
-                      name=${i%-*}
-                      version=${i##*-}
-                      itemcount=`echo $itemcount + 1 | bc` # Get ready for next item
-                      if [ "$packages_reinstall" == "" ]; then
-                        packages_reinstall=$packages_reinstall"{\"name\":\"$name\"," # If it is the first item then we do not want a seperator
-                        packages_reinstall=$packages_reinstall"\"version\":\"$version\"}"
-                      else
-                        packages_reinstall=$packages_reinstall", {\"name\":\"$name\","
-                        packages_reinstall=$packages_reinstall"\"version\":\"$version\"}"
-                      fi
-                    fi
-                  fi
-                fi
-                linecount=`echo $linecount + 1 | bc`
-                if [ "$i" == "REINSTALLED:" ]; then
-                  itemcount=`echo $linecount + 1 | bc`
-                fi
-              done
-
-              # Now check if there are upgrades to install
-              for i in $(cat $tmp_pkg_output_file); do
-                if [ "$itemcount" -gt "$linecount" ]; then
-                  if [  `echo $linecount + 4 | bc` -eq "$itemcount" ]; then
-                    if [ "`echo $i | grep ':'`" == "" ]; then
-                      itemcount=0 # This is not a valid item so reset item count
+                      MODE=
                     else
                       i=`echo $i | tr -d :`
-                      if [ "$packages_upgraded" == "" ]; then
-                        packages_upgraded=$packages_upgraded"{\"name\":\"$i\"," # If it is the first item then we do not want a seperator
-                      else
-                        packages_upgraded=$packages_upgraded", {\"name\":\"$i\","
-                      fi
-                    fi
-                  fi
-                  if [  `echo $linecount + 3 | bc` -eq "$itemcount" ]; then
-                    packages_upgraded=$packages_upgraded"\"current_version\":\"$i\","
-                  fi
-                  if [  `echo $linecount + 1 | bc` -eq "$itemcount" ]; then
-                    packages_upgraded=$packages_upgraded"\"new_version\":\"$i\"}"
-                    itemcount=`echo $itemcount + 4 | bc` # Get ready for next item
-                  fi
-                fi
-                linecount=`echo $linecount + 1 | bc`
-                if [ "$i" == "UPGRADED:" ]; then
-                  itemcount=`echo $linecount + 4 | bc`
-                fi
-              done
-
-              # Now check if there are downgrades to install
-              for i in $(cat $tmp_pkg_output_file); do
-                if [ "$itemcount" -gt "$linecount" ]; then
-                  if [  `echo $linecount + 4 | bc` -eq "$itemcount" ]; then
-                    if [ "`echo $i | grep ':'`" == "" ]; then
-                      itemcount=0 # This is not a valid item so reset item count
-                    else
-                      i=`echo $i | tr -d :`
-                      if [ "$packages_downgraded" == "" ]; then
+                      if [ -z "$packages_downgraded" ]; then
                         packages_downgraded=$packages_downgraded"{\"name\":\"$i\"," # If it is the first item then we do not want a seperator
                       else
                         packages_downgraded=$packages_downgraded", {\"name\":\"$i\","
                       fi
                     fi
                   fi
-                  if [  `echo $linecount + 3 | bc` -eq "$itemcount" ]; then
+                  if [ "$(expr $linecount + 3)" -eq "$itemcount" ]; then
                     packages_downgraded=$packages_downgraded"\"current_version\":\"$i\","
                   fi
-                  if [  `echo $linecount + 1 | bc` -eq "$itemcount" ]; then
+                  if [ "$(expr $linecount + 1)" -eq "$itemcount" ]; then
                     packages_downgraded=$packages_downgraded"\"new_version\":\"$i\"}"
-                    itemcount=`echo $itemcount + 4 | bc` # Get ready for next item
+                    itemcount=$(expr $itemcount + 4) # get ready for next item
                   fi
-                fi
-                linecount=`echo $linecount + 1 | bc`
-                if [ "$i" == "DOWNGRADED:" ]; then
-                  itemcount=`echo $linecount + 4 | bc`
-                fi
+                  ;;
+                INSTALLED:)
+                  if [ "$(expr $linecount + 2)" -eq "$itemcount" ]; then
+                    if [ "${i%:*}" = "${i}" ]; then
+                      itemcount=0 # This is not a valid item so reset item count
+                      MODE=
+                    else
+                      i=`echo $i | tr -d :`
+                      if [ -n "$packages_new" ]; then
+                        packages_new=$packages_new","
+                      fi
+                      packages_new=$packages_new"{\"name\":\"$i\","
+                    fi
+                  fi
+                  if [ "$(expr $linecount + 1)" -eq "$itemcount" ]; then
+                    packages_new=$packages_new"\"version\":\"$i\"}"
+                    itemcount=$(expr $itemcount + 2) # get ready for next item
+                  fi
+                  ;;
+                REINSTALLED:)
+                  if [ "$(expr $linecount + 1)" -eq "$itemcount" ]; then
+                    if [ "${i%-*}" = "${i}" ]; then
+                      itemcount=0 # This is not a valid item so reset item count
+                      MODE=
+                    else
+                      name=${i%-*}
+                      version=${i##*-}
+                      itemcount="$(expr $itemcount + 1)" # get ready for next item
+                      if [ -n "$packages_reinstall" ]; then
+                        packages_reinstall=$packages_reinstall"," # separator for next item
+                      fi
+                      packages_reinstall=$packages_reinstall"{\"name\":\"$name\",\"version\":\"$version\"}"
+                    fi
+                  fi
+                  ;;
+                REMOVED:)
+                  if [ "$(expr $linecount + 1)" -eq "$itemcount" ]; then
+                    if [ "${i%-*}" = "${i}" ]; then
+                      itemcount=0 # This is not a valid item so reset item count
+                      MODE=
+                    else
+                      name=${i%-*}
+                      version=${i##*-}
+                      itemcount="$(expr $itemcount + 1)" # get ready for next item
+                      if [ -n "$packages_remove" ]; then
+                        packages_remove=$packages_remove"," # separator for next item
+                      fi
+                      packages_remove=$packages_remove"{\"name\":\"$name\",\"version\":\"$version\"}"
+                    fi
+                  fi
+                  ;;
+                UPGRADED:)
+                  if [ "$(expr $linecount + 4)" -eq "$itemcount" ]; then
+                    if [ "${i%:*}" = "${i}" ]; then
+                      itemcount=0 # This is not a valid item so reset item count
+                      MODE=
+                    else
+                      i=`echo $i | tr -d :`
+                      if [ -z "$packages_upgraded" ]; then
+                        if [ "$i" = "pkg" ]; then
+                          # prevents leaking base / kernel advertising here
+                          pkg_upgraded="yes"
+                        fi
+                        packages_upgraded=$packages_upgraded"{\"name\":\"$i\"," # If it is the first item then we do not want a seperator
+                      else
+                        packages_upgraded=$packages_upgraded", {\"name\":\"$i\","
+                      fi
+                    fi
+                  fi
+                  if [ "$(expr $linecount + 3)" -eq "$itemcount" ]; then
+                    packages_upgraded=$packages_upgraded"\"current_version\":\"$i\","
+                  fi
+                  if [ "$(expr $linecount + 1)" -eq "$itemcount" ]; then
+                    packages_upgraded=$packages_upgraded"\"new_version\":\"$i\"}"
+                    itemcount=$(expr $itemcount + 4) # get ready for next item
+                  fi
+                  ;;
+                esac
+
+                linecount=$(expr $linecount + 1)
+
+                case $i in
+                INSTALLED:)
+                  itemcount=$(expr $linecount + 2)
+                  MODE=$i
+                  ;;
+                REINSTALLED:|REMOVED:)
+                  itemcount=$(expr $linecount + 1)
+                  MODE=$i
+                  ;;
+                DOWNGRADED:|UPGRADED:)
+                  itemcount=$(expr $linecount + 4)
+                  MODE=$i
+                  ;;
+                esac
               done
             fi
-            if opnsense-update -cbf; then
-              # the main update from package will override this during upgrade
-              if [ -z "$base_to_reboot" ]; then
+
+            # the main update from package will provide this during upgrade
+            if [ -n "$pkg_upgraded" ]; then
+              base_to_reboot=
+            elif [ -z "$base_to_reboot" ]; then
+              if opnsense-update -cbf; then
                   base_to_reboot="$(opnsense-update -v)"
                   base_to_reboot="${base_to_reboot%-*}"
               fi
             fi
+
             if [ -n "$base_to_reboot" ]; then
               base_to_delete="$(opnsense-update -bv)"
               base_to_delete="${base_to_delete%-*}"
@@ -252,13 +293,17 @@ if [ "$pkg_running" == "" ]; then
                 updates=$(expr $updates + 1)
               fi
             fi
-            if opnsense-update -cfk; then
-              # the main update from package will override this during upgrade
-              if [ -z "$kernel_to_reboot" ]; then
+
+            # the main update from package will provide this during upgrade
+            if [ -n "$pkg_upgraded" ]; then
+              kernel_to_reboot=
+            elif [ -z "$kernel_to_reboot" ]; then
+              if opnsense-update -cfk; then
                   kernel_to_reboot="$(opnsense-update -v)"
                   kernel_to_reboot="${kernel_to_reboot%-*}"
               fi
             fi
+
             if [ -n "$kernel_to_reboot" ]; then
               kernel_to_delete="$(opnsense-update -kv)"
               kernel_to_delete="${kernel_to_delete%-*}"
@@ -285,20 +330,35 @@ if [ "$pkg_running" == "" ]; then
             killall pkg
           fi
         fi
-      else
-          # We have an connection issue and could not reach the pkg repository in timely fashion
-          # Kill all running pkg instances
-          pkg_running=`ps -x | grep "pkg " | grep -v "grep"`
-          if [ "$pkg_running" != "" ]; then
-            killall pkg
-          fi
       fi
 
+      upgrade_major_message=$(cat /usr/local/opnsense/firmware-message 2> /dev/null | sed 's/"/\\&/g' | tr '\n' ' ')
+      upgrade_major_version=$(cat /usr/local/opnsense/firmware-upgrade 2> /dev/null)
       product_version=$(cat /usr/local/opnsense/version/opnsense)
       product_name=$(cat /usr/local/opnsense/version/opnsense.name)
       os_version=$(uname -sr)
       last_check=$(date)
 
-      # Write our json structure to disk
-      echo "{\"connection\":\"$connection\",\"repository\":\"$repository\",\"product_version\":\"$product_version\",\"product_name\":\"$product_name\",\"os_version\":\"$os_version\",\"last_check\":\"$last_check\",\"updates\":\"$updates\",\"download_size\":\"$download_size\",\"new_packages\":[$packages_new],\"reinstall_packages\":[$packages_reinstall],\"upgrade_packages\":[$packages_upgraded],\"downgrade_packages\":[$packages_downgraded],\"upgrade_needs_reboot\":\"$upgrade_needs_reboot\"}"
+      # write our json structure
+      cat << EOF
+{
+	"connection":"$connection",
+	"downgrade_packages":[$packages_downgraded],
+	"download_size":"$download_size",
+	"last_check":"$last_check",
+	"new_packages":[$packages_new],
+	"os_version":"$os_version",
+	"product_name":"$product_name",
+	"product_version":"$product_version",
+	"reinstall_packages":[$packages_reinstall],
+	"remove_packages":[$packages_remove],
+	"repository":"$repository",
+	"updates":"$updates",
+	"upgrade_major_message":"$upgrade_major_message",
+	"upgrade_major_version":"$upgrade_major_version",
+	"upgrade_needs_reboot":"$upgrade_needs_reboot",
+	"upgrade_packages":[$packages_upgraded]
+}
+EOF
+
 fi
