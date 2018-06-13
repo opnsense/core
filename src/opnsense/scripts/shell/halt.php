ebbb917b9d46f28322b99313ae9167f5ffd73412
Franco Fichtner <franco@opnsense.org>
2018-06-13 16:05:51 +0200

    rc: small tweak on previous

--- src/opnsense/scripts/shell/halt.php
+++ src/opnsense/scripts/shell/halt.php
@@ -37,6 +37,7 @@ $fp = fopen('php://stdin', 'r');
 echo 'The system will halt and power off. Do you want to proceed? [y/N]: ';
 
 if (strcasecmp(chop(fgets($fp)), 'y') == 0) {
+    echo PHP_EOL;
     system_halt(true);
 }
 
