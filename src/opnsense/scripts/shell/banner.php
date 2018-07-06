00f9b21cb78d9f76a8f94e8e62cbcefad65b7d99
Franco Fichtner <franco@opnsense.org>
2018-07-06 20:31:49 +0000

    shell: pretty-print the SHA256 sums for the banner

--- src/opnsense/scripts/shell/banner.php
+++ src/opnsense/scripts/shell/banner.php
@@ -123,26 +123,20 @@ foreach ($iflist as $ifname => $friendly) {
     }
 }
 
-if (isset($config['system']['ssh']['enabled']) or $config['system']['webgui']['protocol'] == "https") {
-    printf("\n\n");
+printf("\n");
+
+if (isset($config['system']['ssh']['enabled']) || $config['system']['webgui']['protocol'] == 'https') {
+    printf("\n");
+}
+
+if ($config['system']['webgui']['protocol'] == 'https') {
+    printf(' HTTPS: ');
+    passthru('openssl x509 -in /var/etc/cert.pem -noout -fingerprint -sha256 | sed "s/Fingerprint=//" | sed -E "s/(^.{55})/\1,               /" | tr "," "\n"');
 }
 
 if (isset($config['system']['ssh']['enabled'])) {
-    foreach (glob("/conf/sshd/ssh_host_*_key.pub") as $ssh_host_pub_key_file_path) {
-        printf(" SSH: ");
-        /* `| cut -d ' ' -f 1-2,4-` is used to filter out the comment (hostname) from the host key.
-         * With the hostname included, one fingerprint would normally not fit on the console screen.
-         */
-        passthru("ssh-keygen -l -f " . escapeshellarg($ssh_host_pub_key_file_path) . " | cut -d ' ' -f 1-2,4-");
+    foreach (glob('/conf/sshd/ssh_host_*_key.pub') as $ssh_host_pub_key_file_path) {
+        printf(' SSH:   ');
+        passthru("ssh-keygen -l -f " . escapeshellarg($ssh_host_pub_key_file_path) . " | awk '{ print $2 \" \" $4 }' | sed 's/SHA256:/SHA256 /'");
     }
 }
-
-if ($config['system']['webgui']['protocol'] == "https") {
-    printf(" HTTPS X.509 cert: ");
-    /* This output will need two lines in console output.
-     * Not much we can do about this.
-     */
-    passthru("openssl x509 -in /var/etc/cert.pem -noout -fingerprint -sha256");
-}
-
-printf("\n");
