d14f06616f1167038f94b9722c845a58e7f47dbc
João Vilaça <machadovilaca@gmail.com>
2018-08-08 12:22:26 +0100

    Add textarea field to diag_backup (#2616)

--- src/www/diag_backup.php
+++ src/www/diag_backup.php
@@ -452,6 +452,9 @@ $( document ).ready(function() {
 
                         <input name="<?=$fieldId;?>" type="password" value="<?=$field['value'];?>" />
 <?php
+                        elseif ($field['type'] == 'textarea'):?>
+                        <textarea name="<?=$fieldId;?>" type="text" rows="10"><?=$pconfig[$fieldId];?></textarea>
+<?php
                         endif;?>
                         <div class="hidden" data-for="help_for_<?=$fieldId;?>">
                             <?=!empty($field['help']) ? $field['help'] : "";?>
