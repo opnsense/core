66f348cc5eefc590b42a406153bd840761d34749
Franco Fichtner <franco@opnsense.org>
2018-10-17 23:01:40 +0200

    Revert "config/legacy, speedup parsing very large config files."
    This reverts commit 2d360a391b0e0f2687eb64502ab8f8018345785c.

--- src/opnsense/mvc/app/library/OPNsense/Core/Config.php
+++ src/opnsense/mvc/app/library/OPNsense/Core/Config.php
@@ -75,14 +75,13 @@ class Config extends Singleton
      */
     private function isArraySequential(&$arrayData)
     {
-        $all_keys = array_keys($arrayData);
-        $last_key = count($all_keys);
-        // probably a safe bet, first key equals 0, last equals the number of keys minus 1.
-        if ($all_keys[0] == "0" && $all_keys[$last_key-1] == ($last_key-1)) {
-            return true;
+        foreach ($arrayData as $key => &$value) {
+            if (!ctype_digit(strval($key))) {
+                return false;
+            }
         }
 
-        return false;
+        return true;
     }
 
     /**
