8ca654f89c308e21ed5ac1da7e7b85d91b9fa374
Franco Fichtner <franco@opnsense.org>
2018-11-15 09:06:37 +0100

    system: disable help click when disabled in backup fields

--- src/www/diag_backup.php
+++ src/www/diag_backup.php
@@ -438,8 +438,12 @@ $( document ).ready(function() {
                     $fieldId = $providerId . "_" .$field['name'];?>
                     <tr>
                         <td style="width:22%">
-                            <a id="help_for_<?=$fieldId;?>" href="#" class="showhelp">
-                                <i class="fa fa-info-circle <?=empty($field['help']) ? "text-muted" : "";?>"></i></a> <?=$field['label'];?>
+<?php if (!empty($field['help'])): ?>
+                            <a id="help_for_<?=$fieldId;?>" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a>
+<?php else: ?>
+                            <i class="fa fa-info-circle text-muted"></i>
+<?php endif ?>
+                           <?=$field['label'];?>
                         </td>
                         <td style="width:78%">
 <?php
