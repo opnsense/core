e5cb29c3447b3216eb5bd6fc5d6aa91857c3d422
Franco Fichtner <franco@opnsense.org>
2018-11-21 11:45:34 +0100

    firewall: previous broke page actually

--- src/www/firewall_scrub_edit.php
+++ src/www/firewall_scrub_edit.php
@@ -422,7 +422,7 @@ include("head.inc");
                   <tr>
                     <td><a id="help_for_srcport" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Source port"); ?></td>
                     <td>
-                      <div class="hidden">
+                      <div>
                           <input type="button" class="btn btn-default" value="<?=gettext("Advanced"); ?>" id="show_srcport_adv" />
                       </div>
                       <div id="show_srcport" style="display:none;">
