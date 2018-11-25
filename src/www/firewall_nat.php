558414af0ca3b1253c47ae17ddd58d0bc7ade9c5
opnsenseuser <rene@team-rebellion.net>
2018-11-25 15:13:00 +0100

    get rid of colspan in td´s and width:16px?
    get rid of colspan in td´s and width:16px? so it´s like the normal firewall rules!

--- src/www/firewall_nat.php
+++ src/www/firewall_nat.php
@@ -501,23 +501,23 @@ $( document ).ready(function() {
                 </tbody>
                 <tfoot>
                   <tr class="hidden-xs hidden-sm">
-                    <td colspan="11">
+                    <td colspan="12">
                       <table style="width:100%; border:0; cellspacing:0; cellpadding:0">
                         <tr>
-                          <td style="width:16px"><i class="fa fa-play fa-fw text-success"></i></td>
-                          <td colspan="12"><?=gettext("Enabled rule"); ?></td>
+                          <td><i class="fa fa-play fa-fw text-success"></i></td>
+                          <td><?=gettext("Enabled rule"); ?></td>
                           <td><i class="fa fa-exclamation fa-fw text-success"></i></td>
-                          <td colspan="12"><?=gettext("No redirect"); ?></td>
+                          <td><?=gettext("No redirect"); ?></td>
                           <td><i class="fa fa-arrows-h fa-fw text-success"></i></td>
-                          <td colspan="12"><?=gettext("Linked rule");?></td>
+                          <td><?=gettext("Linked rule");?></td>
                         </tr>
                         <tr>
                           <td><i class="fa fa-play fa-fw text-muted"></i></td>
-                          <td colspan="12"><?=gettext("Disabled rule"); ?></td>
+                          <td><?=gettext("Disabled rule"); ?></td>
                           <td><i class="fa fa-exclamation fa-fw text-muted"></i></td>
-                          <td colspan="12"><?=gettext("Disabled no redirect"); ?></td>
+                          <td><?=gettext("Disabled no redirect"); ?></td>
                           <td><i class="fa fa-arrows-h fa-fw text-muted"></i></td>
-                          <td colspan="12"><?=gettext("Disabled linked rule");?></td>
+                          <td><?=gettext("Disabled linked rule");?></td>
                         </tr>
                       </table>
                     </td>
