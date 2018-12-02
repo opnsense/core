616ba48e554fa0ccd775b0a35b88d1b64457c269
Franco Fichtner <franco@opnsense.org>
2018-12-02 11:26:25 +0100

    dhcp: cleanup from master

--- src/www/status_dhcpv6_leases.php
+++ src/www/status_dhcpv6_leases.php
@@ -6,17 +6,17 @@
  * Copyright (C) 2011 Seth Mos <seth.mos@dds.nl>
  * Copyright (C) 2003-2004 Manuel Kasper <mk@neon1.net>
  * All rights reserved.
-
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
-
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
-
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
-
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
  * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -37,7 +37,7 @@ function adjust_gmt($dt)
 {
     global $config;
 
-    $dhcpv6leaseinlocaltime == "no";
+    $dhcpv6leaseinlocaltime = 'no';
     if (is_array($config['dhcpdv6'])) {
         $dhcpdv6 = $config['dhcpdv6'];
         foreach ($dhcpdv6 as $dhcpv6leaseinlocaltime) {
