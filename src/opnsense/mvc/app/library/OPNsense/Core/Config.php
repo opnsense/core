441dbfc90b727f19add79bc630f38c32571b43eb
Franco Fichtner <franco@opnsense.org>
2018-12-17 08:41:33 +0100

    mvc: small refactor, phpdoc updates

--- src/opnsense/mvc/app/library/OPNsense/Core/Config.php
+++ src/opnsense/mvc/app/library/OPNsense/Core/Config.php
@@ -1,31 +1,29 @@
 <?php
 
-/**
- *    Copyright (C) 2015 Deciso B.V.
+/*
+ * Copyright (C) 2015 Deciso B.V.
+ * All rights reserved.
  *
- *    All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    Redistribution and use in source and binary forms, with or without
- *    modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *    POSSIBILITY OF SUCH DAMAGE.
- *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace OPNsense\Core;
@@ -75,13 +73,7 @@ class Config extends Singleton
      */
     private function isArraySequential(&$arrayData)
     {
-        foreach ($arrayData as $key => &$value) {
-            if (!ctype_digit(strval($key))) {
-                return false;
-            }
-        }
-
-        return true;
+        return ctype_digit(implode('', array_keys($arrayData)));
     }
 
     /**
@@ -171,12 +163,12 @@ class Config extends Singleton
 
     /**
      * update (reset) config with array structure (backwards compatibility mode)
-     * @param $source source array structure
+     * @param array $source source array structure
      * @param null $node simplexml node
      * @param null|string $parentTagName
      * @throws ConfigException when config could not be parsed
      */
-    public function fromArray($source, $node = null, $parentTagName = null)
+    public function fromArray(array $source, $node = null, $parentTagName = null)
     {
         $this->checkvalid();
 
@@ -243,7 +235,7 @@ class Config extends Singleton
 
     /**
      * Execute a xpath expression on config.xml (full DOM implementation)
-     * @param $query xpath expression
+     * @param string $query xpath expression
      * @return \DOMNodeList nodes
      * @throws ConfigException when config could not be parsed
      */
@@ -307,7 +299,7 @@ class Config extends Singleton
 
     /**
      * load xml config from file
-     * @param $filename config xml source
+     * @param string $filename config xml source
      * @return \SimpleXMLElement root node
      * @throws ConfigException when config could not be parsed
      */
