7625f0436ac6975c06d76e0ae2a72c777421676d
Ad Schellevis <ad@opnsense.org>
2019-01-29 18:47:04 +0100

    Tokenize2, fix escaping, since our other escape fixes weren't pulled previously, we're fixing this locally for now.
    (ref https://github.com/dragonofmercy/Tokenize2/pull/51)
    
    (cherry picked from commit 2d444e92a28724404c11add9faf58644e025e1aa)

--- src/opnsense/www/js/tokenize2.js
+++ src/opnsense/www/js/tokenize2.js
@@ -281,7 +281,7 @@
      */
     Tokenize2.prototype.tokenAdd = function(value, text, force){
 
-        value = this.escape(value);
+        value = $('<div/>').text(value).html();
         text = text || value;
         force = force || false;
         this.resetInput();
@@ -995,23 +995,6 @@
 
         this.input.val('');
         this.scaleInput();
-
-    };
-
-    /**
-     * Escape string
-     *
-     * @param {string} string
-     * @returns {string}
-     */
-    Tokenize2.prototype.escape = function(string){
-
-        var $escaping = document.createElement('div');
-        $escaping.innerHTML = string;
-        string = ($escaping.textContent || $escaping.innerText || '');
-        return String(string).replace(/["]/g, function(){
-            return '\"';
-        });
 
     };
 
