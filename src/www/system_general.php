883063c49c499697dba0f8b12a7855da9690d0c5
Ad Schellevis <ad@opnsense.org>
2019-03-14 20:13:15 +0100

    get_std_save_message() with hasync enabled mangles some redirected save messages, although we do want to ditch the function at some point, we probably better fix the issue introduced with https://github.com/opnsense/core/issues/3165 first.

--- src/www/system_general.php
+++ src/www/system_general.php
@@ -238,7 +238,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         services_dhcpd_configure();
         filter_configure();
 
-        header(url_safe('Location: /system_general.php?savemsg=%s', array(get_std_save_message(true))));
+        header(url_safe('Location: /system_general.php?savemsg=%s', array('The changes have been applied successfully.')));
         exit;
     }
 }
