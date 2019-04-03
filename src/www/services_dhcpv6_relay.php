401c05efc827696be5a41aa62a9c513ab6259ccd
Ad Schellevis <ad@opnsense.org>
2019-04-03 11:07:37 +0200

    dhcpdiv6 relay, check if interface is active when dhcpd is enabled, https://github.com/opnsense/core/issues/3382
    (cherry picked from commit f4e4867734b96981d4d35d0631b698a473fb4678)

--- src/www/services_dhcpv6_relay.php
+++ src/www/services_dhcpv6_relay.php
@@ -85,8 +85,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
  */
 $dhcpd_enabled = false;
 if (is_array($config['dhcpdv6'])) {
-    foreach($config['dhcpdv6'] as $dhcp) {
-        if (isset($dhcp['enable'])) {
+    foreach($config['dhcpdv6'] as $intf => $dhcp) {
+        if (isset($dhcp['enable']) && !empty($config['interfaces'][$intf]['enable'])) {
             $dhcpd_enabled = true;
         }
     }
