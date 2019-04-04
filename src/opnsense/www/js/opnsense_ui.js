opnsense/core

commit 892e4011b6c0c3c08a9b4b7812f7b688e1e0c781

style sweep on opnsense_ui.js before adding logic for https://github.com/opnsense/plugins/pull/1274

src/opnsense/www/js/opnsense_ui.js
@@ -37,12 +37,12 @@
   */
  function byteFormat(bytes, decimals)
  {
-     if (decimals == undefined) {
+     if (decimals === undefined) {
         decimals = 0;
      }
-     var kb = 1024;
-     var ndx = Math.floor( Math.log(bytes) / Math.log(kb) );
-     var fileSizeTypes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+     const kb = 1024;
+     const ndx = Math.floor(Math.log(bytes) / Math.log(kb));
+     const fileSizeTypes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
      return (bytes / Math.pow(kb, ndx)).toFixed(decimals) + ' ' + fileSizeTypes[ndx];
  }
 
@@ -50,21 +50,22 @@
  * save form to server
  * @param url endpoint url
  * @param formid parent id to grep input data from
+ * @param callback_ok
  * @param disable_dialog don't show input validation message box on failure
  */
 function saveFormToEndpoint(url,formid,callback_ok, disable_dialog) {
     disable_dialog = disable_dialog || false;
-    var data = getFormData(formid);
+    const data = getFormData(formid);
     ajaxCall(url,data,function(data,status){
-        if ( status == "success") {
+        if ( status === "success") {
             // update field validation
             handleFormValidation(formid,data['validations']);
 
             // if there are validation issues, update our screen and show a dialog.
-            if (data['validations'] != undefined) {
+            if (data['validations'] !== undefined) {
                 if (!disable_dialog) {
-                    var detailsid = "errorfrm"+Math.floor((Math.random() * 10000000) + 1);
-                    var errorMessage = $('<div></div>');
+                    const detailsid = "errorfrm" + Math.floor((Math.random() * 10000000) + 1);
+                    const errorMessage = $('<div></div>');
                     errorMessage.append('Please correct validation errors in form <br />');
                     errorMessage.append('<i class="fa fa-bug pull-right" aria-hidden="true" data-toggle="collapse" '+
                                         'data-target="#'+detailsid+'" aria-expanded="false" aria-controls="'+detailsid+'"></i>');
@@ -88,7 +89,7 @@ function saveFormToEndpoint(url,formid,callback_ok, disable_dialog) {
                     });
 
                 }
-            } else if ( callback_ok != undefined ) {
+            } else if ( callback_ok !== undefined ) {
                 // execute callback function
                 callback_ok(data);
             }
@@ -102,28 +103,28 @@ function saveFormToEndpoint(url,formid,callback_ok, disable_dialog) {
  * @return promise object, resolves when all are loaded
  */
 function mapDataToFormUI(data_get_map) {
-    var dfObj = new $.Deferred();
+    const dfObj = new $.Deferred();
 
     // calculate number of items for deferred object to resolve
-    var data_map_seq = 1;
-    var data_map_count = 0;
+    let data_map_seq = 1;
+    let data_map_count = 0;
     $.each(data_get_map, function(){
         data_map_count += 1;
     });
 
-    var collected_data = {};
+    const collected_data = {};
     $.each(data_get_map, function(data_index, data_url) {
         ajaxGet(data_url,{}, function(data, status) {
-            if (status == "success") {
-                $("form").each(function( index ) {
-                    if ( $(this).attr('id') && $(this).attr('id').split('-')[0] == data_index) {
+            if (status === "success") {
+                $("form").each(function() {
+                    if ( $(this).attr('id') && $(this).attr('id').split('-')[0] === data_index) {
                         // related form found, load data
                         setFormData($(this).attr('id'), data);
                         collected_data[$(this).attr('id')] = data;
                     }
                 });
             }
-            if (data_map_seq == data_map_count) {
+            if (data_map_seq === data_map_count) {
                 dfObj.resolve(collected_data);
             }
             data_map_seq += 1;
@@ -138,11 +139,11 @@ function mapDataToFormUI(data_get_map) {
  */
 function updateServiceStatusUI(status)
 {
-    var status_html = '<span class="label label-opnsense label-opnsense-sm ';
+    let status_html = '<span class="label label-opnsense label-opnsense-sm ';
 
-    if (status == "running") {
+    if (status === "running") {
         status_html += 'label-success';
-    } else if (status == "stopped") {
+    } else if (status === "stopped") {
         status_html += 'label-danger';
     } else {
         status_html += 'hidden';
@@ -158,17 +159,17 @@ function updateServiceStatusUI(status)
  */
 function updateServiceControlUI(serviceName)
 {
-    ajaxCall("/api/" + serviceName + "/service/status", {}, function(data,status) {
-        var status_html = '<span class="label label-opnsense label-opnsense-sm ';
-        var status_icon = '';
-        var buttons = '';
+    ajaxCall("/api/" + serviceName + "/service/status", {}, function(data) {
+        let status_html = '<span class="label label-opnsense label-opnsense-sm ';
+        let status_icon = '';
+        let buttons = '';
 
-        if (data['status'] == "running") {
+        if (data['status'] === "running") {
             status_html += 'label-success';
             status_icon = 'play';
             buttons += '<span id="restartService" class="btn btn-sm btn-default"><i class="fa fa-refresh fa-fw"></i></span> ';
             buttons += '<span id="stopService" class="btn btn-sm btn-default"><i class="fa fa-stop fa-fw"></span>';
-        } else if (data['status'] == "stopped") {
+        } else if (data['status'] === "stopped") {
             status_html += 'label-danger';
             status_icon = 'stop';
             buttons += '<span id="startService" class="btn btn-sm btn-default"><i class="fa fa-play fa-fw"></i></span>';
@@ -180,13 +181,13 @@ function updateServiceControlUI(serviceName)
 
         $('#service_status_container').html(status_html + " " + buttons);
 
-        var commands = ["start", "restart", "stop"];
+        const commands = ["start", "restart", "stop"];
         commands.forEach(function(command) {
             $("#" + command + "Service").click(function(){
                 $('#OPNsenseStdWaitDialog').modal('show');
-                ajaxCall("/api/" + serviceName + "/service/" + command, {},function(data,status) {
+                ajaxCall("/api/" + serviceName + "/service/" + command, {},function() {
                     $('#OPNsenseStdWaitDialog').modal('hide');
-                    ajaxCall("/api/" + serviceName + "/service/status", {}, function(data,status) {
+                    ajaxCall("/api/" + serviceName + "/service/status", {}, function() {
                         updateServiceControlUI(serviceName);
                     });
                 });
@@ -200,39 +201,38 @@ function updateServiceControlUI(serviceName)
  */
 function formatTokenizersUI() {
     $('select.tokenize').each(function () {
-        var sender = $(this);
+        const sender = $(this);
         if (!sender.hasClass('tokenize2_init_done')) {
             // only init tokenize2 when not bound yet
-            var hint = $(this).data("hint");
-            var width = $(this).data("width");
-            if (sender.data("size") != undefined) {
-                var number_of_items = $(this).data("size");
-            } else {
-                var number_of_items = 10;
+            const hint = $(this).data("hint");
+            const width = $(this).data("width");
+            let number_of_items = 10;
+            if (sender.data("size") !== undefined) {
+                number_of_items = $(this).data("size");
             }
             sender.tokenize2({
                 'tokensAllowCustom': $(this).data("allownew"),
                 'placeholder': hint,
-                'sortable': $(this).data('sortable') == true,
+                'sortable': $(this).data('sortable') === true,
                 'dropdownMaxItems': number_of_items
             });
             sender.parent().find('ul.tokens-container').css("width", width);
 
             // dropdown on focus (previous displayDropdownOnFocus)
-            sender.on('tokenize:select', function(container){
+            sender.on('tokenize:select', function(){
                 $(this).tokenize2().trigger('tokenize:search', [$(this).tokenize2().input.val()]);
             });
             // bind add / remove events
-            sender.on('tokenize:tokens:add', function(e, value){
+            sender.on('tokenize:tokens:add', function(){
                 sender.trigger("tokenize:tokens:change");
             });
-            sender.on('tokenize:tokens:remove', function(e, value){
+            sender.on('tokenize:tokens:remove', function(){
                 sender.trigger("tokenize:tokens:change");
             });
 
             // hook keydown -> tab to blur event
             sender.on('tokenize:deselect', function(){
-                var e = $.Event("keydown");
+                const e = $.Event("keydown");
                 e.keyCode = 9;
                 sender.tokenize2().trigger('tokenize:keydown', [e]);
             });
@@ -243,7 +243,7 @@ function formatTokenizersUI() {
             sender.unbind('tokenize:tokens:change');
 
             // selected items
-            var items = [];
+            const items = [];
             sender.find('option:selected').each(function () {
                 items.push([$(this).val(), $(this).text()]);
             });
@@ -258,7 +258,7 @@ function formatTokenizersUI() {
         }
 
         // propagate token changes to parent change()
-        sender.on('tokenize:tokens:change', function(e, value){
+        sender.on('tokenize:tokens:change', function(){
             sender.change();
         });
 
@@ -271,7 +271,7 @@ function formatTokenizersUI() {
 function addMultiSelectClearUI() {
     $('[id*="clear-options"]').each(function() {
         $(this).click(function() {
-            var id = $(this).attr("id").replace(/_*clear-options_*/, '');
+            const id = $(this).attr("id").replace(/_*clear-options_*/, '');
             BootstrapDialog.confirm({
                 title: 'Deselect or remove all items ?',
                 message: 'Deselect or remove all items ?',
@@ -283,20 +283,21 @@ function addMultiSelectClearUI() {
                 btnOKClass: 'btn-primary',
                 callback: function(result) {
                     if(result) {
-                        if ($('select[id="' + id + '"]').hasClass("tokenize")) {
+                        let element = $('select[id="' + id + '"]');
+                        if (element.hasClass("tokenize")) {
                             // trigger close on all Tokens
-                            $('select[id="' + id + '"]').tokenize2().trigger('tokenize:clear');
-                            $('select[id="' + id + '"]').change();
+                            element.tokenize2().trigger('tokenize:clear');
+                            element.change();
                         } else {
                             // remove options from selection
-                            $('select[id="' + id + '"]').find('option').prop('selected',false);
-                            if ($('select[id="' + id + '"]').hasClass('selectpicker')) {
-                               $('select[id="' + id + '"]').selectpicker('refresh');
+                            element.find('option').prop('selected',false);
+                            if (element.hasClass('selectpicker')) {
+                                element.selectpicker('refresh');
                             }
                         }
                     }
                     // In case this modal was triggered from another modal, fix focus issues
-                    $('.modal').on("hidden.bs.modal", function (e) {
+                    $('.modal').on("hidden.bs.modal", function () {
                         if($('.modal:visible').length)
                         {
                             $('body').addClass('modal-open');
@@ -320,30 +321,30 @@ function initFormHelpUI() {
     });
 
     // handle all help messages show/hide
-    $('[id*="show_all_help"]').click(function(event) {
-        $('[id*="show_all_help"]').toggleClass("fa-toggle-on fa-toggle-off");
-        $('[id*="show_all_help"]').toggleClass("text-success text-danger");
-        if ($('[id*="show_all_help"]').hasClass("fa-toggle-on")) {
+    let elements = $('[id*="show_all_help"]');
+    elements.click(function(event) {
+        elements.toggleClass("fa-toggle-on fa-toggle-off");
+        elements.toggleClass("text-success text-danger");
+        if (elements.hasClass("fa-toggle-on")) {
             if (window.sessionStorage) {
                 sessionStorage.setItem('all_help_preset', 1);
             }
-            $('[data-for*="help_for"]').addClass("show");
-            $('[data-for*="help_for"]').removeClass("hidden");
+            elements.addClass("show");
+            elements.removeClass("hidden");
         } else {
-            $('[data-for*="help_for"]').addClass("hidden");
-            $('[data-for*="help_for"]').removeClass("show");
+            elements.addClass("hidden");
+            elements.removeClass("show");
             if (window.sessionStorage) {
                 sessionStorage.setItem('all_help_preset', 0);
             }
         }
         event.preventDefault();
     });
-    if (window.sessionStorage && sessionStorage.getItem('all_help_preset') == 1) {
+    if (window.sessionStorage && sessionStorage.getItem('all_help_preset') === 1) {
         // show all help messages when preset was stored
-        $('[id*="show_all_help"]').toggleClass("fa-toggle-on fa-toggle-off");
-        $('[id*="show_all_help"]').toggleClass("text-success text-danger");
-        $('[data-for*="help_for"]').addClass("show");
-        $('[data-for*="help_for"]').removeClass("hidden");
+        elements.toggleClass("fa-toggle-on fa-toggle-off");
+        elements.toggleClass("text-success text-danger");
+        $('[data-for*="help_for"]').addClass("show").removeClass("hidden");
     }
 }
 
@@ -351,20 +352,21 @@ function initFormHelpUI() {
  * handle advanced show/hide
  */
 function initFormAdvancedUI() {
-    if (window.sessionStorage && sessionStorage.getItem('show_advanced_preset') == 1) {
+    let elements = $('[id*="show_advanced"]');
+    if (window.sessionStorage && sessionStorage.getItem('show_advanced_preset') === 1) {
         // show advanced options when preset was stored
-        $('[id*="show_advanced"]').toggleClass("fa-toggle-on fa-toggle-off");
-        $('[id*="show_advanced"]').toggleClass("text-success text-danger");
+        elements.toggleClass("fa-toggle-on fa-toggle-off");
+        elements.toggleClass("text-success text-danger");
     } else {
         $('[data-advanced*="true"]').hide(function(){
             $('[data-advanced*="true"]').after("<tr data-advanced='hidden_row'></tr>"); // the table row is added to keep correct table striping
         });
     }
 
-    $('[id*="show_advanced"]').click(function() {
-        $('[id*="show_advanced"]').toggleClass("fa-toggle-on fa-toggle-off");
-        $('[id*="show_advanced"]').toggleClass("text-success text-danger");
-        if ($('[id*="show_advanced"]').hasClass("fa-toggle-on")) {
+    elements.click(function() {
+        elements.toggleClass("fa-toggle-on fa-toggle-off");
+        elements.toggleClass("text-success text-danger");
+        if (elements.hasClass("fa-toggle-on")) {
             $('[data-advanced*="true"]').show();
             $('[data-advanced*="hidden_row"]').remove(); // the table row is deleted to keep correct table striping
             if (window.sessionStorage) {
@@ -383,18 +385,18 @@ function initFormAdvancedUI() {
  * standard dialog when information is required, wrapper around BootstrapDialog
  */
 function stdDialogInform(title, message, close, callback, type, cssClass) {
-     var types = {
-         "danger": BootstrapDialog.TYPE_DANGER,
-         "default": BootstrapDialog.TYPE_DEFAULT,
-         "info": BootstrapDialog.TYPE_INFO,
-         "primary": BootstrapDialog.TYPE_PRIMARY,
-         "success": BootstrapDialog.TYPE_SUCCESS,
-         "warning": BootstrapDialog.TYPE_WARNING
+    const types = {
+        "danger": BootstrapDialog.TYPE_DANGER,
+        "default": BootstrapDialog.TYPE_DEFAULT,
+        "info": BootstrapDialog.TYPE_INFO,
+        "primary": BootstrapDialog.TYPE_PRIMARY,
+        "success": BootstrapDialog.TYPE_SUCCESS,
+        "warning": BootstrapDialog.TYPE_WARNING
     };
     if (!(type in types)) {
         type = 'info';
     }
-    if (cssClass == undefined) {
+    if (cssClass === undefined) {
         cssClass = '';
     }
     BootstrapDialog.show({
@@ -418,13 +420,13 @@ function stdDialogInform(title, message, close, callback, type, cssClass) {
  * standard dialog when confirmation is required, wrapper around BootstrapDialog
  */
 function stdDialogConfirm(title, message, accept, decline, callback, type) {
-     var types = {
-         "danger": BootstrapDialog.TYPE_DANGER,
-         "default": BootstrapDialog.TYPE_DEFAULT,
-         "info": BootstrapDialog.TYPE_INFO,
-         "primary": BootstrapDialog.TYPE_PRIMARY,
-         "success": BootstrapDialog.TYPE_SUCCESS,
-         "warning": BootstrapDialog.TYPE_WARNING
+    const types = {
+        "danger": BootstrapDialog.TYPE_DANGER,
+        "default": BootstrapDialog.TYPE_DEFAULT,
+        "info": BootstrapDialog.TYPE_INFO,
+        "primary": BootstrapDialog.TYPE_PRIMARY,
+        "success": BootstrapDialog.TYPE_SUCCESS,
+        "warning": BootstrapDialog.TYPE_WARNING
     };
     if (!(type in types)) {
         type = 'warning';

commit 93731e4212efa9587f4c5188df00dcf3e1b049e0

while testing https://github.com/opnsense/core/issues/3377, found another issue introduced in https://github.com/opnsense/core/commit/892e4011b6c0c3c08a9b4b7812f7b688e1e0c781 preventing "full help" to be memorize

src/opnsense/www/js/opnsense_ui.js
@@ -334,23 +334,19 @@ function initFormHelpUI() {
             if (window.sessionStorage) {
                 sessionStorage.setItem('all_help_preset', 1);
             }
-            $("a.showhelp").each(function(){
-                $("*[data-for='" + $(this).attr('id') + "']").addClass("show").removeClass("hidden");
-            });
+            $('[data-for*="help_for"]').addClass("show").removeClass("hidden");
         } else {
-            $("a.showhelp").each(function(){
-                $("*[data-for='" + $(this).attr('id') + "']").addClass("hidden").removeClass("show");
-            });
+            $('[data-for*="help_for"]').addClass("hidden").removeClass("show");
             if (window.sessionStorage) {
                 sessionStorage.setItem('all_help_preset', 0);
             }
         }
         event.preventDefault();
     });
-    if (window.sessionStorage && sessionStorage.getItem('all_help_preset') === 1) {
+
+    if (window.sessionStorage && sessionStorage.getItem('all_help_preset') === "1") {
         // show all help messages when preset was stored
-        elements.toggleClass("fa-toggle-on fa-toggle-off");
-        elements.toggleClass("text-success text-danger");
+        elements.toggleClass("fa-toggle-on fa-toggle-off").toggleClass("text-success text-danger");
         $('[data-for*="help_for"]').addClass("show").removeClass("hidden");
     }
 }