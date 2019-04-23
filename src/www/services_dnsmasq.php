2c6e45fb9d9733c546fd7bd5769e6ac925199bee
Franco Fichtner <franco@opnsense.org>
2019-04-23 12:25:06 +0200

    dnsmasq: also looks safe to drop hardcoded services include
    The code has come a long way...

--- src/www/services_dnsmasq.php
+++ src/www/services_dnsmasq.php
@@ -32,6 +32,7 @@ require_once("interfaces.inc");
 require_once("filter.inc");
 require_once("services.inc");
 require_once("system.inc");
+require_once("plugins.inc.d/dnsmasq.inc");
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $pconfig = array();
