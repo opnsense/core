48244819b542f807886c61cf41a053e032505df4
Franco Fichtner <franco@opnsense.org>
2019-05-11 10:20:53 +0200

    system: tweak this message further, 'page-all' is not visible in the GUI
    "All pages" is there, but this may get lost in translation.

--- src/www/vpn_openvpn_server.php
+++ src/www/vpn_openvpn_server.php
@@ -343,7 +343,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         }
         $prev_opt = (isset($id) && !empty($a_server[$id])) ? $a_server[$id]['custom_options'] : "";
         if ($prev_opt != str_replace("\r\n", "\n", $pconfig['custom_options']) && !userIsAdmin($_SESSION['Username'])) {
-            $input_errors[] = gettext("Advanced options may only be edited by admins (role page-all), due to the increased possibility of privilege escalation.");
+            $input_errors[] = gettext('Advanced options may only be edited by system administrators due to the increased possibility of privilege escalation.');
         }
 
         do_input_validation($pconfig, $reqdfields, $reqdfieldsn, $input_errors);
