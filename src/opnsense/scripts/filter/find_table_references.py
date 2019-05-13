bfdbc02d3fbc4615fc98c90be7a5b251af74769f
Ad Schellevis <ad@opnsense.org>
2019-05-13 13:00:49 +0200

    python3, use symlink in already converted scripts

--- src/opnsense/scripts/filter/find_table_references.py
+++ src/opnsense/scripts/filter/find_table_references.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.6
+#!/usr/local/bin/python3
 
 """
     Copyright (c) 2018-2019 Deciso B.V.
--- src/opnsense/service/configd.py
+++ src/opnsense/service/configd.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.6
+#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
 """
