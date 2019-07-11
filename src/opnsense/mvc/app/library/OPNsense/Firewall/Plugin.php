29b383c33b40196177e171d1575109ba684b6767
Ad Schellevis <ad@opnsense.org>
2019-07-11 22:00:43 +0200

    filter/performance, improve loading speed by parsing legacy_interfaces_details() output through the plugin system. move verbose output while here. for https://github.com/opnsense/core/issues/3567

--- src/opnsense/mvc/app/library/OPNsense/Firewall/Plugin.php
+++ src/opnsense/mvc/app/library/OPNsense/Firewall/Plugin.php
@@ -44,6 +44,7 @@ class Plugin
     private $gatewayMapping = array();
     private $systemDefaults = array();
     private $tables = array();
+    private $ifconfigDetails = array();
 
     /**
      * init firewall plugin component
@@ -196,6 +197,24 @@ class Plugin
             }
         }
     }
+
+    /**
+     * link parsed ifconfig output
+     * @param array $ifconfig from legacy_interfaces_details()
+     */
+    public function setIfconfigDetails($ifconfig)
+    {
+        $this->ifconfigDetails = $ifconfig;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIfconfigDetails()
+    {
+        return $this->ifconfigDetails;
+    }
+
 
     /**
      * register anchor
