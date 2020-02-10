d75cae031af1b8594fcdd752159457aa749b7e4e
Franco Fichtner <franco@opnsense.org>
2020-02-10 20:24:07 +0100

    interfaces: turn dhcpd function into generic function #3310

--- src/www/services_dhcpv6.php
+++ src/www/services_dhcpv6.php
@@ -321,7 +321,7 @@ legacy_html_escape_form_data($pconfig);
 
 include("head.inc");
 
-list ($wifcfgip, $networkv6) = dhcpd_getaddr6($if);
+list ($wifcfgip, $networkv6) = interfaces_primary_address6($if);
 $wifcfgsn = explode('/', $networkv6)[1];
 
 if (isset($config['interfaces'][$if]['dhcpd6track6allowoverride'])) {
