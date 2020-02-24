8325d1716c53d8a45aaf5012a5da8f3bdc842310
Ad Schellevis <ad@opnsense.org>
2020-02-24 13:41:37 +0100

    MVC: add  LegacyLinkField as described in https://github.com/opnsense/docs/commit/a5b31777749395fb53ef5d05f2a818e0f6515011
    - acts as a link to legacy configuration values, which eases migration in some scenarios
    
    (cherry picked from commit e5831a7df17c1864a9d4620e72164b634cf9abaa)

---
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/LegacyLinkField.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *    Copyright (C) 2020 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\Base\FieldTypes;
+
+use OPNsense\Core\Config;
+
+/**
+ * Class LegacyLinkField field, read only referal to a config item in the legacy configuration.
+ * @package OPNsense\Base\FieldTypes
+ */
+class LegacyLinkField extends BaseField
+{
+
+    /**
+     * @var null source referral (e.g. root.node.item)
+     */
+    private $internalSourceLink = null;
+
+    /**
+     * @var bool marks if this is a data node or a container
+     */
+    protected $internalIsContainer = false;
+
+    /**
+     * reflect getter to (legacy) config data
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        if (!empty($this->internalSourceLink)) {
+            $cnf = Config::getInstance()->object();
+            foreach (explode(".", $this->internalSourceLink) as $fieldname) {
+                if (isset($cnf->$fieldname)) {
+                    $cnf = $cnf->$fieldname;
+                } else {
+                    return null;
+                }
+            }
+            return (string)$cnf;
+        } else {
+            return null;
+        }
+    }
+
+
+    /**
+     * read only reference, keep content empty
+     * {@inheritdoc}
+     */
+    public function setValue($value)
+    {
+        return;
+    }
+
+    /**
+     * set source location for this node
+     * @param string $source reference like root.node.item
+     */
+    public function setSource($source)
+    {
+        $this->internalSourceLink = $source;
+    }
+}
