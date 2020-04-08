0dd01c280746a85c926164ef1e74cb840054b526
Ad Schellevis <ad@opnsense.org>
2020-04-08 06:12:27 +0200

    MVC: catch ArgumentCountError as these derive from Error and not the general Exception type. Maybe we might need to add "Error" as well, but the argument count is pretty generic and is easy to trigger.
    Inspired by https://github.com/opnsense/core/issues/4031

--- src/opnsense/www/api.php
+++ src/opnsense/www/api.php
@@ -24,7 +24,7 @@ try {
     $application = new \Phalcon\Mvc\Application($di);
 
     echo $application->handle()->getContent();
-} catch (\Exception $e) {
+} catch (Exception $e) {
     $response = array();
     $response['errorMessage'] = $e->getMessage();
     if (method_exists($e, 'getTitle')) {
@@ -33,6 +33,12 @@ try {
         $response['errorTitle'] = gettext("An API exception occured");
         error_log($e);
     }
+    header('HTTP', true, 500);
+    header("Content-Type: application/json;charset=utf-8");
+    echo json_encode($response, JSON_UNESCAPED_SLASHES);
+} catch (ArgumentCountError $e) {
+    error_log($e);
+    $response = ['errorMessage' => 'endpoint parameter mismatch', 'errorTitle' => gettext("An API exception occured")];
     header('HTTP', true, 500);
     header("Content-Type: application/json;charset=utf-8");
     echo json_encode($response, JSON_UNESCAPED_SLASHES);
