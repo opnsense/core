305d6b35ccfd9feb28ac1dfa4f9ac9b2e30fd31b
Franco Fichtner <franco@opnsense.org>
2020-08-28 12:25:02 +0200

    system: popular temperature topic changes
    * Use priv-sep script to fetch values via configd
    * Use grep on variable names only to avoid false positives
    * Switch the RRD script read to unify temperature reads
    * Use "=" in the script output to match sysctl input

---
+++ src/opnsense/scripts/system/temperature.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Copyright (C) 2020 Franco Fichtner <franco@opnsense.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+CMD=${1}
+
+if [ "${CMD}" = 'rrd' ]; then
+	sysctl -n dev.cpu.0.temperature | sed 's/C//g'
+else
+	# The grep is opportunistic, but at least we only grep the
+	# variable names and not their content at the same time.
+	sysctl -e $(sysctl -aN | grep temperature) | sort
+fi
