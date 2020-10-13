21ef60cf375f5fef55f9dd4b29fb76018481b00a
Ad Schellevis <ad@opnsense.org>
2020-10-13 22:18:56 +0200

    firewall_nat_edit.php fix "PHP Warning:  in_array() expects parameter 2 to be array, null given in /usr/local/www/firewall_nat_edit.php on line 542" when interface is missing, closes https://github.com/opnsense/core/pull/4407
    (cherry picked from commit 3dc47c23a34341d08aece95023c53b30b872bb4a)

--- src/www/firewall_nat_edit.php
+++ src/www/firewall_nat_edit.php
@@ -539,7 +539,7 @@ $( document ).ready(function() {
                       <select name="interface[]" class="selectpicker" data-width="auto" data-live-search="true" multiple="multiple">
 <?php
                         foreach (legacy_config_get_interfaces(array("enable" => true)) as $iface => $ifdetail): ?>
-                        <option value="<?=$iface;?>" <?= in_array($iface, $pconfig['interface']) ? "selected=\"selected\"" : ""; ?>>
+                        <option value="<?=$iface;?>" <?= in_array($iface, $pconfig['interface'] ?? []) ? "selected=\"selected\"" : ""; ?>>
                           <?=htmlspecialchars($ifdetail['descr']);?>
                         </option>
                         <?php endforeach; ?>
