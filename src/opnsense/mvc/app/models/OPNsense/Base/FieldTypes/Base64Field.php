3ee8e907fea2a64b96cde04728eea8a65ff6e5f8
Ad Schellevis <ad@opnsense.org>
2020-10-22 20:30:28 +0200

    add Base64Field type, closes https://github.com/opnsense/core/issues/4398

---
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/Base64Field.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ *    Copyright (C) 2020 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\Base\FieldTypes;
+
+use OPNsense\Base\Validators\CallbackValidator;
+
+/**
+ * Class Base64Field
+ */
+class Base64Field extends TextField
+{
+    protected $internalValidationMessage = "invalid base64 encoded string";
+
+    /**
+     * @param string $value to validate
+     * @return array messages
+     */
+    private function validateBase64($value)
+    {
+        $messages = [];
+        if (base64_encode(base64_decode($value, true)) != str_replace("\n", "", $value)) {
+            $messages[] = $this->internalValidationMessage;
+        }
+        return $messages;
+    }
+
+    /**
+     * retrieve field validators for this field type
+     * @return array
+     */
+    public function getValidators()
+    {
+        $validators = parent::getValidators();
+        if ($this->internalValue != null) {
+            $validators[] = new CallbackValidator(["callback" => function ($data) {
+                return $this->validateBase64($data);
+            }
+            ]);
+        }
+        return $validators;
+    }
+
+}
