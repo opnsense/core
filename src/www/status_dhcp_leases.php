ff7004931c1dc0e2de0c5534ffa30c7e292e80f7
vnxme <46669194+vnxme@users.noreply.github.com>
2020-12-14 15:17:33 +0300

    DHCPv4 leases status: Fix sorting
    Use ipcmp() instead of strnatcasecmp() for sorting by IPv4.

--- src/www/status_dhcp_leases.php
+++ src/www/status_dhcp_leases.php
@@ -248,9 +248,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     usort($leases,
         function ($a, $b) use ($order) {
-            $cmp = strnatcasecmp($a[$order], $b[$order]);
+            $cmp = ($order === 'ip') ? 0 : strnatcasecmp($a[$order], $b[$order]);
             if ($cmp === 0) {
-                $cmp = strnatcasecmp($a['ip'], $b['ip']);
+                $cmp = ipcmp($a['ip'], $b['ip']);
             }
             return $cmp;
         }
