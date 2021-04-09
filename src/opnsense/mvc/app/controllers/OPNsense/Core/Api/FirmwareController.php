3186100a5bbc3af4de8a9bc7ae4ee7b1cc055008
Franco Fichtner <franco@opnsense.org>
2021-04-09 14:18:26 +0200

    firmware: if upstream package is missing complain; closes #4906
    This points to a configuration error that should be resolved one
    way or another.
    
    (cherry picked from commit 35217ba0d16e0d831d500a465d3d267409dfe5f6)
    (cherry picked from commit 0f3fcd2b0575f8ea6256db287008c93ceb6ee0c9)

--- src/opnsense/mvc/app/controllers/OPNsense/Core/Api/FirmwareController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Core/Api/FirmwareController.php
@@ -92,7 +92,6 @@ class FirmwareController extends ApiControllerBase
         $active_size = '';
         $active_status = '';
         $backend = new Backend();
-        $target = null;
 
         $this->sessionClose(); // long running action, close session
 
@@ -104,10 +103,6 @@ class FirmwareController extends ApiControllerBase
         $response = json_decode(trim($backend->configdRun('firmware product')), true);
         if ($response != null && $response['product_check'] != null) {
             $response = $response['product_check'];
-
-            if ($response['product_id'] != $response['product_target']) {
-                $target = $response['product_target'];
-            }
 
             $download_size = !empty($response['download_size']) ? $response['download_size'] : 0;
 
@@ -271,8 +266,8 @@ class FirmwareController extends ApiControllerBase
             } elseif (array_key_exists('repository', $response) && $response['repository'] == 'unsigned') {
                 $response['status_msg'] = gettext('The repository has no fingerprint.');
                 $response['status'] = 'error';
-            } elseif (array_key_exists('repository', $response) && $response['repository'] == 'incomplete' && !empty($target)) {
-                $response['status_msg'] = sprintf(gettext('The release type "%s" is not available on this repository.'), $target);
+            } elseif (array_key_exists('repository', $response) && $response['repository'] == 'incomplete') {
+                $response['status_msg'] = sprintf(gettext('The release type "%s" is not available on this repository.'), $response['product_target']);
                 $response['status'] = 'error';
             } elseif (array_key_exists('repository', $response) && $response['repository'] != 'ok') {
                 $response['status_msg'] = gettext('Could not find the repository on the selected mirror.');
