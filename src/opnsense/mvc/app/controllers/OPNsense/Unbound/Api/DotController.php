3b8c1b5cfc79d8851c9848213c70b528069863a1
Michael <m.muenz@gmail.com>
2021-07-17 12:05:58 +0200

    Create DotController.php

---
+++ src/opnsense/mvc/app/controllers/OPNsense/Unbound/Api/DotController.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *    Copyright (C) 2020 Michael Muenz <m.muenz@gmail.com>
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\Unbound\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+
+class DotController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'dot';
+    protected static $internalModelClass = '\OPNsense\Unbound\Dot';
+
+    public function searchDotAction()
+    {
+        return $this->searchBase('dots.dot', array("enable", "server", "port", "verify"));
+    }
+    public function getDotAction($uuid = null)
+    {
+        $this->sessionClose();
+        return $this->getBase('dot', 'dots.dot', $uuid);
+    }
+    public function addDotAction()
+    {
+        return $this->addBase('dot', 'dots.dot');
+    }
+    public function delDotAction($uuid)
+    {
+        return $this->delBase('dots.dot', $uuid);
+    }
+    public function setDotAction($uuid)
+    {
+        return $this->setBase('dot', 'dots.dot', $uuid);
+    }
+    public function toggleDotAction($uuid)
+    {
+        return $this->toggleBase('dots.dot', $uuid);
+    }
+}
