41b311ecc28520757b5cc38ccd3999d842e4306f
Ad Schellevis <ad@opnsense.org>
2021-07-30 18:45:40 +0200

    Revert "Firewall / Aliases - append most likely cause for pfctl error (Invalid argument isn't very explanatory by itself). closes https://github.com/opnsense/core/issues/5127"
    This reverts commit 2ea90801290856f453126b1e6d63e66c79a5ca6f.

--- src/opnsense/scripts/filter/update_tables.py
+++ src/opnsense/scripts/filter/update_tables.py
@@ -179,7 +179,4 @@ if __name__ == '__main__':
         for filename in to_remove[aliasname]:
             os.remove(filename)
 
-    if 'messages' in result and ''.join(result['messages']).find('Invalid argument') > -1:
-        result['messages'].append("(net.pf.request_maxcount likely undersized in system tunables)")
-
     print (json.dumps(result))
