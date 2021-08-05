9506f4c1c6af909bd05946490d700a847496b9cd
Franco Fichtner <franco@opnsense.org>
2021-08-05 14:27:09 +0200

    firmware: now that we use TEE we should define it #5136

--- src/opnsense/scripts/firmware/sync.subr.sh
+++ src/opnsense/scripts/firmware/sync.subr.sh
@@ -24,8 +24,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-LOCKFILE=/tmp/pkg_upgrade.progress
-MUSTCHECK=yes
+LOCKFILE="/tmp/pkg_upgrade.progress"
+MUSTCHECK="yes"
+TEE="/usr/bin/tee -a"
 
 for PACKAGE in $(/usr/local/sbin/pluginctl -g system.firmware.plugins | \
     /usr/bin/sed 's/,/ /g'); do
