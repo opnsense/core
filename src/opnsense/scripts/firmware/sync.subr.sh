7140e9e6c5c621bb45fcb00401d57f5125dadbc6
Franco Fichtner <franco@opnsense.org>
2021-08-05 13:05:07 +0200

    firmware: visibility issue on #5136
    (cherry picked from commit af7543e79b7c656dc8cb30cdfb1fff75e0ab4e90)
    (cherry picked from commit 9506f4c1c6af909bd05946490d700a847496b9cd)

--- src/opnsense/scripts/firmware/sync.subr.sh
+++ src/opnsense/scripts/firmware/sync.subr.sh
@@ -24,8 +24,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-LOCKFILE=/tmp/pkg_upgrade.progress
-MUSTCHECK=yes
+LOCKFILE="/tmp/pkg_upgrade.progress"
+MUSTCHECK="yes"
+TEE="/usr/bin/tee -a"
 
 for PACKAGE in $(/usr/local/sbin/pluginctl -g system.firmware.plugins | \
     /usr/bin/sed 's/,/ /g'); do
@@ -37,18 +38,18 @@ for PACKAGE in $(/usr/local/sbin/pluginctl -g system.firmware.plugins | \
 
 			# plugins must pass a version check on up-to-date core package
 			if [ "$(pkg version -t ${COREVER} ${REPOVER})" = "<" ]; then
-				echo "Installation out of date. The update to ${COREPKG}-${REPOVER} is required." >> ${LOCKFILE} 2>&1
+				echo "Installation out of date. The update to ${COREPKG}-${REPOVER} is required." | ${TEE} ${LOCKFILE}
 				break
 			fi
 
 			MUSTCHECK=
 		fi
 
-		pkg install -y ${PACKAGE} >> ${LOCKFILE} 2>&1
-		/usr/local/opnsense/scripts/firmware/register.php install ${PACKAGE} >> ${LOCKFILE} 2>&1
+		(pkg install -y ${PACKAGE} 2>&1) | ${TEE} ${LOCKFILE}
+		(/usr/local/opnsense/scripts/firmware/register.php install ${PACKAGE} 2>&1) | ${TEE} ${LOCKFILE}
 	fi
 done
 
 if [ -z "${MUSTCHECK}" ]; then
-	pkg autoremove -y >> ${LOCKFILE} 2>&1
+	(pkg autoremove -y 2>&1) | ${TEE} ${LOCKFILE}
 fi
