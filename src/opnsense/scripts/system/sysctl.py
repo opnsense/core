1ddeeca10878014410bace28a5cf5cbdad4d84b0
Franco Fichtner <franco@opnsense.org>
2021-10-26 10:03:06 +0200

    system: assorted improvements
    o Only modify default sysctls when default is known
    o Let user know a default is not available for tunable
    o Parse system descripton and type and show in GUI

---
+++ src/opnsense/scripts/system/sysctl.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python3
+
+"""
+    Copyright (c) 2021 Franco Fichtner <franco@opnsense.org>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+    --------------------------------------------------------------------------------------
+    return current sysctl(8) information
+"""
+
+import subprocess
+import ujson
+
+# type mapping: r => read-only, t => boot-time, w => runtime
+
+if __name__ == '__main__':
+    result = {}
+    sp = subprocess.run(['/sbin/sysctl', '-a'], capture_output=True, text=True)
+    for line in sp.stdout.split("\n"):
+        parts = line.strip().split(": ", 1)
+        if len(parts) > 1:
+            item = {'name': parts[0], 'value': parts[1], 'type': 'r', 'description': ''}
+            result[parts[0]] = item
+    sp = subprocess.run(['/sbin/sysctl', '-ad'], capture_output=True, text=True)
+    for line in sp.stdout.split("\n"):
+        parts = line.strip().split(": ", 1)
+        if len(parts) > 1:
+            if parts[0] in result:
+                result[parts[0]].update({'description': parts[1]})
+    sp = subprocess.run(['/sbin/sysctl', '-aNT'], capture_output=True, text=True)
+    for line in sp.stdout.split("\n"):
+        part = line.strip()
+        if part in result:
+            result[part].update({'type': 't'})
+    sp = subprocess.run(['/sbin/sysctl', '-aNW'], capture_output=True, text=True)
+    for line in sp.stdout.split("\n"):
+        part = line.strip()
+        if part in result:
+            result[part].update({'type': 'w'})
+    print (ujson.dumps(result))
