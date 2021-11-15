20acfaccd7cc384790e2cac5e38920c9e9153438
Franco Fichtner <franco@opnsense.org>
2021-11-15 13:35:14 +0100

    firmware: fix prompt hint
    (cherry picked from commit 2a4c3a4869446a016675da4e2d9a5f20100eb2cd)

--- src/opnsense/scripts/firmware/check.sh
+++ src/opnsense/scripts/firmware/check.sh
@@ -330,8 +330,8 @@ fi
 
 packages_is_size="$(opnsense-update -SRp)"
 if [ -n "${packages_is_size}" ]; then
-    upgrade_major_message=$(cat /usr/local/opnsense/data/firmware/upgrade.html 2> /dev/null | sed 's/"/\\&/g' | tr '\n' ' ')
     upgrade_major_version=$(opnsense-update -vR)
+    upgrade_major_message=$(sed -e 's/"/\\&/g' -e "s/%%UPGRADE_RELEASE%%/${upgrade_major_version}/g" /usr/local/opnsense/data/firmware/upgrade.html 2> /dev/null | tr '\n' ' ')
     upgrade_needs_reboot="1"
     sets_upgraded="{\"name\":\"packages\",\"size\":\"${packages_is_size}\",\"current_version\":\"${product_version}\",\"new_version\":\"${upgrade_major_version}\",\"repository\":\"${product_repo}\"}"
 
