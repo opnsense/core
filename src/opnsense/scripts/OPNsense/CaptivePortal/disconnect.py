d74bc04ac907b484202d7c3d3fc1ea32b8ad4637
Ad Schellevis <ad@opnsense.org>
2022-01-20 18:22:51 +0100

    Services / Captiveportal - prevent session removal crashing out when we there's no IP address registered
    (cherry picked from commit cebee292d268425449a520ef99bf46bbffbcc3a2)

--- src/opnsense/scripts/OPNsense/CaptivePortal/disconnect.py
+++ src/opnsense/scripts/OPNsense/CaptivePortal/disconnect.py
@@ -49,7 +49,8 @@ if parameters['sessionid'] is not None and parameters['zoneid'] is not None:
     # remove client
     client_session_info = DB().del_client(parameters['zoneid'], parameters['sessionid'])
     if client_session_info is not None:
-        IPFW().delete(parameters['zoneid'], client_session_info['ip_address'])
+        if client_session_info['ip_address']:
+            IPFW().delete(parameters['zoneid'], client_session_info['ip_address'])
         client_session_info['terminateCause'] = 'User-Request'
         response = client_session_info
 
