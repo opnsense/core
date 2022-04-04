c523355085391cd98a68ff85850e11fe66fa3e29
Franco Fichtner <franco@opnsense.org>
2022-04-04 21:42:19 +0200

    interfaces: exclude existing VLANs and load from model instead

--- src/opnsense/mvc/app/models/OPNsense/Interfaces/FieldTypes/VlanInterfaceField.php
+++ src/opnsense/mvc/app/models/OPNsense/Interfaces/FieldTypes/VlanInterfaceField.php
@@ -67,25 +67,19 @@ class VlanInterfaceField extends BaseListField
                             $details['macaddr'],
                             !empty($ifnames[$ifname]) ? $ifnames[$ifname] : ""
                         );
-                    } else {
-                        self::$interface_devices[$ifname] = sprintf(
-                            "%s (%s) [%s]",
-                            $ifname,
-                            sprintf("tag: %s, if: %s", $details['vlan']['tag'], $details['vlan']['parent']),
-                            !empty($ifnames[$ifname]) ? $ifnames[$ifname] : ""
-                        );
                     }
                 }
             }
             // append not yet applied vlan interfaces
             foreach ($this->getParentModel()->vlan->iterateItems() as $key => $vlan) {
-                if (strpos((string)$vlan->vlanif, "qinq") !== 0 && !isset(self::$interface_devices[(string)$vlan->vlanif])) {
-                    self::$interface_devices[(string)$vlan->vlanif] = sprintf(
-                        "%s (%s) []",
-                        (string)$vlan->vlanif,
-                        sprintf("tag: %s, if: %s", (string)$vlan->tag, (string)$vlan->if)
-                    );
+                if (strpos((string)$vlan->vlanif, 'qinq') === 0) {
+                    continue;
                 }
+                self::$interface_devices[(string)$vlan->vlanif] = sprintf(
+                    gettext('%s (Tag: %s, Parent: %s) [%s]'),
+                    (string)$vlan->vlanif, (string)$vlan->tag, (string)$vlan->if,
+		    !empty($ifnames[(string)$vlan->vlanif]) ? $ifnames[(string)$vlan->vlanif] : ''
+                );
             }
         }
         $this->internalOptionList = self::$interface_devices;
