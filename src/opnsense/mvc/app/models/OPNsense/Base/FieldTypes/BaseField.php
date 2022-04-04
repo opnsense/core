bdc99bf8975381e80687fb7a90ae8fe818c5f5b8
oittaa <8972248+oittaa@users.noreply.github.com>
2022-04-04 23:12:15 +0200

    Fix UUID generation
    mt_rand() is in not suitable for UUID generation.

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -141,17 +141,10 @@ abstract class BaseField
      */
     public function generateUUID()
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     /**
