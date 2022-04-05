c41e28ae552ec7808e059ae37321d6b2b7a8db5b
Franco Fichtner <franco@opnsense.org>
2022-04-05 09:47:13 +0200

    system: set up DNS system routes from system_resolvconf_generate()
    Assorted changes for Unbound forwarding/overrides rework included.

--- src/opnsense/scripts/interfaces/ppp-linkup.sh
+++ src/opnsense/scripts/interfaces/ppp-linkup.sh
@@ -12,15 +12,11 @@ if [ "${2}" = "inet" ]; then
 	if echo "${6}" | grep -q dns1; then
 		DNS1=$(echo "${6}" | awk '{print $2}')
 		echo "${DNS1}" >> /tmp/${1}_nameserver
-		route delete "${DNS1}" ${4}
-		route add "${DNS1}" ${4}
 	fi
 
 	if echo "${7}" | grep -q dns2; then
 		DNS2=$(echo "${7}" | awk '{print $2}')
 		echo "${DNS2}" >> /tmp/${1}_nameserver
-		route delete "${DNS2}" ${4}
-		route add "${DNS2}" ${4}
 	fi
 
 	/usr/local/sbin/configctl -d interface newip ${1}
@@ -34,15 +30,11 @@ elif [ "${2}" = "inet6" ]; then
 	if echo "${6}" | grep -q dns1; then
 		DNS1=$(echo "${6}" | awk '{print $2}')
 		echo "${DNS1}" >> /tmp/${1}_nameserverv6
-		route delete -inet6 "${DNS1}" ${4}
-		route add -inet6 "${DNS1}" ${4}
 	fi
 
 	if echo "${7}" | grep -q dns2; then
 		DNS2=$(echo "${7}" | awk '{print $2}')
 		echo "${DNS2}" >> /tmp/${1}_nameserverv6
-		route delete -inet6 "${DNS2}" ${4}
-		route add -inet6 "${DNS2}" ${4}
 	fi
 
 	/usr/local/sbin/configctl -d interface newipv6 ${1}
