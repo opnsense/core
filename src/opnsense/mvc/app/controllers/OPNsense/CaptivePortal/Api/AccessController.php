2414ed3955004d7d31e06b858f2b839f830f4589
Franco Fichtner <franco@opnsense.org>
2022-04-06 09:23:00 +0200

    src: fix typo and sync LICENSE

--- src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/Api/AccessController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/Api/AccessController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 20152-2022 Deciso B.V.
+ * Copyright (C) 2015-2022 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
