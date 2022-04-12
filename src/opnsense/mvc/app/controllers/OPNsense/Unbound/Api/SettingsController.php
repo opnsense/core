b5b7670b487ce5604a066781433faa8a0d7f361c
Stephan de Wit <stephan.dewt@yahoo.co.uk>
2022-04-12 10:33:15 +0200

    Unbound: sort on host/domain name for overrides

--- src/opnsense/mvc/app/controllers/OPNsense/Unbound/Api/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Unbound/Api/SettingsController.php
@@ -126,7 +126,7 @@ class SettingsController extends ApiMutableModelControllerBase
         return $this->searchBase(
             'hosts.host',
             ['enabled', 'hostname', 'domain', 'rr', 'mxprio', 'mx', 'server', 'description'],
-            "sequence",
+            'hostname',
             null,
             SORT_NATURAL | SORT_FLAG_CASE
         );
@@ -179,7 +179,7 @@ class SettingsController extends ApiMutableModelControllerBase
         return $this->searchBase(
             'aliases.alias',
             ['enabled', 'host', 'hostname', 'domain', 'description'],
-            "sequence",
+            "hostname",
             $filter_func,
             SORT_NATURAL | SORT_FLAG_CASE
         );
@@ -228,7 +228,7 @@ class SettingsController extends ApiMutableModelControllerBase
         return $this->searchBase(
             'domains.domain',
             ['enabled', 'domain', 'server', 'description'],
-            "sequence",
+            "domain",
             null,
             SORT_NATURAL | SORT_FLAG_CASE
         );
