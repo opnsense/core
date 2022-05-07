416687de49d8cf3a54f9d34e748fea56d00b478b
Ad Schellevis <ad@opnsense.org>
2022-05-07 20:35:15 +0200

    VPN / IPsec - mobile property (boolean) duplication in phase 2, closes https://github.com/opnsense/core/issues/5598
    do not store "mobile" flag on phase 2 entries, as phase 1 is responsible here.

--- src/www/vpn_ipsec_phase2.php
+++ src/www/vpn_ipsec_phase2.php
@@ -166,10 +166,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $pconfig['ealgos'] = [];
         }
 
-        if (isset($config['ipsec']['phase2'][$p2index]['mobile'])) {
-            $pconfig['mobile'] = true;
-        }
-
         if (!empty($_GET['dup'])) {
             $pconfig['uniqid'] = uniqid();
         }
@@ -187,20 +183,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $pconfig['lifetime'] = "3600";
         $pconfig['uniqid'] = uniqid();
 
-        /* mobile client */
-        foreach ($config['ipsec']['phase1'] as $phase1ent) {
-            if ($phase1ent['ikeid'] == $pconfig['ikeid'] && isset($phase1ent['mobile'])) {
-                $pconfig['mobile'] = true;
-                break;
-            }
-        }
-
         // init empty
         foreach (explode(",", $phase2_fields) as $fieldname) {
             $fieldname = trim($fieldname);
             if (!isset($pconfig[$fieldname])) {
                 $pconfig[$fieldname] = null;
             }
+        }
+    }
+    /* mobile client */
+    foreach ($config['ipsec']['phase1'] as $phase1ent) {
+        if ($phase1ent['ikeid'] == $pconfig['ikeid'] && isset($phase1ent['mobile'])) {
+            $pconfig['mobile'] = true;
+            break;
         }
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -427,9 +422,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             unset($ph2ent['hash-algorithm-option']);
         }
 
-        if (isset($pconfig['mobile'])) {
-            $ph2ent['mobile'] = true;
-        }
         // attach or generate reqid
         if ($p2index !== null && !empty($config['ipsec']['phase2'][$p2index]['reqid'])) {
             $ph2ent['reqid'] = $config['ipsec']['phase2'][$p2index]['reqid'];
