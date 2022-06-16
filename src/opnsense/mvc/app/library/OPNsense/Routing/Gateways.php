82b11d3b5fc10a5d87afd43c8ace00611814e68c
Franco Fichtner <franco@opnsense.org>
2022-06-16 14:52:02 +0200

    interfaces: fix most common undefined access messages
    ipaddr(v6) is not set when not configured which makes sense
    but PHP wants us to acknowlege that we really want nothing
    when the value doesn't exist.  Stuffing it through the parse_config()
    was another idea but in practice in legacy code null equals
    <sometag/> and '' equals <sometag></sometag> so if we prime both
    fields using null we write them back as empty which isn't bad per
    se but also best avoided.

--- src/opnsense/mvc/app/library/OPNsense/Routing/Gateways.php
+++ src/opnsense/mvc/app/library/OPNsense/Routing/Gateways.php
@@ -200,7 +200,7 @@ class Gateways
                 foreach (["inet", "inet6"] as $ipproto) {
                     // filename suffix and interface type as defined in the interface
                     $descr = !empty($ifcfg['descr']) ? $ifcfg['descr'] : $ifname;
-                    $realif = $ipproto == 'inet6' && in_array($ifcfg['ipaddrv6'], ['6to4', '6rd']) ? "{$ifname}_stf" : $ifcfg['if'];
+                    $realif = $ipproto == 'inet6' && in_array($ifcfg['ipaddrv6'] ?? null, ['6to4', '6rd']) ? "{$ifname}_stf" : $ifcfg['if'];
                     $fsuffix = $ipproto == "inet6" ? "v6" : "";
                     $ctype = self::convertType($ipproto, $ifcfg);
                     $ctype = $ctype != null ? $ctype : "GW";
@@ -258,7 +258,7 @@ class Gateways
                         $this->cached_gateways[$gwkey] = $thisconf;
                     } elseif (
                         $ipproto == 'inet6'
-                            && in_array($ifcfg['ipaddrv6'], array('slaac', 'dhcp6', '6to4', '6rd'))
+                            && in_array($ifcfg['ipaddrv6'] ?? null, ['slaac', 'dhcp6', '6to4', '6rd'])
                     ) {
                         // Dynamic IPv6 interface, but no router solicit response received using rtsold.
                         $gwkey = $this->newKey($thisconf['priority'], !empty($thisconf['defaultgw']));
