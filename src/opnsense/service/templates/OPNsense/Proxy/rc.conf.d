ca6db1001d22a5b9236d372e3ca5ab2bc1246bc5
Franco Fichtner <franco@opnsense.org>
2022-08-01 09:53:45 +0200

    rc: use new _setup script feature #5917
    Also see: https://github.com/opnsense/src/commit/3edcfbc578f
    
    (cherry picked from commit af959da8b6384f2aad0422c1289f3177d90bb5cc)

--- src/opnsense/service/templates/OPNsense/Proxy/rc.conf.d
+++ src/opnsense/service/templates/OPNsense/Proxy/rc.conf.d
@@ -1,5 +1,5 @@
 {% if helpers.exists('OPNsense.proxy.general.enabled') and OPNsense.proxy.general.enabled|default("0") == "1" %}
-squid_var_script="/usr/local/opnsense/scripts/proxy/setup.sh"
+squid_setup="/usr/local/opnsense/scripts/proxy/setup.sh"
 squid_enable="YES"
 {% else %}
 squid_enable="NO"
