8436d3374887e8cc7c153afbecdda8860cbdd628
Franco Fichtner <franco@opnsense.org>
2022-08-12 12:12:35 +0200

    interfaces: extend utility of ifctl
    Before we add ifctl use everywhere update the utility and
    manual page to the latest code.
    
    Replace a single file read use with ifctl as an example
    of things to come.

--- src/opnsense/scripts/interfaces/ifctl.sh
+++ src/opnsense/scripts/interfaces/ifctl.sh
@@ -23,9 +23,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+DO_COMMAND=
 DO_CONTENTS=
-DO_DELETE=
-DO_MODE=
 DO_VERBOSE=
 
 AF=
@@ -33,10 +32,22 @@ MD=
 EX=
 IF=
 
+flush_routes()
+{
+	if [ "${MD}" != "nameserver" -o ! -f "${FILE}" ]; then
+		return
+	fi
+
+	for CONTENT in $(cat ${FILE}); do
+		# flush routes here to make sure they are recycled properly
+		route delete -${AF} "${CONTENT}"
+	done
+}
+
 # default to IPv4 with nameserver mode
 set -- -4 -n ${@}
 
-while getopts 46a:di:nprsV OPT; do
+while getopts 46a:cdi:lnprsV OPT; do
 	case ${OPT} in
 	4)
 		AF=inet
@@ -49,26 +60,29 @@ while getopts 46a:di:nprsV OPT; do
 	a)
 		DO_CONTENTS="${DO_CONTENTS} ${OPTARG}"
 		;;
+	c)
+		DO_COMMAND="-c"
+		MD="nameserver prefix router searchdomain"
+		;;
 	d)
-		DO_DELETE="-d"
+		DO_COMMAND="-d"
 		;;
 	i)
 		IF=${OPTARG}
 		;;
+	l)
+		DO_COMMAND="-l"
+		;;
 	n)
-		DO_MODE="-n"
 		MD="nameserver"
 		;;
 	p)
-		DO_MODE="-p"
 		MD="prefix"
 		;;
 	r)
-		DO_MODE="-r"
 		MD="router"
 		;;
 	s)
-		DO_MODE="-s"
 		MD="searchdomain"
 		;;
 	V)
@@ -85,26 +99,95 @@ if [ -n "${DO_VERBOSE}" ]; then
 	set -x
 fi
 
-FILE="/tmp/${IF:-*}_${MD}${EX}"
+if [ "${DO_COMMAND}" = "-c" ]; then
+	if [ -z "${IF}" ]; then
+		echo "Clearing requires interface option" >&2
+		exit 1
+	fi
 
-if [ -z "${IF}" ]; then
-	# list all interfaces that have the requested file
-	for FOUND in $(find -s /tmp -name "${FILE#/tmp/}"); do
-		FOUND=${FOUND#/tmp/}
-		echo ${FOUND%%_*}
+	HAVE_ROUTE=
+
+	# iterate through possible files for cleanup
+	for MD in nameserver prefix router searchdomain; do
+		for IFC in ${IF} ${IF}:slaac; do
+			FILE="/tmp/${IFC}_${MD}${EX}"
+			if [ ! -f ${FILE} ]; then
+				continue
+			fi
+			if [ "${MD}" = "router" ]; then
+				HAVE_ROUTE=1
+			fi
+			flush_routes
+			rm -f ${FILE}
+		done
 	done
 
-	# wait for further user input using "-i"
+        # legacy behaviour originating from interface_bring_down()
+	/usr/sbin/arp -d -i ${IF} -a
+
+	# XXX maybe we do not have to kill states at all
+	if [ -n "${HAVE_ROUTE}" ]; then
+		/sbin/pfctl -i ${IF} -Fs
+	fi
+
+	exit 0
+elif [ "${DO_COMMAND}" = "-l" ]; then
+	if [ -z "${IF}" ]; then
+		EX="*"
+		IF="*"
+	fi
+
+	MATCHES=$(find -s /tmp -name "${IF}_${MD}${EX}" -or -name "${IF}:*_${MD}${EX}")
+	RESULTS=
+
+	for MATCH in ${MATCHES}; do
+		FILE=${MATCH##*/}
+		IF=${FILE%_*}
+		IF=${IF%%:*}
+		MD=${FILE##*_}
+
+		# suffix :slaac sorts before plain interface
+		# so we can export the resulting file name first
+		# and overwrite later
+		if [ -z "$(eval echo \${${IF}_${MD}})" ]; then
+			RESULTS="${RESULTS} ${IF}_${MD}"
+		fi
+		eval export ${IF}_${MD}='${MATCH}'
+	done
+
+	for RESULT in ${RESULTS}; do
+		eval echo \${${RESULT}}
+	done
+
 	exit 0
 fi
 
-if [ -n "${DO_DELETE}" ]; then
-        if [ "${DO_MODE}" = "-n" -a -f ${FILE} ]; then
-		for CONTENT in $(cat ${FILE}); do
-			# flush routes here to make sure they are recycled properly
-			route delete -${AF} "${CONTENT}"
-		done
-	fi
+FILE="/tmp/${IF:-*}_${MD}${EX}"
+
+if [ -z "${IF}" ]; then
+	RESULTS=
+
+	# list all interfaces that have the requested file
+	for FOUND in $(find -s /tmp -name "${FILE#/tmp/}"); do
+		FOUND=${FOUND#/tmp/}
+		FOUND=${FOUND%_*}
+		FOUND=${FOUND%:*}
+		if [ -z "$(eval echo \${${FOUND}_found})" ]; then
+			RESULTS="${RESULTS} ${FOUND}_found"
+		fi
+		eval export ${FOUND}_found='${FOUND}'
+	done
+
+	# only list possible interfaces for user to choose from
+	for RESULT in ${RESULTS}; do
+		eval echo \${${RESULT}}
+	done
+
+	exit 0
+fi
+
+if [ "${DO_COMMAND}" = "-d" ]; then
+	flush_routes
 	rm -f ${FILE}
 fi
 
@@ -112,6 +195,16 @@ for CONTENT in ${DO_CONTENTS}; do
 	echo "${CONTENT}" >> ${FILE}
 done
 
-if [ -z "${DO_CONTENTS}${DO_DELETE}" -a -f ${FILE} ]; then
+if [ -n "${DO_COMMAND}${DO_CONTENT}" ]; then
+	exit 0
+fi
+
+if [ ! -f ${FILE} ]; then
+	# move to :slaac interface suffix if not found
+	FILE="/tmp/${IF}:slaac_${MD}${EX}"
+fi
+
+# if nothing else could be done display data
+if [ -f ${FILE} ]; then
 	cat ${FILE}
 fi
