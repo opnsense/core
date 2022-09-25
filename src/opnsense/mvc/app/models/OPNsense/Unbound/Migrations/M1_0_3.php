2eef9015bf5ee46d90ef6406bdc459e705eb72c4
Stephan de Wit <stephan.de.wit@deciso.com>
2022-09-25 23:03:03 +0000

    unbound: unbound/advanced MVC conversion
    (cherry picked from commit d2b18299b4dc61a6047421cf54c8d0ef8c5759fa)
    (cherry picked from commit 33a253a7487f592d310baedf28196cd406c035f0)
    (cherry picked from commit f3e0e067ec07ca4d71299daf467fe4000387e558)
    (cherry picked from commit 6af75dd12226db33284e94d418c1e7897cb34b9b)
    (cherry picked from commit 346cc08c9919d1e20aa3869394e27bee45045f2e)
    (cherry picked from commit 4f9e12c99959f37614521ed6e998dfbae417d955)

---
+++ src/opnsense/mvc/app/models/OPNsense/Unbound/Migrations/M1_0_3.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * Copyright (C) 2022 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Unbound\Migrations;
+
+use OPNsense\Base\BaseModelMigration;
+use OPNsense\Core\Config;
+
+class M1_0_3 extends BaseModelMigration
+{
+    private $legacy_format = array(
+        'outgoing_num_tcp',
+        'incoming_num_tcp',
+        'num_queries_per_thread',
+        'jostle_timeout',
+        'cache_max_ttl',
+        'cache_min_ttl',
+        'infra_host_ttl',
+        'infra_cache_numhosts',
+        'unwanted_reply_threshold',
+        'log_verbosity',
+        'extended_statistics',
+        'log_queries',
+        'hideidentity',
+        'hideversion',
+        'prefetch',
+        'prefetchkey',
+        'dnssecstripped',
+        'serveexpired',
+        'qnameminstrict',
+        'msgcachesize'
+    );
+
+    /**
+     * Migrate older models into shared model
+     * @param $model
+     */
+    public function run($model)
+    {
+        $config = Config::getInstance()->object();
+
+        $legacy_config = array();
+        foreach ($config->unbound->children() as $key => $value) {
+            if (in_array($key, $this->legacy_format) && !empty((string)$value)) {
+                if ($key == 'msgcachesize') {
+                    $legacy_config[$key] = (string)$value . 'm';
+                    /* Mimic legacy behaviour for the msg cache size value (if applied) */
+                    $legacy_config['rrsetcachesize'] = ($value * 2) . 'm';
+                    continue;
+                }
+
+                if ($key == 'num_queries_per_thread') {
+                    $legacy_config['outgoingrange'] = $value * 2;
+                }
+
+                /* handle differing keys, underscore got removed in model transition */
+                $legacy_config[str_replace('_', '', $key)] = (string)$value;
+            }
+        }
+
+        foreach (['privatedomain', 'insecuredomain'] as $misc_node) {
+            $node_value = (string)$config->OPNsense->unboundplus->miscellaneous->$misc_node;
+            if (!empty($node_value)) {
+                $legacy_config[$misc_node] = $node_value;
+            }
+        }
+
+        $model->advanced->setNodes($legacy_config);
+    }
+
+    /**
+     * cleanup old config after config save
+     * @param $model
+     */
+    public function post($model)
+    {
+        $config = Config::getInstance()->object();
+        foreach ($this->legacy_format as $node) {
+            unset($config->unbound->$node);
+        }
+    }
+}
---
+++ src/opnsense/mvc/app/views/OPNsense/Unbound/advanced.volt
@@ -0,0 +1,72 @@
+{#
+ # Copyright (c) 2022 Deciso B.V.
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without modification,
+ # are permitted provided that the following conditions are met:
+ #
+ # 1. Redistributions of source code must retain the above copyright notice,
+ #    this list of conditions and the following disclaimer.
+ #
+ # 2. Redistributions in binary form must reproduce the above copyright notice,
+ #    this list of conditions and the following disclaimer in the documentation
+ #    and/or other materials provided with the distribution.
+ #
+ # THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ # INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ # AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ # AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ # OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ # POSSIBILITY OF SUCH DAMAGE.
+ #}
+
+<script>
+    $( document ).ready(function() {
+        function row_toggle() {
+            $parent_row = $('.serveexpired_child').closest('tr');
+            $parent_row.find('div:first').css('padding-left', '20px');
+            $('.serveexpired_parent').is(':checked') ? $parent_row.removeClass("hidden") : $parent_row.addClass("hidden");
+
+        }
+
+        var data_get_map = {'frm_AdvancedSettings':"/api/unbound/settings/get"};
+        mapDataToFormUI(data_get_map).done(function(data) {
+            formatTokenizersUI();
+            $('.selectpicker').selectpicker('refresh');
+            row_toggle();
+        });
+
+        $("#reconfigureAct").SimpleActionButton({
+            onPreAction: function() {
+              const dfObj = new $.Deferred();
+              saveFormToEndpoint("/api/unbound/settings/set", 'frm_AdvancedSettings', function(){
+                  dfObj.resolve();
+              });
+              return dfObj;
+            }
+        });
+
+        $('.serveexpired_parent').click(function(){
+            row_toggle();
+        });
+
+        updateServiceControlUI('unbound');
+    });
+</script>
+
+<div class="content-box" style="padding-bottom: 1.5em;">
+{{ partial("layout_partials/base_form",['fields':advancedForm,'id':'frm_AdvancedSettings'])}}
+    <div class="col-md-12">
+        <hr/>
+        <button class="btn btn-primary" id="reconfigureAct"
+                data-endpoint='/api/unbound/service/reconfigure'
+                data-label="{{ lang._('Apply') }}"
+                data-error-title="{{ lang._('Error reconfiguring unbound') }}"
+                type="button">
+        </button>
+    </div>
+</div>
