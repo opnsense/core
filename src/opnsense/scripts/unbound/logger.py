d19912b3b387b1ac4728fbb53b098ac351d79a67
Stephan de Wit <stephan.de.wit@deciso.com>
2022-11-23 10:02:11 +0100

    unbound: remove old test code

--- src/opnsense/scripts/unbound/logger.py
+++ src/opnsense/scripts/unbound/logger.py
@@ -7,7 +7,6 @@ import syslog
 import sqlite3
 import time
 import datetime
-import random, string
 from timeit import default_timer as timer
 from collections import deque
 sys.path.insert(0, "/usr/local/opnsense/site-python")
@@ -99,23 +98,6 @@ class DNSReader:
 
         return True
 
-    def delete(self):
-        self.cursor.execute("DELETE FROM query;")
-
-    def write_test(self, qbuffer):
-        start = timer()
-        for entry in qbuffer:
-            self.cursor.execute("INSERT INTO query VALUES(?, ?, ?, ?, ?, ?, ?)",
-                tuple(entry.strip("\n").split()))
-
-        end = timer()
-        t1 = (end - start)
-        start = timer()
-        self.con.commit()
-        end = timer()
-        t2 = (end - start)
-        return (t1, t2)
-
 def run_logger(target_pipe, flush_interval):
     fd = None
     sel = selectors.DefaultSelector()
@@ -145,52 +127,13 @@ if __name__ == '__main__':
     parser.add_argument('--pid', help='pid file location', default='/var/run/unbound_logger.pid')
     parser.add_argument('--pipe', help='named pipe file location', default='/var/unbound/data/dns_logger')
     parser.add_argument('--foreground', help='run (log) in foreground', default=False, action='store_true')
-    parser.add_argument('--test', help='run in test mode', default=False, action='store_true')
     parser.add_argument('--flush_interval', help='interval to flush to db', default=5)
 
     inputargs = parser.parse_args()
 
     syslog.openlog('unbound', logoption=syslog.LOG_DAEMON, facility=syslog.LOG_LOCAL4)
 
-    if not inputargs.test:
-        syslog.syslog(syslog.LOG_NOTICE, 'Daemonizing unbound logging backend.')
-        cmd = lambda : run_logger(target_pipe=inputargs.pipe, flush_interval=inputargs.flush_interval)
-        daemon = Daemonize(app="unbound_logger", pid=inputargs.pid, action=cmd, foreground=inputargs.foreground)
-        daemon.start()
-    else:
-        # XXX: to be removed
-        r = DNSReader()
-        total = 500000
-        buf_size = 4000
-        buf = []
-        start = timer()
-        for i in range(total // buf_size):
-            slice = []
-            for j in range(buf_size):
-                # generate buf_size records
-                d = ''.join(random.choice(string.ascii_lowercase) for x in range(50))
-                q = "{t} 127.0.0.1 ipv4 A {domain} 1 1".format(t=time.time(), domain=d)
-                slice.append(q)
-            buf.append(slice)
-        end = timer()
-        print("generated {len} random records in {t} seconds".format(t=(end-start), len=len(buf) * buf_size))
-        print("starting db test with buffer size %s and a total of %s" % (buf_size, total))
-        r.delete()
-        total_insert_t = 0
-        total_commit_t = 0
-        total_t = 0
-        count = 0
-        for slice in buf:
-            # t1 = insert time
-            # t2 = commit time
-            t1, t2 = r.write_test(slice)
-            total_insert_t += t1
-            total_commit_t += t2
-            total_t += (t1 + t2)
-            count +=1
-
-        print("Total insert time: %s" % total_insert_t)
-        print("Total commit time: %s" % total_commit_t)
-        print("Total transaction time: %s" % total_t)
-
-
+    syslog.syslog(syslog.LOG_NOTICE, 'Daemonizing unbound logging backend.')
+    cmd = lambda : run_logger(target_pipe=inputargs.pipe, flush_interval=inputargs.flush_interval)
+    daemon = Daemonize(app="unbound_logger", pid=inputargs.pid, action=cmd, foreground=inputargs.foreground)
+    daemon.start()
