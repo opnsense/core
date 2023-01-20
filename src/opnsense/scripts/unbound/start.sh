4e0bdd5bf447ec2394bbe67aa3e3106e4a86feb2
Stephan de Wit <stephan.de.wit@deciso.com>
2023-01-20 12:52:50 +0100

    unbound: fix deadlock by specifying a pidfile to daemon
    (cherry picked from commit cb510320facfb6db3eccea4da99ce8bc12eadc89)

--- src/opnsense/scripts/unbound/start.sh
+++ src/opnsense/scripts/unbound/start.sh
@@ -71,5 +71,6 @@ if [ -n "${DOMAIN}" ]; then
 fi
 
 if [ -f /var/unbound/data/stats ]; then
-    /usr/sbin/daemon -f -S -m 2 -s err -l local4 -T unbound /usr/local/opnsense/scripts/unbound/logger.py
+    /usr/sbin/daemon -p /var/run/unbound_logger.pid -f -S -m 2 -s err -l local4 \
+        -T unbound /usr/local/opnsense/scripts/unbound/logger.py
 fi
