b7a02189682cc3f98e6f14ec94f3bc67b970567d
Steely Wing <steelywing@users.noreply.github.com>
2023-04-16 15:57:53 +0800

    Help tip for enable "Promiscuous mode" for VLAN interface

--- src/www/interfaces.php
+++ src/www/interfaces.php
@@ -1948,7 +1948,7 @@ include("head.inc");
                               <?= gettext('This field can be used to spoof the MAC address of the interface. Enter a ' .
                                   'MAC address in the following format: xx:xx:xx:xx:xx:xx or leave blank if unsure. ' .
                                   'This may only be required e.g. with certain cable connections on a WAN interface. <br /> ' .
-                                  'If this is VLAN interface, need to enable "Promiscuous mode".') ?><br />
+                                  'If this is VLAN interface, after changing MAC address need to enable "Promiscuous mode".') ?><br />
 <?php
                               $ip = getenv('REMOTE_ADDR');
                               $mac = `/usr/sbin/arp -an | grep {$ip} | cut -d" " -f4`;
