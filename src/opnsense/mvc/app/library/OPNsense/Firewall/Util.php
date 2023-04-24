murosorg/muros

MVC / NetworkField: add strict option (disallow host bits in CIDR notation)

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -95,6 +95,36 @@ class Util
     }
 
     /**
+     * is provided network strict (host bits not set)
+     * @param string $network network
+     * @return boolean
+     */
+    public static function isStrict($network)
+    {
+        if (self::isSubnet($network)) {
+            list($net, $mask) = explode('/', $network);
+            $ip_net = inet_pton($net);
+            $bits = (strpos($net, ":") !== false && $mask <= 128) ? 128 : 32;
+
+            $ip_mask = "";
+            $significant_bits = $mask;
+            for ($i = 0; $i < $bits/8; $i++) {
+                if ($significant_bits >= 8) {
+                    $ip_mask .= chr(0xFF);
+                    $significant_bits -= 8;
+                } else {
+                    $ip_mask .= chr(~(0xFF >> $significant_bits));
+                    $significant_bits = 0;
+                }
+            }
+
+            return $ip_net == ($ip_net & $ip_mask);
+        }
+
+        return false;
+    }
+
+    /**
      * is provided network a valid wildcard (https://en.wikipedia.org/wiki/Wildcard_mask)
      * @param string $network network
      * @return boolean

src/opnsense/mvc/app/models/OPNsense/Base/Validators/NetworkValidator.php
@@ -31,6 +31,7 @@
 namespace OPNsense\Base\Validators;
 
 use OPNsense\Base\BaseValidator;
+use OPNsense\Firewall\Util;
 use Phalcon\Messages\Message;
 
 /**
@@ -46,7 +47,7 @@ class NetworkValidator extends BaseValidator
      *      noPrivate   : true, false (default)
      *      noSubnet    : true, false (default)
      *      netMaskRequired : true, false (default)
-     *
+     *      strict:     : true, false (default)
      *
      * @param $validator
      * @param string $attribute
@@ -89,6 +90,7 @@ class NetworkValidator extends BaseValidator
                 if ($this->getOption('netMaskAllowed') === false) {
                     $result = false;
                 } else {
+                    $cidr = $value;
                     $parts = explode("/", $value);
                     if (count($parts) > 2 || !ctype_digit($parts[1])) {
                         // more parts then expected or second part is not numeric
@@ -107,6 +109,10 @@ class NetworkValidator extends BaseValidator
                                 $result = false;
                             }
                         }
+                    }
+
+                    if ($this->getOption('strict') === true && !Util::isStrict($cidr)) {
+                        $result = false;
                     }
                 }
             } elseif ($this->getOption('netMaskRequired') === true) {

opnsense/core

(cherry picked from commit df1fbfbb05622ed4bbaaa80ea8b5a28cdd097493)
(cherry picked from commit ee34fbb6fabfcb24eae585b5adb160789e20b881)

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -95,6 +95,36 @@ class Util
     }
 
     /**
+     * is provided network strict (host bits not set)
+     * @param string $network network
+     * @return boolean
+     */
+    public static function isSubnetStrict($network)
+    {
+        if (self::isSubnet($network)) {
+            list($net, $mask) = explode('/', $network);
+            $ip_net = inet_pton($net);
+            $bits = (strpos($net, ":") !== false && $mask <= 128) ? 128 : 32;
+
+            $ip_mask = "";
+            $significant_bits = $mask;
+            for ($i = 0; $i < $bits/8; $i++) {
+                if ($significant_bits >= 8) {
+                    $ip_mask .= chr(0xFF);
+                    $significant_bits -= 8;
+                } else {
+                    $ip_mask .= chr(~(0xFF >> $significant_bits));
+                    $significant_bits = 0;
+                }
+            }
+
+            return $ip_net == ($ip_net & $ip_mask);
+        }
+
+        return false;
+    }
+
+    /**
      * is provided network a valid wildcard (https://en.wikipedia.org/wiki/Wildcard_mask)
      * @param string $network network
      * @return boolean

src/opnsense/mvc/app/models/OPNsense/Base/Validators/NetworkValidator.php
@@ -31,6 +31,7 @@
 namespace OPNsense\Base\Validators;
 
 use OPNsense\Base\BaseValidator;
+use OPNsense\Firewall\Util;
 use Phalcon\Messages\Message;
 
 /**
@@ -46,7 +47,7 @@ class NetworkValidator extends BaseValidator
      *      noPrivate   : true, false (default)
      *      noSubnet    : true, false (default)
      *      netMaskRequired : true, false (default)
-     *
+     *      strict:     : true, false (default)
      *
      * @param $validator
      * @param string $attribute
@@ -89,6 +90,7 @@ class NetworkValidator extends BaseValidator
                 if ($this->getOption('netMaskAllowed') === false) {
                     $result = false;
                 } else {
+                    $cidr = $value;
                     $parts = explode("/", $value);
                     if (count($parts) > 2 || !ctype_digit($parts[1])) {
                         // more parts then expected or second part is not numeric
@@ -107,6 +109,10 @@ class NetworkValidator extends BaseValidator
                                 $result = false;
                             }
                         }
+                    }
+
+                    if ($this->getOption('strict') === true && !Util::isSubnetStrict($cidr)) {
+                        $result = false;
                     }
                 }
             } elseif ($this->getOption('netMaskRequired') === true) {