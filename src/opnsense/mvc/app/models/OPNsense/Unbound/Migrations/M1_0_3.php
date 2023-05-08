378d9a3a6c34f499a6d5bbf95c0cbe6a0d36361c
Franco Fichtner <franco@opnsense.org>
2023-05-08 10:46:05 +0200

    unbound: fix migration 1.0.3 for #6550

--- src/opnsense/mvc/app/models/OPNsense/Unbound/Migrations/M1_0_3.php
+++ src/opnsense/mvc/app/models/OPNsense/Unbound/Migrations/M1_0_3.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2022 Deciso B.V.
+ * Copyright (C) 2022-2023 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,7 +33,7 @@ use OPNsense\Core\Config;
 
 class M1_0_3 extends BaseModelMigration
 {
-    private $legacy_format = array(
+    private $legacy_format = [
         'outgoing_num_tcp',
         'incoming_num_tcp',
         'num_queries_per_thread',
@@ -54,7 +54,7 @@ class M1_0_3 extends BaseModelMigration
         'serveexpired',
         'qnameminstrict',
         'msgcachesize'
-    );
+    ];
 
     /**
      * Migrate older models into shared model
@@ -63,8 +63,8 @@ class M1_0_3 extends BaseModelMigration
     public function run($model)
     {
         $config = Config::getInstance()->object();
+        $legacy_config = [];
 
-        $legacy_config = array();
         foreach ($config->unbound->children() as $key => $value) {
             if (in_array($key, $this->legacy_format) && !empty((string)$value)) {
                 if ($key == 'msgcachesize') {
@@ -72,10 +72,12 @@ class M1_0_3 extends BaseModelMigration
                     /* Mimic legacy behaviour for the msg cache size value (if applied) */
                     $legacy_config['rrsetcachesize'] = ($value * 2) . 'm';
                     continue;
-                }
-
-                if ($key == 'num_queries_per_thread') {
+                } elseif ($key == 'dnssecstripped') {
+                    $legacy_config['dnssecstripped'] = empty((string)$value) ? 0 : 1;
+                    continue;
+                } elseif ($key == 'num_queries_per_thread') {
                     $legacy_config['outgoingrange'] = $value * 2;
+                    /* FALLTHROUGH */
                 }
 
                 /* handle differing keys, underscore got removed in model transition */
