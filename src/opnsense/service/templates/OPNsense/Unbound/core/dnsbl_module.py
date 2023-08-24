862381660d05ef1f69d69be9bf542f0d03ea1b03
Ad Schellevis <ad@opnsense.org>
2023-08-24 08:39:16 +0200

    Services: Unbound DNS - likely locking issue in https://forum.opnsense.org/index.php?topic=35527.0 , concurrent session closing the handle while still writing data.
    (cherry picked from commit 7406a5067f87c101bb3c92c1d432e68c8fad8bde)

--- src/opnsense/service/templates/OPNsense/Unbound/core/dnsbl_module.py
+++ src/opnsense/service/templates/OPNsense/Unbound/core/dnsbl_module.py
@@ -196,12 +196,13 @@ class Logger:
 
     def close(self):
         if self.stats_enabled:
-            if self._pipe_fd is not None:
-                os.close(self._pipe_fd)
-            try:
-                os.unlink(self._pipe_name)
-            except:
-                pass
+            with self._lock:
+                if self._pipe_fd is not None:
+                    os.close(self._pipe_fd)
+                try:
+                    os.unlink(self._pipe_name)
+                except:
+                    pass
 
     def log_entry(self, query: Query):
         if not self.stats_enabled:
