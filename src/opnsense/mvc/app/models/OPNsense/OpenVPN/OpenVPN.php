e7f19e43da27d23dc082a5b02cf8aa2dbb414d3a
Franco Fichtner <franco@opnsense.org>
2023-09-06 13:16:58 +0200

    openvpn: style

--- src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
+++ src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
@@ -264,7 +264,7 @@ class OpenVPN extends BaseModel
                 $this_caref = null;
                 if (!empty((string)$node->ca)) {
                     $this_caref = (string)$node->ca;
-                } else if (isset(Config::getInstance()->object()->cert)) {
+                } elseif (isset(Config::getInstance()->object()->cert)) {
                     foreach (Config::getInstance()->object()->cert as $cert) {
                         if (isset($cert->refid) && (string)$node->cert == $cert->refid) {
                             $this_caref = (string)$cert->caref;
