ac0499f45726923214d0390fe260f67b7ead2007
Franco Fichtner <franco@opnsense.org>
2023-09-07 08:31:06 +0200

    system: avoid plugin system for native dpinger scripts fetching dpinger_status() #6825
    (cherry picked from commit a2ab96833d9764e34384120d9fa597336da88c77)
    (cherry picked from commit 31593b1e6fa71631ebce2cd2cda41ef0b4656c66)

--- src/opnsense/scripts/routes/gateway_watcher.php
+++ src/opnsense/scripts/routes/gateway_watcher.php
@@ -30,6 +30,7 @@
 require_once 'config.inc';
 require_once 'util.inc';
 require_once 'interfaces.inc';
+require_once 'plugins.inc.d/dpinger.inc';
 
 function signalhandler($signal)
 {
@@ -56,7 +57,7 @@ sleep($wait);
 while (1) {
     pcntl_signal_dispatch();
 
-    $status = return_gateways_status();
+    $status = dpinger_status();
     $alarm = false;
 
     /* clear known gateways in first step to flush unknown in second step */
@@ -140,14 +141,9 @@ while (1) {
         }
     }
 
-   /* react to alarm if backend action was given */
-    if ($alarm) {
-        if ($action != null) {
-            configd_run($action);
-        }
-        /* XXX this blacks out all alarms for the grace period after alarm */
-        sleep($wait);
-    } else {
-        sleep($poll);
+    if ($alarm && $action != null) {
+        configd_run($action);
     }
+
+    sleep($alarm ? $wait : $poll);
 }
