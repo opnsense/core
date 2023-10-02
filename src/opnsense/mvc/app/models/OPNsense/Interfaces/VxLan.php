dfe0be9d0dd8803495207be068efa8c8d5929e86
Monviech <79600909+Monviech@users.noreply.github.com>
2023-10-02 13:14:20 +0200

    interfaces: add input validations to VXLAN model (#6899)
    (cherry picked from commit ef9c2b4df751b5f4c09067be44193e39cd78b391)
    (cherry picked from commit a10cf1c40224ccbb68d9e7058a2c071f903b8c29)
    (cherry picked from commit 9c15cf71573da5c196cd5c3a3d2540e07dc2687f)

--- src/opnsense/mvc/app/models/OPNsense/Interfaces/VxLan.php
+++ src/opnsense/mvc/app/models/OPNsense/Interfaces/VxLan.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2019 Deciso B.V.
+ * Copyright (C) 2019-2023 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,8 +28,65 @@
 
 namespace OPNsense\Interfaces;
 
+use Phalcon\Messages\Message;
 use OPNsense\Base\BaseModel;
 
 class VxLan extends BaseModel
 {
+    public function performValidation($validateFullModel = false)
+    {
+        $messages = parent::performValidation($validateFullModel);
+
+        // Initialize variables
+        foreach ($this->vxlan->iterateItems() as $vxlan) {
+            $vxlangroup = (string) $vxlan->vxlangroup;
+            $vxlanremote = (string) $vxlan->vxlanremote;
+            $vxlandev = (string) $vxlan->vxlandev;
+
+            // Validate that values in Fields have been changed, prevents configuration save lockout when invalid data is present.
+            if (
+                $validateFullModel ||
+                $vxlan->vxlangroup->isFieldChanged() ||
+                $vxlan->vxlanremote->isFieldChanged() ||
+                $vxlan->vxlandev->isFieldChanged()
+            ) {
+                // Validation 1: At least one of vxlangroup and vxlanremote must be populated, but not both.
+                if (
+                    (!empty($vxlangroup) && !empty($vxlanremote)) ||
+                    (empty($vxlangroup) && empty($vxlanremote))
+                ) {
+                    $messages->appendMessage(new Message(
+                        gettext("Remote address -or- Multicast group has to be specified"),
+                        "vxlan.vxlanremote",
+                        "GroupOrRemote"
+                    ));
+                    $messages->appendMessage(new Message(
+                        gettext("Multicast group -or- Remote address has to be specified"),
+                        "vxlan.vxlangroup",
+                        "GroupOrRemote"
+                    ));
+                }
+
+                // Validation 2: If vxlanremote is populated, vxlandev must be an empty string.
+                if (!empty($vxlanremote) && !empty($vxlandev)) {
+                    $messages->appendMessage(new Message(
+                        gettext("Remote address is specified, Device must be None"),
+                        "vxlan.vxlandev",
+                        "DeviceRequirementForRemote"
+                    ));
+                }
+
+                // Validation 3: If vxlangroup is populated, vxlandev must not be an empty string.
+                if (!empty($vxlangroup) && empty($vxlandev)) {
+                    $messages->appendMessage(new Message(
+                        gettext("Multicast group is specified, a Device must also be specified"),
+                        "vxlan.vxlandev",
+                        "DeviceRequirementForGroup"
+                    ));
+                }
+            }
+        }
+
+        return $messages;
+    }
 }
