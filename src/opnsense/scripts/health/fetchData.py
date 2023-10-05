9f588a4ba70bce48f399101b823c14f68bfb9656
Ad Schellevis <ad@opnsense.org>
2023-10-05 15:51:37 +0200

    Reporting / Health - refactor rrd data retrieval and simplify usage
    (cherry picked from commit 972a7d60bf4e16c6ed95fba40db09d0240e526e5)
    (cherry picked from commit 703b071b5db3fc43ff2963678b9aa16f2322d98f)

--- src/opnsense/scripts/health/fetchData.py
+++ src/opnsense/scripts/health/fetchData.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 """
-    Copyright (c) 2015-2019 Ad Schellevis <ad@opnsense.org>
+    Copyright (c) 2015-2023 Ad Schellevis <ad@opnsense.org>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -31,19 +31,72 @@
 """
 import sys
 import glob
-import tempfile
 import subprocess
 import os.path
+import json
+from xml.etree import ElementTree
 
-rrd_reports_dir = '/var/db/rrd'
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
+def get_filename(fname):
+    rrd_reports_dir = '/var/db/rrd'
     # suffix rrd if not already in request
-    if filename.split('.')[-1] != 'rrd':
-        filename += '.rrd'
-
+    if fname.split('.')[-1] != 'rrd':
+        fname += '.rrd'
     # scan rrd directory for requested file
     for rrdFilename in glob.glob('%s/*.rrd' % rrd_reports_dir):
-        if os.path.basename(rrdFilename) == filename:
-            subprocess.run(['/usr/local/bin/rrdtool', 'dump', rrdFilename])
-            break
+        if os.path.basename(rrdFilename).lower() == fname.lower():
+            return rrdFilename
+
+def element_to_dict(elem):
+    dict_result = {}
+    if not len(elem):
+        return elem.text
+
+    for item in elem:
+        if item.tag in dict_result:
+            if type(dict_result[item.tag]) != list:
+                dict_result[item.tag] = [dict_result[item.tag]]
+            dict_result[item.tag].append(element_to_dict(item))
+        else:
+            dict_result[item.tag] = element_to_dict(item)
+
+    return dict_result
+
+
+if len(sys.argv) > 1:
+    result = {}
+    filename = get_filename(sys.argv[1])
+    if filename:
+        sp = subprocess.run(['/usr/local/bin/rrdtool', 'dump', filename], capture_output=True, text=True)
+        ruleData = element_to_dict(ElementTree.fromstring(sp.stdout))
+        if type(ruleData.get('rra')) is list:
+            result['sets'] = []
+            for ifld in ['step', 'lastupdate']:
+                result[ifld] = int(ruleData[ifld]) if ifld in ruleData and ruleData[ifld].isdigit() else 0
+            for rra_idx, rra in  enumerate(ruleData['rra']):
+                if rra.get('cf') == 'AVERAGE':
+                    record = {'ds': []}
+                    ds_count = len(ruleData['ds'])
+                    for ds in ruleData['ds']:
+                        record['ds'].append({
+                            'key': ds['name'].strip() if ds.get('name') else '',
+                            'values': []
+                        })
+                    for ifld in ['pdp_per_row']:
+                        record[ifld] = int(rra[ifld]) if ifld in rra and rra[ifld].isdigit() else 0
+                    record['step_size'] = record['pdp_per_row'] * result['step']
+                    if 'database' in rra and 'row' in rra['database']:
+                        last_ts = int(result['lastupdate'] / record['step_size']) *  record['step_size']
+                        first_ts = last_ts - ((len(rra['database']['row'])-1) * record['step_size'])
+                        record['recorded_time'] = last_ts - first_ts
+                        for idx, row in enumerate(rra['database']['row']):
+                            this_ts = first_ts + (record['step_size'] * idx)
+                            for vidx, v in enumerate(row['v']):
+                                if ds_count >= vidx:
+                                    record['ds'][vidx]['values'].append([
+                                        this_ts * 1000,
+                                        float(v) if v not in ['NaN', 'inf'] else 0
+                                    ])
+
+                    result['sets'].append(record)
+
+    print(json.dumps(result))
