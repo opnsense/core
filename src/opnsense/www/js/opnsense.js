opnsense/core

commit 9206823d60f90430f26415082ba8a7b52931c294

MVC / form handling (opnsense.js) -- Support optgroup atributes for select boxes, when a fieldtype offers an "optgroup" attribute on an element the items will be put into <optgroup> containers.
<optgroup label='my_group'>
  <option value='1'>option 1</option>
</optgroup>

Required for https://github.com/opnsense/core/issues/6928

src/opnsense/www/js/opnsense.js
@@ -147,6 +147,7 @@ function setFormData(parent,data) {
                             targetNode.tokenize2().trigger('tokenize:clear');
                         }
                         targetNode.empty(); // flush
+                        let optgroups = [];
                         if (Array.isArray(node[keypart]) && node[keypart][0] !== undefined && node[keypart][0].key !== undefined) {
                             // key value (sorted) list
                             // (eg node[keypart][0] = {selected: 0, value: 'my item', key: 'item'})
@@ -155,18 +156,33 @@ function setFormData(parent,data) {
                                 if (String(node[keypart][i].selected) !== "0") {
                                     opt.attr('selected', 'selected');
                                 }
-                                targetNode.append(opt);
+                                let optgroup = node[keypart][i].optgroup ?? '';
+                                if (optgroups[optgroup] === undefined) {
+                                    optgroups[optgroup] = [];
+                                }
+                                optgroups[optgroup].push(opt);
                             }
                         } else{
                             // default "dictionary" type select items
                             // (eg node[keypart]['item'] = {selected: 0, value: 'my item'})
                             $.each(node[keypart],function(indxItem, keyItem){
                                 let opt = $("<option>").val(htmlDecode(indxItem)).text(keyItem["value"]);
+                                let optgroup = keyItem.optgroup ?? '';
                                 if (String(keyItem["selected"]) !== "0") {
                                     opt.attr('selected', 'selected');
                                 }
-                                targetNode.append(opt);
+                                if (optgroups[optgroup] === undefined) {
+                                    optgroups[optgroup] = [];
+                                }
+                                optgroups[optgroup].push(opt);
                             });
+                        }
+                        for (const [group, items] of Object.entries(optgroups)) {
+                            if (group == '' && optgroups.length == 1) {
+                                targetNode.append(items);
+                            } else {
+                                targetNode.append($("<optgroup/>").attr('label', group).append(items));
+                            }
                         }
                     } else if (targetNode.prop("type") === "checkbox") {
                         // checkbox type

commit 0d724ec8508ac6952123c7985bf2c8ecbcb53b72

MVC / form handling (opnsense.js) - regression in optgroup handling introduced in https://github.com/opnsense/core/commit/9206823d60f90430f26415082ba8a7b52931c294
Two issues found, when optgroup is not used and filled with an empty item, the length of the group seems to be 0. To retrieve the selected values, we should use val() as iterating the children of the select won't return the options anymore.

src/opnsense/www/js/opnsense.js
@@ -77,14 +77,15 @@ function getFormData(parent) {
                         }
                     }
                     // selectbox, collect selected items
-                    var tmp_str = "";
-                    sourceNode.children().each(function(index){
-                        if ($(this).prop("selected")){
-                            if (tmp_str !== "") tmp_str = tmp_str + separator;
-                            tmp_str = tmp_str + $(this).val();
-                        }
-                    });
-                    node[keypart] = tmp_str;
+                    if (!Array.isArray(sourceNode.val())) {
+                        node[keypart] = sourceNode.val();
+                    } else {
+                        node[keypart] = "";
+                        $.each(sourceNode.val(), function(idx, value){
+                            if (node[keypart] !== "") node[keypart] = node[keypart] + separator;
+                            node[keypart] = node[keypart] + value;
+                        });
+                    }
                 } else if (sourceNode.prop("type") === "checkbox") {
                     // checkbox input type
                     if (sourceNode.prop("checked")) {
@@ -178,7 +179,7 @@ function setFormData(parent,data) {
                             });
                         }
                         for (const [group, items] of Object.entries(optgroups)) {
-                            if (group == '' && optgroups.length == 1) {
+                            if (group == '' && optgroups.length <= 1) {
                                 targetNode.append(items);
                             } else {
                                 targetNode.append($("<optgroup/>").attr('label', group).append(items));