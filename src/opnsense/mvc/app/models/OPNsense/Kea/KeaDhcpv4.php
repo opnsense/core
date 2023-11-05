8e63fbb30201278d64624ac2972fcf7bcaab6195
Ad Schellevis <ad@opnsense.org>
2023-11-05 19:18:49 +0100

    Kea / Dhcp - DHCPv4 replacement [https://github.com/opnsense/core/issues/6971], add "Auto collect option data" checkbox on the subnet.
    Our current default (legacy) scenario is to use the configured interface addresss as gateway and primary dns, but since we don't want to complicate the templates and make the choice explicit, we're adding an option to auto-collect and persist on configuration save/apply.
    
    This commit auto updates selected option data fields on request with the first matching address for the requested subnet found in ifconfig.

--- src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
+++ src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
@@ -30,7 +30,40 @@ namespace OPNsense\Kea;
 
 use OPNsense\Base\BaseModel;
 use OPNsense\Core\Config;
+use OPNsense\Core\Backend;
+use OPNsense\Firewall\Util;
+
 
 class KeaDhcpv4 extends BaseModel
 {
+    /**
+     * Before persisting data into the model, update option_data fields for selected subnets.
+     * setNodes() is used in most cases (at least from our base controller), which should make this a relatvily
+     * save entrypoint to enforce some data.
+     */
+    public function setNodes($data)
+    {
+        $ifconfig = json_decode((new Backend())->configdRun('interface list ifconfig'), true) ?? [];
+        foreach ($this->subnets->subnet4->iterateItems() as $subnet) {
+            if (!empty((string)$subnet->option_data_autocollect)) {
+                // find first possible candidate to use as a gateway.
+                $host_ip = null;
+                foreach ($ifconfig as $if => $details) {
+                    foreach ($details['ipv4'] as $net) {
+                        if (Util::isIPInCIDR($net['ipaddr'], (string)$subnet->subnet)) {
+                            $host_ip = $net['ipaddr'];
+                            break 2;
+                        }
+                    }
+                }
+
+                if (!empty($host_ip)) {
+                    $subnet->option_data->routers = $host_ip;
+                    $subnet->option_data->domain_name_servers = $host_ip;
+                    $subnet->option_data->ntp_servers = $host_ip;
+                }
+            }
+        }
+        return parent::setNodes($data);
+    }
 }
