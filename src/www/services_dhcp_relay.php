7d80f955de6a761a652d714cc778df27a2c23dfe
Franco Fichtner <franco@opnsense.org>
2023-11-21 15:07:31 +0100

    dhcp: DHCRelay model migration #6983
    It would be nice to have both IPv4 and IPv6 in here but it may be
    less desirable in practice.  For now use a shared namespace and
    only migrate IPv4 switching the binary and breaking everything
    in the process.

--- src/www/services_dhcp_relay.php
+++ src/www/services_dhcp_relay.php
@@ -102,11 +102,6 @@ include("head.inc");
   <section class="page-content-main">
     <div class="container-fluid">
       <div class="row">
-<?php
-      if ($dhcpd_enabled) {
-        print_info_box(gettext('DHCP Server is currently enabled. Cannot enable the DHCP Relay service while the DHCP Server is enabled on any interface.'));
-      } else {
-?>
         <?php if (isset($input_errors) && count($input_errors) > 0) print_input_errors($input_errors); ?>
         <?php if (isset($savemsg)) print_info_box($savemsg); ?>
         <section class="col-xs-12">
@@ -179,7 +174,6 @@ include("head.inc");
             </form>
           </div>
         </section>
-        <?php } ?>
       </div>
     </div>
   </section>
