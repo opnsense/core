murosorg/muros

mvc - split treeview template and javascript and hook via controllers. frr uses roughly the same, eventually it would be practical to reuse the source.

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/FirewallController.php
@@ -39,6 +39,25 @@ use OPNsense\Base\IndexController;
 class FirewallController extends IndexController
 {
     /**
+     * {@inheritdoc}
+     */
+    protected function templateJSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), [
+            '/ui/js/tree.jquery.min.js',
+            '/ui/js/opnsense-treeview.js'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateCSSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), ['/css/jqtree.css']);
+    }
+
+    /**
      * firewall live log view
      */
     public function logAction()

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/SystemController.php
@@ -36,6 +36,25 @@
   */
 class SystemController extends IndexController
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateJSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), [
+            '/ui/js/tree.jquery.min.js',
+            '/ui/js/opnsense-treeview.js'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateCSSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), ['/css/jqtree.css']);
+    }
+
     public function memoryAction()
     {
         $this->view->tabs = [

opnsense/core

(cherry picked from commit b7945798c18a8d7c15c1a78aaba28e56231e3f7a)
(cherry picked from commit 0a3621bb08234af6eff48c1075239d2c2decb124)
(cherry picked from commit 7f7bce2e5441fb56eef39f00a296cf886cbd0287)

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/FirewallController.php
@@ -39,6 +39,25 @@ use OPNsense\Base\IndexController;
 class FirewallController extends IndexController
 {
     /**
+     * {@inheritdoc}
+     */
+    protected function templateJSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), [
+            '/ui/js/tree.jquery.min.js',
+            '/ui/js/opnsense-treeview.js'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateCSSIncludes()
+    {
+        return array_merge(parent::templateCSSIncludes(), ['/css/jqtree.css']);
+    }
+
+    /**
      * firewall live log view
      */
     public function logAction()

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/SystemController.php
@@ -36,6 +36,25 @@
   */
 class SystemController extends IndexController
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateJSIncludes()
+    {
+        return array_merge(parent::templateJSIncludes(), [
+            '/ui/js/tree.jquery.min.js',
+            '/ui/js/opnsense-treeview.js'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function templateCSSIncludes()
+    {
+        return array_merge(parent::templateCSSIncludes(), ['/css/jqtree.css']);
+    }
+
     public function memoryAction()
     {
         $this->view->tabs = [