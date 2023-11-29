041cf134f666d776817eb05d050f851715c2c7b7
Ad Schellevis <ad@opnsense.org>
2023-11-29 18:05:58 +0100

    system - remote backup / add an optional random delay before executing remote backups, 3600 seconds max when executed automatically in the nighly run. ref https://github.com/opnsense/plugins/pull/3689

--- src/opnsense/scripts/system/remote_backup.php
+++ src/opnsense/scripts/system/remote_backup.php
@@ -5,6 +5,12 @@ require_once('script/load_phalcon.php');
 
 use OPNsense\Backup\BackupFactory;
 
+/* add random delay in seconds */
+$random_delay = !empty($argv[1]) ? $argv[1] : null;
+if (!empty($random_delay)) {
+    sleep(rand(0, $random_delay));
+}
+
 $backupFact = new BackupFactory();
 foreach ($backupFact->listProviders() as $classname => $provider) {
     if ($provider['handle']->isEnabled()) {
