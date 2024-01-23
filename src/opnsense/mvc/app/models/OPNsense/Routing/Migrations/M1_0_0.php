opnsense/core

commit 7be65661f7932525def311c47343c00e4e6e5b13

System: Gateways: Configuration - Improve migration and fix minor validation issue causing the validation failing. (https://github.com/opnsense/core/pull/6808)
When there are invalid gateway items, it's likely better to skip the ones that we can not migrate in this case. Executing the validation on every record might be expensive, but likely leads to a more consistent end-result. After migration, make sure to remove the old cruft when we end up with at least one gateway.

Without "AllowDynamic" on the intreface selector, we will miss dynamic interfaces like ipsec vti's.

src/opnsense/mvc/app/models/OPNsense/Routing/Migrations/M1_0_0.php
@@ -28,9 +28,14 @@
 
 namespace OPNsense\Routing\Migrations;
 
+use Phalcon\Logger\Logger;
+use Phalcon\Logger\Adapter\Syslog;
+use Phalcon\Logger\Formatter\Line;
+use Phalcon\Messages\Messages;
 use OPNsense\Base\BaseModelMigration;
 use OPNsense\Core\Config;
 use OPNsense\Routing\Gateways;
+
 
 class M1_0_0 extends BaseModelMigration
 {
@@ -42,7 +47,17 @@ class M1_0_0 extends BaseModelMigration
     {
         $config = Config::getInstance()->object();
 
-        if (!empty($config->gateways) && !empty($config->gateways->gateway_item)) {
+        // create logger to save possible consistency issues to
+        $adapter = new Syslog('config', ['option' => LOG_PID,'facility' => LOG_LOCAL2]);
+        $adapter->setFormatter(new Line('%message%'));
+        $logger = new Logger(
+            'messages',
+            [
+                'main' => $adapter
+            ]
+        );
+
+        if (!empty($config->gateways) && count($config->gateways->children()) > 0) {
             foreach ($config->gateways->gateway_item as $gateway) {
                 $node = $model->gateway_item->Add();
 
@@ -87,9 +102,30 @@ class M1_0_0 extends BaseModelMigration
                 if ((string)$node->time_period < $min_time_period) {
                     $node->time_period = $min_time_period;
                 }
+                if ($model->performValidation()->count() > 0) {
+                    $logger->error(sprintf("Migration skipped gateway %s (%s)", $gateway->name, $gateway->gateway));
+                    $model->gateway_item->del($node->getAttribute('uuid'));
+                }
             }
         }
 
         parent::run($model);
+    }
+
+    /**
+     * cleanup old config after config save
+     * @param $model
+     */
+    public function post($model)
+    {
+        if ($model instanceof Gateways) {
+            foreach ($model->gateway_item->iterateRecursiveItems() as $node) {
+                if (!$node->getInternalIsVirtual() && !empty((string)$node)) {
+                    /* There is at least one entry stored. */
+                    unset(Config::getInstance()->object()->gateways);
+                    return;
+                }
+            }
+        }
     }
 }

src/opnsense/mvc/app/models/OPNsense/Routing/Gateways.xml
@@ -26,6 +26,7 @@
             <interface type="InterfaceField">
                 <Required>Y</Required>
                 <Default>wan</Default>
+                <AllowDynamic>Y</AllowDynamic>
                 <filters>
                     <enable>/^(?!0).*$/</enable>
                     <virtual>/^(?!1).*$/</virtual>

commit e21b1a8fe5056f2fdeb435581e7c9cc999bda5c7

System: Gateways: Configuration - fix migration issue introduced in https://github.com/opnsense/core/commit/7be65661f7932525def311c47343c00e4e6e5b13. gateway container contains both "gateway_item" and "gateway_group" items, so we need to be explicit when removing old values. closes https://github.com/opnsense/core/issues/7150

src/opnsense/mvc/app/models/OPNsense/Routing/Migrations/M1_0_0.php
@@ -112,7 +112,7 @@ class M1_0_0 extends BaseModelMigration
             foreach ($model->gateway_item->iterateRecursiveItems() as $node) {
                 if (!$node->getInternalIsVirtual() && !empty((string)$node)) {
                     /* There is at least one entry stored. */
-                    unset(Config::getInstance()->object()->gateways);
+                    unset(Config::getInstance()->object()->gateways->gateway_item);
                     return;
                 }
             }