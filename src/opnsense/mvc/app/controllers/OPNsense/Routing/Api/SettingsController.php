dd394645ac78bfbbb2b4eba54d3235afe94c5be6
Ad Schellevis <ad@opnsense.org>
2024-02-25 20:08:07 +0100

    System: Gateways: Configuration - prevent removal when gateway is bound to an interface, closes https://github.com/opnsense/core/issues/7270

--- src/opnsense/mvc/app/controllers/OPNsense/Routing/Api/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Routing/Api/SettingsController.php
@@ -194,6 +194,16 @@ class SettingsController extends ApiMutableModelControllerBase
             if ($uuid != null) {
                 $gateway = $this->getModel()->getNodeByReference('gateway_item.' . $uuid);
                 $cfg = Config::getInstance()->object();
+                foreach ($cfg->interfaces->children() as $tag => $interface) {
+                    if ((string)$interface->gateway == (string)$gateway->name) {
+                        throw new UserException(sprintf(
+                            gettext("Gateway %s cannot be deleted because it is in use on Interface '%s'"),
+                            $gateway->name,
+                            $interface->descr ?? $tag
+                        ));
+                    }
+                }
+
                 $groups = [];
                 foreach ($cfg->gateways->children() as $tag => $gw_group) {
                     if ($tag == 'gateway_group' && !empty($gw_group)) {
