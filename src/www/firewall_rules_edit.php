b7d3bf122ace3b3d7dc276deff14a903c8a3dbea
Franco Fichtner <franco@opnsense.org>
2024-02-28 12:57:27 +0100

    system: merge static loggin settings into existing MVC page
    (cherry picked from commit d5b84cf46e22e03701cbda14dd93f5402d4cca42)
    (cherry picked from commit 98aabba8a60b0494b3fcd36b6b936e560ad3063b)
    (cherry picked from commit 022ab7ad75a994eb92d8e923ba904e033c593392)
    (cherry picked from commit c149ac2cfb908101b2ddf02195c98a2dfb7d230f)
    (cherry picked from commit b6352ce80d37535b9a1fb76981ea882a39c38563)
    (cherry picked from commit 5247ab3f3a4bb34278b7a3e46fd8db075269c54a)
    (cherry picked from commit 945bd66a3dc8e571e2e8cbce8244daf394d3fc7a)
    (cherry picked from commit 2900f707f157a0f847c722fa96180956181fa976)
    (cherry picked from commit 2dcf83de18e27805c60e792026ef157c951390bb)
    (cherry picked from commit ed1bd094bf3372533c9daaeb5cea131d596a6254)
    (cherry picked from commit 6f9fc2d9827c80f601963bb3bcccc64927edca74)
    (cherry picked from commit b588ded30e4cd8f6ff4a39c9baa9672f2444764d)
    (cherry picked from commit 006be3a0c47c7d54b14c84b860dc0686ad147019)
    (cherry picked from commit 8ea6e7be9b98329578b5d945c66a488633bcbb1a)
    (cherry picked from commit 4c78d5b50b9de67712faa492a242e91886a2d04d)
    (cherry picked from commit 6bc7903fa49639918c7be54e6e2fd11fe67ab6f9)

--- src/www/firewall_rules_edit.php
+++ src/www/firewall_rules_edit.php
@@ -1304,7 +1304,7 @@ include("head.inc");
                       <input name="log" type="checkbox" id="log" value="yes" <?= !empty($pconfig['log']) ? "checked=\"checked\"" : ""; ?> />
                       <?= gettext('Log packets that are handled by this rule') ?>
                       <div class="hidden" data-for="help_for_log">
-                        <?=sprintf(gettext("Hint: the firewall has limited local log space. Don't turn on logging for everything. If you want to do a lot of logging, consider using a %sremote syslog server%s."),'<a href="diag_logs_settings.php">','</a>') ?>
+                        <?=sprintf(gettext("Hint: the firewall has limited local log space. Don't turn on logging for everything. If you want to do a lot of logging, consider using a %sremote syslog server%s."),'<a href="ui/syslog/">','</a>') ?>
                       </div>
                     </td>
                   </tr>
