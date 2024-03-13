496ece0a26e0d440bb347355475a0ad7f4046214
Franco Fichtner <franco@opnsense.org>
2024-03-13 07:42:34 +0100

    ui: add a grid-reload helper to SimpleActionButton for #6983
    (cherry picked from commit 6fe993d1878b484f8449a0d74b1c7611eb07a938)

--- src/opnsense/www/js/opnsense_ui.js
+++ src/opnsense/www/js/opnsense_ui.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Deciso B.V.
+ * Copyright (C) 2015-2024 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -614,6 +614,9 @@ $.fn.SimpleActionButton = function (params) {
                     this_button.find('.reload_progress').removeClass("fa fa-spinner fa-pulse");
                     if (this_button.data('service-widget')) {
                         updateServiceControlUI(this_button.data('service-widget'));
+                    }
+                    if (this_button.data('grid-reload')) {
+                        std_bootgrid_reload(this_button.data('grid-reload'));
                     }
                 });
             }).fail(function () {
