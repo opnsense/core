e172d9222d40ac9e4799c3ca0d5555bb16cdb3c6
Ad Schellevis <ad@opnsense.org>
2024-03-15 18:40:16 +0100

    Trust - move ocsp_validate() from legacy certs.inc to Trust class and refactor its currently single user (openvpn).

--- src/opnsense/scripts/openvpn/tls_verify.php
+++ src/opnsense/scripts/openvpn/tls_verify.php
@@ -28,7 +28,6 @@
  */
 require_once("script/load_phalcon.php");
 require_once("util.inc");
-require_once("certs.inc");
 
 /**
  * verify certificate depth
@@ -47,7 +46,7 @@ function do_verify($serverid)
         return "Certificate depth {$certificate_depth} exceeded max allowed depth of {$allowed_depth}.";
     } elseif ($a_server['use_ocsp'] && $certificate_depth == 0) {
         $serial = getenv('tls_serial_' . $certificate_depth);
-        $ocsp_response = ocsp_validate("/var/etc/openvpn/instance-" . $serverid . ".ca", $serial);
+        $ocsp_response = OPNsense\Trust\Store::ocsp_validate("/var/etc/openvpn/instance-" . $serverid . ".ca", $serial);
         if (!$ocsp_response['pass']) {
             return sprintf(
                 "[serial : %s] @ %s - %s (%s)",
