opnsense/core

commit c8adc2921235f8f37b6709343526596e46281440

ui / tokenizer - One minor annoyance of tokenizers is that it's impossible to edit the separate tokens.
This commit adds a button to switching the items into a textarea and back which eases edits in these cases.
(only aavailable when new items are allowed in the form)

src/opnsense/mvc/app/views/layout_partials/form_input_tr.volt
@@ -74,6 +74,7 @@
         {% elseif type == "checkbox" %}
             <input type="checkbox"  class="{{style|default('')}}" id="{{ id }}">
         {% elseif type in ["select_multiple", "dropdown"] %}
+            <div id="select_{{ id }}">
             <select {% if type == 'select_multiple' %}multiple="multiple"{% endif %}
                     data-size="{{size|default(10)}}"
                     id="{{ id }}"
@@ -91,8 +92,18 @@
                 <a href="#" class="text-danger" id="clear-options_{{ id }}"><i class="fa fa-times-circle"></i> <small>{{ lang._('Clear All') }}</small></a>
               {% if "tokenize" in this_style  %}&nbsp;&nbsp;<a href="#" class="text-danger" id="copy-options_{{ id }}"><i class="fa fa-copy"></i> <small>{{ lang._('Copy') }}</small></a>
               &nbsp;&nbsp;<a href="#" class="text-danger" id="paste-options_{{ id }}" style="display:none"><i class="fa fa-paste"></i> <small>{{ lang._('Paste') }}</small></a>
+              {%    if allownew %}
+              &nbsp;&nbsp;<a href="#" class="text-danger" id="to-text_{{ id }}" ><i class="fa fa-file-text-o"></i> <small>{{ lang._('Text') }}</small> </a>
+              {%    endif %}
               {% endif %}
             {% endif %}
+            </div>
+            <div id="textarea_{{ id }}" style="display: none;">
+                <textarea>
+
+                </textarea>
+                <a href="#" class="text-danger" id="to-select_{{ id }}" ><i class="fa fa-th-list"></i> <small>{{ lang._('Back') }}</small> </a>
+            </div>
         {% elseif type == "password" %}
             <input type="password" autocomplete="new-password" class="form-control {{style|default('')}}" size="{{size|default("50")}}" id="{{ id }}" {{ readonly|default(false) ? 'readonly="readonly"' : '' }} >
         {% elseif type == "textbox" %}

src/opnsense/www/js/opnsense_ui.js
@@ -369,6 +369,37 @@ function addMultiSelectClearUI() {
             });
         });
     });
+    /* Tokenizer <-> text for quick edits */
+    $('[id*="to-text"]').each(function() {
+        $(this).click(function(e) {
+            e.preventDefault();
+            let id = $(this).attr("id").replace(/_*to-text_*/, '');
+            let source = $('div[id="select_' + id + '"]').hide().find('select');
+            let destination = $('div[id="textarea_' + id + '"]').show().find('textarea');
+            if (!source.hasClass('text_area_hooked')) {
+                /* Switch to normal tokenizer view on change() */
+                source.addClass('text_area_hooked');
+                source.change(function(){
+                    $('a[id="to-select_' + id + '"]').click();
+                });
+            }
+            destination.val(source.val().join('\n'));
+            destination.unbind('change').change(function(){
+                source.tokenize2().trigger('tokenize:clear');
+                $.each($(this).val().split("\n"), function( index, value ) {
+                    source.tokenize2().trigger('tokenize:tokens:add', [value, value, true]);
+                });
+            });
+        });
+    });
+    $('[id*="to-select"]').each(function() {
+        $(this).click(function(e) {
+            e.preventDefault();
+            let id = $(this).attr("id").replace(/_*to-select_*/, '');
+            $('div[id="select_' + id + '"]').show();
+            $('div[id="textarea_' + id + '"]').hide();
+        });
+    });
 }
 
 

commit 9b981b7e032f2c5ad1f08fbb8e604d51870cfb47

mvc/views - minor regression in https://github.com/opnsense/core/commit/c8adc2921235f8f37b6709343526596e46281440, allownew needs a default

src/opnsense/mvc/app/views/layout_partials/form_input_tr.volt
@@ -92,7 +92,7 @@
                 <a href="#" class="text-danger" id="clear-options_{{ id }}"><i class="fa fa-times-circle"></i> <small>{{ lang._('Clear All') }}</small></a>
               {% if "tokenize" in this_style  %}&nbsp;&nbsp;<a href="#" class="text-danger" id="copy-options_{{ id }}"><i class="fa fa-copy"></i> <small>{{ lang._('Copy') }}</small></a>
               &nbsp;&nbsp;<a href="#" class="text-danger" id="paste-options_{{ id }}" style="display:none"><i class="fa fa-paste"></i> <small>{{ lang._('Paste') }}</small></a>
-              {%    if allownew %}
+              {%    if allownew|default("false") %}
               &nbsp;&nbsp;<a href="#" class="text-danger" id="to-text_{{ id }}" ><i class="fa fa-file-text-o"></i> <small>{{ lang._('Text') }}</small> </a>
               {%    endif %}
               {% endif %}