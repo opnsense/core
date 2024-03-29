7f6f55814f0447162acdc431689d464ff2b06471
Stephan de Wit <stephan.de.wit@deciso.com>
2024-03-29 16:54:05 +0100

    system: allow (multiple) sysctl calls over configd
    Often many related sysctls are queried in sequence, but since the
    binary allows for multiple params we can save the overhead and
    simply push the result in a parseable structure. Include the original
    OID so the caller can deal with missing sysctls or empty values.

--- src/opnsense/scripts/system/sysctl.py
+++ src/opnsense/scripts/system/sysctl.py
@@ -29,6 +29,7 @@
     return current sysctl(8) information
 """
 
+import argparse
 import os
 import subprocess
 import sys
@@ -37,45 +38,61 @@ import ujson
 # type mapping: r => read-only, t => boot-time, w => runtime
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gather', help='gather syctl info', action='store_true')
+    parser.add_argument('--values', help='comma-separated list of sysctl values to fetch')
+    inputargs = parser.parse_args()
+
     _cache_filename = "/tmp/sysctl_map.cache"
 
-    if os.path.exists(_cache_filename):
-        f = open(_cache_filename, "r")
-        print(f.read())
-        sys.exit(0)
+    if inputargs.values:
+        result = {}
+        params = inputargs.values.split(',')
+        sp = subprocess.run(['/sbin/sysctl', '-i'] + params, capture_output=True, text=True)
+        for line in sp.stdout.split("\n"):
+            # include original oid in output so caller can match back
+            parts = line.strip().split(":")
+            if len(parts) > 1:
+                result[parts[0]] = parts[1].strip()
+        output = ujson.dumps(result)
+    elif inputargs.gather:
+        if os.path.exists(_cache_filename):
+            f = open(_cache_filename, "r")
+            print(f.read())
+            sys.exit(0)
 
-    result = {}
-    sp = subprocess.run(['/sbin/sysctl', '-a'], capture_output=True, text=True)
-    for line in sp.stdout.split("\n"):
-        parts = line.strip().split(": ", 1)
-        if len(parts) > 1:
-            item = {'name': parts[0], 'value': parts[1], 'type': 'r', 'description': ''}
-            result[parts[0]] = item
-    sp = subprocess.run(['/sbin/sysctl', '-ad'], capture_output=True, text=True)
-    for line in sp.stdout.split("\n"):
-        parts = line.strip().split(": ", 1)
-        if len(parts) > 1:
-            if parts[0] in result:
-                result[parts[0]].update({'description': parts[1]})
-            else:
-                # sysctl entries exist that seem to have no value?
-                item = {'name': parts[0], 'value': '', 'type': 'r', 'description': parts[1]}
+        result = {}
+        sp = subprocess.run(['/sbin/sysctl', '-a'], capture_output=True, text=True)
+        for line in sp.stdout.split("\n"):
+            parts = line.strip().split(": ", 1)
+            if len(parts) > 1:
+                item = {'name': parts[0], 'value': parts[1], 'type': 'r', 'description': ''}
                 result[parts[0]] = item
-    sp = subprocess.run(['/sbin/sysctl', '-aNT'], capture_output=True, text=True)
-    for line in sp.stdout.split("\n"):
-        part = line.strip()
-        if part in result:
-            result[part].update({'type': 't'})
-    sp = subprocess.run(['/sbin/sysctl', '-aNW'], capture_output=True, text=True)
-    for line in sp.stdout.split("\n"):
-        part = line.strip()
-        if part in result:
-            result[part].update({'type': 'w'})
+        sp = subprocess.run(['/sbin/sysctl', '-ad'], capture_output=True, text=True)
+        for line in sp.stdout.split("\n"):
+            parts = line.strip().split(": ", 1)
+            if len(parts) > 1:
+                if parts[0] in result:
+                    result[parts[0]].update({'description': parts[1]})
+                else:
+                    # sysctl entries exist that seem to have no value?
+                    item = {'name': parts[0], 'value': '', 'type': 'r', 'description': parts[1]}
+                    result[parts[0]] = item
+        sp = subprocess.run(['/sbin/sysctl', '-aNT'], capture_output=True, text=True)
+        for line in sp.stdout.split("\n"):
+            part = line.strip()
+            if part in result:
+                result[part].update({'type': 't'})
+        sp = subprocess.run(['/sbin/sysctl', '-aNW'], capture_output=True, text=True)
+        for line in sp.stdout.split("\n"):
+            part = line.strip()
+            if part in result:
+                result[part].update({'type': 'w'})
 
-    output = ujson.dumps(result)
+        output = ujson.dumps(result)
 
-    f = open(_cache_filename, "w")
-    f.write(output)
-    f.close()
+        f = open(_cache_filename, "w")
+        f.write(output)
+        f.close()
 
     print (output)
