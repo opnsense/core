1d27c83aea0fef8da0f92430d7ae5d7f8e909148
Ad Schellevis <ad@opnsense.org>
2024-04-16 20:10:53 +0200

    Services: Kea DHCP [new]: Kea DHCPv4 - generate json payload from model, move file generation to a plugin configure hook, keep empty templates to inform people.
    PR: https://github.com/opnsense/core/pull/7361
    
    (cherry picked from commit 29e87aa3e4e4877e9b66ed8050ba49cd438eda6c)
    (cherry picked from commit ac1d9d71f6aa0c2b1a2f07d94baf624d092f17f4)
    (cherry picked from commit b551927c895de141dde21f5dca54a4503ad60e01)
    (cherry picked from commit d241cfda9071ceedbad8b357c9d78f3f132c1781)
    (cherry picked from commit 80b65b0d5d2cb689406be4dc7a2156df8246ae20)
    (cherry picked from commit dc80b7a019e7fc6e8e03a9df72b3d9b5f9d499c7)

--- src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
+++ src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
@@ -32,6 +32,7 @@ use OPNsense\Base\Messages\Message;
 use OPNsense\Base\BaseModel;
 use OPNsense\Core\Config;
 use OPNsense\Core\Backend;
+use OPNsense\Core\File;
 use OPNsense\Firewall\Util;
 
 class KeaDhcpv4 extends BaseModel
@@ -91,6 +92,10 @@ class KeaDhcpv4 extends BaseModel
         return $messages;
     }
 
+    public function isEnabled()
+    {
+        return (string)$this->general->enabled == '1' && !empty((string)(string)$this->general->interfaces);
+    }
 
     /**
      * should filter rules be enabled
@@ -101,5 +106,145 @@ class KeaDhcpv4 extends BaseModel
         return  (string)$this->general->enabled == '1' &&
                 (string)$this->general->fwrules == '1' &&
                 !empty((string)(string)$this->general->interfaces);
+    }
+
+    /**
+     *
+     */
+    private function getConfigPhysicalInterfaces()
+    {
+        $result = [];
+        $cfg = Config::getInstance()->object();
+        foreach (explode(',', $this->general->interfaces) as $if) {
+            if (isset($cfg->interfaces->$if) && !empty($cfg->interfaces->$if->if)) {
+                $result[] = (string)$cfg->interfaces->$if->if;
+            }
+        }
+        return $result;
+    }
+
+    private function getConfigThisServerHostname()
+    {
+        $hostname = (string)$this->ha->this_server_name;
+        if (empty($hostname)) {
+            $hostname = (string)Config::getInstance()->object()->system->hostname;
+        }
+        return $hostname;
+    }
+
+    private function getConfigSubnets()
+    {
+        $result = [];
+        $subnet_id = 1;
+        foreach ($this->subnets->subnet4->iterateItems() as $subnet_uuid => $subnet) {
+            $record = [
+                'id' => $subnet_id++,
+                'subnet' => (string)$subnet->subnet,
+                'option-data' => [],
+                'pools' => [],
+                'reservations' => []
+            ];
+            /* standard option-data elements */
+            foreach ($subnet->option_data->iterateItems() as $key => $value) {
+                $target_fieldname = str_replace('_', '-', $key);
+                if ((string)$value != '') {
+                    $record['option-data'][] = [
+                        'name' => $target_fieldname,
+                        'data' => (string)$value
+                    ];
+                } elseif ($key == 'domain_name') {
+                    $record['option-data'][] = [
+                        'name' => $target_fieldname,
+                        'data' => (string)Config::getInstance()->object()->system->domain
+                    ];
+                }
+            }
+            /* add pools */
+            foreach (array_filter(explode("\n", $subnet->pools)) as $pool) {
+                $record['pools'][] = ['pool' => $pool];
+            }
+            /* static reservations */
+            foreach ($this->reservations->reservation->iterateItems() as $key => $reservation) {
+                if ($reservation->subnet != $subnet_uuid) {
+                    continue;
+                }
+                $res = [];
+                foreach (['hw_address', 'ip_address', 'hostname'] as $key) {
+                    if (!empty((string)$reservation->$key)) {
+                        $res[str_replace('_', '-', $key)] = (string)$reservation->$key;
+                    }
+                }
+                $record['reservations'][] = $res;
+            }
+            $result[] = $record;
+        }
+        return $result;
+    }
+
+    public function generateConfig($target = '/usr/local/etc/kea/kea-dhcp4.conf')
+    {
+        $cnf = [
+            'Dhcp4' => [
+                'valid-lifetime' => (int)$this->general->valid_lifetime->__toString(),
+                'interfaces-config' => [
+                    'interfaces' => $this->getConfigPhysicalInterfaces()
+                ],
+                'lease-database' => [
+                    'type' => 'memfile',
+                    'persist' => true,
+                ],
+                'control-socket' => [
+                    'socket-type' => 'unix',
+                    'socket-name' => '/var/run/kea4-ctrl-socket'
+                ],
+                'loggers' => [
+                    [
+                        'name' => 'kea-dhcp4',
+                        'output_options' => [
+                            [
+                                'output' => 'syslog'
+                            ]
+                        ],
+                        'severity' => 'INFO',
+                    ]
+                ],
+                'subnet4' => $this->getConfigSubnets(),
+            ]
+        ];
+        if (!empty((string)(new KeaCtrlAgent())->general->enabled)) {
+            $cnf['Dhcp4']['hooks-libraries'] = [];
+            $cnf['Dhcp4']['hooks-libraries'][] = [
+                'library' => '/usr/local/lib/kea/hooks/libdhcp_lease_cmds.so'
+            ];
+            if (!empty((string)$this->ha->enabled)) {
+                $record = [
+                    'library' => '/usr/local/lib/kea/hooks/libdhcp_ha.so',
+                    'parameters' => [
+                        'high-availability' => [
+                            [
+                                'this-server-name' => $this->getConfigThisServerHostname(),
+                                'mode' => 'hot-standby',
+                                'heartbeat-delay' => 10000,
+                                'max-response-delay' => 60000,
+                                'max-ack-delay' => 5000,
+                                'max-unacked-clients' => 5,
+                                'sync-timeout' => 60000,
+                            ]
+                        ]
+                    ]
+                ];
+                foreach ($this->ha_peers->peer->iterateItems() as $peer) {
+                    if (!isset($record['parameters']['high-availability'][0]['peers'])) {
+                        $record['parameters']['high-availability'][0]['peers'] = [];
+                    }
+                    $record['parameters']['high-availability'][0]['peers'][] = array_map(
+                        fn($x) => (string)$x,
+                        iterator_to_array($peer->iterateItems())
+                    );
+                }
+                $cnf['Dhcp4']['hooks-libraries'][] = $record;
+            }
+        }
+        File::file_put_contents($target, json_encode($cnf, JSON_PRETTY_PRINT), 0600);
     }
 }
