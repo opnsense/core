7a3e55a863d33fade043f2632750b1874af0363a
Franco Fichtner <franco@opnsense.org>
2024-05-14 08:20:28 +0200

    mvc: more stype updates

--- src/opnsense/mvc/app/library/OPNsense/Mvc/Exceptions/InvalidUriException.php
+++ src/opnsense/mvc/app/library/OPNsense/Mvc/Exceptions/InvalidUriException.php
@@ -25,6 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 namespace OPNsense\Mvc\Exceptions;
 
 class InvalidUriException extends DispatchException
