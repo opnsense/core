590dc5c9d2a0aa0a25259a84dd3a6a5ca8381224
Franco Fichtner <franco@opnsense.org>
2024-05-14 08:08:55 +0200

    mvc: style updates

--- src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/OneToOneController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/OneToOneController.php
@@ -25,6 +25,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 namespace OPNsense\Firewall\Api;
 
 class OneToOneController extends FilterBaseController
