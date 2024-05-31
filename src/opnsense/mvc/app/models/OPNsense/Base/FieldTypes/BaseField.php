f049be47bb7338356209e8d8ac4cb41d544c9c6b
Franco Fichtner <franco@opnsense.org>
2024-05-31 14:53:21 +0200

    src: replace mt_rand() with random_int()
    Interface is the same so result is the same.  Less noise from scanners.

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseField.php
@@ -147,14 +147,14 @@ abstract class BaseField
     {
         return sprintf(
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0x0fff) | 0x4000,
+            random_int(0, 0x3fff) | 0x8000,
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff)
         );
     }
 
