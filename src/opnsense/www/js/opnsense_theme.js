91b7fc8417cab826574b8f8c2d6e03fba5b3c245
René <34602360+opnsenseuser@users.noreply.github.com>
2024-06-22 01:45:15 +0200

    Update opnsense_theme.js

--- src/opnsense/www/js/opnsense_theme.js
+++ src/opnsense/www/js/opnsense_theme.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
             
                 if (currentHeight > (winHeight - li_itemH)) {
 									
-                   /* check if submenu has enough space expanding down  - less space down expand submenu up */
+                   /* check if submenu has enough space expanding down  - if not expand submenu up */
                     var divPos = (divHeight > divTop) ? -((divHeight - divTop) - li_itemH) : 3;
                     nextDiv.css('margin-top', -divHeight - divPos);
                 }
