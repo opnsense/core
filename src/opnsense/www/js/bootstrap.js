b0cab3bdca1180826ffe230ccca7e56280529759
Ad Schellevis <ad@opnsense.org>
2024-07-08 20:28:36 +0200

    ui:bootstrap - backport accessibility update from https://github.com/twbs/bootstrap/commit/b0372bb6588ab12c398e5dbf39950dfd3269575e, closes https://github.com/opnsense/core/pull/7599

--- src/opnsense/www/js/bootstrap.js
+++ src/opnsense/www/js/bootstrap.js
@@ -1784,7 +1784,10 @@ if (typeof jQuery === 'undefined') {
   Tooltip.prototype.fixTitle = function () {
     var $e = this.$element
     if ($e.attr('title') || typeof $e.attr('data-original-title') != 'string') {
-      $e.attr('data-original-title', $e.attr('title') || '').attr('title', '')
+      if (!$e.attr('aria-label') && !$e.text().trim()) {
+        $e.attr('aria-label', $e.attr('title'));
+      }
+      $e.attr('data-original-title', $e.attr('title') || '').attr('title', '');
     }
   }
 
