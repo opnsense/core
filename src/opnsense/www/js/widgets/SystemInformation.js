e885042ce10106ed927bc6fc9522cf5027c0bb05
Franco Fichtner <franco@opnsense.org>
2024-08-05 16:05:16 +0200

    system: add load average back to system info widget
    The API call is not fitting too well, but since it fetches a
    sysctl this is the right spot to do it.
    
    (cherry picked from commit cbd97eaccbb8d287daacce8f47af07fb3c60dcc0)

--- src/opnsense/www/js/widgets/SystemInformation.js
+++ src/opnsense/www/js/widgets/SystemInformation.js
@@ -42,8 +42,9 @@ export default class SystemInformation extends BaseTableWidget {
 
     async onWidgetTick() {
         const data = await this.ajaxCall('/api/diagnostics/system/systemTime');
-        $('#datetime').text(data['datetime']);
         $('#uptime').text(data['uptime']);
+        $('#loadavg').text(data['loadavg']);
+        $('#datetime').text(data['datetime']);
         $('#config').text(data['config']);
     }
 
@@ -64,6 +65,7 @@ export default class SystemInformation extends BaseTableWidget {
         }
 
         rows.push([[this.translations['uptime']], $('<span id="uptime">').prop('outerHTML')]);
+        rows.push([[this.translations['loadavg']], $('<span id="loadavg">').prop('outerHTML')]);
         rows.push([[this.translations['datetime']], $('<span id="datetime">').prop('outerHTML')]);
         rows.push([[this.translations['config']], $('<span id="config">').prop('outerHTML')]);
         super.updateTable('sysinfo-table', rows);
