41086adf85e65d75bf4ebdb96fc9b289f59b8b3a
Franco Fichtner <franco@opnsense.org>
2024-08-14 08:17:19 +0200

    firmware: address duplication and unnecessary shell invokes
    After staring at the issue long enough this seems to make sense.

--- src/etc/rc.syshook.d/upgrade/90-cleanup.sh
+++ src/etc/rc.syshook.d/upgrade/90-cleanup.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-# a copy of this cleanup exists in 10-refresh.sh
-/bin/sh -s << EOF
 # remove our stale pyc files not handled by pkg
 find /usr/local/opnsense -type f -name "*.pyc" -delete
 
 for DIR in /boot /usr/libexec/bsdinstall /usr/local; do
 	# remove spurious files from pkg
-	find \${DIR} ! \( -type d \) -a \
+	find ${DIR} ! \( -type d \) -a \
 	    \( -name "*.pkgsave" -o -name ".pkgtemp.*" \) -delete
 
         # processs spurious directories from pkg
         # (may not be empty so -delete does not work)
-	find \${DIR} -type d -name ".pkgtemp.*" -print0 | xargs -0 -n1 rm -r
+	find ${DIR} -type d -name ".pkgtemp.*" -print0 | xargs -0 -n1 rm -r
 done
-EOF
