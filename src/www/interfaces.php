4de945bdae18c12964aac2f847fd59506edcf7e2
Franco Fichtner <franco@opnsense.org>
2024-09-12 22:17:15 +0200

    interfaces: routing configuration on changed interfaces only

--- src/www/interfaces.php
+++ src/www/interfaces.php
@@ -573,7 +573,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                     interface_configure(false, $ifapply, true);
                 }
 
-                system_routing_configure();
+                system_routing_configure(false, array_keys($toapplylist));
                 filter_configure();
                 plugins_configure('early'); /* ensure GUI and SSH access */
             }
