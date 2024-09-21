83e0ebfeba93d0edb7f3fa05cc34737ddf9dec2a
Ad Schellevis <ad@opnsense.org>
2024-09-21 15:22:11 +0200

    VPN: OpenVPN: Instances - validate "Auth Token Lifetime" requires a non zero Renegotiate time
    PR: https://github.com/opnsense/core/issues/7690
    
    (cherry picked from commit ec23ffc0ef65ee3fe1f25d23c9690b709edb384c)
    (cherry picked from commit d161d7c534bf1227f344f9d94e65c99773d160f1)

--- src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
+++ src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
@@ -156,6 +156,12 @@ class OpenVPN extends BaseModel
                     $key . ".proto"
                 ));
             }
+            if ((string)$instance->{'auth-gen-token'} != '0' && (string)$instance->{'reneg-sec'} == '0') {
+                $messages->appendMessage(new Message(
+                    gettext('A token lifetime requires a non zero Renegotiate time.'),
+                    $key . ".auth-gen-token"
+                ));
+            }
         }
         return $messages;
     }
