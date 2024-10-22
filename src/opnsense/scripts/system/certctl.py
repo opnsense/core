3aac8842887bdfb1442517ac5681ec7ef27932e1
Franco Fichtner <franco@opnsense.org>
2024-10-22 08:27:10 +0200

    system: sync certctl to FreeBSD 14.1 base code et al
    (cherry picked from commit 3b09bc73f88d6f785ec5154286d6fd16c10f62a2)
    (cherry picked from commit 7d77f9f134cfca3532a2dcc9913aa49d9269e4db)
    (cherry picked from commit 95c2c2e49b8363efdf4af6f30f1f2dfede056793)

--- src/opnsense/scripts/system/certctl.py
+++ src/opnsense/scripts/system/certctl.py
@@ -37,10 +37,10 @@ import OpenSSL.crypto
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
 
-TRUSTPATH = ['/usr/share/certs/trusted', '/usr/local/share/certs', '/usr/local/etc/ssl/certs']
-BLACKLISTPATH = ['/usr/share/certs/untrusted', '/usr/share/certs/blacklisted', '/usr/local/etc/ssl/blacklisted']
+TRUSTPATH = ['/usr/share/certs/trusted', '/usr/local/share/certs']
+UNTRUSTEDPATH = ['/usr/share/certs/untrusted', '/usr/local/share/certs/untrusted']
 CERTDESTDIR = '/etc/ssl/certs'
-BLACKLISTDESTDIR = '/etc/ssl/untrusted'
+UNTRUSTDESTDIR = '/etc/ssl/untrusted'
 
 def certificate_iterator(filename):
     fext = os.path.splitext(filename)[1][1:].lower()
@@ -82,11 +82,11 @@ def certificate_iterator(filename):
 
 def get_cert_common_name(filename):
     try:
-        issuer = x509.load_pem_x509_certificate(open(filename, 'rb').read()).issuer
-        for item in issuer:
+        subject = x509.load_pem_x509_certificate(open(filename, 'rb').read()).subject
+        for item in subject:
             if item.rfc4514_attribute_name == 'CN':
                 return item.value
-        return issuer.rfc4514_string()
+        return subject.rfc4514_string()
     except (ValueError, TypeError):
         return None
 
@@ -101,9 +101,9 @@ def cmd_list():
             print('Invalid certificate %s' % basename)
 
 
-def cmd_blacklisted():
-    print('Listing Blacklisted Certificates:')
-    for filename in glob.glob('%s/*.[0-9]' % BLACKLISTDESTDIR):
+def cmd_untrusted():
+    print('Listing Untrusted Certificates:')
+    for filename in glob.glob('%s/*.[0-9]' % UNTRUSTDESTDIR):
         basename = os.path.basename(filename)
         cn = get_cert_common_name(filename)
         if cn:
@@ -113,10 +113,10 @@ def cmd_blacklisted():
 
 
 def cmd_rehash():
-    targets = {'trusted': {}, 'blacklisted': {}}
-    for path in BLACKLISTPATH + TRUSTPATH:
+    targets = {'trusted': {}, 'untrusted': {}}
+    for path in UNTRUSTEDPATH + TRUSTPATH:
         if os.path.isdir(path):
-            targetname = 'trusted' if path in TRUSTPATH else 'blacklisted'
+            targetname = 'trusted' if path in TRUSTPATH else 'untrusted'
             print("Scanning %s for certificates..." % path)
             for filename in glob.glob('%s/*' % path):
                 for record in certificate_iterator(filename):
@@ -134,12 +134,12 @@ def cmd_rehash():
     for target_name in targets:
         for pattern in targets[target_name]:
             for seq, record in enumerate(targets[target_name][pattern]):
-                is_bl = target_name == 'blacklisted'
+                is_ut = target_name == 'untrusted'
                 src_filename = record['filename']
-                dst_filename = "%s/%s" % (BLACKLISTDESTDIR if is_bl else CERTDESTDIR, pattern % seq)
-                if not is_bl and hash in targets['blacklisted']:
+                dst_filename = "%s/%s" % (UNTRUSTDESTDIR if is_ut else CERTDESTDIR, pattern % seq)
+                if not is_ut and hash in targets['untrusted']:
                     print(
-                        "Skipping blacklisted certificate %s (%s/%s)" % (filename, BLACKLISTDESTDIR, pattern % seq)
+                        "Skipping untrusted certificate %s (%s/%s)" % (filename, UNTRUSTDESTDIR, pattern % seq)
                     )
                     continue
 
@@ -161,7 +161,7 @@ def cmd_rehash():
                         os.remove(dst_filename)
                     os.symlink(src_filename, dst_filename)
 
-    for path in [BLACKLISTDESTDIR, CERTDESTDIR]:
+    for path in [UNTRUSTDESTDIR, CERTDESTDIR]:
         for filename in glob.glob('%s/*.[0-9]' % path) + glob.glob('%s/*.r[0-9]' % path):
             if filename in current_target_files:
                 continue
@@ -199,13 +199,13 @@ if __name__ == '__main__':
     cmds =  {
         'list': cmd_list,
         'rehash': cmd_rehash,
-        'blacklisted': cmd_blacklisted
+        'untrusted': cmd_untrusted
     }
     if len(sys.argv) < 2 or sys.argv[1] not in cmds:
         script_name = os.path.basename(sys.argv[0])
         print('Manage the TLS trusted certificates on the system')
         print('%s list\n\tList trusted certificates' % script_name)
-        print('%s blacklisted\n\tList blacklisted certificates' % script_name)
+        print('%s untrusted\n\tList untrusted certificates' % script_name)
         print('%s rehash\n\tGenerate hash links for all certificates' % script_name)
     else:
         cmds[sys.argv[1]]()
