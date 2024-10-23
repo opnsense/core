2bb5c9c5b262fe35ad278f7a82d78ae1ef9a91d0
Ad Schellevis <ad@opnsense.org>
2024-10-23 15:41:27 +0200

    Firewall: Automation: Filter - use uuid as rule labels to ease tracking.
    (cherry picked from commit 2c4c5cf09bb7472ba4292696c30159e8c118cc4b)
    (cherry picked from commit 87687b7c3975cc248eda9bed59138b991c4d1f6a)

--- src/opnsense/scripts/filter/read_log.py
+++ src/opnsense/scripts/filter/read_log.py
@@ -85,7 +85,8 @@ def fetch_rule_details():
                     lbl = line.split(' label ')[-1]
                     if lbl.count('"') >= 2:
                         rule_md5 = lbl.split('"')[1]
-                        if len(rule_md5) == 32 and set(rule_md5).issubset(HEX_DIGITS):
+                        # detect either calculated md5 hash (calcRuleHash) or rule uuid
+                        if len(rule_md5) >= 32 and set(rule_md5.replace('-', '')).issubset(HEX_DIGITS):
                             rule_map[rule_md5] = ''.join(lbl.split('"')[2:]).strip().strip('# : ')
 
     return rule_map
@@ -146,7 +147,7 @@ def parse_record(record, running_conf_descr):
     elif rule['action'] not in ['pass', 'block']:
         # no id for translation rules
         rule['label'] = "%s rule" % rule['action']
-    elif len(rulep) > 0 and len(rulep[-1]) == 32 and set(rulep[-1]).issubset(HEX_DIGITS):
+    elif len(rulep) > 0 and len(rulep[-1]) >= 32 and set(rulep[-1].replace('-', '')).issubset(HEX_DIGITS):
         # rule id appended in record format, don't use rule sequence number in that case either
         rule['rid'] = rulep[-1]
         if rulep[-1] in running_conf_descr:
