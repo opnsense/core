594d4c9224f8dff342db040a6d90b06ac3fb07bd
Ad Schellevis <ad@opnsense.org>
2024-10-29 14:14:05 +0100

    System: Trust: Settings - add SignatureAlgorithms option and fix minor form glitch
    To prevent clients from offering SignatureAlgorithms beyond specific boundaries, offer the option to set the list manually.
    Unfortunately we can't seem to query the options via openssl, so we will have to add the ones we know manually to an option group.
    
    [*] https://docs.openssl.org/3.0/man3/SSL_CONF_cmd/#supported-configuration-file-commands
    
    (cherry picked from commit 052eea325386d58a851b0e435c8406a61fe24a29)

--- src/opnsense/mvc/app/models/OPNsense/Trust/General.php
+++ src/opnsense/mvc/app/models/OPNsense/Trust/General.php
@@ -40,7 +40,14 @@ class General extends BaseModel
     {
         $messages = parent::performValidation($validateFullModel);
         $enable_config_constraints = false;
-        foreach (['CipherString', 'Ciphersuites', 'groups', 'MinProtocol', 'MinProtocol_DTLS'] as $fieldname) {
+        foreach ([
+            'CipherString',
+            'Ciphersuites',
+            'groups',
+            'MinProtocol',
+            'MinProtocol_DTLS',
+            'SignatureAlgorithms'
+        ] as $fieldname) {
             if (!empty((string)$this->$fieldname)) {
                 $enable_config_constraints = true;
             }
