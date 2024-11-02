4cb1f6d57d3a07f75ba2995370e5cc07714f1a01
Ad Schellevis <ad@opnsense.org>
2024-11-02 15:37:48 +0100

    Authentication: add a "time-loop" around authentication for failed attempts, closes https://github.com/opnsense/core/issues/8031
    This commit adds a wrapper in base for the authenticate() method, which calls the protected _authenticate() method for a timed auth sequence.
    When authentication is fully handled elsewhere (LDAP, RADIUS), we trust the provider for proper constraints, for local cases we move our implementations to _authenticate() in this commit.

--- src/opnsense/mvc/app/library/OPNsense/Auth/API.php
+++ src/opnsense/mvc/app/library/OPNsense/Auth/API.php
@@ -134,7 +134,7 @@ class API extends Base implements IAuthConnector
      * @param string $password user password
      * @return bool authentication status
      */
-    public function authenticate($username, $password)
+    public function _authenticate($username, $password)
     {
         // reset auth properties
         $this->lastAuthProperties = array();
