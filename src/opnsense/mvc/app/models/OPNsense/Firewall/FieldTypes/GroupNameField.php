murosorg/muros

firewall: groups: Enhance validation that group name can not start or end with a digit. (#8022)

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/GroupNameField.php
@@ -58,10 +58,10 @@ class GroupNameField extends BaseField
                         $result[] = gettext('Only letters, digits and underscores are allowed as the group name.');
                     }
                     if (!empty($value) && strlen($value) > 15) {
-                        $result[] = gettext('The group name shall not be longer than 15 characters.');
+                        $result[] = gettext('The group name must not be longer than 15 characters.');
                     }
-                    if (preg_match('/[0-9]$/', $value, $match)) {
-                        $result[] = gettext('The group name shall not end in a digit.');
+                    if (preg_match('/^[0-9]|[0-9]$/', $value, $match)) {
+                        $result[] = gettext('The group name must not start or end with a digit.');
                     }
                     $cnf = Config::getInstance()->object();
                     if (!empty($cnf->$value) && empty($cnf->$value->virtual)) {

opnsense/core

(cherry picked from commit 9425ca700db91d5ea56825c03b4c81afc9f6b94d)
(cherry picked from commit 2616ace119f590ea39403bd80b4735eeae9d4ad1)

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/GroupNameField.php
@@ -60,8 +60,8 @@ class GroupNameField extends BaseField
                     if (!empty($value) && strlen($value) > 15) {
                         $result[] = gettext('The group name shall not be longer than 15 characters.');
                     }
-                    if (preg_match('/[0-9]$/', $value, $match)) {
-                        $result[] = gettext('The group name shall not end in a digit.');
+                    if (preg_match('/^[0-9]|[0-9]$/', $value, $match)) {
+                        $result[] = gettext('The group name shall not start or end with a digit.');
                     }
                     $cnf = Config::getInstance()->object();
                     if (!empty($cnf->$value) && empty($cnf->$value->virtual)) {