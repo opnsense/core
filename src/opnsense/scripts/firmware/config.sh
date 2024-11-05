452a7355b00a3fb4a8b9b6b39073ac749e8eb08d
Franco Fichtner <franco@opnsense.org>
2024-11-05 13:19:34 +0100

    firmware: rework CRL check in config.sh

--- src/opnsense/scripts/firmware/config.sh
+++ src/opnsense/scripts/firmware/config.sh
@@ -55,15 +55,6 @@ upgrade
 # initialize environment to operate in
 env_init()
 {
-	CRL_FILE=/tmp/libfetch_crl.$(date +"%y%m%d%H")
-
-	# clean up old CRL files if found
-	for FILE in $(find /tmp/ -name "libfetch_crl.*"); do
-		if [ "${FILE}" != "${CRL_FILE}" ]; then
-			rm -f ${FILE}
-		fi
-	done
-
 	if [ -n "$(opnsense-update -x)" ]; then
 		# business mirror compliance requires
 		# disabling the use of TLS below 1.3
@@ -71,30 +62,16 @@ env_init()
 		export SSL_NO_TLS1_1="yes"
 		export SSL_NO_TLS1_2="yes"
 
-		# refresh CRL file for libfetch consumption
-		if [ ! -s ${CRL_FILE} ]; then
-			# collect firmware-relevant hostnames using TLS
-			# in order to prepare a matching CRL bundle
-			HOSTS=$(/usr/local/opnsense/scripts/firmware/hostnames.sh)
-			CRL_TMP=$(mktemp -q /tmp/libfetch_tmp.XXXXXX)
-
-			if /usr/local/opnsense/scripts/system/update-crl-fetch.py \
-			    ${HOSTS} > ${CRL_TMP}; then
-				mv ${CRL_TMP} ${CRL_FILE}
-			else
-				# in case of problems clear the file and leave an
-				# empty one for the next run also in order to let
-				# libfetch complain about the missing CRLs
-				rm -f ${CRL_TMP}
-				: > ${CRL_FILE}
-			fi
+		# refresh CRL files for libfetch consumption...
+		HOSTS=$(/usr/local/opnsense/scripts/firmware/hostnames.sh)
+		if /usr/local/opnsense/scripts/system/update-crl-fetch.py ${HOSTS}; then
+			/usr/local/opnsense/scripts/system/certctl.py rehash
 		fi
 
-		# CRL file is ready for use now
-		export SSL_CRL_FILE="${CRL_FILE}"
-	else
-		# unused so ok to remove
-		rm -f ${CRL_FILE}
+		# ...and then tell libfetch to verify from trust store
+		export SSL_CA_CERT_PATH="/etc/ssl/certs"
+		export SSL_CRL_OPTIONAL="yes"
+		export SSL_CRL_VERIFY="yes"
 	fi
 }
 
