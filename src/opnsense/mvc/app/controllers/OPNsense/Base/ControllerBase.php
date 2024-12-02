9d6e6e5c7ce83a29f5e4963dbec97aa8f5eb8061
Ad Schellevis <ad@opnsense.org>
2024-12-02 11:35:55 +0100

    ui: upgrade fontawesome icons to version 6, closes https://github.com/opnsense/core/issues/8101
    to ease maintenance, move all files into assets/fontawesome and swap css includes to new location.

--- src/opnsense/mvc/app/controllers/OPNsense/Base/ControllerBase.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Base/ControllerBase.php
@@ -84,7 +84,10 @@ class ControllerBase extends ControllerRoot
             // bootstrap dialog
             '/css/bootstrap-dialog.css',
             // Font awesome
-            '/ui/css/font-awesome.min.css',
+            '/ui/assets/fontawesome/css/fontawesome.min.css',
+            '/ui/assets/fontawesome/css/solid.min.css',
+            '/ui/assets/fontawesome/css/brands.min.css',
+            '/ui/assets/fontawesome/css/v4-shims.min.css',
             // JQuery Tokenize2 (https://zellerda.github.io/Tokenize2/)
             '/css/tokenize2.css',
             // Bootgrid (grid system from http://www.jquery-bootgrid.com/ )
