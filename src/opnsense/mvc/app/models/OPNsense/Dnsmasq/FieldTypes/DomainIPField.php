e93833258b67974a1ee15fe0f604d64161308daa
Ad Schellevis <AdSchellevis@users.noreply.github.com>
2025-02-03 10:07:02 +0000

    dnsmasq: migrate to MVC/API - phase 1 : frontend migration (#8260)
    * dnsmasq: migrate to MVC/API, initial boilerplate for https://github.com/opnsense/core/issues/7905
    
    * dnsmasq: migrate to MVC/API, work in progress for https://github.com/opnsense/core/issues/7905
    
    * add general tab fields
    * initial hosts tab with inline migration
    
    * dnsmasq: migrate to MVC/API, replace current frontend for https://github.com/opnsense/core/issues/7905
    
    More or less feature complete refactory of the current frontend, which should be a drop-in replacement for the legacy frontend.
    The default migration will force an inline migration for aliases and domain ip addresses (which in legacy is a combined field).
    
    * dnsmasq: migrate to MVC/API, missing isset() -> !empty() for  https://github.com/opnsense/core/issues/7905

---
+++ src/opnsense/mvc/app/models/OPNsense/Dnsmasq/FieldTypes/DomainIPField.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Copyright (C) 2025 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Dnsmasq\FieldTypes;
+
+use OPNsense\Base\FieldTypes\NetworkField;
+
+class DomainIPField extends NetworkField
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setValue($value)
+    {
+        if (is_a($value, 'SimpleXMLElement')) {
+            if (strpos((string)$value, '#') > 1 || strpos((string)$value, '@') > 1) {
+                /* convert legacy input */
+                $parts = explode("@", (string)$value);
+                if (count($parts) > 1) {
+                    $this->getParentNode()->srcip = $parts[1];
+                }
+                $parts = explode("#", $parts[0]);
+                if (count($parts) > 1) {
+                    $this->getParentNode()->port = $parts[1];
+                }
+                return parent::setValue($parts[0]);
+            } else {
+                return parent::setValue($value);
+            }
+        } elseif ($value == '!') {
+            return parent::setValue('');
+        } elseif (!empty($value)) {
+            /* update only */
+            return parent::setValue($value);
+        }
+    }
+}
