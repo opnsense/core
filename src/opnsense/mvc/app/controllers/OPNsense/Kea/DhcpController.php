e55a84ce552c38598a60ec3dcbbdbb252d8d5800
Monviech <79600909+Monviech@users.noreply.github.com>
2025-02-04 11:05:02 +0100

    dhcp/kea: Integrate layout_partials/base_bootgrid_table and base_apply_button (#8253)
    (cherry picked from commit 855bd7a65bda7c1531b67ff67847f6b9cbbbd124)
    (cherry picked from commit 24a61420cf764118f17d8732584cd27bcda54f98)
    (cherry picked from commit 8a9fe288c3db6ca5bccb133b8614b6896c7d9031)
    (cherry picked from commit bac5dfe99ec3a0456635777eca2305a72cc9eba2)

--- src/opnsense/mvc/app/controllers/OPNsense/Kea/DhcpController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Kea/DhcpController.php
@@ -50,9 +50,15 @@ class DhcpController extends \OPNsense\Base\IndexController
     {
         $this->view->pick('OPNsense/Kea/dhcpv4');
         $this->view->formGeneralSettings = $this->getForm("generalSettings4");
+
         $this->view->formDialogSubnet = $this->getForm("dialogSubnet4");
+        $this->view->formGridSubnet = $this->getFormGrid("dialogSubnet4");
+
         $this->view->formDialogReservation = $this->getForm("dialogReservation4");
+        $this->view->formGridReservation = $this->getFormGrid("dialogReservation4");
+
         $this->view->formDialogPeer = $this->getForm("dialogPeer4");
+        $this->view->formGridPeer = $this->getFormGrid("dialogPeer4");
     }
 
     public function leases4Action()
