murosorg/muros

dnsmasq: migrate to MVC/API - phase 1 : frontend migration (#8260)
* dnsmasq: migrate to MVC/API, initial boilerplate for https://github.com/opnsense/core/issues/7905

* dnsmasq: migrate to MVC/API, work in progress for https://github.com/opnsense/core/issues/7905

* add general tab fields
* initial hosts tab with inline migration

* dnsmasq: migrate to MVC/API, replace current frontend for https://github.com/opnsense/core/issues/7905

More or less feature complete refactory of the current frontend, which should be a drop-in replacement for the legacy frontend.
The default migration will force an inline migration for aliases and domain ip addresses (which in legacy is a combined field).

* dnsmasq: migrate to MVC/API, missing isset() -> !empty() for  https://github.com/opnsense/core/issues/7905

src/opnsense/mvc/app/controllers/OPNsense/Dnsmasq/Api/SettingsController.php
@@ -1,0 +1,87 @@
+<?php
+
+/*
+ * Copyright (C) 2025 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Dnsmasq\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+
+class SettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'dnsmasq';
+    protected static $internalModelClass = '\OPNsense\Dnsmasq\Dnsmasq';
+
+    public function searchHostAction()
+    {
+        return $this->searchBase('hosts');
+    }
+
+    public function getHostAction($uuid = null)
+    {
+        return $this->getBase('host', 'hosts', $uuid);
+    }
+
+    public function setHostAction($uuid)
+    {
+        return $this->setBase('host', 'hosts', $uuid);
+    }
+
+    public function addHostAction()
+    {
+        return $this->addBase('host', 'hosts');
+    }
+
+    public function delHostAction($uuid)
+    {
+        return $this->delBase('hosts', $uuid);
+    }
+
+    public function searchDomainAction()
+    {
+        return $this->searchBase('domainoverrides');
+    }
+
+    public function getDomainAction($uuid = null)
+    {
+        return $this->getBase('domainoverride', 'domainoverrides', $uuid);
+    }
+
+    public function setDomainAction($uuid)
+    {
+        return $this->setBase('domainoverride', 'domainoverrides', $uuid);
+    }
+
+    public function addDomainAction()
+    {
+        return $this->addBase('domainoverride', 'domainoverrides');
+    }
+
+    public function delDomainAction($uuid)
+    {
+        return $this->delBase('domainoverride', $uuid);
+    }
+}

opnsense/core

(cherry picked from commit e93833258b67974a1ee15fe0f604d64161308daa)
(cherry picked from commit 163cbd273598d28f7bb34eccf7b6352fae5ec806)
(cherry picked from commit 65a74b42d59925646cd94eaf13409f3c6fc6756e)
(cherry picked from commit 44eaf077b8b931a14870b3ee0078671833041148)
(cherry picked from commit 986dcb23245593df49c0dceb4fb1520c225bb694)
(cherry picked from commit d620298cbe7e724c91876d183cb22606c4d589ba)
(cherry picked from commit 0063b16b3ed2328f5d10e5c09edef25c757bee44)
(cherry picked from commit e19e1edff38b48f4cb1dfa66c1d5bb120a756ac7)

src/opnsense/mvc/app/controllers/OPNsense/Dnsmasq/Api/SettingsController.php
@@ -1,0 +1,87 @@
+<?php
+
+/*
+ * Copyright (C) 2025 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Dnsmasq\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+
+class SettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'dnsmasq';
+    protected static $internalModelClass = '\OPNsense\Dnsmasq\Dnsmasq';
+
+    public function searchHostAction()
+    {
+        return $this->searchBase('hosts');
+    }
+
+    public function getHostAction($uuid = null)
+    {
+        return $this->getBase('host', 'hosts', $uuid);
+    }
+
+    public function setHostAction($uuid)
+    {
+        return $this->setBase('host', 'hosts', $uuid);
+    }
+
+    public function addHostAction()
+    {
+        return $this->addBase('host', 'hosts');
+    }
+
+    public function delHostAction($uuid)
+    {
+        return $this->delBase('hosts', $uuid);
+    }
+
+    public function searchDomainAction()
+    {
+        return $this->searchBase('domainoverrides');
+    }
+
+    public function getDomainAction($uuid = null)
+    {
+        return $this->getBase('domainoverride', 'domainoverrides', $uuid);
+    }
+
+    public function setDomainAction($uuid)
+    {
+        return $this->setBase('domainoverride', 'domainoverrides', $uuid);
+    }
+
+    public function addDomainAction()
+    {
+        return $this->addBase('domainoverride', 'domainoverrides');
+    }
+
+    public function delDomainAction($uuid)
+    {
+        return $this->delBase('domainoverrides', $uuid);
+    }
+}