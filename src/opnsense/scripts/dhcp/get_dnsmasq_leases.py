bcf8f9ae757d96cd14470719bc87922fc53e4d9b
Ad Schellevis <AdSchellevis@users.noreply.github.com>
2025-02-19 16:40:55 +0000

    dnsmasq: Backend migration and add dhcp support for https://github.com/opnsense/core/issues/8329 (#8355)
    This rather large commit implements most relevant dhcp options and rewrites dnsmasq's backend.
    
    By default dnsmasq is disabled, eventually we do want dnsmasq enabled for dhcp services by default, but dns itself disabled. For this reason we support port "0" as implemented at dnsmasq (not listening for dns).
    
    For cases where users want to integrate dns and dhcp services, the advise is to make dnsmasq listen on a non standard port and point unbound to the zones where dnsmasq is responsible for. This has the advantage of a direct connection between dhcp registered hosts and the requesting service. In these cases dnsmasq's dns service acts like a "connector".
    
    In the long run we should deprecate `regdhcpstatic` and `regdhcp` as these either belong to legacy isc-dhcp or hook kea entries (which are better served via unbound).
    
    The first mvc migration phase implemented IndexController.php, which we rename to SettingsController.php now as these results in more logical ui endpoints.
    
    Since we don't bind to addresses directly (unless specifically configured and adviced only for static setups), we can skip the newwanip event which means we don't restart the service on interface changes. dnsmasq is able to filter the relevant networks on the fly, which is the advised scenario and can cope more easily with changes.
    
    When different clients need to receive different options, we can use "tags" now. Requests can add tags to filter options which will be offered to the client, in the most simple scenario one would tag on a range or a host reservation, but more advanced choices can also be achieved using match statements (for example architecture [client-arch])

---
+++ src/opnsense/scripts/dhcp/get_dnsmasq_leases.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python3
+
+"""
+    Copyright (c) 2025 Ad Schellevis <ad@opnsense.org>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import ipaddress
+import subprocess
+import os
+import ujson
+
+
+if __name__ == '__main__':
+    filename = '/var/db/dnsmasq.leases'
+    ranges = {}
+    leases = []
+    this_interface = None
+    ifconfig = subprocess.run(['/sbin/ifconfig', '-f', 'inet:cidr,inet6:cidr'], capture_output=True, text=True).stdout
+    for line in ifconfig.split('\n'):
+        if not line.startswith("\t") and line.find(':') > -1:
+            this_interface = line.strip().split(':')[0]
+        elif this_interface is not None and line.startswith("\tinet") and line.find('-->') == -1:
+            ranges[ipaddress.ip_network(line.split()[1], strict=False)] = this_interface
+
+    if os.path.isfile(filename):
+        with open(filename, 'r') as leasefile:
+            for line in leasefile:
+                parts = line.split()
+                if len(parts) > 4 and parts[0].isdigit():
+                    lease = {
+                        'expire': int(parts[0]),
+                        'hwaddr': parts[1],
+                        'address': parts[2],
+                        'hostname': parts[3],
+                        'client_id': parts[4]
+                    }
+                    for net in ranges:
+                        if net.overlaps(ipaddress.ip_network(lease['address'])):
+                            lease['if'] = ranges[net]
+                            break
+                    leases.append(lease)
+
+    print (ujson.dumps({'records': leases}))
