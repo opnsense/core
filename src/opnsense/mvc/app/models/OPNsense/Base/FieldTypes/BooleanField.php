7365ed5f5e09edc30ce2675a23470057463bea34
Ad Schellevis <ad@opnsense.org>
2025-03-04 11:22:19 +0100

    mvc: BooleanField defaults to 0 on creation, this will not change any empty node, just changes how these are  being created for the first time.
    (cherry picked from commit f569ead1a5371731ed3fe06cdd93b89b999445e0)

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BooleanField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BooleanField.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015 Deciso B.V.
+ * Copyright (C) 2015-2025 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -40,6 +40,12 @@ class BooleanField extends BaseField
      * @var bool marks if this is a data node or a container
      */
     protected $internalIsContainer = false;
+
+    /**
+     * @var string force default to 0 when unspecified
+     */
+    protected $internalDefaultValue = "0";
+    protected $internalValue = "0";
 
     /**
      * {@inheritdoc}
