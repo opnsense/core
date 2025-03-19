aa02477fa9ddec9a5416cb28584c5a51895a94b4
Monviech <gitacc@pischem.com>
2025-03-19 10:51:14 +0000

    dnsmasq: Add option6 to dhcp_options_match

--- src/opnsense/mvc/app/models/OPNsense/Dnsmasq/Dnsmasq.php
+++ src/opnsense/mvc/app/models/OPNsense/Dnsmasq/Dnsmasq.php
@@ -187,6 +187,31 @@ class Dnsmasq extends BaseModel
             }
         }
 
+        foreach ($this->dhcp_options_match->iterateItems() as $match) {
+            if (!$validateFullModel && !$match->isFieldChanged()) {
+                continue;
+            }
+            $key = $match->__reference;
+
+            if (!$match->option->isEmpty() && !$match->option6->isEmpty()) {
+                $messages->appendMessage(
+                    new Message(
+                        gettext("'Option' and 'Option6' cannot be selected at the same time."),
+                        $key . ".option"
+                    )
+                );
+            }
+
+            if ($match->option->isEmpty() && $match->option6->isEmpty()) {
+                $messages->appendMessage(
+                    new Message(
+                        gettext("Either 'Option' or 'Option6' is required."),
+                        $key . ".option"
+                    )
+                );
+            }
+        }
+
         if (
             ($validateFullModel || $this->enable->isFieldChanged() || $this->port->isFieldChanged()) &&
             !empty((string)$this->enable)
