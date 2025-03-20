murosorg/muros

vpn/openvpn: Implement base_bootgrid_table and base_apply_button (#8332)
* vpn/openvpn: Implement base_bootgrid_table and base_apply_button for https://github.com/opnsense/core/issues/8318

* vpn/openvpn: overflow-y in column dropdown due to amount of items in grid

src/opnsense/mvc/app/views/OPNsense/OpenVPN/instances.volt
@@ -28,7 +28,7 @@
     'use strict';
 
     $( document ).ready(function () {
-        let grid_instances = $("#grid-instances").UIBootgrid({
+        const grid_instances = $("#{{formGridInstance['table_id']}}").UIBootgrid({
             search:'/api/openvpn/instances/search/',
             get:'/api/openvpn/instances/get/',
             add:'/api/openvpn/instances/add/',
@@ -52,7 +52,7 @@
             }
         });
 
-        let grid_statickeys = $("#grid-statickeys").UIBootgrid({
+        const grid_statickeys = $("#{{formGridStaticKey['table_id']}}").UIBootgrid({
             search:'/api/openvpn/instances/search_static_key/',
             get:'/api/openvpn/instances/get_static_key/',
             add:'/api/openvpn/instances/add_static_key/',
@@ -61,11 +61,11 @@
         });
 
         $("#instance\\.role, #instance\\.dev_type").change(function(){
-            let show_advanced = $("#show_advanced_formDialogDialogInstance").hasClass("fa-toggle-on");
-            let this_role = $("#instance\\.role").val();
-            let this_dev_type = $("#instance\\.dev_type").val();
+            const show_advanced = $("#show_advanced_formDialogDialogInstance").hasClass("fa-toggle-on");
+            const this_role = $("#instance\\.role").val();
+            const this_dev_type = $("#instance\\.dev_type").val();
             $(".role").each(function(){
-                let tr = $(this).closest("tr").hide();
+                const tr = $(this).closest("tr").hide();
 
                 if ((tr.data('advanced') === true && show_advanced) || !tr.data('advanced')) {
                     if ($(this).hasClass('role_' + this_role) || $(this).hasClass('role_' + this_role + '_' + this_dev_type)) {
@@ -94,6 +94,15 @@
 
 </script>
 
+<style>
+    /* The instances grid column dropdown has many items */
+    .actions .dropdown-menu.pull-right {
+        max-height: 400px;
+        min-width: max-content;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+</style>
 
 <ul class="nav nav-tabs" data-tabs="tabs" id="maintabs">
     <li class="active"><a data-toggle="tab" href="#instances">{{ lang._('Instances') }}</a></li>
@@ -101,71 +110,17 @@
 </ul>
 <div class="tab-content content-box">
     <div id="instances" class="tab-pane fade in active">
-        <table id="grid-instances" class="table table-condensed table-hover table-striped table-responsive" data-editDialog="DialogInstance" data-editAlert="InstanceChangeMessage">
-            <thead>
-                <tr>
-                    <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-                    <th data-column-id="enabled" data-width="6em" data-type="string" data-formatter="rowtoggle">{{ lang._('Enabled') }}</th>
-                    <th data-column-id="description" data-type="string">{{ lang._('Description') }}</th>
-                    <th data-column-id="role" data-type="string">{{ lang._('Role') }}</th>
-                    <th data-column-id="dev_type" data-type="string">{{ lang._('Type') }}</th>
-                    <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td>
-                        <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                        <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-                    </td>
-                </tr>
-            </tfoot>
-          </table>
-        <div class="col-md-12">
-            <div id="InstanceChangeMessage" class="alert alert-info" style="display: none" role="alert">
-                {{ lang._('After changing settings, please remember to apply them') }}
-            </div>
-            <hr/>
-            <button class="btn btn-primary" id="reconfigureAct"
-                    data-endpoint='/api/openvpn/service/reconfigure'
-                    data-label="{{ lang._('Apply') }}"
-                    data-error-title="{{ lang._('Error reconfiguring openvpn') }}"
-                    type="button"
-            ></button>
-            <br/><br/>
-        </div>
-      </div>
-      <div id="statickeys" class="tab-pane fade in">
+        {{ partial('layout_partials/base_bootgrid_table', formGridInstance)}}
+    </div>
+    <div id="statickeys" class="tab-pane fade in">
         <span id="keygen_div" style="display:none" class="pull-right">
             <button id="keygen" type="button" class="btn btn-secondary" title="{{ lang._('Generate new.') }}" data-toggle="tooltip">
-              <i class="fa fa-fw fa-gear"></i>
+                <i class="fa fa-fw fa-gear"></i>
             </button>
         </span>
-        <table id="grid-statickeys" class="table table-condensed table-hover table-striped table-responsive" data-editDialog="DialogStaticKey">
-            <thead>
-                <tr>
-                    <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-                    <th data-column-id="description" data-type="string">{{ lang._('Description') }}</th>
-                    <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td>
-                        <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                        <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-      </div>
+        {{ partial('layout_partials/base_bootgrid_table', formGridStaticKey)}}
+    </div>
 </div>
-
-{{ partial("layout_partials/base_dialog",['fields':formDialogInstance,'id':'DialogInstance','label':lang._('Edit Instance')])}}
-{{ partial("layout_partials/base_dialog",['fields':formDialogStaticKey,'id':'DialogStaticKey','label':lang._('Edit Static Key')])}}
+{{ partial('layout_partials/base_apply_button', {'data_endpoint': '/api/openvpn/service/reconfigure'}) }}
+{{ partial("layout_partials/base_dialog",['fields':formDialogInstance,'id':formGridInstance['edit_dialog_id'],'label':lang._('Edit Instance')])}}
+{{ partial("layout_partials/base_dialog",['fields':formDialogStaticKey,'id':formGridStaticKey['edit_dialog_id'],'label':lang._('Edit Static Key')])}}

opnsense/core

(cherry picked from commit dd999d732c0feee3bfa9c0497827321089cd0083)
(cherry picked from commit 01ceab0f025babd1577ca485c0a339df5edb67fb)

src/opnsense/mvc/app/views/OPNsense/OpenVPN/instances.volt
@@ -28,7 +28,7 @@
     'use strict';
 
     $( document ).ready(function () {
-        let grid_instances = $("#grid-instances").UIBootgrid({
+        const grid_instances = $("#{{formGridInstance['table_id']}}").UIBootgrid({
             search:'/api/openvpn/instances/search/',
             get:'/api/openvpn/instances/get/',
             add:'/api/openvpn/instances/add/',
@@ -52,7 +52,7 @@
             }
         });
 
-        let grid_statickeys = $("#grid-statickeys").UIBootgrid({
+        const grid_statickeys = $("#{{formGridStaticKey['table_id']}}").UIBootgrid({
             search:'/api/openvpn/instances/search_static_key/',
             get:'/api/openvpn/instances/get_static_key/',
             add:'/api/openvpn/instances/add_static_key/',
@@ -61,11 +61,11 @@
         });
 
         $("#instance\\.role, #instance\\.dev_type").change(function(){
-            let show_advanced = $("#show_advanced_formDialogDialogInstance").hasClass("fa-toggle-on");
-            let this_role = $("#instance\\.role").val();
-            let this_dev_type = $("#instance\\.dev_type").val();
+            const show_advanced = $("#show_advanced_formDialogdialog_dialogInstance").hasClass("fa-toggle-on");
+            const this_role = $("#instance\\.role").val();
+            const this_dev_type = $("#instance\\.dev_type").val();
             $(".role").each(function(){
-                let tr = $(this).closest("tr").hide();
+                const tr = $(this).closest("tr").hide();
 
                 if ((tr.data('advanced') === true && show_advanced) || !tr.data('advanced')) {
                     if ($(this).hasClass('role_' + this_role) || $(this).hasClass('role_' + this_role + '_' + this_dev_type)) {
@@ -74,7 +74,7 @@
                 }
             });
         });
-        $("#show_advanced_formDialogDialogInstance").click(function(){
+        $("#show_advanced_formDialogdialog_dialogInstance").click(function(){
             $("#instance\\.role").change();
         });
 
@@ -94,6 +94,15 @@
 
 </script>
 
+<style>
+    /* The instances grid column dropdown has many items */
+    .actions .dropdown-menu.pull-right {
+        max-height: 400px;
+        min-width: max-content;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+</style>
 
 <ul class="nav nav-tabs" data-tabs="tabs" id="maintabs">
     <li class="active"><a data-toggle="tab" href="#instances">{{ lang._('Instances') }}</a></li>
@@ -101,71 +110,17 @@
 </ul>
 <div class="tab-content content-box">
     <div id="instances" class="tab-pane fade in active">
-        <table id="grid-instances" class="table table-condensed table-hover table-striped table-responsive" data-editDialog="DialogInstance" data-editAlert="InstanceChangeMessage">
-            <thead>
-                <tr>
-                    <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-                    <th data-column-id="enabled" data-width="6em" data-type="string" data-formatter="rowtoggle">{{ lang._('Enabled') }}</th>
-                    <th data-column-id="description" data-type="string">{{ lang._('Description') }}</th>
-                    <th data-column-id="role" data-type="string">{{ lang._('Role') }}</th>
-                    <th data-column-id="dev_type" data-type="string">{{ lang._('Type') }}</th>
-                    <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td>
-                        <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                        <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-                    </td>
-                </tr>
-            </tfoot>
-          </table>
-        <div class="col-md-12">
-            <div id="InstanceChangeMessage" class="alert alert-info" style="display: none" role="alert">
-                {{ lang._('After changing settings, please remember to apply them') }}
-            </div>
-            <hr/>
-            <button class="btn btn-primary" id="reconfigureAct"
-                    data-endpoint='/api/openvpn/service/reconfigure'
-                    data-label="{{ lang._('Apply') }}"
-                    data-error-title="{{ lang._('Error reconfiguring openvpn') }}"
-                    type="button"
-            ></button>
-            <br/><br/>
-        </div>
-      </div>
-      <div id="statickeys" class="tab-pane fade in">
+        {{ partial('layout_partials/base_bootgrid_table', formGridInstance)}}
+    </div>
+    <div id="statickeys" class="tab-pane fade in">
         <span id="keygen_div" style="display:none" class="pull-right">
             <button id="keygen" type="button" class="btn btn-secondary" title="{{ lang._('Generate new.') }}" data-toggle="tooltip">
-              <i class="fa fa-fw fa-gear"></i>
+                <i class="fa fa-fw fa-gear"></i>
             </button>
         </span>
-        <table id="grid-statickeys" class="table table-condensed table-hover table-striped table-responsive" data-editDialog="DialogStaticKey">
-            <thead>
-                <tr>
-                    <th data-column-id="uuid" data-type="string" data-identifier="true" data-visible="false">{{ lang._('ID') }}</th>
-                    <th data-column-id="description" data-type="string">{{ lang._('Description') }}</th>
-                    <th data-column-id="commands" data-width="7em" data-formatter="commands" data-sortable="false">{{ lang._('Commands') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td>
-                        <button data-action="add" type="button" class="btn btn-xs btn-primary"><span class="fa fa-fw fa-plus"></span></button>
-                        <button data-action="deleteSelected" type="button" class="btn btn-xs btn-default"><span class="fa fa-fw fa-trash-o"></span></button>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-      </div>
+        {{ partial('layout_partials/base_bootgrid_table', formGridStaticKey)}}
+    </div>
 </div>
-
-{{ partial("layout_partials/base_dialog",['fields':formDialogInstance,'id':'DialogInstance','label':lang._('Edit Instance')])}}
-{{ partial("layout_partials/base_dialog",['fields':formDialogStaticKey,'id':'DialogStaticKey','label':lang._('Edit Static Key')])}}
+{{ partial('layout_partials/base_apply_button', {'data_endpoint': '/api/openvpn/service/reconfigure'}) }}
+{{ partial("layout_partials/base_dialog",['fields':formDialogInstance,'id':formGridInstance['edit_dialog_id'],'label':lang._('Edit Instance')])}}
+{{ partial("layout_partials/base_dialog",['fields':formDialogStaticKey,'id':formGridStaticKey['edit_dialog_id'],'label':lang._('Edit Static Key')])}}