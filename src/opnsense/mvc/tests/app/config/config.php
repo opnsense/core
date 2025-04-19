454253ade304c1245839a4f900726dd53521d7e8
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-19 16:28:42 +0100

    style: remove alignment

--- src/opnsense/mvc/tests/app/config/config.php
+++ src/opnsense/mvc/tests/app/config/config.php
@@ -31,12 +31,12 @@ require_once __DIR__ . '/../../../app/config/AppConfig.php';
 return new OPNsense\Core\AppConfig([
     'application' => [
         'controllersDir' => __DIR__ . '/../../../app/controllers/',
-        'modelsDir'      => __DIR__ . '/../../../app/models/',
-        'viewsDir'       => __DIR__ . '/../../../app/views/',
-        'pluginsDir'     => __DIR__ . '/../../../app/plugins/',
-        'libraryDir'     => __DIR__ . '/../../../app/library/',
-        'cacheDir'       => __DIR__ . '/../../../app/cache/',
-        'contribDir'     => __DIR__ . '/../../../../contrib/',
+        'modelsDir' => __DIR__ . '/../../../app/models/',
+        'viewsDir' => __DIR__ . '/../../../app/views/',
+        'pluginsDir' => __DIR__ . '/../../../app/plugins/',
+        'libraryDir' => __DIR__ . '/../../../app/library/',
+        'cacheDir' => __DIR__ . '/../../../app/cache/',
+        'contribDir' => __DIR__ . '/../../../../contrib/',
         'baseUri' => '/opnsense_gui/',
     ],
     'globals' => [
