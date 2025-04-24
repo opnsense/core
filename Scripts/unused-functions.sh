bc9f73a5622b607944f9291885b6489962168dae
Franco Fichtner <franco@opnsense.org>
2025-04-24 09:35:40 +0200

    Scripts: typo

--- Scripts/unused-functions.sh
+++ Scripts/unused-functions.sh
@@ -25,7 +25,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-PLUGINS="configure cron devices firewall interfaces run services syslog xmlprc_sync"
+PLUGINS="configure cron devices firewall interfaces run services syslog xmlrpc_sync"
 
 if [ -n "${1}" ]; then
 	cd "${1}"
