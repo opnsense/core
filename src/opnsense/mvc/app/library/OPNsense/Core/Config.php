9de60e6bae7ff11a711984c6c58c4c306597d173
Ad Schellevis <ad@opnsense.org>
2025-05-07 20:58:23 +0200

    webui: (experimental) "non root" user mode. (https://github.com/opnsense/core/issues/8521)
    Allow the webui to run as wwwonly and move related (temp) files to their own directories so we limit the choice of mangling rights.
    
    When trying to transition back from wwwonly to root, require a file (/var/run/www_non_root) to be removed via the console as an extra barrier.

--- src/opnsense/mvc/app/library/OPNsense/Core/Config.php
+++ src/opnsense/mvc/app/library/OPNsense/Core/Config.php
@@ -348,6 +348,7 @@ class Config extends Singleton
             // in case there are no backups, restore defaults.
             $logger->error(gettext('No valid config.xml found, attempting to restore factory config.'));
             $this->restoreBackup('/usr/local/etc/config.xml');
+            chown('/conf/config.xml', 'wwwonly');   /* frontend owns file */
         }
     }
 
