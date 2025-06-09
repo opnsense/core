opnsense/core

commit 78cf96c579b20643b806228a55f54ca8b5692b52

System: Access: migrate Users and Groups to MVC/API (#8046)
* System: Access: Groups - in preperation for https://github.com/opnsense/core/issues/7904, add support for comma separated member lists.

If we convert groups to a model, we will switch the nested <member> tags into comma separated fields, e.g.

	<member>1</member>
	<member>12</member>

will convert to:

	<member>1,12</member>

using this commit we support both for areas where these are being read.

* System: Access: Users - refactor to MVC (https://github.com/opnsense/core/issues/7904)

* add initial boilerplate
* unpack `<priv/>` field on first access
* unpack '<apikeys/>' field on first access and implement key actions into ApiKeyField
* add apikey grid in user management view
* change isset() to !empty() for users disabled flag in backend code
* move user atributes into dialog
* hook PrivField type to \OPNsense\Core\ACL()
* refactor Auth/API to use new User class
* otp seed logic  with simple api call to generate new seeds and some JS glue for the frontend
* uid autonumber field
* language selector using get_locale_list() via configd (cached)
* add StoreB64Field field for authorizedkeys so we can keep the field contents backwards compatible.
* ExpiresField for custom date parsing, supporting previous input formats as well.
* group membership using a volatile custom field type, controller is responisble for persisting the configuration data to avoid entanglement between models
* add button which links to most likely user certs (based on commonname), to avoid all sorts of magic to reflect certs back into the usermanager.
* add getUserPrivs() to model so we can fetch a full list of privs for a user
* show user icons, long this might be less relevant
* add addApiKeyAction() to create a new api key for a user (by name)
* download new api key from user view
* implement hashing when setting a new (or scrabled) password

src/opnsense/mvc/app/library/OPNsense/Auth/Base.php
@@ -47,7 +47,7 @@ abstract class Base
     /**
      * @var array internal list of LDAP errors
      */
-    protected $lastAuthErrors = array();
+    protected $lastAuthErrors = [];
 
     /**
      * return group memberships
@@ -56,7 +56,7 @@ abstract class Base
      */
     private function groups($username)
     {
-        $groups = array();
+        $groups = [];
         $user = $this->getUser($username);
         if ($user != null) {
             $uid = (string)$user->uid;
@@ -86,7 +86,7 @@ abstract class Base
      */
     public function checkPolicy($username, $old_password, $new_password)
     {
-        return array();
+        return [];
     }
 
     /**
@@ -187,7 +187,7 @@ abstract class Base
             // update when changed
             if ($user == null && $createuser) {
                 // user creation when enabled
-                $add_user = json_decode((new Backend())->configdpRun("auth add user", array($username)), true);
+                $add_user = json_decode((new Backend())->configdpRun("auth add user",[$username]), true);
                 if (!empty($add_user) && $add_user['status'] == 'ok') {
                     Config::getInstance()->forceReload();
                     $user = $this->getUser($username);
@@ -202,34 +202,33 @@ abstract class Base
             foreach ($cnf->system->group as $group) {
                 $lc_groupname = strtolower((string)$group->name);
                 if (in_array($lc_groupname, $sync_groups)) {
-                    if (
-                        in_array((string)$user->uid, (array)$group->member)
-                          && empty($ldap_groups[$lc_groupname])
-                    ) {
-                        while (in_array((string)$user->uid, (array)$group->member)) {
-                            unset($group->member[array_search((string)$user->uid, (array)$group->member)]);
+                    $members = [];
+                    foreach ($group->member as $member) {
+                        $members = array_merge($members, explode(',', $member));
+                    }
+                    if (in_array((string)$user->uid, $members) && empty($ldap_groups[$lc_groupname])) {
+                        while (in_array((string)$user->uid, $members) && empty($ldap_groups[$lc_groupname])) {
+                            unset($members[array_search((string)$user->uid, $members)]);
                         }
+                        $group->member = implode(',', $members);
                         syslog(LOG_NOTICE, sprintf(
                             'User: policy change for %s unlink group %s',
                             $username,
                             (string)$group->name
                         ));
-                    } elseif (
-                        !in_array((string)$user->uid, (array)$group->member)
-                          && !empty($ldap_groups[$lc_groupname])
-                    ) {
+                    } elseif (!in_array((string)$user->uid, $members) && !empty($ldap_groups[$lc_groupname])) {
                         syslog(LOG_NOTICE, sprintf(
                             'User: policy change for %s link group %s [%s]',
                             $username,
                             (string)$group->name,
                             $ldap_groups[$lc_groupname]
                         ));
-                        $group->addChild('member', (string)$user->uid);
+                        $group->member = implode(',', array_merge($members, [(string)$user->uid]));
                     }
                 }
             }
             Config::getInstance()->save();
-            (new Backend())->configdpRun("auth user changed", array($username));
+            (new Backend())->configdpRun("auth user changed",[$username]);
         }
     }
 

plist
@@ -215,6 +215,15 @@
 /usr/local/opnsense/mvc/app/config/AppConfig.php
 /usr/local/opnsense/mvc/app/config/config.php
 /usr/local/opnsense/mvc/app/config/loader.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/Api/GroupController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/Api/PrivController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/Api/UserController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/GroupController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/PrivController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/UserController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/forms/dialogGroup.xml
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/forms/dialogPriv.xml
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Auth/forms/dialogUser.xml
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Base/ApiMutableModelControllerBase.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Base/ApiMutableServiceControllerBase.php
@@ -562,6 +571,20 @@
 /usr/local/opnsense/mvc/app/library/OPNsense/System/Status/SystemBootingStatus.php
 /usr/local/opnsense/mvc/app/library/OPNsense/System/SystemStatus.php
 /usr/local/opnsense/mvc/app/library/OPNsense/Trust/Store.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/ApiKeyField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/ExpiresField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/GidField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/GroupMembershipField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/MemberField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/PrivField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/StoreB64Field.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/FieldTypes/UidField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/Group.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/Group.xml
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/Priv.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/Priv.xml
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/User.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Auth/User.xml
 /usr/local/opnsense/mvc/app/models/OPNsense/Base/BaseModel.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Base/BaseModelMigration.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Base/BaseValidator.php
@@ -828,6 +851,9 @@
 /usr/local/opnsense/mvc/app/models/OPNsense/Wireguard/Migrations/M1_0_0.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Wireguard/Server.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Wireguard/Server.xml
+/usr/local/opnsense/mvc/app/views/OPNsense/Auth/group.volt
+/usr/local/opnsense/mvc/app/views/OPNsense/Auth/priv.volt
+/usr/local/opnsense/mvc/app/views/OPNsense/Auth/user.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/CaptivePortal/clients.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/CaptivePortal/index.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/CaptivePortal/vouchers.volt
@@ -1008,6 +1034,8 @@
 /usr/local/opnsense/scripts/Wireguard/wg_show.py
 /usr/local/opnsense/scripts/auth/add_user.php
 /usr/local/opnsense/scripts/auth/list_group_members.php
+/usr/local/opnsense/scripts/auth/sync_group.php
+/usr/local/opnsense/scripts/auth/sync_user.php
 /usr/local/opnsense/scripts/dhcp/cleanup_leases4.php
 /usr/local/opnsense/scripts/dhcp/cleanup_leases6.php
 /usr/local/opnsense/scripts/dhcp/dnsmasq_watcher.py
@@ -1231,6 +1259,7 @@
 /usr/local/opnsense/scripts/system/cpu.py
 /usr/local/opnsense/scripts/system/crl_fetch.py
 /usr/local/opnsense/scripts/system/flush_config_history
+/usr/local/opnsense/scripts/system/get_locales.php
 /usr/local/opnsense/scripts/system/nameservers.php
 /usr/local/opnsense/scripts/system/remote_backup.php
 /usr/local/opnsense/scripts/system/rfc5246_cipher_suites.csv
@@ -2243,10 +2272,6 @@
 /usr/local/www/system_gateway_groups.php
 /usr/local/www/system_gateway_groups_edit.php
 /usr/local/www/system_general.php
-/usr/local/www/system_groupmanager.php
-/usr/local/www/system_usermanager.php
-/usr/local/www/system_usermanager_addprivs.php
-/usr/local/www/system_usermanager_import_ldap.php
 /usr/local/www/system_usermanager_passwordmg.php
 /usr/local/www/system_usermanager_settings_ldapacpicker.php
 /usr/local/www/vpn_ipsec_mobile.php

src/etc/inc/auth.inc
@@ -216,31 +216,6 @@ function userIsAdmin($username)
     return userHasPrivilege($user, 'page-all');
 }
 
-function auth_get_shells($uid = 0)
-{
-    $shells = ['' => '/usr/sbin/nologin'];
-
-    if ($uid == 0) {
-        $shells = ['' => '/usr/local/sbin/opnsense-shell'];
-    }
-
-    $etc_shells = @file_get_contents('/etc/shells');
-    if (!empty($etc_shells)) {
-        $etc_shells = explode("\n", $etc_shells);
-        foreach ($etc_shells as $shell) {
-            $shell = trim($shell);
-            if (
-                !empty($shell) && strpos($shell, '#') !== 0 &&
-                strpos($shell, '/usr/local/sbin/opnsense-') !== 0
-            ) {
-                $shells[$shell] = $shell;
-            }
-        }
-    }
-
-    return $shells;
-}
-
 function local_sync_accounts()
 {
     global $config;
@@ -428,17 +403,6 @@ function local_user_set(&$user, $force_password = false, $userattrs = null)
     mwexecf('/usr/sbin/pw %s %s', array($lock_account, $user_name), true);
 }
 
-function local_user_del($user)
-{
-    /* remove all memberships */
-    local_user_set_groups($user);
-
-    /* delete from pw db */
-    mwexecf('/usr/sbin/pw userdel -n %s -r', $user['name']);
-
-    /* Delete user from groups needs a call to write_config() */
-    local_group_del_user($user);
-}
 
 function local_user_set_password(&$user, $password = null)
 {
@@ -508,77 +472,6 @@ function local_user_get_groups($user)
     return $groups;
 }
 
-function local_user_set_groups($user, $new_groups = null)
-{
-    global $config, $groupindex;
-
-    if (!isset($config['system']['group'])) {
-        return;
-    }
-
-    $cur_groups = local_user_get_groups($user);
-    $mod_groups = array();
-
-    if (!is_array($new_groups)) {
-        $new_groups = array();
-    }
-
-    if (!is_array($cur_groups)) {
-        $cur_groups = array();
-    }
-
-    /* determine which memberships to add */
-    foreach ($new_groups as $groupname) {
-        if (in_array($groupname, $cur_groups) || !isset($groupindex[$groupname])) {
-            // continue if group is already in current list or the groupname is invalid
-            continue;
-        }
-        $group = &config_read_array('system', 'group', $groupindex[$groupname]);
-        $group['member'][] = $user['uid'];
-        $mod_groups[] = $group;
-    }
-
-    /* determine which memberships to remove */
-    foreach ($cur_groups as $groupname) {
-        if (in_array($groupname, $new_groups)) {
-            continue;
-        }
-        if (!isset($config['system']['group'][$groupindex[$groupname]])) {
-            continue;
-        }
-        $group = &config_read_array('system', 'group', $groupindex[$groupname]);
-        if (is_array($group['member'])) {
-            $index = array_search($user['uid'], $group['member']);
-            array_splice($group['member'], $index, 1);
-            $mod_groups[] = $group;
-        }
-    }
-
-    /* sync all modified groups */
-    foreach ($mod_groups as $group) {
-        local_group_set($group);
-    }
-}
-
-function local_group_del_user($user)
-{
-    global $config;
-
-    if (!isset($config['system']['group'])) {
-        return;
-    }
-
-    foreach ($config['system']['group'] as $group) {
-        if (isset($group['member'])) {
-            foreach ($group['member'] as $idx => $uid) {
-                if ($user['uid'] == $uid) {
-                    unset($config['system']['group']['member'][$idx]);
-                }
-            }
-        }
-    }
-}
-
 function local_group_set($group)
 {
     if (!isset($group['name']) || !isset($group['gid'])) {
@@ -605,12 +498,6 @@ function local_group_set($group)
     }
 
     mwexecf('/usr/sbin/pw %s %s -g %s -M %s', array($group_op, $group_name, $group_gid, $group_members));
-}
-
-function local_group_del($group)
-{
-    /* delete from group db */
-    mwexecf('/usr/sbin/pw groupdel %s', $group['name']);
 }
 
 

src/opnsense/mvc/app/controllers/OPNsense/Auth/Api/GroupController.php
@@ -1,0 +1,106 @@
+<?php
+
+/*
+ * Copyright (C) 2024 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Auth\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+use OPNsense\Base\UserException;
+use OPNsense\Core\ACL;
+use OPNsense\Core\Backend;
+use OPNsense\Core\Config;
+
+/**
+ * Class GroupController
+ * @package OPNsense\Auth\Api
+ */
+class GroupController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'group';
+    protected static $internalModelClass = 'OPNsense\Auth\Group';
+
+    protected function setBaseHook($node)
+    {
+        $this->getModel()->serializeToConfig(false, true);
+        if (!(new ACL())->isPageAccessible($this->getUserName(), '/api/auth/group')) {
+            throw new UserException(
+                sprintf(gettext("User %s can not lock itself out"), $this->getUserName()),
+                gettext("Usermanager")
+            );
+        }
+    }
+
+    public function searchAction()
+    {
+        return $this->searchBase('group');
+    }
+
+    public function getAction($uuid = null)
+    {
+        return $this->getBase('group', 'group', $uuid);
+    }
+
+    public function addAction()
+    {
+        $result = $this->addBase('group', 'group');
+        if ($result['result'] != 'failed') {
+            $data = $this->request->getPost(static::$internalModelName);
+            (new Backend())->configdRun('auth sync group '. $data['name']);
+        }
+        return $result;
+    }
+
+    public function setAction($uuid = null)
+    {
+        $result = $this->setBase('group', 'group', $uuid);
+        if ($result['result'] != 'failed') {
+            $data = $this->request->getPost(static::$internalModelName);
+            (new Backend())->configdRun('auth sync group '. $data['name']);
+        }
+        return $result;
+    }
+
+    public function delAction($uuid)
+    {
+        $groupname = null;
+        if ($this->request->isPost()) {
+            Config::getInstance()->lock();
+            $node = $this->getModel()->getNodeByReference('group.' . $uuid);
+            if ($node->scope == 'system') {
+                throw new UserException(sprintf(gettext("Not allowed to delete system group %s"), $node->name));
+            }
+            if (!empty($node)) {
+                $groupname = (string)$node->name;
+            }
+        }
+        $result = $this->delBase('group', $uuid);
+        if ($groupname != null) {
+            (new Backend())->configdRun('auth sync group '. $groupname);
+        }
+        return $result;
+    }
+}

src/opnsense/mvc/app/controllers/OPNsense/Auth/Api/PrivController.php
@@ -1,0 +1,143 @@
+<?php
+
+/*
+ * Copyright (C) 2024 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Auth\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+use OPNsense\Auth\User;
+use OPNsense\Auth\Group;
+use OPNsense\Core\ACL;
+use OPNsense\Core\Config;
+
+
+/**
+ * Class PrivController
+ * @package OPNsense\Auth\Api
+ */
+class PrivController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'priv';
+    protected static $internalModelClass = 'OPNsense\Auth\Priv';
+
+
+    public function searchAction()
+    {
+        $userprivs = [];
+        $groupprivs = [];
+        foreach ((new User())->user->iterateItems() as $user) {
+            foreach (explode(',', $user->priv->getCurrentValue()) as $priv) {
+                if (!isset($userprivs[$priv])) {
+                    $userprivs[$priv] = [];
+                }
+                $userprivs[$priv][] = (string)$user->name;
+            }
+        }
+        foreach ((new Group())->group->iterateItems() as $group) {
+            foreach (explode(',', $group->priv->getCurrentValue()) as $priv) {
+                if (!isset($groupprivs[$priv])) {
+                    $groupprivs[$priv] = [];
+                }
+                $groupprivs[$priv][] = (string)$group->name;
+            }
+        }
+
+        $records = [];
+        foreach ((new ACL())->getPrivList() as $auth => $props) {
+            $records[] = [
+                'id' => $auth,
+                'name' => $props['name'],
+                'match' => implode("\n", $props['match'] ?? []),
+                'users' =>  $userprivs[$auth] ?? [],
+                'groups' =>  $groupprivs[$auth] ?? [],
+            ];
+        }
+        return $this->searchRecordsetBase($records);
+    }
+
+    public function getItemAction($id)
+    {
+        $result = parent::getAction();
+        if (isset($result['priv'])) {
+            $result['priv']['id'] = $id;
+            foreach ((new User())->user->iterateItems() as $uuid => $user) {
+                if (
+                    in_array($id, explode(',', $user->priv->getCurrentValue())) &&
+                    isset($result['priv']['users'][$uuid])
+                ) {
+                    $result['priv']['users'][$uuid]['selected'] = 1;
+                }
+            }
+            foreach ((new Group())->group->iterateItems() as $uuid => $group) {
+                if (
+                    in_array($id, explode(',', $group->priv->getCurrentValue())) &&
+                    isset($result['priv']['groups'][$uuid])
+                ) {
+                    $result['priv']['groups'][$uuid]['selected'] = 1;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function setItemAction($id)
+    {
+        if ($this->request->isPost()) {
+            Config::getInstance()->lock();
+        }
+        $result = parent::setAction();
+        if ($result['result'] != 'failed') {
+            $mdl = $this->getModel();
+            $usermdl = new User();
+            $groupmdl = new Group();
+            foreach ([$usermdl->user, $groupmdl->group] as $topic) {
+                if ($topic == $usermdl->user) {
+                   $uuids = explode(',', $mdl->users->getCurrentValue());
+                } else {
+                   $uuids = explode(',', $mdl->groups->getCurrentValue());
+                }
+                foreach ($topic->iterateItems() as $uuid => $item) {
+                    $privlist = array_filter(explode(',', $item->priv->getCurrentValue()));
+                    if (!in_array($uuid, $uuids) && in_array($id, $privlist)) {
+                        unset($privlist[array_search($id, $privlist)]);
+                    } elseif (in_array($uuid, $uuids) && !in_array($id, $privlist)) {
+                        $privlist[] = $id;
+                    } else {
+                        continue;
+                    }
+                    $item->priv = implode(',', $privlist);
+                }
+            }
+            $usermdl->serializeToConfig(false, true);
+            $groupmdl->serializeToConfig(false, true);
+            Config::getInstance()->save();
+        }
+        return $result;
+    }
+
+}

commit 5d4317ee21be317700ebef5eff9fdd395aa71863

Auth: regression in setGroupMembership() introduced with https://github.com/opnsense/core/pull/8046
As members are comma separated now, we should split them before processing. To keep old and new formats (arrays/csv) compatible as we now do in the rest of the codebase, we normalize arrays with cvs strings into a single list of members.

src/opnsense/mvc/app/library/OPNsense/Auth/Base.php
@@ -156,7 +156,8 @@ abstract class Base
             foreach ($cnf->system->group as $group) {
                 $known_groups[] = strtolower((string)$group->name);
                 // when user is known, collect current groups
-                if ($user != null && in_array((string)$user->uid, (array)$group->member)) {
+                $group_members = explode(',', implode(',', (array)$group->member));
+                if ($user != null && in_array((string)$user->uid, $group_members)) {
                     $user_groups[] = strtolower((string)$group->name);
                 }
             }