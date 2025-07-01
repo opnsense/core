8e312fd4e5f847ad14a4e057002dde7662523239
Monviech <gitacc@pischem.com>
2025-07-01 11:40:03 +0000

    dnsmasq: Rename get_dnsmasq_leases.py to dnsmasq_leases.py

--- src/opnsense/scripts/dhcp/dnsmasq_leases.py
+++ src/opnsense/scripts/dhcp/dnsmasq_leases.py
@@ -2,6 +2,7 @@
 
 """
     Copyright (c) 2025 Ad Schellevis <ad@opnsense.org>
+    Copyright (c) 2025 Cedrik Pischem
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -124,4 +125,3 @@ if __name__ == '__main__':
         if not args.ip:
             parser.error("delete requires an IP or 'all'")
         delete_lease(args.ip)
-
