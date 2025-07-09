1e4683f9dfc1ff3eeb839ad378c9969e5692f848
Franco Fichtner <franco@opnsense.org>
2025-07-09 13:28:20 +0200

    mvc: remove SelectOptions support for CSVListField
    There are no tests and the only consumer is a simple OptionField
    in Monit code.

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/CSVListField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/CSVListField.php
@@ -43,12 +43,6 @@ class CSVListField extends BaseSetField
     protected $internalAsList = true;
 
     /**
-     * selectable options, key/value store.
-     * value = display option
-     */
-    private $selectOptions = [];
-
-    /**
      * @var string basic regex validation to use for the complete field
      */
     protected $internalMask = null;
@@ -85,17 +79,13 @@ class CSVListField extends BaseSetField
     }
 
     /**
-     * retrieve data including selection options (from setSelectOptions)
+     * retrieve data
      * @return array
      */
     public function getNodeData()
     {
         $selectlist = $this->iterateInput($this->internalValue);
         $result = [];
-
-        foreach ($this->selectOptions as $optKey => $optValue) {
-            $result[$optKey] = ['value' => $optValue, 'selected' => 0];
-        }
 
         foreach ($selectlist as $optKey) {
             if (strlen($optKey) > 0) {
@@ -108,32 +98,6 @@ class CSVListField extends BaseSetField
         }
 
         return $result;
-    }
-
-    /**
-     * set all options for this select item.
-     * push a key/value array type to set all options or deliver a comma-separated list with keys and optional values
-     * divided by a pipe | sign.
-     * example :    optionA|text for option A, optionB|test for option B
-     * @param array|string $list key/value option list
-     */
-    public function setSelectOptions($list)
-    {
-        if (is_array($list)) {
-            foreach ($list as $optKey => $optValue) {
-                $this->selectOptions[$optKey] = $optValue;
-            }
-        } else {
-            // string list
-            foreach ($this->iterateInput($list) as $option) {
-                if (strpos($option, '|') !== false) {
-                    $tmp = explode('|', $option);
-                    $this->selectOptions[$tmp[0]] = $tmp[1];
-                } else {
-                    $this->selectOptions[$option] = $option;
-                }
-            }
-        }
     }
 
     /**
