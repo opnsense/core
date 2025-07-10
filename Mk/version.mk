4f92da88675309c968a4f2995d12bf149a36c366
Franco Fichtner <franco@opnsense.org>
2025-07-10 08:00:24 +0200

    pkg: prep for 25.7

--- Mk/version.mk
+++ Mk/version.mk
@@ -23,12 +23,12 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-CORE_ABI?=	25.1
+CORE_ABI?=	25.7
 CORE_ADDITIONS?=#empty
-CORE_MESSAGE?=	Carry on my wayward son
-CORE_NAME?=	opnsense-devel
+CORE_MESSAGE?=	TBA
+CORE_NAME?=	opnsense
 # adjust src/root/boot/lua/logo-hourglass.lua.in accordingly:
 CORE_NICKNAME?=	Not Yet
-CORE_TYPE?=	development
+CORE_TYPE?=	community
 # plugins that were migrated to core are here
 CORE_CONFLICTS?=firewall wireguard wireguard-go
