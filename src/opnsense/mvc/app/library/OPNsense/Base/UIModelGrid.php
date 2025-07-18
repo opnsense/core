64bce5956946b10c2a04cb735052e2592bdee1d8
Franco Fichtner <franco@opnsense.org>
2025-07-18 08:13:19 +0200

    mvc: comment and copyright year
    This was recently changed.

--- src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
+++ src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015 Deciso B.V.
+ * Copyright (C) 2015-2025 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -122,7 +122,7 @@ class UIModelGrid
                 }
 
                 // parse rows, because we may need to convert some (list) items we need to know the actual content
-                // before searching. We flatten the resulting array in case of nested containers
+                // before searching we flatten the resulting array in case of nested containers
                 $row = iterator_to_array($this->flatten(array_merge(['uuid' => $record->getAttributes()['uuid']], $record->getNodeContent())));
 
                 // if a search phrase is provided, use it to search in all requested fields
