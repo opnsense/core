ff7cf967419ef4518298adccc4934663461058d1
Monviech <gitacc@pischem.com>
2025-08-01 07:50:08 +0000

    vpn/openvpn: Add nopool directive. A mix of dynamic and cso enabled clients can cause IP duplication in the pool. With nopool, only CSO will manage all addresses, clients without a CSO will not receive an IP address.

--- src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
+++ src/opnsense/mvc/app/models/OPNsense/OpenVPN/OpenVPN.php
@@ -600,7 +600,7 @@ class OpenVPN extends BaseModel
                             $options['ifconfig'] = "{$ip1} {$ip2}";
                             $options['ifconfig-pool'] = "{$ip2} {$ip3}";
                         } else {
-                            $options['server'] = $parts[0] . " " . $mask;
+                            $options['server'] = $parts[0] . " " . $mask . ($node->nopool == '1' ? ' nopool' : '');
                         }
                     } elseif ((string)$node->dev_type == 'tap') {
                         if (!$node->bridge_gateway->isEmpty()) {
