caf321d0a756b773f6c1255d33bfe26af8288a78
Monviech <79600909+Monviech@users.noreply.github.com>
2025-08-13 09:46:55 +0200

    bootgrid: Fix expandGroupBy() call to use correct context (#9083)
    (cherry picked from commit 595875c1e7626e2f3d4dc5c1bfcc1e426dac5518)

--- src/opnsense/www/js/opnsense_bootgrid.js
+++ src/opnsense/www/js/opnsense_bootgrid.js
@@ -1855,7 +1855,7 @@ class UIBootgrid {
                         } else {
                             $("#" + editDlg).change();
                         }
-                        expandGroupBy(editDlg);
+                        this.expandGroupBy(editDlg);
                         this._reload(true);
                         this.showSaveAlert(event);
                         saveDlg.find('i').removeClass("fa fa-spinner fa-pulse");
