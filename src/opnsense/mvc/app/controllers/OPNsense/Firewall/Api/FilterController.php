693ddb792ff9cc0965802f0fd8dffdab0a777c18
Franco Fichtner <franco@opnsense.org>
2025-09-08 19:42:19 +0200

    mvc: sweep no 2
    (cherry picked from commit ffe7bfa6511d01b427344571d42a13893dc6df36)

--- src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -329,8 +329,8 @@ class FilterController extends FilterBaseController
             return ["status" => "error", "message" => gettext("Invalid request method")];
         }
         /* validate */
-        $target_node = $this->getModel()->getNodeByReference('rules.rule.'.$target_uuid);
-        $selected_node = $this->getModel()->getNodeByReference('rules.rule.'.$selected_uuid);
+        $target_node = $this->getModel()->getNodeByReference('rules.rule.' . $target_uuid);
+        $selected_node = $this->getModel()->getNodeByReference('rules.rule.' . $selected_uuid);
         if ($target_node === null || $selected_node === null) {
             throw new UserException(
                 gettext("Either source or destination is not a rule managed with this component"),
