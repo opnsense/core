opnsense/core

commit 7fe2d72f48f723ebdc0e6a2f2a8af0ae93cde1aa

firewall: live log: refactor to ringbuffer approach (#9114)
* firewall: live log: refactor to ringbuffer approach

* firewall: live log: typos

* firewall: live log: reorder interface as first column

src/opnsense/www/js/opnsense_bootgrid.js
@@ -118,7 +118,7 @@ class UIBootgrid {
         this.persistenceID = `${window.location.pathname}#${this.id}`;
         this.dataAvailable = false;
         this.customCommands = null;
-        this.loading = false;
+        this.loading = true;
         this.groupStorageKey = `tabulator-${this.persistenceID}-openGroups`;
         this.rememberedGroupKeys = new Set(JSON.parse(localStorage.getItem(this.groupStorageKey) || '[]'));
         this.treeStorageKey = `tabulator-${this.persistenceID}-openTree`;
@@ -126,6 +126,7 @@ class UIBootgrid {
 
         // wrapper-specific options
         this.options = {
+            disableScroll: false,
             sorting: true,
             selection: true,
             rowCount: [7, 14, 20, 50, 100, true],
@@ -264,6 +265,7 @@ class UIBootgrid {
         // navigation, determines whether actionbar, pagination and footer is rendered
         if ((bootGridOptions?.navigation ?? 3) === 0) {
             this.options.navigation = false;
+            this.compatOptions['pagination'] = false;
         }
 
         if (bootGridOptions?.rowCount ?? false) {
@@ -426,6 +428,10 @@ class UIBootgrid {
         this.options.virtualDOM = bootGridOptions?.virtualDOM ?? false;
         if (this.options.virtualDOM) {
             this.compatOptions['renderVertical'] = 'virtual';
+        }
+
+        if (bootGridOptions?.disableScroll ?? false) {
+            this.options.disableScroll = true;
         }
 
         this.tabulatorOptions = compatOptions.tabulatorOptions ??= {};
@@ -677,7 +683,9 @@ class UIBootgrid {
                 }
             }));
 
-            resizeObserver.observe($(`#${this.id} .tabulator-table`)[0]);
+            if (!this.options.disableScroll) {
+                resizeObserver.observe($(`#${this.id} .tabulator-table`)[0]);
+            }
 
             window.addEventListener('resize', this._debounce(() => {
                 // this is mainly intended for scaling the width of the table if
@@ -1981,6 +1989,10 @@ class UIBootgrid {
                 }, wait);
             }
         };
+    }
+
+    getTable() {
+        return this.table;
     }
 
     /**

commit 9b0c230a9489fe2dfc862f203b3af300339bb87a

firewall: small regression in https://github.com/opnsense/core/commit/7fe2d72f48f723ebdc0e6a2f2a8af0ae93cde1aa, keep pagination when navigation isn't rendered
While this is somewhat unintuitive, the default mode for UIBootgrid
is to always expect pagination request parameters. If navigation is
turned off, these are simply set to null so all results are returned.
The previous commit broke the data fetch for cases where the navigation
was turned off (i.e. local and remote auth for ipsec connections).

the firewall live log is an exception here, as this table is fully
managed with custom logic and therefore doesn't need to use the
default request parameters.

src/opnsense/mvc/app/views/OPNsense/Diagnostics/fw_log.volt
@@ -808,6 +808,7 @@
                 persistence: false, // ideally persistence should be on, but we have no reset button at the moment due to missing navigation
                 height:undefined,
                 layout:"fitColumns",
+                pagination: false
             }
         });
 

src/opnsense/www/js/opnsense_bootgrid.js
@@ -268,7 +268,6 @@ class UIBootgrid {
         // navigation, determines whether actionbar, pagination and footer is rendered
         if ((bootGridOptions?.navigation ?? 3) === 0) {
             this.options.navigation = false;
-            this.compatOptions['pagination'] = false;
         }
 
         if (bootGridOptions?.rowCount ?? false) {