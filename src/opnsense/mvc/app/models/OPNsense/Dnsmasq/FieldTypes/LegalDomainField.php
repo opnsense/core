6f15cc1b68cc72c8307d370aadce88c13013e034
Monviech <gitacc@pischem.com>
2025-09-23 07:32:04 +0000

    Add LegalDomainField to get rid of the regex in domain field

--- src/opnsense/mvc/app/models/OPNsense/Dnsmasq/FieldTypes/LegalDomainField.php
+++ src/opnsense/mvc/app/models/OPNsense/Dnsmasq/FieldTypes/LegalDomainField.php
@@ -31,20 +31,21 @@ namespace OPNsense\Dnsmasq\FieldTypes;
 use OPNsense\Base\FieldTypes\BaseSetField;
 use OPNsense\Base\Validators\CallbackValidator;
 
-class LegalHostnameField extends BaseSetField
+class LegalDomainField extends BaseSetField
 {
     /**
      * {@inheritdoc}
      */
     protected function defaultValidationMessage()
     {
-        return gettext("Please enter a valid hostname.");
+        return gettext("Please enter a valid domain name.");
     }
 
     /**
-     * Dnsmasq only processes the first label in a hostname (separated by dots).
-     * That is why we do not allow dots at all. We consider "label = hostname".
-     * IP addresses are not allowed implicitely as "." and ":" are not valid.
+     * Almost the same logic as the LegalHostnameField apply,
+     * but with some differences as individual labels are processed here.
+     * IPv4 addresses are valid, "." and numbers are allowed in labels.
+     * IPv6 addresses are rejected implicitly as ":" is not allowed.
      * https://github.com/imp/dnsmasq/blob/770bce967cfc9967273d0acfb3ea018fb7b17522/src/util.c#L191
      *
      * @return array list of validators for this field
@@ -59,32 +60,41 @@ class LegalHostnameField extends BaseSetField
                     $response = [];
 
                     foreach ($sender->iterateInput($data) as $value) {
-                        // Allow empty label (valid in some cases)
+                        // Allow empty domain (combination of all labels)
                         if ($value === '') {
                             continue;
                         }
 
-                        // Allow host wildcard
-                        if ($value === '*') {
-                            continue;
-                        } elseif (str_contains($value, '*')) {
-                            $response[] = gettext("Hostname wildcard '*' must be a single character.");
+                        // Split domain into labels
+                        $labels = explode('.', $value);
+
+                        foreach ($labels as $label) {
+                            // Empty labels are not valid
+                            if ($label === '') {
+                                $response[] = gettext("Domain labels cannot be empty.");
+                                continue;
+                            }
+
+                            // First character must be alphanumeric
+                            if (!ctype_alnum($label[0])) {
+                                $response[] = gettext("Domain labels must start with a letter or digit.");
+                            }
+
+                            // Remaining characters: only alphanumeric and some special
+                            $alnumTail = str_replace(['-', '_'], '', substr($label, 1));
+                            if ($alnumTail !== '' && !ctype_alnum($alnumTail)) {
+                                $response[] = gettext("Domain labels may only contain letters, digits, '-' and '_'.");
+                            }
+
+                            // RFC1035 2.3.1: labels max length
+                            if (strlen($label) > 63) {
+                                $response[] = gettext("Domain labels must not exceed 63 characters.");
+                            }
                         }
 
-                        // First character must be alphanumeric
-                        if (!ctype_alnum($value[0])) {
-                            $response[] = gettext("Hostname must start with a letter or digit.");
-                        }
-
-                        // Remaining characters only alphanumeric and some special
-                        $alnumTail = str_replace(['-', '_'], '', substr($value, 1));
-                        if ($alnumTail !== '' && !ctype_alnum($alnumTail)) {
-                            $response[] = gettext("Hostname may only contain letters, digits, '-' and '_'.");
-                        }
-
-                        // RFC1035 2.3.1 applies here
-                        if (strlen($value) > 63) {
-                            $response[] = gettext("Hostname must not exceed 63 characters.");
+                        // RFC1035 2.3.4: domain max length
+                        if (strlen($value) > 255) {
+                            $response[] = gettext("Domain name must not exceed 255 characters in total.");
                         }
                     }
 
