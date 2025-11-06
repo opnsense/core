928a899525172f2404baa0ecf68c4f4cfd1f2aeb
Ad Schellevis <ad@opnsense.org>
2025-11-06 18:59:50 +0100

    opnsense_bootgrid - rewire loaded.rs.jquery.bootgrid to renderComplete event, but push it forward until no new events are fired within 200ms. closes https://github.com/opnsense/core/issues/9362

--- src/opnsense/www/js/opnsense_bootgrid.js
+++ src/opnsense/www/js/opnsense_bootgrid.js
@@ -126,6 +126,7 @@ class UIBootgrid {
         this.treeStorageKey = `tabulator-${this.persistenceID}-openTree`;
         this.rememberedTreeIds = new Set(JSON.parse(localStorage.getItem(this.treeStorageKey) || '[]'));
         this.isVisible = false;
+        this.legacyLoadedTimeout = null;
 
         // wrapper-specific options
         this.options = {
@@ -662,6 +663,19 @@ class UIBootgrid {
             this.tableInitialized = true;
         });
 
+        this.table.on('renderComplete', () =>  {
+            /**
+             * backwards compat.
+             * renderComplete is triggered per visible row, so we push the event forward until the the last row is
+             * rendered.
+             */
+            clearTimeout(this.legacyLoadedTimeout);
+            let $target = this.$element;
+            this.legacyLoadedTimeout = setTimeout(function(){
+                $target.trigger("loaded.rs.jquery.bootgrid");
+            }, 200);
+        });
+
         this.table.on('dataProcessed', () =>  {
             this._onDataProcessed();
         });
@@ -887,9 +901,6 @@ class UIBootgrid {
                 if (kids.length) queue.push(...kids);
             }
         }
-
-        // backwards compat
-        this.$element.trigger("loaded.rs.jquery.bootgrid");
     }
 
     _onCellRendered(cell, formatterParams) {
