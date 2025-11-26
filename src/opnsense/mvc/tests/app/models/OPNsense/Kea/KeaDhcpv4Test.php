055d377493f74e8916aa6d0ae232f581e1036000
Monviech <gitacc@pischem.com>
2025-11-26 08:48:37 +0100

    Add a few more small scoped tests for enabled, firewall rules, and interface parsing

--- src/opnsense/mvc/tests/app/models/OPNsense/Kea/KeaDhcpv4Test.php
+++ src/opnsense/mvc/tests/app/models/OPNsense/Kea/KeaDhcpv4Test.php
@@ -27,4 +27,64 @@ class KeaDhcpv4Test extends JsonSampleTestCase
     {
         $this->runJsonSampleTest();
     }
+
+    public function testIsEnabled(): void
+    {
+        $m = $this->getModelInstance();
+
+        $m->general->enabled = "0";
+        $m->general->interfaces = "";
+
+        $this->assertFalse($m->isEnabled());
+
+        $m->general->enabled = "1";
+        $this->assertFalse($m->isEnabled());
+
+        $m->general->interfaces = "lan";
+        $this->assertTrue($m->isEnabled());
+    }
+
+    public function testFwRulesEnabled(): void
+    {
+        $m = $this->getModelInstance();
+
+        $m->general->enabled = "0";
+        $m->general->fwrules = "0";
+        $m->general->interfaces = "";
+
+        $this->assertFalse($m->fwrulesEnabled());
+
+        $m->general->enabled = "1";
+        $this->assertFalse($m->fwrulesEnabled());
+
+        $m->general->fwrules = "1";
+        $this->assertFalse($m->fwrulesEnabled());
+
+        $m->general->interfaces = "lan";
+        $this->assertTrue($m->fwrulesEnabled());
+    }
+
+    public function testPhysicalInterfacesParsing(): void
+    {
+        $xml = <<<XML
+    <opnsense>
+    <interfaces>
+        <lan><if>igc0</if></lan>
+        <wan><if>igc1</if></wan>
+    </interfaces>
+    </opnsense>
+    XML;
+
+        \OPNsense\Core\Config::getInstance()->setXml($xml);
+
+        $m = $this->getModelInstance();
+        $m->general->interfaces = "lan,wan";
+
+        $ifs = (new \ReflectionClass($m))
+            ->getMethod('getConfigPhysicalInterfaces')
+            ->invoke($m);
+
+        $this->assertSame(['igc0','igc1'], $ifs);
+    }
+
 }
