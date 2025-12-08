bc5e67002ecb99ca64fca0337dc215cb261c2273
Monviech <gitacc@pischem.com>
2025-12-08 10:09:11 +0100

    nat: Refactor network prefix translation (NPT) view

--- src/opnsense/mvc/app/controllers/OPNsense/Firewall/NptController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Firewall/NptController.php
@@ -31,29 +31,8 @@ class NptController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        $this->view->pick('OPNsense/Firewall/filter');
-        $this->view->ruleController = "npt";
-        $this->view->gridFields = [
-            [
-                'id' => 'enabled', 'formatter' => 'rowtoggle' ,'width' => '6em', 'heading' => gettext('Enabled')
-            ],
-            [
-                'id' => 'sequence','width' => '9em', 'heading' => gettext('Sequence')
-            ],
-            [
-                'id' => 'source_net', 'heading' => gettext('Internal IPv6 Prefix')
-            ],
-            [
-                'id' => 'destination_net', 'heading' => gettext('External IPv6 Prefix')
-            ],
-            [
-                'id' => 'trackif', 'heading' => gettext('Track if')
-            ],
-            [
-                'id' => 'description', 'heading' => gettext('Description')
-            ]
-        ];
-
-        $this->view->formDialogFilterRule = $this->getForm("dialogNptRule");
+        $this->view->pick('OPNsense/Firewall/npt_rule');
+        $this->view->formDialogNptRule= $this->getForm('dialogNptRule');
+        $this->view->formGridNptRule = $this->getFormGrid('dialogNptRule');
     }
 }
