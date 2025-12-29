515eddf69db23ca486d093515090d3fbe938ffaa
Stephan de Wit <stephan.de.wit@deciso.com>
2025-12-29 17:09:38 +0100

    interfaces: settings: convert to MVC (fixes https://github.com/opnsense/core/issues/9141)

---
+++ src/opnsense/mvc/app/models/OPNsense/Interfaces/FieldTypes/DUIDField.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Copyright (C) 2025 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Interfaces\FieldTypes;
+
+use OPNsense\Base\FieldTypes\BaseField;
+use OPNsense\Base\FieldTypes\TextField;
+use OPNsense\Base\Validators\CallbackValidator;
+
+class DUIDField extends TextField
+{
+    /* Note: function is public for use in SettingsController */
+    public function isValidDuid($duid)
+    {
+        // Duid's can be any length. Just check the format is correct.
+        $values = explode(":", $duid);
+
+        // need to get the DUID type. There are four types, in the
+        // first three the type number is in byte[2] in the fourth it's
+        // in byte[4]. Offset is either 0 or 2 depending if it's the read #
+        // from file duid or the user input.
+
+        $valid_duid = false;
+
+        $duid_length = count($values);
+        $test1 = hexdec($values[1]);
+        $test2 = hexdec($values[3]);
+        if (($test1 == 1 && $test2 == 1 ) || ($test1 == 3 && $test2 == 1 ) || ($test1 == 0 && $test2 == 4 ) || ($test1 == 2)) {
+            $valid_duid = true;
+        }
+
+        /* max DUID length is 128, but with the separators it could be up to 254 */
+        if ($duid_length < 6 || $duid_length > 254) {
+            $valid_duid = false;
+        }
+
+        if ($valid_duid == false) {
+            return false;
+        }
+
+        for ($i = 0; $i < count($values); $i++) {
+            if (ctype_xdigit($values[$i]) == false) {
+                return false;
+            }
+            if (hexdec($values[$i]) < 0 || hexdec($values[$i]) > 255) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function getValidators()
+    {
+        $validators = parent::getValidators();
+
+        $validators[] = new CallbackValidator(["callback" => function($data) {
+            $messages = [];
+            if (!empty($data) && !$this->isValidDuid($data)) {
+                $messages[] = gettext('A valid DUID must be specified.');
+            }
+            return $messages;
+        }]);
+
+        return $validators;
+    }
+}
---
+++ src/opnsense/mvc/app/views/OPNsense/Interface/settings.volt
@@ -0,0 +1,67 @@
+{#
+ # Copyright (c) 2023 Deciso B.V.
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without modification,
+ # are permitted provided that the following conditions are met:
+ #
+ # 1. Redistributions of source code must retain the above copyright notice,
+ #    this list of conditions and the following disclaimer.
+ #
+ # 2. Redistributions in binary form must reproduce the above copyright notice,
+ #    this list of conditions and the following disclaimer in the documentation
+ #    and/or other materials provided with the distribution.
+ #
+ # THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ # INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ # AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ # AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ # OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ # INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ # POSSIBILITY OF SUCH DAMAGE.
+ #}
+
+<script>
+    $(document).ready(function() {
+        const getMap = {'frm_settings':'/api/interfaces/settings/get'};
+        mapDataToFormUI(getMap).done(function(data) {
+            console.log(data);
+            const duids = data.frm_settings.duids;
+            formatTokenizersUI();
+            $('.selectpicker').selectpicker('refresh');
+            const $container = $(`
+                <div>
+                    <a id="current" href="#">{{ lang._('Insert the currently configured DUID') }}</a><br/>
+                    <a id="llt" href="#">{{ lang._('Insert a new LLT DUID') }}</a><br/>
+                    <a id="ll" href="#">{{ lang._('Insert a new LL DUID') }}</a><br/>
+                    <a id="uuid" href="#">{{ lang._('Insert a new UUID DUID') }}</a><br/>
+                    <a id="en" href="#">{{ lang._('Insert a new EN DUID') }}</a>
+                </div>
+            `);
+            $container.insertAfter($('#settings\\.ipv6duid'));
+            ['current', 'llt', 'll', 'uuid', 'en'].forEach(id => {
+                $(`#${id}`).click(function() {
+                    $('#settings\\.ipv6duid').val(duids[id] ?? '');
+                });
+            });
+        });
+
+        $("#reconfigureAct").SimpleActionButton({
+            onPreAction: function() {
+                const dfObj = new $.Deferred();
+                saveFormToEndpoint("/api/interfaces/settings/set", 'frm_settings', function () { dfObj.resolve(); }, true, function () { dfObj.reject(); });
+                return dfObj;
+            },
+        });
+
+    });
+</script>
+
+<div class="content-box">
+        {{ partial("layout_partials/base_form",['fields':formDialogSettings,'id':'frm_settings'])}}
+</div>
+
+{{ partial('layout_partials/base_apply_button', {'data_endpoint': '/api/interfaces/settings/reconfigure'}) }}
