6e6369a77be60c60885ff0302547e04d18835c38
Stephan de Wit <stephan.de.wit@deciso.com>
2026-01-07 14:24:49 +0100

    interfaces: settings: convert to MVC (#9569)

--- src/opnsense/mvc/app/models/OPNsense/Core/Migrations/TUN1_0_2.php
+++ src/opnsense/mvc/app/models/OPNsense/Core/Migrations/TUN1_0_2.php
@@ -1,8 +1,7 @@
-#!/usr/local/bin/php
 <?php
 
 /*
- * Copyright (C) 2024 Franco Fichtner <franco@opnsense.org>
+ * Copyright (C) 2025 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,38 +26,43 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-require_once 'config.inc';
-require_once 'util.inc';
-require_once 'system.inc';
-require_once 'interfaces.inc';
-require_once 'filter.inc';
-require_once 'auth.inc';
+namespace OPNsense\Core\Migrations;
 
-if (empty($argv[1]) || empty($argv[2])) {
-    exit(1);
+use OPNsense\Base\BaseModelMigration;
+use OPNsense\Core\Tunables;
+use OPNsense\Core\Config;
+
+class TUN1_0_2 extends BaseModelMigration
+{
+    /**
+     * Migrate sharednet settings
+     * @param $model
+     */
+    public function run($model)
+    {
+        if (!($model instanceof Tunables)) {
+            return;
+        }
+
+        $config = Config::getInstance()->object();
+
+        if (isset($config?->system?->sharednet)) {
+            $model->item->add()->setNodes([
+                'tunable' => 'net.link.ether.inet.log_arp_movements',
+                'value' => '0',
+            ]);
+            $model->item->add()->setNodes([
+                'tunable' => 'net.link.ether.inet.log_arp_wrong_iface',
+                'value' => '0',
+            ]);
+        }
+    }
+
+    public function post($model)
+    {
+        $config = Config::getInstance()->object();
+        if (isset($config?->system?->sharednet)) {
+            unset($config->system->sharednet);
+        }
+    }
 }
-
-$interface = convert_real_interface_to_friendly_interface_name($argv[1]);
-$family = $argv[2];
-
-if (empty($interface) || ($family != 4 && $family != 6)) {
-    exit(1);
-}
-
-if (!interface_ppps_bound($interface, $family)) {
-    exit(1);
-}
-
-switch (is_ipv6_allowed() ? ($config['interfaces'][$interface]['ipaddrv6'] ?? 'none') : 'none') {
-    case 'dhcp6':
-    case 'slaac':
-        interface_dhcpv6_prepare($interface, $config['interfaces'][$interface]);
-        interface_dhcpv6_configure($interface, $config['interfaces'][$interface]);
-        /* signal this succeeded to avoid triggering a newwanip event right away */
-        exit(0);
-    default:
-        interface_static6_configure($interface, $config['interfaces'][$interface]);
-        system_routing_configure(false, $interface, true, 'inet6');
-}
-
-exit(1);
