70ee0f879531d844381aa2bede1ee728bb8f4860
Ad Schellevis <AdSchellevis@users.noreply.github.com>
2026-01-20 16:31:13 +0100

    Firewall: Rules:  Migration assistant - add export option and guidance for migrations to the new mvc system. (#9606)
    * Firewall: Rules:  Migration assistant - add export option and guidance for migrations to the new mvc system.
    
    Add new "Firewall: Rules: Migration assistant" to help people moving to our new mvc based firewall system by offering a couple of simple steps for exporting the old rules into a csv file and importing them into the new system. When all rules are migrated the user may drop all old ones using the ConfigMaintenance module used in "defaults".
    
    There's one small issue remaining in fbegin.inc as it doesn't render the icon correctly due to VisibleName being escaped currently (which isn't the case in our mvc template).
    
    closes https://github.com/opnsense/core/issues/9579
    
    * Update src/opnsense/service/conf/actions.d/actions_filter.conf
    
    ---------
    
    Co-authored-by: Franco Fichtner <franco@opnsense.org>

---
+++ src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/MigrationController.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+namespace OPNsense\Firewall\Api;
+
+use OPNsense\Base\ApiControllerBase;
+use OPNsense\Core\Backend;
+use OPNsense\Core\Config;
+use OPNsense\Core\ConfigMaintenance;
+
+class MigrationController extends ApiControllerBase
+{
+    public function downloadRulesAction()
+    {
+        if ($this->request->isGet()) {
+            $data = json_decode((new Backend())->configdRun('filter list legacy_rules') ?? '', true) ?? [];
+            $this->exportCsv($data);
+        }
+    }
+
+    public function flushAction()
+    {
+        if ($this->request->isPost()) {
+            (new ConfigMaintenance())->delItem('filter.rule');
+            Config::getInstance()->save();
+            return ["status" => "ok"];
+        } else {
+            return ['status' => 'failed'];
+        }
+    }
+}
--- src/opnsense/mvc/app/models/OPNsense/Base/Menu/MenuSystem.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/Menu/MenuSystem.php
@@ -329,8 +329,13 @@ class MenuSystem
         }
 
         // add interfaces to "Firewall: Rules" menu tab...
+        $this->appendItem('Firewall.Rules', 'Migration', [
+                'url' => '/ui/firewall/migration',
+                'fixedname' => sprintf("<i class='fa fa-fw fa-gears'> </i> %s", gettext('Migration assistant')),
+                'order' => 0,
+        ]);
         $iftargets['fw'] = array_merge(['FloatingRules' => gettext('Floating')], $iftargets['fw']);
-        $ordid = 0;
+        $ordid = 1;
         foreach ($iftargets['fw'] as $key => $descr) {
             $this->appendItem('Firewall.Rules', $key, [
                 'url' => '/firewall_rules.php?if=' . $key,
