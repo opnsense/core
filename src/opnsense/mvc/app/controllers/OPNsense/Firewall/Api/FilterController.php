d90621d2cf2bfe15ddc7e98afcb19d63d46c8e3c
Monviech <gitacc@pischem.com>
2026-02-11 10:21:17 +0100

    Ensure UUIDs are always searchable

--- src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -198,8 +198,12 @@ class FilterController extends FilterBaseController
         $ORG_REQ = $_REQUEST;
         unset($_REQUEST['rowCount']);
         unset($_REQUEST['current']);
-        if ($show_all) {
-            /* searchBase should not filter here since we later search for IP addresses in aliases */
+        $searchPhrase = (string)$this->request->getPost('searchPhrase', null, '');
+        if ($show_all || $this->isValidUUID($searchPhrase)) {
+            /*
+             * searchBase should not filter here since we later search for IP addresses in aliases
+             * additionally UUIDs should always return a result
+             */
             unset($_REQUEST['searchPhrase']);
         }
         $filterset = $this->searchBase("rules.rule", null, "sort_order", $filter_funct_mvc)['rows'];
