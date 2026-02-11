2be1c506c102662927ec4a71e49ffcefe8e7ebff
Franco Fichtner <franco@opnsense.org>
2026-02-11 15:53:00 +0100

    firmware: put 99e4cd28b64b2f back where it belongs
    Without backgrounding because this is important.
    
    (cherry picked from commit 8a52f03b37542ff6978afc4a5edef6428b9563ef)

--- src/etc/rc.syshook.d/update/10-refresh.sh
+++ src/etc/rc.syshook.d/update/10-refresh.sh
@@ -2,3 +2,6 @@
 
 # refresh relevant configuration files
 /usr/local/etc/rc.configure_firmware
+
+# remove our stale pyc files not handled by pkg
+find /usr/local/opnsense -type f -name "*.pyc" -delete
