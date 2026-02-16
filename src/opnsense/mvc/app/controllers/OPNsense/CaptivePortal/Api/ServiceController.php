e83d6eb54c895d69a72ad980d387dc1fe322ed9e
Monviech <gitacc@pischem.com>
2026-02-16 12:09:25 +0100

    Services: Captive Portal: Move template actions out of the ServiceController into its own TemplateController, so it can use the ApiMutableModelControllerBase methods

--- src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/Api/ServiceController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/CaptivePortal/Api/ServiceController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2015-2025 Deciso B.V.
+ * Copyright (C) 2015-2026 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,11 +29,6 @@
 namespace OPNsense\CaptivePortal\Api;
 
 use OPNsense\Base\ApiMutableServiceControllerBase;
-use OPNsense\Base\UIModelGrid;
-use OPNsense\Core\AppConfig;
-use OPNsense\Core\Backend;
-use OPNsense\Core\Config;
-use OPNsense\Core\SanitizeFilter;
 
 /**
  * Class ServiceController
@@ -55,117 +50,6 @@ class ServiceController extends ApiMutableServiceControllerBase
         return true;
     }
 
-    /**
-     * @param null $fileid unique template id (fileid field)
-     * @return mixed
-     * @throws \Exception
-     */
-    public function getTemplateAction($fileid = null)
-    {
-        // get template name
-        $templateFileId = $fileid != null ? (new SanitizeFilter())->sanitize($fileid, 'alnum') : 'default';
+    // XXX: Breaking API change, template actions moved to TemplateController
 
-        // request template data and output result (zipfile)
-        $response = (new Backend())->configdpRun('captiveportal fetch_template', [$templateFileId]);
-        $result = json_decode($response, true);
-        if ($result != null) {
-            $response = $result['payload'];
-            $this->response->setRawHeader("Content-Type: application/octet-stream");
-            $this->response->setRawHeader("Content-Disposition: attachment; filename=template_" . $templateFileId . ".zip");
-            return base64_decode($response);
-        } else {
-            // return empty response on error
-            return "";
-        }
-    }
-
-
-    /**
-     * save template, updates existing or create new.
-     * @return string
-     */
-    public function saveTemplateAction()
-    {
-        if ($this->request->isPost() && $this->request->hasPost("name")) {
-            Config::getInstance()->lock();
-            $content = $this->request->getPost("content", "striptags", "");
-            $templateName = $this->request->getPost("name", "striptags");
-            if ($this->request->hasPost("uuid")) {
-                $uuid = $this->request->getPost("uuid", "striptags");
-                $template = $this->getModel()->getNodeByReference('templates.template.' . $uuid);
-                if ($template == null) {
-                    return ["name" => $templateName, "error" => "node not found"];
-                }
-            } else {
-                $template = $this->getModel()->getTemplateByName($templateName);
-            }
-
-            // cleanse input content, we only want to save changed files into our config
-            if (strlen($content) > 20 || strlen((string)$template->content) == 0) {
-                $temp_filename = (new AppConfig())->application->tempDir;
-                $temp_filename .= '/cp_' . $template->getAttributes()['uuid'] . '.tmp';
-                file_put_contents($temp_filename, $content);
-                // strip defaults and unchanged files from template (standard js libs, etc)
-                $response = (new Backend())->configdpRun('captiveportal strip_template', [$temp_filename]);
-                unlink($temp_filename);
-                $result = json_decode($response, true);
-                if (is_array($result) && !array_key_exists('error', $result)) {
-                    $template->content = $result['payload'];
-                } else {
-                    return ["name" => $templateName, "error" => $result['error']];
-                }
-            }
-
-            $template->name = $templateName;
-            $errorMsg = [];
-            foreach ($this->getModel()->performValidation() as $validation_message) {
-                $errorMsg[] = (string)$validation_message;
-            }
-
-            if (!empty($errorMsg)) {
-                return ["name" => (string)$template->name, "error" => implode("\n", $errorMsg)];
-            } else {
-                // data is valid, save and return.
-                $this->getModel()->serializeToConfig();
-                Config::getInstance()->save();
-                return ["name" => (string)$template->name];
-            }
-        }
-        return null;
-    }
-
-    /**
-     * delete template by uuid
-     * @param $uuid item unique id
-     * @return array status
-     */
-    public function delTemplateAction($uuid)
-    {
-        $result = ["result" => "failed"];
-        if ($this->request->isPost()) {
-            Config::getInstance()->lock();
-            if ($uuid != null) {
-                if ($this->getModel()->templates->template->del($uuid)) {
-                    // if item is removed, serialize to config and save
-                    $this->getModel()->serializeToConfig();
-                    Config::getInstance()->save();
-                    $result['result'] = 'deleted';
-                } else {
-                    $result['result'] = 'not found';
-                }
-            }
-        }
-        return $result;
-    }
-
-
-    /**
-     * search captive portal zones
-     * @return array
-     */
-    public function searchTemplatesAction()
-    {
-        $grid = new UIModelGrid($this->getModel()->templates->template);
-        return $grid->fetchBindRequest($this->request, ["name", "fileid"], "name");
-    }
 }
