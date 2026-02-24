d348a53d03f54b3b1429b7b61daf1b67adf3486d
Ad Schellevis <ad@opnsense.org>
2026-02-24 21:02:59 +0100

    Firewall: Rules [new] - default ipprotocol mismatch, when not specified, both are indicated. closes https://github.com/opnsense/core/issues/9858

--- src/opnsense/scripts/filter/list_non_mvc_rules.php
+++ src/opnsense/scripts/filter/list_non_mvc_rules.php
@@ -75,7 +75,7 @@ foreach ($fw->iterateFilterRules() as $prio => $item) {
             }
         }
         $rule['action'] = $rule['action'] ?? 'pass';
-        $rule['ipprotocol'] = $rule['ipprotocol'] ?? 'inet';
+        $rule['ipprotocol'] = $rule['ipprotocol'] ?? 'inet46';
         if (!empty($rule['from_not'])) {
             unset($rule['from_not']);
             $rule['source_not'] = true;
