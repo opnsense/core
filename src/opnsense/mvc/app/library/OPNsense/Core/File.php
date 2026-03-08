e6ef8ce5d6565de58b9317ecebcdb74cd00d559a
Ad Schellevis <ad@opnsense.org>
2026-03-08 13:53:19 +0100

    File::file_update_contents() add simple wrapper which reads a file before updating it, which can be practical when flushing out data that hardly changes.
    (cherry picked from commit 95653c52503708b9ec7003a5d69ebfeb050f3b26)

--- src/opnsense/mvc/app/library/OPNsense/Core/File.php
+++ src/opnsense/mvc/app/library/OPNsense/Core/File.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2023 Deciso B.V.
+ * Copyright (C) 2023-2026 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -56,5 +56,29 @@ class File
             }
         }
         return file_put_contents($filename, $data, $flags);
+    }
+
+    /**
+     * file_put_contents wrapper which sets permissions before writing data if contents have changed.
+     * @param string $filename Path to the file where to write the data.
+     * @param mixed $data The data to write.
+     * @param int $permissions permissions to set, see chmod for usage
+     * @param int $flags flags to pass to file_put_contents()
+     * @param string $chown username to chown to
+     * @return int|false
+     */
+    public static function file_update_contents(
+        string $filename,
+        mixed $data,
+        int $permissions = 0640,
+        int $flags = 0,
+        string $chown = null
+    ) {
+        if (!file_exists($filename) || hash('sha256', $data) !== hash_file('sha256', $filename)) {
+            return self::file_put_contents($filename, $data, $permissions, $flags, $chown);
+        } else {
+            /* size "written" (already on disk) */
+            return strlen($data);
+        }
     }
 }
