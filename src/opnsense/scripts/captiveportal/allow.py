5b85f4659ddf068eb221be413f1ffe0b1aa76f84
Alexander Goodkind <alex@goodkind.io>
2026-03-11 20:36:53 -0700

    Finish captive portal dual-stack authorization flow.
    This makes IPv4 and IPv6 portal entry points behave consistently, fixes proxied client IP detection, and lets roaming sessions discover IPv6 addresses quickly enough to authorize privacy and secondary addresses on the same client.

--- src/opnsense/scripts/captiveportal/allow.py
+++ src/opnsense/scripts/captiveportal/allow.py
@@ -41,7 +41,6 @@ parser.add_argument('--username', help='username', type=str, required=True)
 parser.add_argument('--zoneid', help='zone number to allow this user in', type=str, required=True)
 parser.add_argument('--authenticated_via', help='authentication source', type=str)
 parser.add_argument('--ip_address', help='source ip address', type=str)
-parser.add_argument('--roaming', help='roaming allowed for this client', type=str)
 args = parser.parse_args()
 
 arp = ARP()
@@ -56,13 +55,8 @@ response = db.add_client(
     mac_address=arp_entry['mac'] if arp_entry is not None else None
 )
 
-session_ips = {args.ip_address}
-if args.roaming:
-    session_ips = db.update_roaming_ips(args.zoneid, response.sessionId, arp.get_all_addresses_by_mac(arp_entry['mac']))
-
-for ip in session_ips:
-    PF.add_to_table(zoneid=args.zoneid, address=args.ip_address)
-    IPFW.add_accounting(args.ip_address)
+PF.add_to_table(zoneid=args.zoneid, address=args.ip_address)
+IPFW.add_accounting(args.ip_address)
 
 response['clientState'] = 'AUTHORIZED'
 print(ujson.dumps(response))
