14c9eb1215909c7d2f40ff44eab417d5d9861a18
Monviech <79600909+Monviech@users.noreply.github.com>
2026-03-26 08:27:47 +0100

    Services: Kea: DDNS: Add subnet specific qualifying suffix and prevent updates if no server is set. (#10038)

--- src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
+++ src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv4.php
@@ -161,7 +161,7 @@ class KeaDhcpv4 extends BaseModel
         return $result;
     }
 
-    private function getConfigSubnets()
+    private function getConfigSubnets($ddns_enabled = false)
     {
         $result = [];
         $subnet_id = 1;
@@ -238,6 +238,13 @@ class KeaDhcpv4 extends BaseModel
                 }
                 $record['option-data'][] = $entry;
             }
+            /* DDNS per subnet settings */
+            if ($ddns_enabled) {
+                if (!$subnet->ddns_qualifying_suffix->isEmpty()) {
+                    $record['ddns-qualifying-suffix'] = $subnet->ddns_qualifying_suffix->getValue();
+                }
+                $record['ddns-send-updates'] = !$subnet->ddns_dns_server->isEmpty();
+            }
             $result[] = $record;
         }
         return $result;
@@ -273,6 +280,8 @@ class KeaDhcpv4 extends BaseModel
 
     public function generateConfig($target = '/usr/local/etc/kea/kea-dhcp4.conf')
     {
+        $ddns = new KeaDdns();
+        $ddns_enabled = !$ddns->general->enabled->isEmpty();
         $cnf = [
             'Dhcp4' => [
                 'valid-lifetime' => $this->general->valid_lifetime->asInt(),
@@ -299,7 +308,7 @@ class KeaDhcpv4 extends BaseModel
                         'severity' => 'INFO',
                     ]
                 ],
-                'subnet4' => $this->getConfigSubnets(),
+                'subnet4' => $this->getConfigSubnets($ddns_enabled),
             ]
         ];
         $client_classes = $this->getConfigClientClasses();
@@ -347,8 +356,7 @@ class KeaDhcpv4 extends BaseModel
                 $cnf['Dhcp4']['hooks-libraries'][] = $record;
             }
         }
-        $ddns = new KeaDdns();
-        if (!$ddns->general->enabled->isEmpty()) {
+        if ($ddns_enabled) {
             $cnf['Dhcp4']['dhcp-ddns'] = [
                 'enable-updates' => true,
                 'server-ip' => $ddns->general->server_ip->getValue(),
