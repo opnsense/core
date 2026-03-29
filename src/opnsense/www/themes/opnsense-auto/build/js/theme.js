e02afc6f78983ba1f09a093c41cda5b2e7c73f3f
Ad Schellevis <ad@opnsense.org>
2026-03-29 15:22:11 +0200

    themes: add "opnsense-auto" which switches between "opnsense" and "opnsense-dark" depending browser settings, inspired by https://github.com/opnsense/core/pull/9916
    It's a bit of an experiment, but since its so small, it shouldn't be an issue to push this to master.
    The trick is actually pretty simple, symlink the relevant directories in build to the standard opnsense theme and add a theme.js override to handle the logic.

---
+++ src/opnsense/www/themes/opnsense-auto/build/js/theme.js
@@ -0,0 +1,28 @@
+(function() {
+    function themeSwitcher(){
+        let theme_name = 'opnsense';
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            theme_name = 'opnsense-dark';
+        }
+        let links = document.getElementsByTagName('link');
+        let imgs = document.getElementsByTagName('img');
+        for (let i=0; i < links.length; i++) {
+            if (links[i].href && links[i].href.indexOf('/ui/themes/') !== -1) {
+                links[i].href = links[i].href.replace(/\/ui\/themes\/[^\/]+\//, '/ui/themes/' + theme_name + '/');
+            }
+        }
+        console.log(imgs);
+        for (let i=0; i < imgs.length; i++) {
+            if (imgs[i].src && imgs[i].src.indexOf('/ui/themes/') !== -1) {
+                imgs[i].src = imgs[i].src.replace(/\/ui\/themes\/[^\/]+\//, '/ui/themes/' + theme_name + '/');
+            }
+        }
+        /* D3 needs a resize event, but it likely doesn't harm to fire one in all cases after changing references */
+        window.dispatchEvent(new Event('resize'));
+    }
+    
+    if (window.matchMedia) {
+        document.addEventListener('DOMContentLoaded',themeSwitcher);
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', themeSwitcher);
+    }
+})();
