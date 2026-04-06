546086d35736131558cf5e0e7e06fec1e3df1651
Ad Schellevis <ad@opnsense.org>
2026-04-06 19:09:02 +0200

    Interfaces: Neighbors: Automatic Discovery - configurable cleanups
    PR: https://github.com/opnsense/core/issues/9953
    
    (cherry picked from commit 26b9169d44d6e897732e5ff40fdfde1b4197ce97)
    (cherry picked from commit a7034c0ede0edbb89c589d9cc47f97410d03c464)
    (cherry picked from commit cdf172056fb579e7af7834a619bdaf39d5fe5024)

--- src/opnsense/service/templates/OPNsense/Hostdiscovery/rc.conf.d
+++ src/opnsense/service/templates/OPNsense/Hostdiscovery/rc.conf.d
@@ -1,13 +1,22 @@
 {% if not helpers.empty('OPNsense.Hostwatch.general.enabled') %}
 hostwatch_enable="YES"
-hostwatch_flags="{% if helpers.empty('OPNsense.Hostwatch.general.promisc') %}-p{% endif %} -c -S"
+hostwatch_flags="-c -S"
+{%  if helpers.empty('OPNsense.Hostwatch.general.promisc') %}
+hostwatch_flags="${hostwatch_flags} -p"
+{%  endif %}
+{%  if not helpers.empty('OPNsense.Hostwatch.general.expire4_interval') %}
+hostwatch_flags="${hostwatch_flags} -E '{{OPNsense.Hostwatch.general.expire4_interval}}'"
+{%  endif %}
+{%  if not helpers.empty('OPNsense.Hostwatch.general.expire6_interval') %}
+hostwatch_flags="${hostwatch_flags} -e '{{OPNsense.Hostwatch.general.expire6_interval}}'"
+{%  endif %}
 hostwatch_pidfile="/var/run/hostwatch/hostwatch.pid"
-{%   if  not helpers.empty('OPNsense.Hostwatch.general.skip_nets') %}
+{%  if not helpers.empty('OPNsense.Hostwatch.general.skip_nets') %}
 hostwatch_skip_nets="{{ OPNsense.Hostwatch.general.skip_nets|replace(',', ' ')}}"
-{%   endif %}
-{%   if  not helpers.empty('OPNsense.Hostwatch.general.interface') %}
+{%  endif %}
+{%  if not helpers.empty('OPNsense.Hostwatch.general.interface') %}
 hostwatch_interfaces="{{ helpers.physical_interfaces(OPNsense.Hostwatch.general.interface.split(','))|join(' ')}}"
-{%   endif %}
+{%  endif %}
 hostwatch_setup="/usr/local/opnsense/scripts/interfaces/setup_hostwatch.sh"
 {% else %}
 hostwatch_enable="NO"
