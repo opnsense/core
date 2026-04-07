1e8b46060161968c796e8c0cf0880898ef28561a
Konstantinos Spartalis <scoon405@gmail.com>
2026-04-07 13:28:53 +0300

    Backups migrate to MVC: initial commit
    * bulk
    
    * ..
    
    * ..
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update backup.xml
    
    * Update BackupController.php
    
    * reorder tabs
    
    * .
    
    * Update backup.volt
    
    * .
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * tabs
    
    * .
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * .
    
    * Update backup.volt
    
    * Update backup.volt
    
    * .
    
    * test
    
    * Revert "test"
    
    This reverts commit 710d2e70a407d868e143dd519ef11262a3918de3.
    
    * test2
    
    * Update backup.volt
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * ..
    
    * Update BackupController.php
    
    * ll
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * l
    
    * v
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Revert "Update backup.volt"
    
    This reverts commit e4df8ddcb9f136f5f83145e6213776eef405f162.
    
    * Revert "Update backup.volt"
    
    This reverts commit 368aec212dda5cd2f7cfe360701b1eaf3e4a15f7.
    
    * Revert "Update BackupController.php"
    
    This reverts commit 0045cba40a7e509e07b81312dc5e25a385fc4943.
    
    * Revert "Update BackupController.php"
    
    This reverts commit 57444ab8f14fcf7d7e13523e0ab3d86d9af3782c.
    
    * Revert "v"
    
    This reverts commit 98f95efbb571dd81913755afa5e41ccef38d7356.
    
    * Update BackupController.php
    
    * Update core.inc
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update backup.volt
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * cron + plugin message
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * .
    
    * Revert "."
    
    This reverts commit 1aebe2b7a588dd9c26af1ee969fe540d61560eff.
    
    * Revert "Update BackupController.php"
    
    This reverts commit e80d9dd0190c0e9479829deb3a0a62081952cf68.
    
    * Update core.inc
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * Update BackupController.php
    
    * .
    
    * 2 checks working
    
    * val1
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * mm
    
    * Revert "mm"
    
    This reverts commit 7f9326b0a4146c0c92523d6f5be548b667bd8bb1.
    
    * Revert "Update backup.volt"
    
    This reverts commit 56ddeaa96d2defb7044a348a21c119813b04e23e.
    
    * Revert "Update backup.volt"
    
    This reverts commit 865c663f5a5d72a0211031ffb7efd17a580602b2.
    
    * Revert "Update backup.volt"
    
    This reverts commit 7dacceef0522cd49262963e241ef76bbf0cdc552.
    
    * Revert "val1"
    
    This reverts commit 5b93a5e834988704f62313601a16d2bc371319bc.
    
    * minor still working
    
    * Update backup.volt
    
    * .
    
    * Update Backup.xml
    
    * 8
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * Update backup.volt
    
    * amusing
    
    * 0
    
    * 00
    
    * Update backup.volt
    
    * 000
    
    * Update backup.volt

--- src/opnsense/mvc/app/controllers/OPNsense/Core/BackupController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Core/BackupController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2023 Deciso B.V.
+ * Copyright (C) 2026 Konstantinos Spartalis <cspartalis@potatonetworks.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,6 +34,46 @@ namespace OPNsense\Core;
  */
 class BackupController extends \OPNsense\Base\IndexController
 {
+    public function indexAction()
+    {
+        require_once("system.inc");
+        require_once("plugins.inc");
+
+        $backupFactory = new \OPNsense\Backup\BackupFactory();
+        $this->view->providers = $backupFactory->listProviders();
+
+        $this->view->backupLocalForm = $this->getForm("backup_local");
+        $this->view->backupRemoteForm = $this->getForm("backup_remote");
+
+        $baksz = '0B';
+        if (is_dir('/conf/backup')) {
+            $files = glob("/conf/backup/*.xml");
+            $bytes = $files ? array_sum(array_map('filesize', $files)) : 0;
+            $baksz = round($bytes / 1024 / 1024, 2) . ' MB';
+        }
+        $this->view->backupSize = $baksz;
+
+        $areas = [
+            'bridges'   => gettext('Bridge Devices'),
+            'gifs'      => gettext('GIF Devices'),
+            'interfaces' => gettext('Interfaces'),
+            'laggs'     => gettext('LAGG Devices'),
+            'ppps'      => gettext('Point-to-Point Devices'),
+            'rrddata'   => gettext('RRD Data'),
+            'vlans'     => gettext('VLAN Devices'),
+            'wireless'  => gettext('Wireless Devices'),
+        ];
+        foreach (plugins_xmlrpc_sync() as $area) {
+            if (!empty($area['section'])) {
+                $areas[$area['section']] = $area['description'];
+            }
+        }
+        natcasesort($areas);
+        $this->view->areas = $areas;
+
+        $this->view->pick('OPNsense/Core/backup');
+    }
+
     public function historyAction($selected_host = null)
     {
         $this->view->selected_host = $selected_host;
