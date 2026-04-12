b3a5707df5b7a6aaae1c21b6b439ada3c7fcdbeb
Franco Fichtner <franco@opnsense.org>
2026-04-12 10:01:21 +0200

    interfaces: minor whitespace change
    (cherry picked from commit f1a3150fa9b2401d515ab651affa125469decc1e)

--- src/opnsense/scripts/interfaces/gen_duid.php
+++ src/opnsense/scripts/interfaces/gen_duid.php
@@ -26,6 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
 require_once("interfaces.inc");
 require_once("config.inc");
 require_once("util.inc");
