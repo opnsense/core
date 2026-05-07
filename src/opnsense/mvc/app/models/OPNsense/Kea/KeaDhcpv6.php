72a60e3964ab819e6a86e48b465313ef4b41dc92
Monviech <gitacc@pischem.com>
2026-05-07 15:19:07 +0200

    Only evaluate the client class inside a subnet scope if its required.

--- src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv6.php
+++ src/opnsense/mvc/app/models/OPNsense/Kea/KeaDhcpv6.php
@@ -315,7 +315,7 @@ class KeaDhcpv6 extends BaseModel
                 $record['user-context']['prefix_source'] = $idassoc['prefix_source'] ?? $if;
                 // If the prefix is temporary placeholder, we will not send leases to any client
                 if (empty($idassoc['prefix_valid'])) {
-                    $record['client-classes'] = ['NO_LEASES_PLEASE'];
+                    $record['require-client-classes'] = ['NO_LEASES_PLEASE'];
                 }
             }
             /* standard option-data elements */
@@ -539,11 +539,12 @@ class KeaDhcpv6 extends BaseModel
         ];
         $client_classes = $this->getConfigClientClasses();
 
-        // If this client_class is added to a subnet scope, the test will always be false
-        // Meaning no client will receive a lease since it cannot pass the test
+        // Used by temporary dynamic-prefix placeholder subnets.
+        // The class can never match, and is only evaluated when required by such a subnet.
         $client_classes[] = [
             'name' => 'NO_LEASES_PLEASE',
             'test' => "not member('ALL')",
+            'only-if-required' => true,
         ];
 
         if (!empty($client_classes)) {
