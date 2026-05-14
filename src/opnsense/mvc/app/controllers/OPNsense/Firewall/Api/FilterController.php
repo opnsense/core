opnsense/core

commit c4aefc08f41167b921595cd3f606aadc72b46730

Firewall: Rules [new] - refactor searchRuleAction() to use the same filtering and sorting logic on MVC and legacy data.
The current implementation is applying our filter logic twice for MVC records, first it uses the default searchBase() construct, which it then needs to pipe through searchRecordsetBase() again. There are a couple of downsides here, it's more expensive (although the user likely won't notice), but also requires duplication of filter logic.

With the logic introduced in https://github.com/opnsense/core/commit/c81417f26747a9e4e46f608c2791bbae805e79fd , we can extract the exact same content from our model so we can merge all at once and push it through our filtering and sorting logic.

The idea is to be able to "smarten" searchRecordsetBase() a bit so we can apply some additional logic based on types at some point in time, which requires all data to passthrough the same pipeline at least.

This commit should be backwards compatible with the previous code.

(cherry picked from commit dad956e3143e7aa26f6be140e4c0ff48d1758e22)
(cherry picked from commit c2dcb5ba2fb4716d0e7880fac0c62680293d1b21)

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -98,36 +98,16 @@ class FilterController extends FilterBaseController
                 }
             }
         }
-
-        /* filter logic for mvc rules */
-        $filter_funct_mvc = function ($record) use ($categories, $interfaces, $show_all) {
-            $is_cat = empty($categories) || array_intersect(explode(',', $record->categories), $categories);
-            $rule_interfaces = $record->interface->getValues();
-
-            // ALL rules, skip interface logic entirely
-            if ($interfaces === null) {
-                return $is_cat;
+        /* extract all mvc records so we can filter and sort all at once */
+        $allrules = [];
+        foreach ($this->getModel()->rules->rule->iterateItems() as $uuid => $record) {
+            $row = ['uuid' => $record->getAttributes()['uuid']];
+            $reflen = strlen($record->__reference) + 1;
+            foreach ($record->getFlatNodes() as $key => $val) {
+                $row[substr($key, $reflen)] = $val->getValue();
             }
-
-            if (!$record->interfacenot->isEmpty()) {
-                $if_intersects = !array_intersect($interfaces, $rule_interfaces); /* All but interface */
-            } else {
-                $if_intersects = array_intersect($interfaces, $rule_interfaces);
-            }
-
-            if (empty($interfaces)) {
-                $is_if = count($rule_interfaces) != 1 || !$record->interfacenot->isEmpty();
-            } elseif ($show_all) {
-                $is_if = $if_intersects || empty($rule_interfaces);
-            } elseif (!$record->interfacenot->isEmpty()) {
-                // Exclude as it should only be returned with show_all
-                $is_if = false;
-            } else {
-                // Include only an exact match, not a partial overlap
-                $is_if = $if_intersects && (count($interfaces) == count($rule_interfaces));
-            }
-            return $is_cat && $is_if;
-        };
+            $allrules[] = $row;
+        }
 
         if ($show_all) {
             /* only query stats when fill info is requested */
@@ -136,39 +116,25 @@ class FilterController extends FilterBaseController
             $rule_stats = [];
         }
 
-
-        $filter_funct_rs  = function (&$record) use ($categories, $interfaces, $rule_stats) {
-            /* always merge stats when found */
-            if (!empty($record['uuid']) && !empty($rule_stats[$record['uuid']])) {
-                $record = array_merge($record, $rule_stats[$record['uuid']]);
-            }
-            /* frontend can format aliases with an alias icon */
-            foreach (['source_net','source_port','destination_net','destination_port'] as $field) {
-                if (!empty($record[$field])) {
-                    $record["alias_meta_{$field}"] = $this->getNetworks($record[$field]);
-                }
-            }
-
-            /* frontend can format categories with colors */
+        $filter_funct_rs = function (&$record) use ($categories, $interfaces, $rule_stats) {
+            /* Filter criteria */
             $r_categories = !empty($record['categories']) ? array_map('trim', explode(',', $record['categories'])) : [];
-            $record['category_colors'] = $this->getCategoryColors($r_categories);
-
-            if (empty($record['legacy'])) {
-                /* mvc already filtered */
-                return true;
-            }
             $is_cat = empty($categories) || array_intersect($r_categories, $categories);
-
-            if (!empty($record['interfacenot'])) {
-                $is_if = !array_intersect(explode(',', $record['interface'] ?? ''), $interfaces ?? []);
+            $rule_interfaces = array_filter(explode(',', $record['interface'] ?? ''));
+            if ($interfaces === null || empty($record['interface'])) {
+                $is_if = true; // ALL interfaces or floating always matches
+            } elseif (!empty($record['interfacenot'])) {
+                $is_if = !array_intersect($rule_interfaces, $interfaces ?? []);
             } else {
-                $is_if = array_intersect(explode(',', $record['interface'] ?? ''), $interfaces ?? []);
+                $is_if = array_intersect($rule_interfaces, $interfaces ?? []);
             }
-            // ALL interfaces or floating always matches
-            $is_if = $is_if || $interfaces === null || empty($record['interface']);
 
-            if ($is_cat && $is_if) {
-                /* translate/convert legacy fields before returning, similar to mvc handling */
+            if (!$is_cat || !$is_if) {
+                return false; /* not reached */
+            }
+
+            /* translate/convert legacy fields before returning, similar to mvc handling */
+            if (!empty($record['legacy'])) {
                 foreach ($this->getLegacyFieldMap() as $topic => $data) {
                     if (!empty($record[$topic])) {
                         $tmp = [];
@@ -178,41 +144,41 @@ class FilterController extends FilterBaseController
                         $record[$topic] = implode(',', $tmp);
                     }
                 }
-                // Tag legacy rules as "Automatic generated rules" if they have an empty category
-                if (!empty($record['is_automatic'])) {
-                    $label = gettext('Automatically generated rules');
-                    $record['categories'] = $label;  // Grouping key for tree view
-                    $record['category_colors'] = [['name'  => $label]];  // Category formatter metadata
-                }
-
-                return true;
-            } else {
-                return false;
             }
-        };
 
-        /**
-         * XXX: fetch mvc results first, we need to collect all to ensure proper pagination
-         *      as pagination is passed using the request, we need to reset it temporary here as we don't know
-         *      which page we need (yet) and don't want to duplicate large portions of code.
-         **/
-        $ORG_REQ = $_REQUEST;
-        unset($_REQUEST['rowCount']);
-        unset($_REQUEST['current']);
-        if ($show_all) {
-            /* searchBase should not filter here since we later search for IP addresses in aliases */
-            unset($_REQUEST['searchPhrase']);
-        }
-        $filterset = $this->searchBase("rules.rule", null, "sort_order", $filter_funct_mvc)['rows'];
+            /* Formatting */
+
+            /* always merge stats when found */
+            if (!empty($record['uuid']) && !empty($rule_stats[$record['uuid']])) {
+                $record = array_merge($record, $rule_stats[$record['uuid']]);
+            }
+
+            // Tag legacy rules as "Automatic generated rules" if they have an empty category
+            if (!empty($record['is_automatic'])) {
+                $label = gettext('Automatically generated rules');
+                $record['categories'] = $label;  // Grouping key for tree view
+                $record['category_colors'] = [['name'  => $label]];  // Category formatter metadata
+            }
+
+            /* frontend can format aliases with an alias icon */
+            foreach (['source_net','source_port','destination_net','destination_port'] as $field) {
+                if (!empty($record[$field])) {
+                    $record["alias_meta_{$field}"] = $this->getNetworks($record[$field]);
+                }
+            }
+
+            /* frontend can format categories with colors */
+            $record['category_colors'] = $this->getCategoryColors($r_categories);
+            return true;
+        };
 
         /* only fetch internal and legacy rules when 'show_all' is set */
         if ($show_all) {
-            $otherrules = json_decode((new Backend())->configdRun('filter list non_mvc_rules'), true) ?? [];
-        } else {
-            $otherrules = [];
+            $allrules = array_merge(
+                $allrules,
+                json_decode((new Backend())->configdRun('filter list non_mvc_rules'), true) ?? []
+            );
         }
-
-        $_REQUEST = $ORG_REQ; /* XXX:  fix me ?*/
 
         $search_clauses = [];
         $backend = new Backend();
@@ -229,7 +195,7 @@ class FilterController extends FilterBaseController
             }
         }
         $result = $this->searchRecordsetBase(
-            array_merge($otherrules, $filterset),
+            $allrules,
             null,
             "sort_order",
             $filter_funct_rs,

commit 9b8ee2a92f14092f6971a96c86ca2fe138cd7b89

Firewall: Rules [new] - fix unintended change in filtering logic caused by https://github.com/opnsense/core/commit/c4aefc08f41167b921595cd3f606aadc72b46730
When inspect mode is not selected, only full matches are expected when filtering specific interfaces.
This also means when selecting the inverse of an interface, it automatically belongs to the "floating" group for not being a direct match.

To explain the options a bit better, comments are inserted in the filtereing block.

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -121,19 +121,25 @@ class FilterController extends FilterBaseController
             $rule_stats = [];
         }
 
-        $filter_funct_rs = function (&$record) use ($categories, $interfaces, $rule_stats) {
+        $filter_funct_rs = function (&$record) use ($categories, $interfaces, $rule_stats, $show_all) {
             /* Filter criteria */
             $r_categories = !empty($record['categories']) ? array_map('trim', explode(',', $record['categories'])) : [];
             $is_cat = empty($categories) || array_intersect($r_categories, $categories);
             $rule_interfaces = array_filter(explode(',', $record['interface'] ?? ''));
-            if ($interfaces === null || empty($record['interface'])) {
+            if ($interfaces === null || (empty($record['interface']) && $show_all)) {
+                /* ALL interfaces always matches, when inspecting, also show rules that apply to all  */
                 $is_if = true; // ALL interfaces or floating always matches
-            } elseif (!empty($record['interfacenot'])) {
+            } elseif (!empty($record['interfacenot']) && $show_all) {
+                /* Inverted interface, show where applicable when inspecting */
                 $is_if = !array_intersect($rule_interfaces, $interfaces ?? []);
-            } elseif (empty($interfaces) && count($rule_interfaces) > 1) {
-                $is_if = true; // floating, multiple interfaces selected
+            } elseif (empty($interfaces) && (count($rule_interfaces) > 1 || !empty($record['interfacenot']))) {
+                /* Floating, multiple interfaces selected */
+                $is_if = true;
             } else {
-                $is_if = array_intersect($rule_interfaces, $interfaces ?? []);
+                /* Interfaces overlap, when inspecting all overlaps are relevant, otherwise only exact matches */
+                $is_if = array_intersect($rule_interfaces, $interfaces ?? []) && (
+                    count($rule_interfaces) == 1 || $show_all
+                ) && empty($record['interfacenot']);
             }
 
             if (!$is_cat || !$is_if) {