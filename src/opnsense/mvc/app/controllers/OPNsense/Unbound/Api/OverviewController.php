d7f455bdfb3586eb943d533edded011bd9972014
Konstantinos Spartalis <scoon405@gmail.com>
2026-05-21 12:13:21 +0300

    unbound: blocklist improvements (#10149)
    * Organizes DNSBLs by provider/category.
    * Adds the Social Network blocklist by hegizi.
    * The tester now gives you the DNSBL name and category instead of its shortcode.

--- src/opnsense/mvc/app/controllers/OPNsense/Unbound/Api/OverviewController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Unbound/Api/OverviewController.php
@@ -72,8 +72,12 @@ class OverviewController extends ApiControllerBase
             return [];
         }
 
+        $types = $this->mdl->dnsbl->blocklist->getTemplateNode()->type->getNodeData();
         foreach ($parsed['top_blocked'] as $domain => $props) {
-            $parsed['top_blocked'][$domain]['blocklist'] ??= $this->getBlocklistDescription($props['blocklist']);
+            if (isset($types[$props['blocklist']]['optgroup'])) {
+                $parsed['top_blocked'][$domain]['category'] = $types[$props['blocklist']]['optgroup'];
+            }
+            $parsed['top_blocked'][$domain]['blocklist'] = $types[$props['blocklist']]['value'] ?? $props['blocklist'];
         }
 
         return $parsed;
@@ -100,6 +104,9 @@ class OverviewController extends ApiControllerBase
         $types = $this->mdl->dnsbl->blocklist->getTemplateNode()->type->getNodeData();
 
         foreach ($parsed as $idx => $query) {
+            if (isset($types[$query['blocklist']]['optgroup'])) {
+                $parsed[$idx]['category'] = $types[$query['blocklist']]['optgroup'];
+            }
             $parsed[$idx]['blocklist'] = $types[$query['blocklist']]['value'] ?? $query['blocklist'];
             $parsed[$idx]['policy'] = $policies[$query['uuid']]['description'] ?? '';
             /* Handle front-end color status mapping, start off with OK */
