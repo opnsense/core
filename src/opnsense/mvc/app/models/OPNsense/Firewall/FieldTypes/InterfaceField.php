fe52ea7dcd56d91e9e8a05be3e788a1a9953ed09
Ad Schellevis <ad@opnsense.org>
2026-05-24 10:15:20 +0200

    Firewall - fix interface sortng by value for live-log and groups, closes https://github.com/opnsense/core/issues/10348
    We might consider moving the sorting logic to BaseListField at some point as most cases expect sort by value anyway and we only have a couple of exceptions to that logic.
    
    (cherry picked from commit 6ffad1bb73da9db36c409130fcf00a9af286f94f)

--- src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/InterfaceField.php
+++ src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/InterfaceField.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2023 Deciso B.V.
+ * Copyright (C) 2023-2026 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,6 +50,7 @@ class InterfaceField extends BaseListField
             }
         }
         $this->internalOptionList = self::$parent_interfaces;
+        natcasesort($this->internalOptionList);
         return parent::actionPostLoadingEvent();
     }
 }
