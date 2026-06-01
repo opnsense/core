f8ff0735104d4f861a45358ff9d04ef2b8cb2be6
Franco Fichtner <franco@opnsense.org>
2026-06-01 10:12:15 +0200

    src: typo

--- src/opnsense/scripts/health/updaterrd.php
+++ src/opnsense/scripts/health/updaterrd.php
@@ -2,7 +2,7 @@
 <?php
 
 /*
- * Copyright (C) 2024-206 Deciso B.V.
+ * Copyright (C) 2024-2026 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
