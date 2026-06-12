fd57498a9eaf2140f14d7c59f43bf39a1eb4b93c
Konstantinos Spartalis <scoon405@gmail.com>
2026-06-12 16:52:53 +0300

    Update opnsense_bootgrid.js

--- src/opnsense/www/js/opnsense_bootgrid.js
+++ src/opnsense/www/js/opnsense_bootgrid.js
@@ -455,6 +455,7 @@ class UIBootgrid {
 
         if (bootGridOptions?.disableScroll ?? false) {
             this.options.disableScroll = true;
+            this.compatOptions['height'] = false;
         }
 
         this.tabulatorOptions = compatOptions.tabulatorOptions ??= {};
