8ff00fa61aceef434f8efd4723b87463615cde91
Konstantinos Spartalis <scoon405@gmail.com>
2026-06-12 12:41:10 +0300

    ui: fix excessive padding on apply section (#10409)
    (cherry picked from commit 7a53cf1547beac857393ef97f4008bb91b5d4a52)
    (cherry picked from commit 70853ba8ac3f6ec0e80cf639539e3da1b141d6bb)

--- src/opnsense/www/js/opnsense_ui.js
+++ src/opnsense/www/js/opnsense_ui.js
@@ -657,9 +657,9 @@ $.fn.SimpleActionButton = function (params) {
         if (excludeScope) {
             const toggleVisiblity = (tabId) => {
                 if (excludeScope.split(',').map(s => s.trim()).includes(tabId)) {
-                    this_button.closest('.page-content-main').hide();
+                    this_button.closest('section').hide();
                 } else {
-                    this_button.closest('.page-content-main').show();
+                    this_button.closest('section').show();
                 }
             }
 
