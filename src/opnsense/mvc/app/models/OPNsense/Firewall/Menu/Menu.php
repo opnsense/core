1aceb823dabc64cfc7098dd48b48f4c0a2e3fd9a
Monviech <gitacc@pischem.com>
2026-06-30 08:29:48 +0200

    Firewall: Migration Assistant: Show migration assistant if at least one Outbound NAT rule exists, remove conditional hiding based on NAT mode

--- src/opnsense/mvc/app/models/OPNsense/Firewall/Menu/Menu.php
+++ src/opnsense/mvc/app/models/OPNsense/Firewall/Menu/Menu.php
@@ -50,10 +50,7 @@ class Menu extends MenuContainer
         // add interfaces to "Firewall: Rules" menu tab...
         $has_legacy_fw = !empty($config->filter?->rule?->count());
         $has_mvc_fw = !empty($config->OPNsense?->Firewall?->Filter?->rules?->count());
-        $has_legacy_outbound_nat = (
-            !empty($config->nat?->outbound?->rule?->count()) &&
-            !in_array((string)($config->nat?->outbound?->mode), ['automatic', 'disabled'], true)
-        );
+        $has_legacy_outbound_nat = !empty($config->nat?->outbound?->rule?->count());
         if ($has_legacy_fw || $has_legacy_outbound_nat) {
             $this->appendItem('Firewall', 'Migration', [
                 'url' => '/ui/firewall/migration',
