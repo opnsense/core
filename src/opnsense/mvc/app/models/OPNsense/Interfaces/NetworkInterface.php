45f9214fb5a7645eaa65467fdda579da673aab01
Kemoy Campbell <kscics@rit.edu>
2026-07-23 14:25:37 -0400

    modify the serializeToConfig to set the uuid of the new network ident… (#10600)
    (cherry picked from commit 68c017486468f7a6b29327ff4ee178af8e3a3f25)
    (cherry picked from commit 221f639176d7b50d409d13c5b2bceb2f6937497a)

--- src/opnsense/mvc/app/models/OPNsense/Interfaces/NetworkInterface.php
+++ src/opnsense/mvc/app/models/OPNsense/Interfaces/NetworkInterface.php
@@ -137,12 +137,18 @@ class NetworkInterface extends BaseModel
         }
 
         foreach ($interfaces as $key => $intf) {
+            $newIdentifier = 'opt' . $next_if;
             if (!isset(Config::getInstance()->object()->interfaces->$key)) {
-                $newif = Config::getInstance()->object()->interfaces->addChild('opt' . $next_if);
+                $newif = Config::getInstance()->object()->interfaces->addChild($newIdentifier);
                 $newif->if = $intf['if'];
                 $newif->descr = $intf['descr'];
                 $newif->lock = $intf['lock'];
                 $next_if++;
+
+                /* We want the node to return the new network identifier as the uuid not
+                the internal random generated uuid by the ArrayField.
+                Assignments use identifier as the uuid in the config.xml */
+                $this->getNodeByReference('interface.' . $key)?->setAttributeValue('uuid', $newIdentifier);
             }
         }
         return true;
