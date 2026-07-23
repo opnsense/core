opnsense/core

commit f10505148f1b7dd1d8210b18d3a8d8b185a1bd4e

Gateway Groups: convert to MVC (#10205)
Code performs an inline migration and updates only some callers that directly depended on the old configuration bits. Some convenience functions have been added to GatewayGroups.php to allow for easy configuration access and to satisfy some of the caller demands.

In addition, this code Adjusts the gateway_watcher to now trigger based on Gateway Group trigger level as well, limiting the amount of times alarms/alerts are being generated (and thus potentially unnecessary fw reconfigurations).

src/opnsense/scripts/routes/gateway_watcher.php
@@ -35,9 +35,11 @@ require_once 'plugins.inc.d/dpinger.inc';
 function signalhandler($signal)
 {
     global $config;
+    global $gwgroups;
 
     OPNsense\Core\Config::getInstance()->forceReload();
     $config = parse_config();
+    $gwgroups = new \OPNsense\Routing\GatewayGroups();
 
     syslog(LOG_NOTICE, 'Reloaded gateway watcher configuration on SIGHUP');
 }
@@ -55,6 +57,7 @@ $cache_file = '/tmp/gateways.status';
 @unlink($cache_file);
 
 $mode = [];
+$gwgroups = new \OPNsense\Routing\GatewayGroups();
 
 sleep($wait);
 
@@ -105,19 +108,8 @@ while (1) {
             }
         }
 
-        foreach (config_read_array('gateways', 'gateway_group') as $group) {
-            foreach ($group['item'] as $item) {
-                $itemsplit = explode('|', $item);
-                if ($itemsplit[0] == $report['name']) {
-                    /* consider all state transitions as they depend on individual trigger setting */
-                    if (!empty($rprev) && $rprev != $rcurr) {
-                        /* XXX consider trigger conditions later on */
-                        $ralarm = true;
-                        break;
-                    }
-                }
-            }
-        }
+        /* determine transition based on individual gateway group trigger levels */
+        $ralarm = $gwgroups->gatewayStateChange($report['name'], $rprev, $rcurr);
 
         if ($ralarm) {
             $alarm_gateways[] = $report['name'];

plist
@@ -452,9 +452,12 @@
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routes/Api/RoutesController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routes/IndexController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routes/forms/dialogEditRoute.xml
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/Api/GroupSettingsController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/Api/SettingsController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/ConfigurationController.php
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/GatewayGroupsController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/forms/dialogEditGateway.xml
+/usr/local/opnsense/mvc/app/controllers/OPNsense/Routing/forms/dialogEditGatewayGroup.xml
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Syslog/Api/ServiceController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Syslog/Api/SettingsController.php
 /usr/local/opnsense/mvc/app/controllers/OPNsense/Syslog/IndexController.php
@@ -887,6 +890,9 @@
 /usr/local/opnsense/mvc/app/models/OPNsense/Routes/Route.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Routes/Route.xml
 /usr/local/opnsense/mvc/app/models/OPNsense/Routing/FieldTypes/GatewayField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Routing/FieldTypes/GatewayGroupItemField.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Routing/GatewayGroups.php
+/usr/local/opnsense/mvc/app/models/OPNsense/Routing/GatewayGroups.xml
 /usr/local/opnsense/mvc/app/models/OPNsense/Routing/Gateways.php
 /usr/local/opnsense/mvc/app/models/OPNsense/Routing/Gateways.xml
 /usr/local/opnsense/mvc/app/models/OPNsense/Routing/Migrations/M1_0_0.php
@@ -1026,6 +1032,7 @@
 /usr/local/opnsense/mvc/app/views/OPNsense/Radvd/settings.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/Routes/index.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/Routing/configuration.volt
+/usr/local/opnsense/mvc/app/views/OPNsense/Routing/groups.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/Syslog/index.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/TrafficShaper/index.volt
 /usr/local/opnsense/mvc/app/views/OPNsense/TrafficShaper/statistics.volt
@@ -2560,8 +2567,6 @@
 /usr/local/www/system_advanced_firewall.php
 /usr/local/www/system_advanced_misc.php
 /usr/local/www/system_authservers.php
-/usr/local/www/system_gateway_groups.php
-/usr/local/www/system_gateway_groups_edit.php
 /usr/local/www/system_general.php
 /usr/local/www/system_usermanager_passwordmg.php
 /usr/local/www/system_usermanager_settings_ldapacpicker.php

src/etc/inc/filter.lib.inc
@@ -56,6 +56,7 @@ function filter_core_get_initialized_plugin_system()
     $fw = new \OPNsense\Firewall\Plugin();
     $ifdetails = legacy_interfaces_details();
     $gateways = new \OPNsense\Routing\Gateways();
+    $gatewaygroups = new \OPNsense\Routing\GatewayGroups();
     $cnfint = legacy_config_get_interfaces(["enable" => true]);
     foreach ($cnfint as $key => &$value) {
         // to set "reply-to" we need to know the gateway for our interface, let's collect it here and pass it on to
@@ -82,7 +83,7 @@ function filter_core_get_initialized_plugin_system()
     $fw->setInterfaceMapping($cnfint);
     $fw->setGateways($gateways);
     $fw->setIfconfigDetails($ifdetails);
-    $fw->setGatewayGroups($gateways->getGroups(return_gateways_status()));
+    $fw->setGatewayGroups($gatewaygroups->getActiveGroups(return_gateways_status()));
     return $fw;
 }
 

src/opnsense/mvc/app/controllers/OPNsense/Routes/Api/GatewayController.php
@@ -44,7 +44,7 @@ class GatewayController extends ApiControllerBase
         $gw_status = json_decode($backend->configdRun('interface gateways status'), true);
 
         if (!empty($gw_status)) {
-            $result['items'] = $gw_status;
+            $result['items'] = array_Values($gw_status);
             $result['status'] = "ok";
         }
 

src/opnsense/mvc/app/controllers/OPNsense/Routing/Api/GroupSettingsController.php
@@ -1,0 +1,146 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace OPNsense\Routing\Api;
+
+use OPNsense\Base\ApiMutableModelControllerBase;
+use OPNsense\Base\UserException;
+use OPNsense\Core\Backend;
+use OPNsense\Core\Config;
+use OPNsense\Routing\GatewayGroups;
+
+class GroupSettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelClass = '\OPNsense\Routing\GatewayGroups';
+    protected static $internalModelName = 'gateway_group';
+
+    public function reconfigureAction()
+    {
+        $result = ["status" => "failed"];
+        if ($this->request->isPost()) {
+            (new Backend())->configdRun('interface routes configure');
+            $result = ["status" => "ok"];
+        }
+
+        return $result;
+    }
+
+    public function searchAction()
+    {
+        $gateways_status = json_decode((new Backend())->configdRun('interface gateways status'), true) ?? [];
+        $config = (new GatewayGroups())->getGroupsConfig();
+        $result = $this->searchBase('gateway_group');
+
+        foreach ($result['rows'] as $idx => $group) {
+            foreach ($config[$group['name']]['tiers'] as $tieridx => $gws) {
+                $result['rows'][$idx]['gateways'][$tieridx] = [];
+                foreach ($gws as $gwname) {
+                    if (empty($gwname)) {
+                        continue;
+                    }
+
+                    if (!empty($gateways_status[$gwname])) {
+                        $gateways_status[$gwname]['label'] = 'default';
+                        if (str_contains($gateways_status[$gwname]['status'], 'down')) {
+                            $gateways_status[$gwname]['label'] = 'danger';
+                        } else if (str_contains($gateways_status[$gwname]['status'], 'loss') || str_contains($gateways_status[$gwname]['status'], 'delay')) {
+                            $gateways_status[$gwname]['label'] = 'warning';
+                        } else if (str_contains($gateways_status[$gwname]['status'], 'none')) {
+                            $gateways_status[$gwname]['label'] = 'success';
+                        }
+                    } else {
+                        $gateways_status[$gwname] = [
+                            'name' => $gwname,
+                            'label' => 'danger',
+                            'status_translated' => gettext('Disabled or inactive')
+                        ];
+                    }
+
+                    $result['rows'][$idx]['gateways'][$tieridx][] = $gateways_status[$gwname];
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function getAction($uuid = null)
+    {
+        return $this->getBase('gateway_group', 'gateway_group', $uuid);
+    }
+
+    public function setAction($uuid = null)
+    {
+        return $this->setBase('gateway_group', 'gateway_group', $uuid);
+    }
+
+    public function addAction()
+    {
+        return $this->addBase('gateway_group', 'gateway_group');
+    }
+
+    public function delAction($uuids)
+    {
+        $result = ["result" => "failed"];
+
+        if ($this->request->isPost()) {
+            Config::getInstance()->lock();
+            $groups = new GatewayGroups();
+
+            foreach ((!empty($uuids) ? explode(",", $uuids) : []) as $uuid) {
+                $node = $groups->getNodeByReference('gateway_group.' . $uuid);
+                if ($node != null) {
+                    $name = $node->name->getValue();
+                    foreach (Config::getInstance()->object()->xpath("//*[text() = '{$name}']") as $node) {
+                        if ($node->getName() == 'gateway') {
+                            $referring_node = $node->xpath("..")[0];
+                            $descr = "";
+                            foreach (["description", "descr", "name"] as $key) {
+                                if (!empty($referring_node->$key)) {
+                                    $descr = (string)$referring_node->$key;
+                                    break;
+                                }
+                            }
+                            $message = sprintf(
+                                gettext("Gateway group %s in use by %s %s (%s)"),
+                                $name, $referring_node->getName(),
+                                $descr,
+                                $referring_node->attributes()['uuid']
+                            );
+                            throw new UserException($message);
+                        }
+                    }
+                }
+            }
+
+            $result = $this->delBase('gateway_group', $uuids);
+        }
+
+        return $result;
+    }
+}

src/opnsense/mvc/app/controllers/OPNsense/Routing/Api/SettingsController.php
@@ -95,18 +95,18 @@ class SettingsController extends ApiMutableModelControllerBase
             $gateways[$idx]['interface_descr'] = (string)$cfg->interfaces->{$gateway['interface']}->descr ?: strtoupper($gateway['interface']);
 
             /* parse gateway and monitoring status */
-            $i = array_search($gateway['name'], array_column($gateways_status, 'name'));
-            $gateways[$idx]['status'] = $i !== false ? $gateways_status[$i]['status_translated'] : 'Pending';
+            $this_gw_status = $gateways_status[$gateway['name']] ?? [];
+            $gateways[$idx]['status'] = $this_gw_status['status_translated'] ?? gettext('Pending');
             foreach (['delay', 'stddev', 'loss'] as $status_kw) {
-                $gateways[$idx][$status_kw] = $i !== false ? $gateways_status[$i][$status_kw] : '~';
+                $gateways[$idx][$status_kw] = $this_gw_status[$status_kw] ?? '~';
             }
             $gateways[$idx]['label_class'] = 'fa fa-plug text-default';
-            if ($i !== false) {
-                if (str_contains($gateways_status[$i]['status'], 'down')) {
+            if (!empty($this_gw_status)) {
+                if (str_contains($this_gw_status['status'], 'down')) {
                     $gateways[$idx]['label_class'] = 'fa fa-plug text-danger';
-                } elseif (str_contains($gateways_status[$i]['status'], 'loss') || str_contains($gateways_status[$i]['status'], 'delay')) {
+                } elseif (str_contains($this_gw_status['status'], 'loss') || str_contains($this_gw_status['status'], 'delay')) {
                     $gateways[$idx]['label_class'] = 'fa fa-plug text-warning';
-                } elseif (str_contains($gateways_status[$i]['status'], 'none')) {
+                } elseif (str_contains($this_gw_status['status'], 'none')) {
                     $gateways[$idx]['label_class'] = 'fa fa-plug text-success';
                 }
             } elseif (empty($gateway['disabled']) && !empty($gateway['monitor_disable'])) {
@@ -211,23 +211,15 @@ class SettingsController extends ApiMutableModelControllerBase
                     }
                 }
 
-                $groups = [];
-                foreach ($cfg->gateways->children() as $tag => $gw_group) {
-                    if ($tag == 'gateway_group' && !empty($gw_group)) {
-                        foreach ($gw_group->item as $item) {
-                            $name = explode("|", (string)$item);
-                            if ($name[0] == $gateway->name) {
-                                $groups[] = (string)$gw_group->name;
-                            }
-                        }
-                    }
-                }
-
+                $groups = (new \OPNsense\Routing\GatewayGroups())->gatewayGroupsByGateway($gateway->name->getValue());
                 if (!empty($groups)) {
+                    $names = array_map(function($item) {
+                        return $item->name->getValue();
+                    }, $groups);
                     throw new UserException(sprintf(
                         gettext("Gateway %s cannot be deleted because it is in use on Gateway Group(s) '%s'"),
                         $gateway->name,
-                        implode(', ', $groups)
+                        implode(', ', $names)
                     ));
                 }
 

src/opnsense/mvc/app/controllers/OPNsense/Routing/GatewayGroupsController.php
@@ -1,8 +1,7 @@
-#!/usr/local/bin/php
 <?php
 
 /*
- * Copyright (c) 2017 Fabian Franz
+ * Copyright (C) 2026 Deciso B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,36 +26,16 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-require_once("script/load_phalcon.php");
+namespace OPNsense\Routing;
 
-use OPNsense\Firewall\Util;
-use OPNsense\Routing\Gateways;
-use OPNsense\Routing\GatewayGroups;
+use \OPNsense\Base\IndexController;
 
-$opts = getopt('ghl', [], $optind);
-$args = array_slice($argv, $optind);
-
-if (isset($opts['h'])) {
-    echo "Usage: gateways.php [-g] [-h]\n\n";
-    echo "\t-g add gateway groups\n";
-    echo "\t-l exclude disabled, localhost, inactive gateways\n";
-} else {
-    $mdl = new Gateways();
-    $gateways = isset($opts['l']) ? $mdl->gatewaysIndexedByName() : $mdl->gatewaysIndexedByName(true, true, true);
-
-    $ret = [];
-
-    foreach ($gateways as $gateway) {
-        if (Util::isIpAddress($gateway['gateway'] ?? '')) {
-            $ret[$gateway['name']] = "{$gateway['name']} - {$gateway['gateway']}";
-        } else {
-            $ret[$gateway['name']] = "{$gateway['name']} - {$gateway['ipprotocol']}";
-        }
+class GatewayGroupsController extends IndexController
+{
+    public function indexAction()
+    {
+        $this->view->formDialogEditGatewayGroup = $this->getForm('dialogEditGatewayGroup');
+        $this->view->formGridGatewayGroups = $this->getFormGrid('dialogEditGatewayGroup');
+        $this->view->pick('OPNsense/Routing/groups');
     }
-    if (isset($opts['g'])) {
-        foreach ((new GatewayGroups())->getGroupNames() as $name) {
-            $ret[$name] = $name;
-        }
-    }
-    echo json_encode($ret) . PHP_EOL;
 }

src/opnsense/mvc/app/controllers/OPNsense/Routing/forms/dialogEditGatewayGroup.xml
@@ -1,0 +1,78 @@
+<form>
+    <field>
+        <id>gateway_group.name</id>
+        <label>Name</label>
+        <type>text</type>
+    </field>
+    <field>
+        <id>gateway_group.gateways</id>
+        <type>hidden</type>
+        <label>Gateways</label>
+        <grid_view>
+            <formatter>gateways</formatter>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.item</id>
+        <label>Tier 1</label>
+        <type>select_multiple</type>
+        <style>selectpicker</style>
+        <grid_view>
+            <visible>false</visible>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.item2</id>
+        <label>Tier 2</label>
+        <type>select_multiple</type>
+        <grid_view>
+            <visible>false</visible>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.item3</id>
+        <label>Tier 3</label>
+        <type>select_multiple</type>
+        <grid_view>
+            <visible>false</visible>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.item4</id>
+        <label>Tier 4</label>
+        <type>select_multiple</type>
+        <grid_view>
+            <visible>false</visible>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.item5</id>
+        <label>Tier 5</label>
+        <type>select_multiple</type>
+        <grid_view>
+            <visible>false</visible>
+        </grid_view>
+    </field>
+    <field>
+        <id>gateway_group.trigger</id>
+        <label>Trigger Level</label>
+        <type>dropdown</type>
+        <help>When to trigger exclusion of a member</help>
+    </field>
+    <field>
+        <id>gateway_group.poolopts</id>
+        <label>Pool Options</label>
+        <type>dropdown</type>
+        <help>
+            Default: Round Robin, Sticky Address determined by advanced settings
+            Round Robin: Loops through the translation addresses.
+            Sticky Address: The Sticky Address option can be used with the Round Robin pool type to ensure that a particular source address is always mapped to the same translation address.
+        </help>
+    </field>
+    <field>
+        <id>gateway_group.descr</id>
+        <label>Description</label>
+        <type>text</type>
+        <help>You may enter a description here for your reference (not parsed). </help>
+    </field>
+</form>

commit 94b3771b774f86d12f17fc23db4c594bd78b18aa

system: regression in f10505148f1b7d clears $ralarm if true for default gateway switching

src/opnsense/scripts/routes/gateway_watcher.php
@@ -109,7 +109,7 @@ while (1) {
         }
 
         /* determine transition based on individual gateway group trigger levels */
-        $ralarm = $gwgroups->gatewayStateChange($report['name'], $rprev, $rcurr);
+        $ralarm |= $gwgroups->gatewayStateChange($report['name'], $rprev, $rcurr);
 
         if ($ralarm) {
             $alarm_gateways[] = $report['name'];