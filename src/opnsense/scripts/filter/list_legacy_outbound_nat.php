64ad2017d930455a5cdde43ecc0cbb200b4e870c
Monviech <gitacc@pischem.com>
2026-08-28 14:35:14 +0200

    Firewall: NAT: Source NAT: add pool options

--- src/opnsense/scripts/filter/list_legacy_outbound_nat.php
+++ src/opnsense/scripts/filter/list_legacy_outbound_nat.php
@@ -115,6 +115,7 @@ if (count($nat_rules)) {
             'destination_port' => normalize_port($rule['dstport'] ?? ''),
             'target' => legacy_target_to_network($rule),
             'target_port' => normalize_port($rule['natport'] ?? ''),
+            'poolopts' => $rule['poolopts'] ?? '',
             'staticnatport' => !empty($rule['staticnatport']) ? '1' : '0',
             'log' => !empty($rule['log']) ? '1' : '0',
             'categories' => $rule['category'] ?? '',
