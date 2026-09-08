20832ffbc168da9adae4a0422d28ea8991377c96
Franco Fichtner <franco@opnsense.org>
2026-09-08 13:43:58 +0200

    firewall: render readable timestamp in audit metadata; closes #10852
    (cherry picked from commit 650291905a567f40763bc0f3ba49776b594059c9)

--- src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/JsonAuditField.php
+++ src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/JsonAuditField.php
@@ -139,9 +139,12 @@ class JsonAuditField extends JsonField implements IStructuredInput
     public function getNodeData()
     {
         // Always return the full structure to prevent stale values in reused forms.
-        return array_replace_recursive(
-            self::SCHEMA,
-            $this->deserialize()
-        );
+        $ret = array_replace_recursive( self::SCHEMA, $this->deserialize());
+        foreach(['created', 'updated'] as $type) {
+            if (!empty($ret[$type]['time'])) {
+                $ret[$type]['%time'] = date('Y-m-d H:i:s', (int)$ret[$type]['time']);;
+            }
+        }
+        return $ret;
     }
 }
