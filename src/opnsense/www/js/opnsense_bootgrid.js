opnsense/core

commit 050f3c8277a33db3245021e28fbbfde4fdc46c0b

bootgrid: ensure a minimum amount of rows to render
This is a continuation of https://github.com/opnsense/core/pull/10408

(cherry picked from commit 87cb2d055dd0cb7e07ab30e8656c020a82803a91)

src/opnsense/www/js/opnsense_bootgrid.js
@@ -660,6 +660,7 @@ class UIBootgrid {
 
         const scrollbarGutterOffset = 16;
         const defaultHeight = 120;
+        const minimumRowCount = 10;
 
         const tableEl = document.getElementById(this.id);
         const holderEl = tableEl?.querySelector(".tabulator-tableholder");
@@ -670,13 +671,23 @@ class UIBootgrid {
         const holderHeight = holderEl.offsetHeight;
 
         let nextContentHeight;
+        let minimumHeight = defaultHeight;
+        const visibleRows = this.table.getRows("visible");
+        if (visibleRows.length > 0) {
+            const rowsHeight = visibleRows.slice(0, minimumRowCount).reduce((height, row) => {
+                return height + (row.getElement()?.offsetHeight || 0);
+            }, 0);
+            const headerHeight = tableEl.querySelector(".tabulator-header")?.offsetHeight || 0;
+            const footerHeight = tableEl.querySelector(".tabulator-footer")?.offsetHeight || 0;
+            minimumHeight = rowsHeight + headerHeight + footerHeight;
+        }
 
         if (!this.dataAvailable && !this.loading && holderHeight > this.tableHeight) {
-            nextContentHeight = defaultHeight;
+            nextContentHeight = minimumHeight;
         } else {
             const adjustedHeight = currentTotalHeight + (this.tableHeight - holderHeight);
             nextContentHeight = Math.min(adjustedHeight, this.pageHeight);
-            nextContentHeight = Math.max(defaultHeight, nextContentHeight);
+            nextContentHeight = Math.max(minimumHeight, nextContentHeight);
         }
 
         const nextHeight = nextContentHeight + scrollbarGutterOffset;
@@ -888,9 +899,23 @@ class UIBootgrid {
         this.table.on('scrollVertical', (top) => {
             this.scrollPos = top;
         });
-        this.table.on('renderComplete', (top) => {
+        this.table.on('renderComplete', () => {
             /* tooltips may stick, remove them on redraw */
             $("div.tooltip.fade.top.in").remove();
+
+            if (this.isResizing) {
+                return;
+            }
+
+            // Schedule a dimension change to prevent recursion
+            this.isResizing = true;
+            requestAnimationFrame(() => {
+                try {
+                    this._onDimensionChange();
+                } finally {
+                    this.isResizing = false;
+                }
+            });
         });
     }
 

commit 6e3cda346c858d0b070b77439bdd322d6a0db7c1

bootgrid: only schedule dimension changes after data processing, window resizing and table visibility changes
The height recalculation scheduled on renderComplete was too aggressive,
since Tabulator emits that event while virtual rows are rendered during scrolling.
Also prevent setHeight() from running mid-scroll at all times.

This commit fixes the visual glitch of resetting scroll positions introduced
by https://github.com/opnsense/core/commit/050f3c8277

src/opnsense/www/js/opnsense_bootgrid.js
@@ -128,6 +128,8 @@ class UIBootgrid {
         this.rememberedTreeIds = new Set(JSON.parse(localStorage.getItem(this.treeStorageKey) || '[]'));
         this.isVisible = false;
         this.scrollPos = 0;
+        this.isScrolling = false;
+        this.scrollEndTimer = null;
         this.addButton = false;
         this.deleteSelectedButton = false;
 
@@ -697,6 +699,21 @@ class UIBootgrid {
         }
     }
 
+    _scheduleDimensionChange() {
+        if (this.isResizing) {
+            return;
+        }
+
+        this.isResizing = true;
+        requestAnimationFrame(() => {
+            try {
+                this._onDimensionChange();
+            } finally {
+                this.isResizing = false;
+            }
+        });
+    }
+
     _registerEvents() {
         this.table.on('dataLoading', () => {
             this._getPlaceholder().html('');
@@ -713,6 +730,7 @@ class UIBootgrid {
                 // this is mainly intended for scaling the width of the table if
                 // the width of the window changes.
                 this.table.redraw();
+                this._scheduleDimensionChange();
             }));
 
             if (!this.options.ajax) {
@@ -869,6 +887,10 @@ class UIBootgrid {
                 pageObserver.observe(pageTarget);
 
                 const tableObserver = new ResizeObserver(debounce((entries) => {
+                    if (this.isScrolling) {
+                        return;
+                    }
+
                     for (let entry of entries) {
                         if (this.tableHeight != entry.contentRect.height) {
                             this.tableHeight = entry.contentRect.height;
@@ -888,6 +910,7 @@ class UIBootgrid {
                         this.isVisible = isVisible;
                         if (isVisible) {
                             this.table.redraw();
+                            this._scheduleDimensionChange();
                         }
                     }
                 });
@@ -898,24 +921,15 @@ class UIBootgrid {
 
         this.table.on('scrollVertical', (top) => {
             this.scrollPos = top;
+            this.isScrolling = true;
+            clearTimeout(this.scrollEndTimer);
+            this.scrollEndTimer = setTimeout(() => {
+                this.isScrolling = false;
+            }, 100);
         });
         this.table.on('renderComplete', () => {
             /* tooltips may stick, remove them on redraw */
             $("div.tooltip.fade.top.in").remove();
-
-            if (this.isResizing) {
-                return;
-            }
-
-            // Schedule a dimension change to prevent recursion
-            this.isResizing = true;
-            requestAnimationFrame(() => {
-                try {
-                    this._onDimensionChange();
-                } finally {
-                    this.isResizing = false;
-                }
-            });
         });
     }
 
@@ -988,6 +1002,8 @@ class UIBootgrid {
                 if (kids.length) queue.push(...kids);
             }
         }
+
+        this._scheduleDimensionChange();
 
         // backwards compat
         this.$element.trigger("loaded.rs.jquery.bootgrid");