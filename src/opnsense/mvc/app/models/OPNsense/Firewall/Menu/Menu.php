ac1cafd6a62710d932236607f0a7462f948adfb9
Franco Fichtner <franco@opnsense.org>
2026-09-23 09:55:45 +0200

    firewall: outbound NAT moves to legacy plugin
    (cherry picked from commit cd3b5670aede6d9be722bd7abdaafd6b94079cb0)
    (cherry picked from commit d6c238f69528488c015eb2324a6bf84f89b59a65)
    (cherry picked from commit 61a728b608e5c737c36fead2e225f2403981e7e3)
    (cherry picked from commit b58fcd2a1ee04cfeb7e575df96c7ea3b6855f11d)

--- src/opnsense/mvc/app/models/OPNsense/Firewall/Menu/Menu.php
+++ src/opnsense/mvc/app/models/OPNsense/Firewall/Menu/Menu.php
@@ -108,11 +108,11 @@ class Menu extends MenuContainer
             ]);
         }
 
-        /* XXX do not hide the legacy outbound NAT rules GUI when the plugin is installed */
-        if ($has_legacy_outbound_nat && file_exists('/usr/local/www/firewall_nat_out.php')) {
+        /* do not hide the legacy outbound NAT rules GUI when the plugin is installed */
+        if (file_exists('/usr/local/www/firewall_nat_out.php')) {
             $this->appendItem('Firewall.NAT', 'Outbound', [
                 'url' => '/firewall_nat_out.php',
-                'fixedname' => gettext('Outbound'),
+                'fixedname' => gettext('Outbound [legacy]'),
                 'order' => 300,
             ]);
         }
